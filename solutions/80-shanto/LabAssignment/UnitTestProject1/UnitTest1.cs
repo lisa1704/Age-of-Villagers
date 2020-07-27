40516b8e77c0de634f6f56e3c704915e29b9e9bd
saimulhaque@iut-dhaka.edu <saimulhaque@iut-dhaka.edu>
2020-07-28 01:31:27 +0600

    #15 fix the bug in test case

---
+++ solutions/80-shanto/LabAssignment/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var actual = 2;
+            var expected = 2;
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
