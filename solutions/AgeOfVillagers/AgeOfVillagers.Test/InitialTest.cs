790f7251e20c13f4634b9868be4bd8b940e7d74e
67-tusar <zubairrahman@iut-dhaka.edu>
2020-07-27 21:01:52 +0600

    #15 Test framework created

---
+++ solutions/AgeOfVillagers/AgeOfVillagers.Test/InitialTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AgeOfVillagers.Test
+{
+    public class InitialTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+        }
+    }
+}
