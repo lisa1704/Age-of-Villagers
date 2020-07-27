ab89b3b507ad39720b15be823a144e9b8370424f
saimulhaque@iut-dhaka.edu <saimulhaque@iut-dhaka.edu>
2020-07-28 00:22:58 +0600

    #15 test framework created

---
+++ solutions/80-shanto/LabAssignment/LabAssignment/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using Xunit;
+
+
+namespace LabAssignment
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int var = 2;
+            var expectedValue = 2;
+            Assert.Equals(expectedValue, var);
+        }
+    }
+}
