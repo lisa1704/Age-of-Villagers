a062bcbe9a3ae5a036b1396bd136bad4afe84fbd
Sadat Bin Sharfuddin <sadat.bs@gmail.com>
2020-07-28 01:54:46 +0600

    #15 Test framework prepared along with a demo test

---
+++ solutions/68-sadat/age-of-villagers/age-of-villagers/age-of-villagers.Tests/demoTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace age_of_villagers.age_of_villagers.Tests
+{
+    public class demoTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(2, 2);
+        }
+
+    }
+}
