9e4a9ffbc4fd0913fdb04b866659a9a6b2f0f714
sharminnaj@iut-dhaka.edu <sharminnaj@iut-dhaka.edu>
2020-07-28 16:16:47 +0600

    #15 Prepared my test framework

---
+++ solutions/74-mou/AgeOfVillagersTest/AovMainTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace AgeOfVillagersTest
+{
+    public class AovMainTest
+    {
+        [Fact]
+        public void AppIsRunnig()
+        {
+            Assert.True(8 > 5);
+
+        }
+
+        [Fact]
+        public void TestTest()
+        {
+            Assert.Equal(2, 2);
+        }
+    }
+}
