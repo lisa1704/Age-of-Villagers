6cfff90a6ae7c28fecb7017f74ce4c3207210c73
ShahEftakher <shaheftakher@iut-dhaka.edu>
2020-07-28 14:42:51 +0600

    #15 Test framework created

---
+++ solutions/08-sazid/tests/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testCase1(){
+        String test="Test created";
+        assertEquals(test,"Test created");
+    }
+}
