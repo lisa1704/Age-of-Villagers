bca73df9e6dd2f6ec25e20664f0bd9902d4ed58b
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-07-28 13:33:28 +0600

    #15 Prepare your test framework

---
+++ solutions/09-rafid/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = 9;
+            var expected = 9;
+            Assert.AreEqual(x, expected);
+
+        }
+    }
+}
