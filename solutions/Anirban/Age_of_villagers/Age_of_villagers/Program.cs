10fcf050ddc762741612e357434117deb422045d
Anirban Azad <azadarannya@gmail.com>
2020-07-28 20:52:26 +0600

    #14 Prepare your project structure

---
+++ solutions/Anirban/Age_of_villagers/Age_of_villagers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Age_of_villagers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
