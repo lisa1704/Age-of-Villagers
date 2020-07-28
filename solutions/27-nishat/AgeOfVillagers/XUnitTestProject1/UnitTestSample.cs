27713f630129cea10618e2bda229e45892860561
mdnishat27 <59754386+mdnishat27@users.noreply.github.com>
2020-07-28 09:51:21 +0600

    #15 test sample

---
+++ solutions/27-nishat/AgeOfVillagers/XUnitTestProject1/UnitTestSample.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTestSample
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+
+        }
+    }
+}
