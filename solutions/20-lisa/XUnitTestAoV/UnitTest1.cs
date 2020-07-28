1d5a4c806c42f09d540d311af8904e1c816d56cf
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-07-28 16:22:19 +0600

    #14 test framework prepared

---
+++ solutions/20-lisa/XUnitTestAoV/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace XUnitTestAoV
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+        }
+        [Fact]
+        public void IsOK()
+        {
+            Assert.True(2 > 1);
+        }
+    }
+}
