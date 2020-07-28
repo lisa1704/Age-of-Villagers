2740a1ef740a047e764b76cd3db263c8677656ad
Ratun Rahman <47448476+Ratun011@users.noreply.github.com>
2020-07-28 12:39:14 +0600

    #15 Prepared test framework

---
+++ solutions/11-ratun/First Framework/First Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("First Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("First Framework")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63661863-a782-4839-a9e4-acde8067d95a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ solutions/11-ratun/First Framework/First Framework/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace First_Framework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var actualValue = 10;
+            var expectedValue = 10;
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
