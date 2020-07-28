60a05f937a6dcd54633f6bec5edce92fdb141696
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-07-28 13:21:05 +0600

    #15 Unit test added

---
+++ solutions/90-safayat/AgeOfVillagersTEST90/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace AgeOfVillagersTEST90
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int s = 90;
+            Assert.Equal(90, s);
+        }
+    }
+}
