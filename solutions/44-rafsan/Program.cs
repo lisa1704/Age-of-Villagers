e7aba135b80e4c7c11aa44e541b715bfddddc084
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-07-28 20:16:28 +0600

    this is for testing

---
+++ solutions/44-rafsan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This module is for testing");
+        }
+    }
+}
