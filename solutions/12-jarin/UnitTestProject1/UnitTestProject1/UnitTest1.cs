01f7a2d69fb782b6a7c8c01e616fb9eeb79556a6
jarin_12 <jarintanzim@iut-dhaka.edu>
2020-07-28 22:18:25 +0600

    #15 prepared test framework

---
+++ solutions/12-jarin/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = 12;
+            var expected = 12;
+            Assert.AreEqual(x, expected);
+        }
+    }
+}
