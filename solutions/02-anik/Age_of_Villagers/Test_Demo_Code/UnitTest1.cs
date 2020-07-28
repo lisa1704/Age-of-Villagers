928e1a7d2011cf37f6eface93198b03f9d65f275
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-07-28 13:11:38 +0600

    #15 completed with XUnit
    Written a test asserting two equals two.

---
+++ solutions/02-anik/Age_of_Villagers/Test_Demo_Code/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Test_Demo_Code
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+        }
+    }
+}
