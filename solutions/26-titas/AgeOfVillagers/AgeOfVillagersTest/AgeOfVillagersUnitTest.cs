13b037c574f53425ff2382fad75d05c0dea80bc0
raihantitas <aburaihan@iut-dhaka.edu>
2020-07-28 20:59:04 +0600

    #15 Unit test framework created with a unit test

---
+++ solutions/26-titas/AgeOfVillagers/AgeOfVillagersTest/AgeOfVillagersUnitTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace AgeOfVillagersTest
+{
+    public class AgeOfVillagersUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+
+        }
+    }
+}
