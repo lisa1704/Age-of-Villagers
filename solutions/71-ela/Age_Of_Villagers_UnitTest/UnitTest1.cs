cd584d3aa3dec4df60b9972c0710855ef516732f
Ela Zaman <raziazaman@iut-dhaka.edu>
2020-07-28 15:16:28 +0600

    #15 prepared test framework

---
+++ solutions/71-ela/Age_Of_Villagers_UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Age_Of_Villagers_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(71, 71);
+        }
+    }
+}
