da80aff0fcc15b66fec027b70ab183da8214c9b8
67-tusar <zubairrahman@iut-dhaka.edu>
2020-07-28 16:06:01 +0600

    #15 Test framework re-created. Test stuck at "Not run" state solved. Using .NET Framework ms test unit tests

---
+++ solutions/67-tusar/AgeOfVillagers/Age_of_VIllagers.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Age_of_VIllagers.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Age_of_VIllagers.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f26d0b29-a233-418e-a820-3ceb29e28c2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ solutions/67-tusar/AgeOfVillagers/Age_of_VIllagers.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Age_of_VIllagers.Test
+{
+    [TestClass]
+    public class InitialUnitTest
+    {
+        [TestMethod]
+        public void InitialTest()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
