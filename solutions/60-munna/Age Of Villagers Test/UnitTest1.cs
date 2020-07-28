8af28d765049798535163c84dd52a0ca5c6c1125
sakibul-munnaa <sakibulislam1@iut-dhaka.edu>
2020-07-28 21:06:50 +0600

    #15 Test Framework has been created.

---
+++ solutions/60-munna/Age Of Villagers Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Age_Of_Villagers_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+
+        }
+    }
+}
