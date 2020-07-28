fc0844e50f3268b2edc48ca19bf321d9ebb38dfa
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-07-28 23:35:31 +0600

    #15 Prepared test framework.

---
+++ solutions/53-silvi/AgeOfVillagers.test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace AgeOfVillagers.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int actual = 2;
+            var expected = 2;
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
