d1ff0d340cdf54ef75c1049c6ec6bba71b9f561f
67-tusar <zubairrahman@iut-dhaka.edu>
2020-07-28 09:45:28 +0600

    moved project to sub-directory "67-tusar"

---
+++ solutions/67-tusar/AgeOfVillagers/AgeOfVillagers.MSTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgeOfVillagers.MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Equals(2, 2);
+        }
+    }
+}
