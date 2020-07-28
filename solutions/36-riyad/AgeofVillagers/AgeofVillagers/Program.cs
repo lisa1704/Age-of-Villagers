bba4a43a0c14aa0181b2a18c75871e2c2ba33f43
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-07-28 20:31:55 +0600

    #14 prepared project structure

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgeofVillagers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hope we will find fun in it!!");
+        }
+    }
+}
