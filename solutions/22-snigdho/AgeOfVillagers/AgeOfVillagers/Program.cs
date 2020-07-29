1b0ad5803de06cb852259dc2bd3bc0eab6badca1
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-07-29 10:21:33 +0600

    #14-Creating project framework

---
+++ solutions/22-snigdho/AgeOfVillagers/AgeOfVillagers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgeOfVillagers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
