0ca78cac3b74e3e4d8f573daf9b3db4de5032124
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-08-03 17:33:31 +0600

    #12 edited Inuit Hunter

--- solutions/43-sristy/AgeOfVillagers/Inuit.cs
+++ solutions/43-sristy/AgeOfVillagers/Inuit.cs
@@ -5,19 +5,23 @@ namespace Age_of_villagers
 {
     public class Inuit : INation
     {
+        Pen p = new Pen(Color.Black);
         public void Draw_house(Graphics g,int X, int Y)
         {
-            throw new NotImplementedException();
+            g.DrawArc(p, X + 25, Y+25, 50, 50, 180, 180);
+            g.DrawArc(p, X - 25, Y-25, 150, 150, 180, 180);
+            g.DrawLine(p, X - 25, Y+50, X + 125, Y+50);
+            
         }
 
         public void Draw_tree(Graphics g,int X, int Y)
         {
-            throw new NotImplementedException();
+            
         }
 
         public void Draw_watersource(Graphics g,int X, int Y)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
