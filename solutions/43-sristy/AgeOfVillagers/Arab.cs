78d5914f807676936f3f3371c48cb67a8d9cc699
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-08-03 17:25:46 +0600

    #10 edited Arab bedouin

--- solutions/43-sristy/AgeOfVillagers/Arab.cs
+++ solutions/43-sristy/AgeOfVillagers/Arab.cs
@@ -5,19 +5,35 @@ namespace Age_of_villagers
 {
     public class Arab : INation
     {
+        Pen p = new Pen(Color.Gold);
         public void Draw_house(Graphics g,int X, int Y)
         {
-            throw new NotImplementedException();
+            g.DrawLine(p, X, Y + 50, X + 25, Y);
+            g.DrawLine(p, X + 25, Y, X + 50, Y + 50);
+            g.DrawLine(p, X + 50, Y + 50, X, Y + 50);
+            g.DrawLine(p, X + 25, Y, X + 75, Y - 25);
+            g.DrawLine(p, X + 75, Y - 25, X + 100, Y + 25);
+            g.DrawLine(p, X + 100, Y + 25, X + 50, Y + 50);
+
+
         }
 
         public void Draw_tree(Graphics g,int X, int Y)
         {
-            throw new NotImplementedException();
+            g.DrawLine(p, X + 48, Y + 50, X + 52, Y + 50);
+            g.DrawLine(p, X + 52, Y + 50, X + 52, Y + 100);
+            g.DrawLine(p, X + 52, Y + 100, X + 48, Y + 100);
+            g.DrawLine(p, X + 48, Y + 100, X + 48, Y + 50);
+            g.DrawLine(p, X + 50, Y + 50, X+50, Y+15);
+            g.DrawLine(p, X + 50, Y + 50, X + 25, Y+25);
+            g.DrawLine(p, X + 50, Y + 50, X + 75, Y+25);
+            g.DrawLine(p, X + 50, Y + 50, X +15, Y+40);
+            g.DrawLine(p, X + 50, Y + 50, X + 85, Y+40);
         }
 
         public void Draw_watersource(Graphics g,int X, int Y)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
