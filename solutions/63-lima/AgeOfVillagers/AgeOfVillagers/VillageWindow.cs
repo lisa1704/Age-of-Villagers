186a273cc60c57e512d04859a3c4a84c89537bd3
Maksuda Islam <maksudalima321@outlook.com>
2020-08-03 18:45:56 -0700

    #4  drawing board with a 600x400 drawing space created

--- solutions/63-lima/AgeOfVillagers/AgeOfVillagers/VillageWindow.cs
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/VillageWindow.cs
@@ -4,13 +4,11 @@ namespace AgeOfVillagers
 {
     class VillageWindow : Form
     {
-        private Panel panel2;
         private Panel panel1;
 
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // panel1
@@ -20,17 +18,9 @@ namespace AgeOfVillagers
             this.panel1.Size = new System.Drawing.Size(600, 400);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(598, 2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 600);
-            this.panel2.TabIndex = 1;
-            // 
             // VillageWindow
             // 
             this.ClientSize = new System.Drawing.Size(800, 600);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "VillageWindow";
             this.ResumeLayout(false);
