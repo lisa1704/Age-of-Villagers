365d986efcda83ac5f4f1c2a15afa73075401b91
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-08-04 22:30:51 +0600

    #15 Test framework prepared

---
+++ solutions/42-rifah/AgeOfVillagers.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace AgeOfVillagers.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+
+        }
+    }
+}
