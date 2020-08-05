485f0b824b091da44c23c7ca1189cadb8a2f68cc
Mahamudur Rahaman Khan <44021897+khansun@users.noreply.github.com>
2020-08-05 18:45:44 +0600

    #14 initial  project

---
+++ solutions/17-sun/AgeOfVillagers/Age of Villagers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Age_of_Villagers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Age of Villagers!!! Shh ho! ");
+            Console.ReadLine();
+        }
+    }
+}
