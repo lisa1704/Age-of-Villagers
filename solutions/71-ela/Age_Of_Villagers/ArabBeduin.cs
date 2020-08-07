896dc486127a532a6b7a8d88e4a1d87fe8540382
Ela Zaman <raziazaman@iut-dhaka.edu>
2020-08-08 00:52:16 +0600

    # 10 added arabbeduin nation

---
+++ solutions/71-ela/Age_Of_Villagers/ArabBeduin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    class ArabBeduin : INations
+    {
+        IShapes triangle;
+
+        IShapes rectangle;
+
+
+        public  int house_width = 16;
+        public  int house_height = 16;
+        private  int tree_width = 16;
+        private  int tree_height = 24;
+
+        public ArabBeduin()
+        {
+            this.triangle = new Triangle();
+            this.rectangle = new Rectangle();
+
+        }
+
+        public  void DrawHouse(Graphics g, int x, int y)
+        {
+            triangle.Draw(g, x, y, house_height / 2, house_width / 2);
+            rectangle.Draw(g, x+ house_height / 2, y, house_height / 2, house_width / 3);
+
+
+        }
+
+        public  void DrawTree(Graphics g, int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  void DrawWaterSource(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetBackground(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  void SetWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
