45c9d6ba65f08c40d285e847cb80cc0edff1eff7
Ela Zaman <raziazaman@iut-dhaka.edu>
2020-08-08 00:14:40 +0600

    #8   Added INations abstract class

---
+++ solutions/71-ela/Age_Of_Villagers/INations.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    abstract class INations
+    {
+        public static int house_width =16;
+        public static int house_height=16;
+        private static int tree_width=16;
+        private static int tree_height =24;
+
+        
+
+    
+
+
+        public abstract void DrawTree(Graphics g,int x,int y);
+        public abstract void DrawHouse(Graphics g,int x,int y);
+        public abstract void DrawWaterSource(Graphics g);
+        public abstract void SetBackground(Graphics g); 
+        public abstract void SetWaterSource();
+
+      
+
+
+
+
+
+
+
+    }
+}
