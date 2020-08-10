121ffb69e6096b2438f878260f7c7679f54f3db1
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-08-10 15:08:26 +0600

    #12 Created Inuit Hunters

---
+++ solutions/32-zahin/AgeOfVillagers/InuitHunters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class InuitHunters : Nations
+    {
+        public void DrawHouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
