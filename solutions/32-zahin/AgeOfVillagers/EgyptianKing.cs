0633f5814f6db203c0f8dbfd940fdcf3d92001b6
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-08-10 15:08:12 +0600

    #11 created Egyptian Kings

---
+++ solutions/32-zahin/AgeOfVillagers/EgyptianKing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class EgyptianKing : Nations
+    {
+        public void DrawHouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
