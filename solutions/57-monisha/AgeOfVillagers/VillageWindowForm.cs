5b3b2861c20d63ff1082307c8e172cc9bdfb3779
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2020-08-11 10:12:16 +0600

    #4 create new village window added

--- solutions/57-monisha/AgeOfVillagers/VillageWindowForm.cs
+++ solutions/57-monisha/AgeOfVillagers/VillageWindowForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
-    public partial class Form1 : Form
+    public partial class VillageWindow : Form
     {
-        public Form1()
+        public VillageWindow()
         {
             InitializeComponent();
         }
