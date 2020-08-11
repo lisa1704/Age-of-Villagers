9010179b6d2db3e2b4ecfaaca84e8a68022c0162
Suzad <61018495+Suzad@users.noreply.github.com>
2020-08-12 00:38:33 +0600

    #4 age of villagers panel

--- solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/Form1.Designer.cs
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.pnlDrawingSpace = new System.Windows.Forms.Panel();
             this.pnlToolSide = new System.Windows.Forms.Panel();
+            this.lblAgeOfVillagers = new System.Windows.Forms.Label();
             this.pnlDrawingSpace.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,16 +49,28 @@
             this.pnlToolSide.Size = new System.Drawing.Size(195, 400);
             this.pnlToolSide.TabIndex = 1;
             // 
+            // lblAgeOfVillagers
+            // 
+            this.lblAgeOfVillagers.AutoSize = true;
+            this.lblAgeOfVillagers.Font = new System.Drawing.Font("SimSun-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblAgeOfVillagers.Location = new System.Drawing.Point(606, 9);
+            this.lblAgeOfVillagers.Name = "lblAgeOfVillagers";
+            this.lblAgeOfVillagers.Size = new System.Drawing.Size(185, 20);
+            this.lblAgeOfVillagers.TabIndex = 1;
+            this.lblAgeOfVillagers.Text = "Age of Villagers";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.lblAgeOfVillagers);
             this.Controls.Add(this.pnlDrawingSpace);
             this.Name = "Form1";
             this.Text = "Age of Villagers";
             this.pnlDrawingSpace.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -65,6 +78,7 @@
 
         private System.Windows.Forms.Panel pnlDrawingSpace;
         private System.Windows.Forms.Panel pnlToolSide;
+        private System.Windows.Forms.Label lblAgeOfVillagers;
     }
 }
 
