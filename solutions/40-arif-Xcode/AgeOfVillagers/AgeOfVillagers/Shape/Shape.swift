c87e50bb4cafb90423a9abf73bbe90a77541123d
Md. Ariful Islam, 170042040 <arifulislam40@iut-dhaka.edu>
2020-08-24 14:14:59 +0600

    #7 placing basic item in village area

---
+++ solutions/40-arif-Xcode/AgeOfVillagers/AgeOfVillagers/Shape/Shape.swift
@@ -0,0 +1,20 @@
+//
+//  Shape.swift
+//  AgeOfVillagers
+//
+//  Created by Md Ariful Islam on 24/8/20.
+//  Copyright © 2020 Md Ariful Islam. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class Shape{
+    var name: String?
+    
+    func line() {
+        let line = NSBezierPath()
+//        line.line(to: <#T##NSPoint#>)
+        
+    }
+}
--- solutions/40-arif-Xcode/AgeOfVillagers/AgeOfVillagers/ViewController.swift
+++ solutions/40-arif-Xcode/AgeOfVillagers/AgeOfVillagers/ViewController.swift
@@ -9,11 +9,14 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    var asset: String = ""
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
     override var representedObject: Any? {
@@ -22,6 +25,35 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func OnHouseClicked(_ sender: Any) {
+        print("house Selected")
+        asset = "House"
+    }
+    @IBAction func OnWaterClicked(_ sender: Any) {
+        print("Water Selected")
+        asset = "Water"
+    }
+    @IBAction func OnTreeClicked(_ sender: Any) {
+        print("Tree Selected")
+        asset = "Tree"
+    }
+    
+    
+    @IBAction func getPoint(_ sender: Any) {
+        
+        
+        let currentMouseLocation = NSEvent.mouseLocation
+        let xPosition = currentMouseLocation.x
+        let yPosition = currentMouseLocation.y
+        print("point touch")
+        print(xPosition)
+        print(yPosition)
+        
+        let label = NSTextField(frame: NSMakeRect(0,0,100,50))
+        label.frame = CGRect(origin:CGPoint(x: Int(xPosition), y: Int(yPosition)), size: CGSize(width: label.bounds.width, height: label.bounds.height))
+        label.stringValue = asset
+        label.isEditable = false
+        self.view.addSubview(label)
+    }
 }
 
