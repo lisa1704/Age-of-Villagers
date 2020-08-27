585537dae8f599d9f4120d20048f40274b8f2e1b
Ela Zaman <raziazaman@iut-dhaka.edu>
2020-08-27 21:33:20 +0600

    #10 implemented ArabBeduin_ItemSize_UnitTest

---
+++ solutions/71-ela/Age_Of_Villagers_UnitTest/Items/ArabBeduin_ItemSize_Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using Age_Of_Villagers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rectangle = Age_Of_Villagers.Rectangle;
+
+namespace Age_Of_Villagers_UnitTest.Items
+{
+
+
+    [TestClass]
+    public class ArabBeduin_ItemSize_Tests
+    {
+
+        Point location;
+        Rectangle bBox;
+        ItemSize houseSize, treeSize, waterSourceSize;
+
+
+        public ArabBeduin_ItemSize_Tests()
+        {
+            location = new Point(0, 0);
+            houseSize = new ItemSize(16, 16);
+            treeSize = new ItemSize(16, 24);
+            waterSourceSize = new ItemSize(24, 16);
+
+
+        }
+        [TestMethod()]
+        public void ArabBeduin_HouseTest()
+        {
+
+
+            ArabBeduinHouse house = new ArabBeduinHouse(location, houseSize);
+            bBox = house.GetBBox();
+
+
+            Assert.AreEqual(16, bBox.GetWidth());
+            Assert.AreEqual(16, bBox.GetHeight());
+
+
+        }
+
+
+        [TestMethod()]
+        public void GetBBox_ArabBeduin_TreeTest()
+        {
+
+            ArabBeduinTree tree = new ArabBeduinTree(location, treeSize);
+            bBox = tree.GetBBox();
+
+
+            //Assert.AreEqual(16, bBox.GetWidth());
+            Assert.AreEqual(24, bBox.GetHeight());
+
+        }
+    }
+}
+
