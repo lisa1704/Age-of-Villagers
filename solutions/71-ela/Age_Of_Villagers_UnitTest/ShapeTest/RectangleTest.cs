4a42017e33081369ae606ac247990556e6721307
Ela Zaman <raziazaman@iut-dhaka.edu>
2020-08-28 20:51:14 +0600

    #1 Remove redundacy

---
+++ solutions/71-ela/Age_Of_Villagers_UnitTest/ShapeTest/RectangleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Age_Of_Villagers_UnitTest.ShapeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ solutions/71-ela/Age_Of_Villagers_UnitTest/UnitTest2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Age_Of_Villagers_UnitTest
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
