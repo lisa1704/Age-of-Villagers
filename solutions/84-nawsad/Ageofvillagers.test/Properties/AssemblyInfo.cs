cf726f65368af7b611076e8c119065e8f8298f43
JAZZHAND1 <rotorblade52@outlook.com>
2020-08-30 02:39:00 +0600

    #15  All test case related problems solved

--- solutions/84-nawsad/Ageofvillagers.test/Properties/AssemblyInfo.cs
+++ solutions/84-nawsad/Ageofvillagers.test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ageofvillagers")]
+[assembly: AssemblyTitle("Ageofvillagers.test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ageofvillagers")]
+[assembly: AssemblyProduct("Ageofvillagers.test")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d203aa22-4f59-4808-8cfe-ec122e9cd72e")]
+[assembly: Guid("622d9d54-209a-45b9-8c24-ed85d1eda54e")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ solutions/84-nawsad/Ageofvillagers.test/Unittest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace Ageofvillagers.test
+{
+    public class Unittest
+    {   
+        [Fact]
+        void unittest1()
+        {
+            int output = 5 * 1;
+            int expected = 5;
+            Assert.Equal(expected, output);
+        }
+
+    }
+}
