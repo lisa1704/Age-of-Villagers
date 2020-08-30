8209e4aff46aad3f4f779ab69bab3f2482a4814c
JAZZHAND1 <rotorblade52@outlook.com>
2020-08-31 00:45:45 +0600

    #4 Drawing board upon clicking the new Village button is added.

--- solutions/84-nawsad/Ageofvillagers/AgeofVillagers.Designer.cs
+++ solutions/84-nawsad/Ageofvillagers/AgeofVillagers.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ageofvillagers
 {
-    partial class Form1
+    partial class Ageofvillagers
     {
         /// <summary>
         /// Required designer variable.
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,15 +55,46 @@
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.panel1.Location = new System.Drawing.Point(596, 0);
+            this.panel1.Location = new System.Drawing.Point(605, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(204, 400);
+            this.panel1.Size = new System.Drawing.Size(215, 400);
             this.panel1.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.button6.Location = new System.Drawing.Point(48, 327);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 28);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Save Village";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(111, 361);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Open Village";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(12, 361);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "New Village";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Create_new__Click);
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
-            this.button3.Location = new System.Drawing.Point(48, 209);
+            this.button3.Location = new System.Drawing.Point(57, 209);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(101, 26);
             this.button3.TabIndex = 4;
@@ -73,7 +104,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(102, 170);
+            this.button2.Location = new System.Drawing.Point(114, 170);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 23);
             this.button2.TabIndex = 3;
@@ -104,7 +135,7 @@
             // 
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(33, 72);
+            this.textBox2.Location = new System.Drawing.Point(33, 77);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(134, 15);
             this.textBox2.TabIndex = 1;
@@ -115,7 +146,7 @@
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Location = new System.Drawing.Point(12, 0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(200, 92);
@@ -123,44 +154,14 @@
             this.textBox1.Text = "                        Age of villagers";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
-            this.button4.Location = new System.Drawing.Point(6, 361);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(93, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "New Village";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
-            this.button5.Location = new System.Drawing.Point(99, 361);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(98, 23);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "Open Village";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
-            this.button6.Location = new System.Drawing.Point(48, 327);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(101, 28);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "Save Village";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // Form1
+            // Ageofvillagers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 761);
+            this.ClientSize = new System.Drawing.Size(819, 561);
             this.Controls.Add(this.panel1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Ageofvillagers";
+            this.Text = "AgeofVillagers";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -179,6 +180,7 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
+        
     }
 }
 
