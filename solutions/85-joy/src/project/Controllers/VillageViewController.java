e3b649d53d60a8341adf384c80d66ca5555e6476
MD ASHIQUR RAHMAN JOY <ashiqurrahman85@iut-dhaka.edu>
2020-09-11 00:05:19 +0600

    #4 A dummy method is implemented to test WaterSource Button and Also FXML and Controller are linked

--- solutions/85-joy/src/project/Controllers/VillageViewController.java
+++ solutions/85-joy/src/project/Controllers/VillageViewController.java
@@ -1,4 +1,35 @@
 package project.Controllers;
 
-public class VillageViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+
+
+public class VillageViewController implements Initializable {
+    @FXML
+    private TextField Village_Name;
+    @FXML private Button WaterSource = new Button();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void WaterSourceClicked(javafx.event.ActionEvent actionEvent) {
+        try{
+
+            System.out.println("NIGGA!");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
