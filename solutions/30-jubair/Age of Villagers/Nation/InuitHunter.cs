b374d246d5a5cb73fa097ab033127ac19f05e000
30-jubair <jobairt03@gmail.com>
2020-09-11 19:44:19 +0600

    #8 #12  Inuit Hunters nation Added

---
+++ solutions/30-jubair/Age of Villagers/Nation/InuitHunter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_Villagers.Nation
+{
+    class InuitHunter : INation
+    {
+        string name;
+
+        public InuitHunter(string name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return name;
+        }
+    }
+}
