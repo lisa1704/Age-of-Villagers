582d5bd5b77ef0c1bf64f7fd7e4e2ef6c71c8e86
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2020-09-11 12:10:10 +0600

    #4 buttons edited

--- solutions/57-monisha/AgeOfVillagers/VillageWindow.Designer.cs
+++ solutions/57-monisha/AgeOfVillagers/VillageWindow.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SAVEbutton = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.OpenVbutton = new System.Windows.Forms.Button();
+            this.WaterSource = new System.Windows.Forms.RadioButton();
+            this.Tree = new System.Windows.Forms.RadioButton();
+            this.House = new System.Windows.Forms.RadioButton();
             this.NewVbutton = new System.Windows.Forms.Button();
+            this.OpenVbutton = new System.Windows.Forms.Button();
+            this.SAVEbutton = new System.Windows.Forms.Button();
+            this.gameTitle = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,97 +43,102 @@
             // 
             this.panel1.AutoScroll = true;
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.WaterSource);
+            this.panel1.Controls.Add(this.Tree);
+            this.panel1.Controls.Add(this.House);
             this.panel1.Controls.Add(this.NewVbutton);
             this.panel1.Controls.Add(this.OpenVbutton);
-            this.panel1.Controls.Add(this.checkBox3);
-            this.panel1.Controls.Add(this.checkBox2);
-            this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.SAVEbutton);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.gameTitle);
             this.panel1.Cursor = System.Windows.Forms.Cursors.PanNorth;
-            this.panel1.Location = new System.Drawing.Point(363, 0);
+            this.panel1.Location = new System.Drawing.Point(378, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(236, 400);
+            this.panel1.Size = new System.Drawing.Size(221, 400);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // WaterSource
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(36, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(172, 27);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Age of Villagers";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.WaterSource.AutoSize = true;
+            this.WaterSource.Location = new System.Drawing.Point(41, 158);
+            this.WaterSource.Name = "WaterSource";
+            this.WaterSource.Size = new System.Drawing.Size(116, 21);
+            this.WaterSource.TabIndex = 10;
+            this.WaterSource.TabStop = true;
+            this.WaterSource.Text = "Water Source";
+            this.WaterSource.UseVisualStyleBackColor = true;
             // 
-            // SAVEbutton
+            // Tree
             // 
-            this.SAVEbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.SAVEbutton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.SAVEbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SAVEbutton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.SAVEbutton.Location = new System.Drawing.Point(41, 259);
-            this.SAVEbutton.Margin = new System.Windows.Forms.Padding(3, 3, 3, 5);
-            this.SAVEbutton.Name = "SAVEbutton";
-            this.SAVEbutton.Size = new System.Drawing.Size(130, 23);
-            this.SAVEbutton.TabIndex = 1;
-            this.SAVEbutton.Text = "SAVE VILLAGE";
-            this.SAVEbutton.UseVisualStyleBackColor = true;
-            this.SAVEbutton.Click += new System.EventHandler(this.button1_Click);
+            this.Tree.AutoSize = true;
+            this.Tree.Location = new System.Drawing.Point(41, 130);
+            this.Tree.Name = "Tree";
+            this.Tree.Size = new System.Drawing.Size(59, 21);
+            this.Tree.TabIndex = 9;
+            this.Tree.TabStop = true;
+            this.Tree.Text = "Tree";
+            this.Tree.UseVisualStyleBackColor = true;
             // 
-            // checkBox1
+            // House
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.Location = new System.Drawing.Point(41, 101);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(74, 24);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "House";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox2.Location = new System.Drawing.Point(41, 129);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(60, 24);
-            this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "Tree";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox3.Location = new System.Drawing.Point(41, 157);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(121, 24);
-            this.checkBox3.TabIndex = 4;
-            this.checkBox3.Text = "Water Source";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // OpenVbutton
-            // 
-            this.OpenVbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.OpenVbutton.Location = new System.Drawing.Point(41, 328);
-            this.OpenVbutton.Name = "OpenVbutton";
-            this.OpenVbutton.Size = new System.Drawing.Size(130, 23);
-            this.OpenVbutton.TabIndex = 6;
-            this.OpenVbutton.Text = "OPEN VILLAGE";
-            this.OpenVbutton.UseVisualStyleBackColor = true;
+            this.House.AutoSize = true;
+            this.House.Location = new System.Drawing.Point(41, 102);
+            this.House.Name = "House";
+            this.House.Size = new System.Drawing.Size(70, 21);
+            this.House.TabIndex = 8;
+            this.House.TabStop = true;
+            this.House.Text = "House";
+            this.House.UseVisualStyleBackColor = true;
+            this.House.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // NewVbutton
             // 
+            this.NewVbutton.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.NewVbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NewVbutton.Location = new System.Drawing.Point(41, 290);
             this.NewVbutton.Name = "NewVbutton";
             this.NewVbutton.Size = new System.Drawing.Size(130, 23);
             this.NewVbutton.TabIndex = 7;
             this.NewVbutton.Text = "NEW VILLAGE";
-            this.NewVbutton.UseVisualStyleBackColor = true;
+            this.NewVbutton.UseVisualStyleBackColor = false;
+            // 
+            // OpenVbutton
+            // 
+            this.OpenVbutton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.OpenVbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenVbutton.Location = new System.Drawing.Point(41, 328);
+            this.OpenVbutton.Name = "OpenVbutton";
+            this.OpenVbutton.Size = new System.Drawing.Size(130, 23);
+            this.OpenVbutton.TabIndex = 6;
+            this.OpenVbutton.Text = "OPEN VILLAGE";
+            this.OpenVbutton.UseVisualStyleBackColor = false;
+            // 
+            // SAVEbutton
+            // 
+            this.SAVEbutton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.SAVEbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.SAVEbutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SAVEbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SAVEbutton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.SAVEbutton.Location = new System.Drawing.Point(41, 259);
+            this.SAVEbutton.Margin = new System.Windows.Forms.Padding(3, 3, 5, 5);
+            this.SAVEbutton.Name = "SAVEbutton";
+            this.SAVEbutton.Size = new System.Drawing.Size(130, 23);
+            this.SAVEbutton.TabIndex = 5;
+            this.SAVEbutton.Text = "SAVE VILLAGE";
+            this.SAVEbutton.UseVisualStyleBackColor = false;
+            this.SAVEbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gameTitle
+            // 
+            this.gameTitle.AutoSize = true;
+            this.gameTitle.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameTitle.Location = new System.Drawing.Point(36, 38);
+            this.gameTitle.Name = "gameTitle";
+            this.gameTitle.Size = new System.Drawing.Size(172, 27);
+            this.gameTitle.TabIndex = 0;
+            this.gameTitle.Text = "Age of Villagers";
+            this.gameTitle.Click += new System.EventHandler(this.label1_Click);
             // 
             // VillageWindow
             // 
@@ -156,12 +161,12 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label gameTitle;
         private System.Windows.Forms.Button SAVEbutton;
         private System.Windows.Forms.Button OpenVbutton;
-        private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.Button NewVbutton;
+        private System.Windows.Forms.RadioButton House;
+        private System.Windows.Forms.RadioButton Tree;
+        private System.Windows.Forms.RadioButton WaterSource;
     }
 }
--- solutions/57-monisha/AgeOfVillagers/VillageWindow.cs
+++ solutions/57-monisha/AgeOfVillagers/VillageWindow.cs
@@ -29,6 +29,14 @@ namespace AgeOfVillagers
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
+
+        }
+
+      
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
 
         }
 
