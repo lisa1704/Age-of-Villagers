5bd53e10509899ab8b04274d7bcf883d94fe8ee0
30-jubair <jobairt03@gmail.com>
2020-09-11 19:36:22 +0600

    #8 #9  Bangladeshi Nation class added

---
+++ solutions/30-jubair/Age of Villagers/Nation/Bangladeshi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_Villagers.Nation
+{
+    class Bangladeshi : INation
+    {
+        string name;
+        
+        public Bangladeshi(string name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return name;
+        }
+    }
+}
