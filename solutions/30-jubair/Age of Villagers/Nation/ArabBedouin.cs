ee3f027cc2d2e158f838309bc9a7ed5c611675c4
30-jubair <jobairt03@gmail.com>
2020-09-11 19:38:22 +0600

    #8  #10  ArabBedoin Nation Added

---
+++ solutions/30-jubair/Age of Villagers/Nation/ArabBedouin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_Villagers.Nation
+{
+    class ArabBedouin : INation
+    {
+
+        string name;
+
+        public ArabBedouin(string name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return name;
+        }
+    }
+}
