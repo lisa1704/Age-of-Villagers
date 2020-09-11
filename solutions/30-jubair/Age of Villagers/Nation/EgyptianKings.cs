7de1f4a9e333d17513f5d7d82a08bbe0258f8859
30-jubair <jobairt03@gmail.com>
2020-09-11 19:40:06 +0600

    #8 #11  Egyptian King Nation added

---
+++ solutions/30-jubair/Age of Villagers/Nation/EgyptianKings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_Villagers.Nation
+{
+    class EgyptianKings : INation
+    {
+
+        string name;
+
+        public EgyptianKings(string name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return name;
+        }
+    }
+}
