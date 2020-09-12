84e254416d0832de54f1ad95e1a0e88b2849f08d
67-tusar <zubairrahman@iut-dhaka.edu>
2020-09-12 22:40:58 +0600

    #1 decoupled drawing method from factory to the ArabHouse Element

---
+++ solutions/67-tusar/AgeOfVillagers/Age_of_Villagers/ArabHouse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_of_Villagers
+{
+    class ArabHouse : IElement
+    {
+        public void draw(Axes axes, Panel drawingSpace)
+        {
+            Pen pen = new Pen(Color.Black);
+            Graphics elem = drawingSpace.CreateGraphics();
+
+            //draw triangle
+            elem.DrawLine(pen, new PointF(axes.X, axes.Y), new PointF(axes.X - 4, axes.Y - 8));
+            elem.DrawLine(pen, new PointF(axes.X - 4, axes.Y - 8), new PointF(axes.X - 8, axes.Y));
+            elem.DrawLine(pen, new PointF(axes.X, axes.Y), new PointF(axes.X - 8, axes.Y));
+
+            //draw square
+            elem.DrawLine(pen, new PointF(axes.X - 4, axes.Y - 8), new PointF(axes.X - 8, axes.Y - 8));
+            elem.DrawLine(pen, new PointF(axes.X - 8, axes.Y), new PointF(axes.X - 12, axes.Y));
+            elem.DrawLine(pen, new PointF(axes.X - 8, axes.Y - 8), new PointF(axes.X - 12, axes.Y));
+        }
+    }
+}
--- solutions/67-tusar/AgeOfVillagers/Age_of_Villagers/IElement.cs
+++ solutions/67-tusar/AgeOfVillagers/Age_of_Villagers/IElement.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Age_of_Villagers
 {
     interface IElement
     {
-        int getX();
-        int getY();
-        void setLocation();
+        void draw(Axes axes, Panel panel);
     }
 }
