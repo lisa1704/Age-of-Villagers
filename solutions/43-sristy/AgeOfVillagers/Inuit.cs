3ac975c05d41cb00ab33e9f379a333b3c2394836
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-12 20:29:31 +0600

    #12 updated inuit hunters

--- solutions/43-sristy/AgeOfVillagers/Inuit.cs
+++ solutions/43-sristy/AgeOfVillagers/Inuit.cs
@@ -6,22 +6,27 @@ namespace Age_of_villagers
     public class Inuit : INation
     {
         Pen p = new Pen(Color.Black);
-        public void Draw_house(Graphics g,int X, int Y)
+        public void Draw_house(Graphics g,Point mptop)
         {
-            g.DrawArc(p, X + 25, Y+25, 50, 50, 180, 180);
+            Compositeobj house = new Inuit_house(mptop);
+            house.paint(p, g);
+            /*g.DrawArc(p, X + 25, Y+25, 50, 50, 180, 180);
             g.DrawArc(p, X - 25, Y-25, 150, 150, 180, 180);
-            g.DrawLine(p, X - 25, Y+50, X + 125, Y+50);
-            
+            g.DrawLine(p, X - 25, Y+50, X + 125, Y+50);*/
+
         }
 
-        public void Draw_tree(Graphics g,int X, int Y)
+        public void Draw_tree(Graphics g,Point mptop)
         {
-            
+            IObject tree = new null_obj();
+            tree.paint(p, g);
+
         }
 
-        public void Draw_watersource(Graphics g,int X, int Y)
+        public void Draw_watersource(Graphics g,Point mptop)
         {
-            
+            IObject waterresource = new null_obj();
+            waterresource.paint(p, g);
         }
     }
 }
---
+++ solutions/43-sristy/AgeOfVillagers/Inuit_house.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_villagers
+{
+    public class Inuit_house: Compositeobj
+    {
+        public Inuit_house(Point mptop)
+        {
+            Point outtop = new Point(mptop.X - 50, mptop.Y - 50);
+            Point lpbottom = new Point(mptop.X - 75, mptop.Y);
+            Point rpbottom = new Point(mptop.X + 75, mptop.Y);
+            AddComponent(new obj_arc(mptop, 50, 50, 180, 180));
+            AddComponent(new obj_arc(outtop, 150, 150, 180, 180));
+            AddComponent(new obj_line(lpbottom, rpbottom));
+        }
+    }
+}
