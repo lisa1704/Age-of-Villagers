282d468675bc39fa28c513e5c3ead8d1d2850e61
musfiksifat <musfiksifat766@gmail.com>
2020-09-12 21:36:55 +0600

    #8  interface INation & INationFactory created.

--- solutions/88-musfik/AgeOfVillagers/Village.cs
+++ solutions/88-musfik/AgeOfVillagers/Village.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,13 @@ namespace AgeOfVillagers
 {
     public interface INation
     {
+        Color getTerrainColor();
+        string getNationName();
 
+    }
+    public interface INationFactory
+    {
+        INation getNation();
     }
     public class Village
     {
