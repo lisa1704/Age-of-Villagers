7476e0d2430021c24f08e8ce1f20419e868d369f
67-tusar <zubairrahman@iut-dhaka.edu>
2020-09-12 23:08:50 +0600

    #1 decoupled null tree creation

---
+++ solutions/67-tusar/AgeOfVillagers/Age_of_Villagers/NullTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_of_Villagers
+{
+    class NullTree : ITree
+    {
+        public void drawTree(Axes axes, Panel drawingSpace)
+        {
+            MessageBox.Show("Selected nation do not have Trees", "Attention");
+        }
+    }
+}
