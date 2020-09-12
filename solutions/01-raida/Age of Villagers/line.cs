0dda79fe32a40646e329b9730acad5619df27a94
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-09-12 14:53:39 +0600

    #9  #10  #11 #12 shape drawing

---
+++ solutions/01-raida/Age of Villagers/line.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Age_of_Villagers
+{
+    class line:IShape
+    {
+        private readonly Point point1;
+        private readonly Point point2;
+        public line(Point p1, Point p2)
+        {
+            this.point1 = p1;
+            this.point2 = p2;
+        }
+        public void draw(Graphics g,Pen pen)
+        {
+            g.DrawLine(pen, point1, point2);
+        }
+    }
+}
