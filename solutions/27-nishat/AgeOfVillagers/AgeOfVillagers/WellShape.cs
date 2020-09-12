0ca0e11804f05bac8329cbf2c6c76da6cbb65a39
mdnishat27 <59754386+mdnishat27@users.noreply.github.com>
2020-09-12 19:17:25 +0600

    #11 house can be drawn

--- solutions/27-nishat/AgeOfVillagers/AgeOfVillagers/WellShape.cs
+++ solutions/27-nishat/AgeOfVillagers/AgeOfVillagers/WellShape.cs
@@ -19,11 +19,17 @@ namespace AgeOfVillagers
     {
         private readonly Point _topleft;
         private readonly Point _bottomright;
+        private readonly float height, width;
 
         public EgyptianHouseShape(Point topleft, Point bottomright)
         {
             _topleft = topleft;
             _bottomright = bottomright;
+            height = _bottomright.Y - _topleft.Y;
+            width = _bottomright.X - _topleft.X;
+
+            AddComponent(new MyTriangle(new Point((int)(_topleft.X + width * 0.45), _topleft.Y),new Point(_topleft.X, (int)(_topleft.Y + height * 0.8)), new Point((int)(_topleft.X + width * 0.63), _bottomright.Y)));
+            AddComponent(new MyTriangle(new Point(_bottomright.X, (int)(_topleft.Y + height * 0.6)), new Point((int)(_topleft.X + width * 0.45), _topleft.Y), new Point((int)(_topleft.X + width * 0.63), _bottomright.Y)));
         }
     }
 
@@ -31,11 +37,14 @@ namespace AgeOfVillagers
     {
         private readonly Point _topleft;
         private readonly Point _bottomright;
+        private readonly float height, width;
 
         public EgyptianTreeShape(Point topleft, Point bottomright)
         {
             _topleft = topleft;
             _bottomright = bottomright;
+            height = _bottomright.Y - _topleft.Y;
+            width = _bottomright.X - _topleft.X;
         }
     }
 
