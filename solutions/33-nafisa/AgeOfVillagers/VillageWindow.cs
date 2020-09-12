8cae39f325bfdd94e76800367a3e43644a399367
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-13 02:13:55 +0600

    Form renamed to VillageWindow

---
+++ solutions/33-nafisa/AgeOfVillagers/VillageWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    public partial class VillageWindow : Form
+    {
+        public VillageWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // VillageWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(485, 313);
+            this.Name = "VillageWindow";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
