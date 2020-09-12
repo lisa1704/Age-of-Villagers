79441b00c8c825540970f7a6f5b7f69d85eb311a
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-13 02:57:43 +0600

    #9 #10 #11 2 abstract class village has been exchaged with Interface IItem

--- solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/Interface/IItem.cs
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/Interface/IItem.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AgeOfVillagers.AbstractClass
+namespace AgeOfVillagers.Interface
 {
-    class Village
+    interface IItem
     {
     }
 }
---
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/Village extended classes/BangladeshiFarmersHouse.cs
@@ -0,0 +1,16 @@
+﻿using AgeOfVillagers.AbstractClass;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.Village_extended_classes
+{
+    class BangladeshiFarmersHouse : Village
+    {
+        public override void DrawItems(Graphics g)
+        {
+            
+        }
+    }
+}
