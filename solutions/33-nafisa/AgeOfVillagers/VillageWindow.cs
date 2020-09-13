77956000fc34d3217bd2354e1d2701b18b509588
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-13 23:13:20 +0600

    #4 added SaveButton

--- solutions/33-nafisa/AgeOfVillagers/VillageWindow.cs
+++ solutions/33-nafisa/AgeOfVillagers/VillageWindow.cs
@@ -10,10 +10,13 @@ using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
-    public partial class VillageWindow : Form
+    public class VillageWindow : Form
     {
         private Panel drawingPanel;
         private Panel toolsPanel;
+        string item;
+        //string villagename;
+        //string nationname;
 
         public VillageWindow()
         {
@@ -60,12 +63,14 @@ namespace AgeOfVillagers
             AddRadioButton("Tree");
             AddRadioButton("House");
             AddRadioButton("WaterSource");
-
             Addlabel("");
+            AddButton("Save Village");
+            AddButton("New Village");
+            AddButton("Open Village");
 
         }
 
-       
+        
 
         private void InitVillageWindow()
         {
@@ -76,7 +81,42 @@ namespace AgeOfVillagers
             Controls.Add(toolsPanel);
             Name = "VillageWindow";
             Text = "Age of Villagers";
-        
+            //Load += new System.EventHandler(this.NewVillage_Load);
+
+        }
+
+       
+
+        private void AddButton(string text)
+        {
+            var button = new Button
+            {
+                Text = text,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Width = int.MaxValue
+            };
+            toolsPanel.Controls.Add(button);
+            if (button.Text == "Save Village")
+            {
+                button.Click += new System.EventHandler(this.SaveVillage_Click);
+            }
+        }
+
+        private void SaveVillage_Click(object sender, EventArgs e)
+        {
+            
+            if (item == "Tree")
+            {
+                MessageBox.Show(item);
+            }
+            if (item == "House")
+            {
+                MessageBox.Show(item);
+            }
+            if (item == "WaterSource")
+            {
+                MessageBox.Show(item);
+            }
         }
 
         private void AddRadioButton(string text)
@@ -86,6 +126,36 @@ namespace AgeOfVillagers
                 Text = text
             };
             toolsPanel.Controls.Add(radiobutton);
+            if (radiobutton.Text == "Tree")
+            {
+                radiobutton.CheckedChanged += new System.EventHandler(this.tree_CheckedChanged);
+            }
+            if (radiobutton.Text == "House")
+            {
+                radiobutton.CheckedChanged += new System.EventHandler(this.House_CheckedChanged);
+            }
+            if (radiobutton.Text == "WaterSource")
+            {
+                radiobutton.CheckedChanged += new System.EventHandler(this.WaterSource_CheckedChanged);
+            }
+
+        }
+
+        private void WaterSource_CheckedChanged(object sender, EventArgs e)
+        {
+            item = "WaterSource";
+        }
+
+        private void House_CheckedChanged(object sender, EventArgs e)
+        {
+            item = "House";
+        }
+
+        private void tree_CheckedChanged(object sender, EventArgs e)
+        {
+
+            item = "Tree";
+
         }
 
         private void Addlabel(string text)
@@ -101,6 +171,8 @@ namespace AgeOfVillagers
             
         }
 
-        
+       
+
+
     }
 }
