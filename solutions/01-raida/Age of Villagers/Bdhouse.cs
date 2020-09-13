e7ececa54721991aeb2dc720f70138320a795fd0
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-09-13 19:47:47 +0600

    Revert "#9 rectangle and bdtree implementation changed"
    This reverts commit 79f9ea97bea0f11f444348ef731e151c1baa8966.

--- solutions/01-raida/Age of Villagers/Bdhouse.cs
+++ solutions/01-raida/Age of Villagers/Bdhouse.cs
@@ -7,11 +7,15 @@ namespace Age_of_Villagers
         public Bdhouse(Point p, int length, int width) : base(p, length, width) { }
         public override void createHouse()
         {
-            cs.addShape(new rectangle(p,length,width));
+            Point p2 = p;
+            Point p3 = new Point(p2.X + length, p2.Y);
+            Point p4 = new Point(p2.X, p.Y + length);
+            Point p5 = new Point(p2.X + length, p2.Y + length);
+            cs.addShape(new rectangle(p2, p3, p5, p4));
             Point p1 = p;
-            Point p2 = new Point(p.X+length,p.Y);
-            Point p3 = new Point(p.X + length / 2, p.Y - length/2);
-            cs.addShape(new triangle(p1,p2,p3));
+            p2 = new Point(p.X + length, p.Y);
+            p3 = new Point(p.X + length / 2, p.Y - length / 2);
+            cs.addShape(new triangle(p1, p2, p3));
         }
         
     }
