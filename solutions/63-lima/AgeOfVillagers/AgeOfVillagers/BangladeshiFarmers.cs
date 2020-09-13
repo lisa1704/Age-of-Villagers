55561e6ad8b83ba577b69dddb9e6597d1decdeae
Maksuda Islam <maksudalima321@outlook.com>
2020-09-13 22:48:41 +0600

    #9 BangladeshiFarmers class updated

--- solutions/63-lima/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
@@ -1,25 +1,52 @@
 ﻿using System;
 using System.Drawing;
+using System.Windows.Forms;
 
 public class BangladeshiFarmers : INation
 {
+    Pen p = new Pen(Color.White);
+    Panel panel;
+    public BangladeshiFarmers(Panel panel)
+    {
+        this.panel = panel;
+    }
     public void paintTerrain()
     {
-        throw new NotImplementedException();
+        panel.BackColor = Color.Green;
     }
 
     public void drawHouse(Graphics g, int X, int Y)
     {
-        throw new NotImplementedException();
+        g.DrawLine(p, X, Y + 25, X + 25, Y);
+        g.DrawLine(p, X + 25, Y, X + 50, Y + 25);
+        g.DrawLine(p, X, Y + 25, X + 50, Y + 25);
+        g.DrawLine(p, X + 50, Y + 25, X + 50, Y + 50);
+        g.DrawLine(p, X + 50, Y + 50, X, Y + 50);
+        g.DrawLine(p, X, Y + 50, X, Y + 25);
+
     }
 
     public void drawTree(Graphics g, int X, int Y)
     {
-        throw new NotImplementedException();
+        g.DrawArc(p, X + 25, Y, 50, 50, 0, 360);
+
+        g.DrawLine(p, X + 48, Y + 50, X + 52, Y + 50);
+        g.DrawLine(p, X + 52, Y + 50, X + 52, Y + 75);
+        g.DrawLine(p, X + 52, Y + 75, X + 48, Y + 75);
+        g.DrawLine(p, X + 48, Y + 75, X + 48, Y + 50);
+
     }
 
     public void drawWaterSource(Graphics g, int X, int Y)
     {
-        throw new NotImplementedException();
+        g.DrawLine(p, X, Y + 50, X + 25, Y + 25);
+        g.DrawLine(p, X + 25, Y + 25, X + 50, Y + 50);
+        g.DrawLine(p, X + 50, Y + 50, X + 75, Y);
+        g.DrawLine(p, X + 75, Y, X + 50, Y - 25);
+        g.DrawLine(p, X + 50, Y - 25, X + 25, Y);
+        g.DrawLine(p, X + 25, Y, X + 25, Y - 25);
+        g.DrawLine(p, X + 25, Y - 25, X, Y - 25);
+        g.DrawLine(p, X, Y - 25, X - 25, Y + 25);
+        g.DrawLine(p, X - 25, Y + 25, X, Y + 50);
     }
 }
--- solutions/63-lima/AgeOfVillagers/AgeOfVillagers/CreateVillageWindow.cs
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/CreateVillageWindow.cs
@@ -50,21 +50,73 @@ namespace AgeOfVillagers
         private void drawing_panel_MouseClick(object sender, MouseEventArgs e)
         {
             g = drawing_panel.CreateGraphics();
-            EgyptianKings egyptianKings = new EgyptianKings(drawing_panel);
-            egyptianKings.paintTerrain();
-            if (tree.Checked == true)
-            {
-                egyptianKings.drawTree(g , e.X, e.Y);
-            }
-            else if(house.Checked == true)
-            {
-                egyptianKings.drawHouse(g, e.X, e.Y);
-            }
-            else
-            {
-                egyptianKings.drawWaterSource(g, e.X, e.Y);
-            }
-            
+            //if (select_nation.Text == "Egyptian Kings")
+            //{
+            //    EgyptianKings egyptianKings = new EgyptianKings(drawing_panel);
+            //    egyptianKings.paintTerrain();
+            //    if (tree.Checked == true)
+            //    {
+            //        egyptianKings.drawTree(g, e.X, e.Y);
+            //    }
+            //    else if (house.Checked == true)
+            //    {
+            //        egyptianKings.drawHouse(g, e.X, e.Y);
+            //    }
+            //    else
+            //    {
+            //        egyptianKings.drawWaterSource(g, e.X, e.Y);
+            //    }
+
+            //}
+            ////EgyptianKings egyptianKings = new EgyptianKings(drawing_panel);
+            ////egyptianKings.paintTerrain();
+            ////if (tree.Checked == true)
+            ////{
+            ////    egyptianKings.drawTree(g , e.X, e.Y);
+            ////}
+            ////else if(house.Checked == true)
+            ////{
+            ////    egyptianKings.drawHouse(g, e.X, e.Y);
+            ////}
+            ////else
+            ////{
+            ////    egyptianKings.drawWaterSource(g, e.X, e.Y);
+            ////}
+
+            //else if(select_nation.Text == "Bangladeshi Farmers") 
+            //{
+            //    BangladeshiFarmers bangladeshiFarmers = new BangladeshiFarmers(drawing_panel);
+            //    bangladeshiFarmers.paintTerrain();
+            //    if (tree.Checked == true)
+            //    {
+            //        bangladeshiFarmers.drawTree(g, e.X, e.Y);
+            //    }
+            //    else if (house.Checked == true)
+            //    {
+            //        bangladeshiFarmers.drawHouse(g, e.X, e.Y);
+            //    }
+            //    else
+            //    {
+            //        bangladeshiFarmers.drawWaterSource(g, e.X, e.Y);
+            //    }
+            //}
+            ////BangladeshiFarmers bangladeshiFarmers = new BangladeshiFarmers(drawing_panel);
+            ////bangladeshiFarmers.paintTerrain();
+            ////if (tree.Checked == true)
+            ////{
+            ////    bangladeshiFarmers.drawTree(g, e.X, e.Y);
+            ////}
+            ////else if (house.Checked == true)
+            ////{
+            ////    bangladeshiFarmers.drawHouse(g, e.X, e.Y);
+            ////}
+            ////else
+            ////{
+            ////    bangladeshiFarmers.drawWaterSource(g, e.X, e.Y);
+            ////}
+
+
+
         }
 
         private void tree_CheckedChanged(object sender, EventArgs e)
@@ -80,6 +132,11 @@ namespace AgeOfVillagers
         private void water_resource_CheckedChanged(object sender, EventArgs e)
         {
             //text = "Water Resource";
+        }
+
+        private void select_nation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 
