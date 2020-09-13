0ce72e49b6ee437ab2c15198f3b3da1863ad2d8c
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-13 21:31:25 +0600

    #9 #10 #11 # 12 Ishape interface deleted Shape Abstract class added

--- solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/ItemFactory.cs
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/ItemFactory.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AgeOfVillagers.Interface
+namespace AgeOfVillagers
 {
-    interface IShapes
+    class ItemFactory
     {
     }
 }
