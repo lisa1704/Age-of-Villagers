e9814186c44de18c051e02af59ba94db703b5f83
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-13 20:52:40 +0600

    #1 arc edited.

--- solutions/90-safayat/AgeOfVillagers90/Arc.cs
+++ solutions/90-safayat/AgeOfVillagers90/Arc.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgeOfVillagers90
 {
-    class Arc
+    class Arc : IShapeIItem
     {
+        private readonly Point point1;
+        private readonly Point point2;
+        public int x, y, width, heigth, initialngle, sweepAngle;
+        public Arc(Point point1, Point point2)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+        }
 
+        public void Draw(Graphics g)
+        {
+            var pen = new Pen(Color.Black, 1);
+            g.DrawArc(pen, x, y, width, heigth, initialngle, sweepAngle);
+        }
     }
 }
