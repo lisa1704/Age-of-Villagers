8e3af20b7fd3e4f0ccb181b5f59231ab30bd5758
Maksuda Islam <maksudalima321@outlook.com>
2020-09-13 23:07:42 +0600

    #10 ArabBedouin class created

---
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/ArabBedouin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+
+public class ArabBedouin : INation
+{
+	public ArabBedouin()
+	{
+	}
+
+    public void drawHouse(Graphics g, int X, int Y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void drawTree(Graphics g, int X, int Y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void drawWaterSource(Graphics g, int X, int Y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void paintTerrain()
+    {
+        throw new NotImplementedException();
+    }
+}
