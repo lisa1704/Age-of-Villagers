8b2fd7b7a05d8896263f29b9289bc664b1ceb4e9
Maksuda Islam <maksudalima321@outlook.com>
2020-09-13 19:17:33 +0600

    #11 classes added for different nations

---
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/EgyptianKings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class EgyptianKings : INation
+{
+    public void drawHouse()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void drawTree()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void drawWaterSource()
+    {
+
+    }
+}
