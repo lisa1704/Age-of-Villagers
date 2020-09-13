a7ba5a1ec87ece06f3e453adb79d39b3e19202b2
Maksuda Islam <maksudalima321@outlook.com>
2020-09-13 23:03:57 +0600

    #9 #11 CreateVillageWindow factory changed

--- solutions/63-lima/AgeOfVillagers/AgeOfVillagers/CreateVillageWindow.cs
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/CreateVillageWindow.cs
@@ -50,94 +50,62 @@ namespace AgeOfVillagers
         private void drawing_panel_MouseClick(object sender, MouseEventArgs e)
         {
             g = drawing_panel.CreateGraphics();
-            //if (select_nation.Text == "Egyptian Kings")
+            if (select_nation.Text == "egyptian kings")
+            {
+                EgyptianKings egyptiankings = new EgyptianKings(drawing_panel);
+                egyptiankings.paintTerrain();
+                if (tree.Checked == true)
+                {
+                    egyptiankings.drawTree(g, e.X, e.Y);
+                }
+                else if (house.Checked == true)
+                {
+                    egyptiankings.drawHouse(g, e.X, e.Y);
+                }
+                else
+                {
+                    egyptiankings.drawWaterSource(g, e.X, e.Y);
+                }
+            }
+
+
+            else if (select_nation.Text == "Bangladeshi Farmers")
+            {
+                BangladeshiFarmers bangladeshiFarmers = new BangladeshiFarmers(drawing_panel);
+                bangladeshiFarmers.paintTerrain();
+                if (tree.Checked == true)
+                {
+                    bangladeshiFarmers.drawTree(g, e.X, e.Y);
+                }
+                else if (house.Checked == true)
+                {
+                    bangladeshiFarmers.drawHouse(g, e.X, e.Y);
+                }
+                else
+                {
+                    bangladeshiFarmers.drawWaterSource(g, e.X, e.Y);
+                }
+            }
+
+            //private void tree_CheckedChanged(object sender, EventArgs e)
             //{
-            //    EgyptianKings egyptianKings = new EgyptianKings(drawing_panel);
-            //    egyptianKings.paintTerrain();
-            //    if (tree.Checked == true)
-            //    {
-            //        egyptianKings.drawTree(g, e.X, e.Y);
-            //    }
-            //    else if (house.Checked == true)
-            //    {
-            //        egyptianKings.drawHouse(g, e.X, e.Y);
-            //    }
-            //    else
-            //    {
-            //        egyptianKings.drawWaterSource(g, e.X, e.Y);
-            //    }
+            //    //text = "Tree";
+            //}
+
+            //private void house_CheckedChanged(object sender, EventArgs e)
+            //{
+            //    //text = "House";
+            //}
+
+            //private void water_resource_CheckedChanged(object sender, EventArgs e)
+            //{
+            //    //text = "Water Resource";
+            //}
+
+            //private void select_nation_SelectedIndexChanged(object sender, EventArgs e)
+            //{
 
             //}
-            ////EgyptianKings egyptianKings = new EgyptianKings(drawing_panel);
-            ////egyptianKings.paintTerrain();
-            ////if (tree.Checked == true)
-            ////{
-            ////    egyptianKings.drawTree(g , e.X, e.Y);
-            ////}
-            ////else if(house.Checked == true)
-            ////{
-            ////    egyptianKings.drawHouse(g, e.X, e.Y);
-            ////}
-            ////else
-            ////{
-            ////    egyptianKings.drawWaterSource(g, e.X, e.Y);
-            ////}
-
-            //else if(select_nation.Text == "Bangladeshi Farmers") 
-            //{
-            //    BangladeshiFarmers bangladeshiFarmers = new BangladeshiFarmers(drawing_panel);
-            //    bangladeshiFarmers.paintTerrain();
-            //    if (tree.Checked == true)
-            //    {
-            //        bangladeshiFarmers.drawTree(g, e.X, e.Y);
-            //    }
-            //    else if (house.Checked == true)
-            //    {
-            //        bangladeshiFarmers.drawHouse(g, e.X, e.Y);
-            //    }
-            //    else
-            //    {
-            //        bangladeshiFarmers.drawWaterSource(g, e.X, e.Y);
-            //    }
-            //}
-            ////BangladeshiFarmers bangladeshiFarmers = new BangladeshiFarmers(drawing_panel);
-            ////bangladeshiFarmers.paintTerrain();
-            ////if (tree.Checked == true)
-            ////{
-            ////    bangladeshiFarmers.drawTree(g, e.X, e.Y);
-            ////}
-            ////else if (house.Checked == true)
-            ////{
-            ////    bangladeshiFarmers.drawHouse(g, e.X, e.Y);
-            ////}
-            ////else
-            ////{
-            ////    bangladeshiFarmers.drawWaterSource(g, e.X, e.Y);
-            ////}
-
-
-
-        }
-
-        private void tree_CheckedChanged(object sender, EventArgs e)
-        {
-            //text = "Tree";
-        }
-
-        private void house_CheckedChanged(object sender, EventArgs e)
-        {
-            //text = "House";
-        }
-
-        private void water_resource_CheckedChanged(object sender, EventArgs e)
-        {
-            //text = "Water Resource";
-        }
-
-        private void select_nation_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
         }
     }
-
 }
