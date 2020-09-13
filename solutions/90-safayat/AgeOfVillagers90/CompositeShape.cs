336138920ae87e71258f2618f59949a9177302eb
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-14 00:42:17 +0600

    #1 Triangle class created.

---
+++ solutions/90-safayat/AgeOfVillagers90/CompositeShape.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace AgeOfVillagers90
+{
+    public abstract class CompositeShape : IShapeIItem
+    {
+        private List<IShapeIItem> ShapeComponents;
+
+        protected CompositeShape()
+        {
+            ShapeComponents = new List<IShapeIItem>();
+        }
+
+        protected void AddComponent(IShapeIItem shape)
+        {
+            ShapeComponents.Add(shape);
+        }
+
+        public void Draw(Graphics g)
+        {
+            foreach (var component in ShapeComponents)
+            {
+                component.Draw(g);
+            }
+        }
+
+    }
+}
--- solutions/90-safayat/AgeOfVillagers90/IShapeIItem.cs
+++ solutions/90-safayat/AgeOfVillagers90/IShapeIItem.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,29 +9,5 @@ namespace AgeOfVillagers90
     public interface IShapeIItem
     {
         void Draw(Graphics g);
-    }
-
-    public abstract class CompositeShape : IShapeIItem
-    {
-        private List<IShapeIItem> ShapeComponents;
-
-        protected CompositeShape()
-        {
-            ShapeComponents = new List<IShapeIItem>();
-        }
-
-        protected void AddComponent(IShapeIItem shape)
-        {
-            ShapeComponents.Add(shape);
-        }
-
-        public void Draw(Graphics g)
-        {
-            foreach (var component in ShapeComponents)
-            {
-                component.Draw(g);
-            }
-        }
-
     }
 }
