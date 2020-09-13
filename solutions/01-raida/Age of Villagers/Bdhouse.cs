089bbd6f142509f75f74560b5ba33982dd31c133
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-09-13 11:25:49 +0600

    #9 Bdhouse removed from house

---
+++ solutions/01-raida/Age of Villagers/Bdhouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Age_of_Villagers
+{
+    class Bdhouse : house
+    {
+        public Bdhouse(Point p, int length, int width) : base(p, length, width) { }
+        public override void createHouse()
+        {
+            cs.addShape(new rectangle(p,length,width));
+            Point p1 = p;
+            Point p2 = new Point(p.X+length,p.Y);
+            Point p3 = new Point(p.X + length / 2, p.Y - width);
+            cs.addShape(new triangle(p1,p2,p3));
+        }
+        
+    }
+}
