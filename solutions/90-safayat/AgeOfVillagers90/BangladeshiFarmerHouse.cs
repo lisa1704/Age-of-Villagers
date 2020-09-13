02af207ebf06ac3d219de1253769a34f4665e840
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-14 01:39:55 +0600

    #1 #9 implemented BangladeshiFarmerHouse.

--- solutions/90-safayat/AgeOfVillagers90/BangladeshiFarmerHouse.cs
+++ solutions/90-safayat/AgeOfVillagers90/BangladeshiFarmerHouse.cs
@@ -1,7 +1,13 @@
-﻿namespace AgeOfVillagers90
+﻿using System.Drawing;
+
+namespace AgeOfVillagers90
 {
     public class BangladeshiFarmerHouse : CompositeShape
     {
-
+        public BangladeshiFarmerHouse(Point top,Point topleft, Point bottomright)
+        {
+            AddComponent(new Rectangle(topleft, bottomright));
+            AddComponent(new TAgle(top, topleft, new Point(bottomright.X, topleft.Y)));
+        }
     }
 }
