f5b30212d3fc20d68c112269e2d23dd16bcf68dc
Maksuda Islam <maksudalima321@outlook.com>
2020-09-14 00:30:59 +0600

    #12 constructor for panel added

--- solutions/63-lima/AgeOfVillagers/AgeOfVillagers/InuitHunters.cs
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/InuitHunters.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Drawing;
+using System.Windows.Forms;
 
 public class InuitHunters : INation
 {
+    Pen p = new Pen(Color.Black);
+    Panel panel;
+    public InuitHunters(Panel panel)
+    {
+        this.panel = panel;
+    }
     public void drawTree(Graphics g, int X, int Y)
     {
         throw new NotImplementedException();
