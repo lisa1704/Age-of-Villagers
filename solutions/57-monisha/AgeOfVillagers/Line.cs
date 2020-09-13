eff9591f5de3589704e463228d3938cec03be95a
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2020-09-13 13:03:28 +0600

    #7 basis Shape component lLine added

---
+++ solutions/57-monisha/AgeOfVillagers/Line.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class Line : IShapeItem
+    {
+        private readonly Point point1;
+        private readonly Point point2;
+
+        public Line(Point pt1,Point pt2)
+        {
+            point1 = pt1;
+            point2 = pt2;
+        }
+        public void Paint(Graphics g)
+        {
+            var pen = new Pen(Color.Black,1);
+            g.DrawLine(pen, point1, point2);
+        }
+    }
+}
