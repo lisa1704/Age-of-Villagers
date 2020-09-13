fe315b326ecb6caff0d02d416a69b29a434d6c87
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-13 15:23:56 +0600

    #4 600*400 drawing panel created

--- solutions/33-nafisa/AgeOfVillagers/VillageWindow.cs
+++ solutions/33-nafisa/AgeOfVillagers/VillageWindow.cs
@@ -10,22 +10,42 @@ using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
-    public partial class VillageWindow : Form
+    public partial class DrawingPanel : Form
     {
-        public VillageWindow()
+        private Panel drawingPanel;
+    
+
+        public DrawingPanel()
         {
             InitializeComponent();
         }
 
         private void InitializeComponent()
-        {
+        {   
             this.SuspendLayout();
             // 
-            // VillageWindow
+            // DrawingPanel
             // 
-            this.ClientSize = new System.Drawing.Size(485, 313);
-            this.Name = "VillageWindow";
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Name = "DrawingPanel";
+            this.Load += new System.EventHandler(this.DrawingPanel_Load);
             this.ResumeLayout(false);
+
+        }
+        private void InitDrawingPanel()
+        {
+            drawingPanel = new Panel
+            {
+                Location = new Point(0, 0),
+                Name = "DrawingVillagePanel",
+                Size = new Size(600, 400),
+                TabIndex = 0,
+                BackColor =Color.White
+            };
+        }
+
+        private void DrawingPanel_Load(object sender, EventArgs e)
+        {
 
         }
     }
