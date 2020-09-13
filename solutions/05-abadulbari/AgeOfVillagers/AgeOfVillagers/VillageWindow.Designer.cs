ac77b74b7887b4f94af3814a494ca91986e89dd4
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-14 02:09:48 +0600

    #4 VillageWindow : UI designed

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillageWindow.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace AgeOfVillagers
+{
+    partial class VillageWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingPanel = new System.Windows.Forms.Panel();
+            this.toolPanel = new System.Windows.Forms.Panel();
+            this.openButton = new System.Windows.Forms.Button();
+            this.newButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.waterRadioBtn = new System.Windows.Forms.RadioButton();
+            this.houseRadioBtn = new System.Windows.Forms.RadioButton();
+            this.treeRadioBtn = new System.Windows.Forms.RadioButton();
+            this.nationLabel = new System.Windows.Forms.Label();
+            this.villageLabel = new System.Windows.Forms.Label();
+            this.gameLabel = new System.Windows.Forms.Label();
+            this.toolPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // drawingPanel
+            // 
+            this.drawingPanel.Location = new System.Drawing.Point(0, 0);
+            this.drawingPanel.Name = "drawingPanel";
+            this.drawingPanel.Size = new System.Drawing.Size(600, 400);
+            this.drawingPanel.TabIndex = 0;
+            // 
+            // toolPanel
+            // 
+            this.toolPanel.AllowDrop = true;
+            this.toolPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.toolPanel.Controls.Add(this.openButton);
+            this.toolPanel.Controls.Add(this.newButton);
+            this.toolPanel.Controls.Add(this.saveButton);
+            this.toolPanel.Controls.Add(this.waterRadioBtn);
+            this.toolPanel.Controls.Add(this.houseRadioBtn);
+            this.toolPanel.Controls.Add(this.treeRadioBtn);
+            this.toolPanel.Controls.Add(this.nationLabel);
+            this.toolPanel.Controls.Add(this.villageLabel);
+            this.toolPanel.Controls.Add(this.gameLabel);
+            this.toolPanel.Location = new System.Drawing.Point(600, 0);
+            this.toolPanel.Name = "toolPanel";
+            this.toolPanel.Size = new System.Drawing.Size(200, 400);
+            this.toolPanel.TabIndex = 1;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(38, 354);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(133, 34);
+            this.openButton.TabIndex = 4;
+            this.openButton.Text = "Open Village";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(39, 314);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(132, 34);
+            this.newButton.TabIndex = 4;
+            this.newButton.Text = "New Village";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(38, 274);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(133, 34);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save Village";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // waterRadioBtn
+            // 
+            this.waterRadioBtn.AutoSize = true;
+            this.waterRadioBtn.Location = new System.Drawing.Point(38, 215);
+            this.waterRadioBtn.Name = "waterRadioBtn";
+            this.waterRadioBtn.Size = new System.Drawing.Size(142, 29);
+            this.waterRadioBtn.TabIndex = 3;
+            this.waterRadioBtn.TabStop = true;
+            this.waterRadioBtn.Text = "Water Source";
+            this.waterRadioBtn.UseVisualStyleBackColor = true;
+            this.waterRadioBtn.CheckedChanged += new System.EventHandler(this.waterRadioBtn_CheckedChanged);
+            // 
+            // houseRadioBtn
+            // 
+            this.houseRadioBtn.AutoSize = true;
+            this.houseRadioBtn.Location = new System.Drawing.Point(38, 180);
+            this.houseRadioBtn.Name = "houseRadioBtn";
+            this.houseRadioBtn.Size = new System.Drawing.Size(88, 29);
+            this.houseRadioBtn.TabIndex = 3;
+            this.houseRadioBtn.TabStop = true;
+            this.houseRadioBtn.Text = "House";
+            this.houseRadioBtn.UseVisualStyleBackColor = true;
+            this.houseRadioBtn.CheckedChanged += new System.EventHandler(this.houseRadioBtn_CheckedChanged);
+            // 
+            // treeRadioBtn
+            // 
+            this.treeRadioBtn.AutoSize = true;
+            this.treeRadioBtn.Location = new System.Drawing.Point(38, 145);
+            this.treeRadioBtn.Name = "treeRadioBtn";
+            this.treeRadioBtn.Size = new System.Drawing.Size(68, 29);
+            this.treeRadioBtn.TabIndex = 3;
+            this.treeRadioBtn.TabStop = true;
+            this.treeRadioBtn.Text = "Tree";
+            this.treeRadioBtn.UseVisualStyleBackColor = true;
+            this.treeRadioBtn.CheckedChanged += new System.EventHandler(this.treeRadioBtn_CheckedChanged);
+            // 
+            // nationLabel
+            // 
+            this.nationLabel.AutoSize = true;
+            this.nationLabel.Location = new System.Drawing.Point(47, 88);
+            this.nationLabel.Name = "nationLabel";
+            this.nationLabel.Size = new System.Drawing.Size(117, 25);
+            this.nationLabel.TabIndex = 2;
+            this.nationLabel.Text = "Nation Name";
+            this.nationLabel.Click += new System.EventHandler(this.nationLabel_Click);
+            // 
+            // villageLabel
+            // 
+            this.villageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.villageLabel.AutoSize = true;
+            this.villageLabel.Location = new System.Drawing.Point(48, 45);
+            this.villageLabel.Name = "villageLabel";
+            this.villageLabel.Size = new System.Drawing.Size(116, 25);
+            this.villageLabel.TabIndex = 1;
+            this.villageLabel.Text = "Village Name";
+            this.villageLabel.Click += new System.EventHandler(this.villageLabel_Click);
+            // 
+            // gameLabel
+            // 
+            this.gameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gameLabel.AutoSize = true;
+            this.gameLabel.Font = new System.Drawing.Font("Algerian", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gameLabel.Location = new System.Drawing.Point(16, 9);
+            this.gameLabel.Name = "gameLabel";
+            this.gameLabel.Size = new System.Drawing.Size(172, 20);
+            this.gameLabel.TabIndex = 0;
+            this.gameLabel.Text = "Age Of Villagers";
+            this.gameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.gameLabel.Click += new System.EventHandler(this.gameLabel_Click);
+            // 
+            // VillageWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.toolPanel);
+            this.Controls.Add(this.drawingPanel);
+            this.Name = "VillageWindow";
+            this.Text = "Age of Villagers";
+            this.toolPanel.ResumeLayout(false);
+            this.toolPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel drawingPanel;
+        private System.Windows.Forms.Panel toolPanel;
+        private System.Windows.Forms.Label gameLabel;
+        private System.Windows.Forms.Label nationLabel;
+        private System.Windows.Forms.Label villageLabel;
+        private System.Windows.Forms.RadioButton waterButton;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton treeButton;
+        private System.Windows.Forms.RadioButton houseButton;
+        private System.Windows.Forms.RadioButton treeRadioBtn;
+        private System.Windows.Forms.RadioButton houseRadioBtn;
+        private System.Windows.Forms.RadioButton waterRadioBtn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button openButton;
+    }
+}
+
---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillageWindow.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    public partial class VillageWindow : Form
+    {
+        public VillageWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void gameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void villageLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nationLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void waterRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+
+        }
+     
+        private void houseRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
