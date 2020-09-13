8648f4697fb9937731b926b46176dc3353386155
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-14 00:49:43 +0600

    #1 Triangle Class Implemented.

--- solutions/90-safayat/AgeOfVillagers90/TAgle.cs
+++ solutions/90-safayat/AgeOfVillagers90/TAgle.cs
@@ -1,7 +1,14 @@
-﻿namespace AgeOfVillagers90
+﻿using System.Drawing;
+
+namespace AgeOfVillagers90
 {
     public class TAgle : CompositeShape
     {
-
+        public TAgle(Point pt1,Point pt2,Point pt3)
+        {
+            AddComponent(new Line(pt1, pt2));
+            AddComponent(new Line(pt2, pt3));
+            AddComponent(new Line(pt3, pt1));
+        }
     }
 }
