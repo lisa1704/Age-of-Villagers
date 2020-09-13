d7320930a72848ede1af513dfc28b89fe08094c3
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-14 00:29:22 +0600

    #1 #7 Refactored and implementation of rectangle class.

--- solutions/90-safayat/AgeOfVillagers90/Rectangle.cs
+++ solutions/90-safayat/AgeOfVillagers90/Rectangle.cs
@@ -7,25 +7,30 @@ using System.Threading.Tasks;
 
 namespace AgeOfVillagers90
 {
-    class Rectangle : IShapeIItem
+    class Rectangle : CompositeShape
     {
         private readonly Point TopLeft;
         private readonly Point BottomRight;
         public Rectangle(Point topLeft, Point BottomRight)
         {
-            this.TopLeft = topLeft;
-            this.BottomRight = BottomRight;
+            var TopRight = new Point(BottomRight.X, topLeft.Y);
+            var BottomLeft = new Point(topLeft.X, BottomRight.Y);
+
+            AddComponent(new Line(topLeft, BottomLeft));
+            AddComponent(new Line(topLeft, TopRight));
+            AddComponent(new Line(BottomLeft, BottomRight));
+            AddComponent(new Line(BottomRight, TopRight));
         }
 
        
-        public void Draw(Graphics g)
+        /*public void Draw(Graphics g)
         {
             var pen = new Pen(Color.Black, 1);
             g.DrawLine(pen, TopLeft.X, TopLeft.Y, BottomRight.X, TopLeft.Y);
             g.DrawLine(pen, TopLeft.X, TopLeft.Y, TopLeft.X, BottomRight.Y);
             g.DrawLine(pen, TopLeft.X, BottomRight.Y, BottomRight.X, BottomRight.Y);
             g.DrawLine(pen, BottomRight.X, TopLeft.Y, BottomRight.X, BottomRight.Y);
-        }
+        }*/
 
 
     }
