2930e41d94fee910cdc12c37d90daf6f6987949f
30-jubair <jobairt03@gmail.com>
2020-09-13 23:12:10 +0600

    #8 #9 #10 #11 #12 Strategy Pattern Implementation for Different types of Water Source Based on Different Nation

---
+++ solutions/30-jubair/Age of Villagers/Water Source/ArabBedouinWaterSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Age_of_Villagers.Water_Source
+{
+    class ArabBedouinWaterSource : IWaterSource
+    {
+        public void draw_watersource(MouseEventArgs e, Graphics graphics, Pen pen)
+        {
+
+        }
+    }
+}
---
+++ solutions/30-jubair/Age of Villagers/Water Source/BangladeshiFarmerWaterSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Age_of_Villagers.Water_Source
+{
+    class BangladeshiFarmerWaterSource : IWaterSource
+    {
+        public void draw_watersource(MouseEventArgs e, Graphics graphics, Pen pen)
+        {
+
+        }
+
+    }
+}
