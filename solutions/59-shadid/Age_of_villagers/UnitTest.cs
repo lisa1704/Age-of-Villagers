4721d1e09a2740dd4450643f7f0d59d51b34e26b
shadidintisar <shadidintisar@gmail.com>
2020-09-14 23:18:34 +0600

    #14 & #15 Prepared Project Structure and created test framework.

---
+++ solutions/59-shadid/Age_of_villagers/UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Age_of_villagers
+{
+    public class UnitTest
+    {
+
+        public void Test()
+        {
+            int a = 10;
+            int b = 20;
+            var c = a + b;
+          
+            Assert.Equals(30, c);
+        }
+
+        public void EqualityTest()
+        {
+            int a = 2;
+            int b = 2;
+            Assert.Equals(a, b);
+        }
+
+
+    }
+}
