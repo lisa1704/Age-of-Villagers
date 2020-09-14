3d68b847e4eb8302abf490943ab653ba626e89ef
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-14 23:24:25 +0600

    #1 #7 #9 Tree class implemented.

--- solutions/90-safayat/AgeOfVillagers90/BangladeshFarmersTree.cs
+++ solutions/90-safayat/AgeOfVillagers90/BangladeshFarmersTree.cs
@@ -1,7 +1,24 @@
-﻿namespace AgeOfVillagers90
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AgeOfVillagers90
 {
     public class BangladeshFarmersTree : CompositeShape
     {
+        public BangladeshFarmersTree(Point MainPoint)
+        {
+            Point TopRight = new Point(MainPoint.X + 20, MainPoint.Y);
+            Point BottomRight = new Point(MainPoint.X + 25, MainPoint.Y+50);
+            Point TopPoint = new Point(MainPoint.X + 40, MainPoint.Y+40);
+            Point TopLeft = new Point(MainPoint.X + 20, MainPoint.Y+20);
 
+            AddComponent(new Rectangle(TopLeft, BottomRight));
+            AddComponent(new ShapeEllipse(MainPoint, TopPoint));
+        }
     }
 }
