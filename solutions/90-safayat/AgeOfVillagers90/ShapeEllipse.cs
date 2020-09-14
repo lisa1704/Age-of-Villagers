566d27d39ca492efb00a075da34aaa6d5c86100d
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-14 21:19:57 +0600

    #1 Edited.

---
+++ solutions/90-safayat/AgeOfVillagers90/ShapeEllipse.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace AgeOfVillagers90
+{
+    public class ShapeEllipse : CompositeShape
+    {
+        private readonly Point topleft;
+        private readonly Point bottomright;
+
+        public ShapeEllipse(Point topleft, Point bottomright)
+        {
+            this.topleft = topleft;
+            this.bottomright = bottomright;
+            AddComponent(new Arc(topleft.X, topleft.Y, bottomright.X - topleft.X, bottomright.Y - topleft.Y, 0, 360));
+        }
+    }
+}
