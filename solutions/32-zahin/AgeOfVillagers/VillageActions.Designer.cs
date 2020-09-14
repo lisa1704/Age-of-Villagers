7e637b1ed5134614c3f64cfcc36a803b4ddcc303
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-14 18:41:04 +0600

    #4 #5 #6 #7 #1 Re-draw Panels

--- solutions/32-zahin/AgeOfVillagers/VillageActions.Designer.cs
+++ solutions/32-zahin/AgeOfVillagers/VillageActions.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AgeOfVillagers
 {
-    partial class Form1
+    partial class VillageActions
     {
         /// <summary>
         /// Required designer variable.
@@ -28,182 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.OptionsBoard = new System.Windows.Forms.GroupBox();
-            this.VillageName = new System.Windows.Forms.TextBox();
-            this.WaterSourceButton = new System.Windows.Forms.RadioButton();
-            this.HouseButton = new System.Windows.Forms.RadioButton();
-            this.TreeButton = new System.Windows.Forms.RadioButton();
-            this.NewVillage = new System.Windows.Forms.Button();
-            this.OpenVillage = new System.Windows.Forms.Button();
-            this.SaveVillage = new System.Windows.Forms.Button();
-            this.Nations = new System.Windows.Forms.ComboBox();
             this.ConfirmSaveVillage = new System.Windows.Forms.SaveFileDialog();
             this.ConfirmOpenVillage = new System.Windows.Forms.OpenFileDialog();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.OptionsBoard.SuspendLayout();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // OptionsBoard
-            // 
-            this.OptionsBoard.Controls.Add(this.VillageName);
-            this.OptionsBoard.Controls.Add(this.WaterSourceButton);
-            this.OptionsBoard.Controls.Add(this.HouseButton);
-            this.OptionsBoard.Controls.Add(this.TreeButton);
-            this.OptionsBoard.Controls.Add(this.NewVillage);
-            this.OptionsBoard.Controls.Add(this.OpenVillage);
-            this.OptionsBoard.Controls.Add(this.SaveVillage);
-            this.OptionsBoard.Controls.Add(this.Nations);
-            this.OptionsBoard.Font = new System.Drawing.Font("Arial", 10F);
-            this.OptionsBoard.Location = new System.Drawing.Point(609, 1);
-            this.OptionsBoard.Name = "OptionsBoard";
-            this.OptionsBoard.Size = new System.Drawing.Size(257, 400);
-            this.OptionsBoard.TabIndex = 0;
-            this.OptionsBoard.TabStop = false;
-            this.OptionsBoard.Text = "Age Of Villagers";
-            this.OptionsBoard.Enter += new System.EventHandler(this.OptionsBoard_Enter);
-            // 
-            // VillageName
-            // 
-            this.VillageName.Location = new System.Drawing.Point(48, 63);
-            this.VillageName.Name = "VillageName";
-            this.VillageName.Size = new System.Drawing.Size(154, 27);
-            this.VillageName.TabIndex = 0;
-            this.VillageName.Text = "Enter Village Name";
-            this.VillageName.TextChanged += new System.EventHandler(this.VillageName_TextChanged);
-            // 
-            // WaterSourceButton
-            // 
-            this.WaterSourceButton.AutoSize = true;
-            this.WaterSourceButton.Location = new System.Drawing.Point(60, 220);
-            this.WaterSourceButton.Name = "WaterSourceButton";
-            this.WaterSourceButton.Size = new System.Drawing.Size(132, 23);
-            this.WaterSourceButton.TabIndex = 9;
-            this.WaterSourceButton.TabStop = true;
-            this.WaterSourceButton.Text = "Water Source";
-            this.WaterSourceButton.UseVisualStyleBackColor = true;
-            this.WaterSourceButton.CheckedChanged += new System.EventHandler(this.WaterSourceButton_CheckedChanged);
-            // 
-            // HouseButton
-            // 
-            this.HouseButton.AutoSize = true;
-            this.HouseButton.Location = new System.Drawing.Point(60, 194);
-            this.HouseButton.Name = "HouseButton";
-            this.HouseButton.Size = new System.Drawing.Size(76, 23);
-            this.HouseButton.TabIndex = 8;
-            this.HouseButton.TabStop = true;
-            this.HouseButton.Text = "House";
-            this.HouseButton.UseVisualStyleBackColor = true;
-            this.HouseButton.CheckedChanged += new System.EventHandler(this.House_CheckedChanged);
-            // 
-            // TreeButton
-            // 
-            this.TreeButton.AutoSize = true;
-            this.TreeButton.Location = new System.Drawing.Point(60, 168);
-            this.TreeButton.Name = "TreeButton";
-            this.TreeButton.Size = new System.Drawing.Size(62, 23);
-            this.TreeButton.TabIndex = 7;
-            this.TreeButton.TabStop = true;
-            this.TreeButton.Text = "Tree";
-            this.TreeButton.UseVisualStyleBackColor = true;
-            this.TreeButton.CheckedChanged += new System.EventHandler(this.TreeButton_CheckedChanged);
-            // 
-            // NewVillage
-            // 
-            this.NewVillage.Font = new System.Drawing.Font("Arial", 10F);
-            this.NewVillage.Location = new System.Drawing.Point(48, 301);
-            this.NewVillage.Name = "NewVillage";
-            this.NewVillage.Size = new System.Drawing.Size(154, 35);
-            this.NewVillage.TabIndex = 6;
-            this.NewVillage.Text = "New Village";
-            this.NewVillage.UseVisualStyleBackColor = true;
-            this.NewVillage.Click += new System.EventHandler(this.NewVillage_Click);
-            // 
-            // OpenVillage
-            // 
-            this.OpenVillage.Font = new System.Drawing.Font("Arial", 10F);
-            this.OpenVillage.Location = new System.Drawing.Point(48, 342);
-            this.OpenVillage.Name = "OpenVillage";
-            this.OpenVillage.Size = new System.Drawing.Size(154, 36);
-            this.OpenVillage.TabIndex = 5;
-            this.OpenVillage.Text = "Open Village";
-            this.OpenVillage.UseVisualStyleBackColor = true;
-            this.OpenVillage.Click += new System.EventHandler(this.OpenVillage_Click);
-            // 
-            // SaveVillage
-            // 
-            this.SaveVillage.Font = new System.Drawing.Font("Arial", 10F);
-            this.SaveVillage.Location = new System.Drawing.Point(48, 263);
-            this.SaveVillage.Name = "SaveVillage";
-            this.SaveVillage.Size = new System.Drawing.Size(154, 32);
-            this.SaveVillage.TabIndex = 4;
-            this.SaveVillage.Text = "Save Village";
-            this.SaveVillage.UseVisualStyleBackColor = true;
-            this.SaveVillage.Click += new System.EventHandler(this.SaveVillage_Click);
-            // 
-            // Nations
-            // 
-            this.Nations.AllowDrop = true;
-            this.Nations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.Nations.FormattingEnabled = true;
-            this.Nations.Items.AddRange(new object[] {
-            "Bangladeshi Farmers",
-            "Arab Bedouin",
-            "Egyptian Kings",
-            "Inuit Hunters"});
-            this.Nations.Location = new System.Drawing.Point(48, 104);
-            this.Nations.Name = "Nations";
-            this.Nations.Size = new System.Drawing.Size(154, 27);
-            this.Nations.TabIndex = 0;
-            this.Nations.SelectedIndexChanged += new System.EventHandler(this.NationsName);
-            // 
-            // ConfirmSaveVillage
-            // 
-            this.ConfirmSaveVillage.FileOk += new System.ComponentModel.CancelEventHandler(this.ConfirmSaveVillage_FileOk);
-            // 
-            // ConfirmOpenVillage
-            // 
-            this.ConfirmOpenVillage.InitialDirectory = "D:\\3-1\\SWE 4502 (Design Pattern Lab)\\Assignment Test\\AgeOfVillagers";
-            this.ConfirmOpenVillage.FileOk += new System.ComponentModel.CancelEventHandler(this.ConfirmOpenVillage_FileOk);
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(3, 1);
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(600, 400);
-            this.panel1.TabIndex = 10;
-            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.Size = new System.Drawing.Size(539, 402);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Location = new System.Drawing.Point(545, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(322, 402);
+            this.panel2.TabIndex = 1;
             // 
             // VillageActions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(869, 403);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.OptionsBoard);
             this.Font = new System.Drawing.Font("Arial", 7.8F);
             this.Name = "VillageActions";
             this.Text = "VillageBoard";
-            this.Load += new System.EventHandler(this.NewVillage_Load);
-            this.OptionsBoard.ResumeLayout(false);
-            this.OptionsBoard.PerformLayout();
+            this.Load += new System.EventHandler(this.VillageActions_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox OptionsBoard;
-        private System.Windows.Forms.ComboBox Nations;
-        private System.Windows.Forms.Button SaveVillage;
-        private System.Windows.Forms.Button NewVillage;
-        private System.Windows.Forms.Button OpenVillage;
         private System.Windows.Forms.SaveFileDialog ConfirmSaveVillage;
         private System.Windows.Forms.OpenFileDialog ConfirmOpenVillage;
-        private System.Windows.Forms.RadioButton WaterSourceButton;
-        private System.Windows.Forms.RadioButton HouseButton;
-        private System.Windows.Forms.RadioButton TreeButton;
-        private System.Windows.Forms.TextBox VillageName;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- solutions/32-zahin/AgeOfVillagers/VillageActions.cs
+++ solutions/32-zahin/AgeOfVillagers/VillageActions.cs
@@ -10,86 +10,19 @@ using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
-    public partial class Form1 : Form
+    public partial class VillageActions : Form
     {
         string item;
         string villageName;
         string villageType;
-        public Form1()
+        public VillageActions()
         {
             InitializeComponent();
         }
 
-        private void NewVillage_Load(object sender, EventArgs e) {}
-
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        private void VillageActions_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void NationsName(object sender, EventArgs e)
-        {
-            villageType = Nations.Text;
-            Village village = new Village(villageType);
-            //Console.WriteLine(villageType);
-        }
-
-        private void NewVillage_Click(object sender, EventArgs e)
-        {
-           // DrawingBoard.Refresh();
-            OptionsBoard.Refresh();
-            //InitializeComponent();
-        }
-
-        private void SaveVillage_Click(object sender, EventArgs e)
-        {
-            villageName = VillageName.Text;
-            ConfirmSaveVillage.ShowDialog();
-        }
-
-        private void OpenVillage_Click(object sender, EventArgs e)
-        {
-            DialogResult result = ConfirmOpenVillage.ShowDialog();
-        }
-
-        private void House_CheckedChanged(object sender, EventArgs e)
-        {
-            item = "house";
-
-        }
-
-        private void TreeButton_CheckedChanged(object sender, EventArgs e)
-        {
-            item = "tree";
-        }
-
-        private void WaterSourceButton_CheckedChanged(object sender, EventArgs e)
-        {
-            item = "water";
-        }
-
-        private void DrawingBoardActions(object sender, EventArgs e)
-        {
-            Village v = new Village(villageType);
-            if (item == "house")
-                v.GetVillage().DrawHouse();
-            if (item == "tree")
-                v.GetVillage().DrawTree();
-            if (item == "water")
-                v.GetVillage().DrawWaterSource();
-        }
-
-        private void OptionsBoard_Enter(object sender, EventArgs e) {} // buttons stay inside this panel
-        private void VillageName_TextChanged(object sender, EventArgs e) {}
-
-        private void ConfirmOpenVillage_FileOk(object sender, CancelEventArgs e) {}
-
-        private void ConfirmSaveVillage_FileOk(object sender, CancelEventArgs e) {}
-
-        private void panel1_MouseClick(object sender, MouseEventArgs e)
-        {
-            Point p = e.Location;
-        }
-
     }
 }
