1ab34e8087fb0ff29c141613f808296b032d11c1
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-09-14 13:35:37 +0600

    #4 Buttons Added
    They don't do anything.

--- solutions/72-rafidhaque/src/VillageWindow.java
+++ solutions/72-rafidhaque/src/VillageWindow.java
@@ -5,6 +5,7 @@ public class VillageWindow extends JPanel {
     private final String gameName = "Age of Villagers";
     private String villageName;
     private String nationName;
+    private JFrame f = new JFrame("Title");
 
     public VillageWindow(String villageName, String nationName) {
         this.villageName = villageName;
@@ -22,10 +23,41 @@ public class VillageWindow extends JPanel {
         g.setFont(new Font("TimesRoman", Font.PLAIN, 13));
         g.drawString(this.villageName, 620, 90);
         g.drawString(this.nationName, 620, 130);
+
+        JButton treebutton=new JButton("Tree");
+        treebutton.setFont(new Font("Arial", Font.PLAIN, 10));
+        treebutton.setBounds(620,160,70,30);
+        f.add(treebutton);
+
+        JButton housebutton=new JButton("House");
+        housebutton.setFont(new Font("Arial", Font.PLAIN, 10));
+        housebutton.setBounds(700,160,70,30);
+        f.add(housebutton);
+
+        JButton waterbutton=new JButton("Water Source");
+        waterbutton.setFont(new Font("Arial", Font.PLAIN, 10));
+        waterbutton.setBounds(625,195,140,30);
+        f.add(waterbutton);
+
+        JButton savebutton=new JButton("Save Village");
+        savebutton.setFont(new Font("Arial", Font.PLAIN, 10));
+        savebutton.setBounds(625,260,140,30);
+        f.add(savebutton);
+
+        JButton newbuttton=new JButton("New\nVillage");
+        newbuttton.setFont(new Font("Arial", Font.PLAIN, 10));
+        newbuttton.setMargin(new Insets(1,1,1,1));
+        newbuttton.setBounds(620,300,70,30);
+        f.add(newbuttton);
+
+        JButton openbutton=new JButton("Open\nVillage");
+        openbutton.setFont(new Font("Arial", Font.PLAIN, 10));
+        openbutton.setMargin(new Insets(1,1,1,1));
+        openbutton.setBounds(700,300,70,30);
+        f.add(openbutton);
     }
 
     public void showWindow() {
-        JFrame f = new JFrame("Title");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.add(this);
         f.setSize(800, 400);
