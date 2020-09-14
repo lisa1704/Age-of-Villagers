8fde15b573886c72d288deab371a8171cc48d10f
30-jubair <jobairt03@gmail.com>
2020-09-14 21:54:38 +0600

    #9 #10 #11 #12  Rectangle and Triangle class added implementing Composite Shape

---
+++ solutions/30-jubair/Age of Villagers/Shape/MyRectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers.Shape
+{
+    class MyRectangle : CompositeShape
+    {
+       
+
+        public MyRectangle(Point topLeft, Point bottomRight)
+        {
+            var topRight = new Point(bottomRight.X, topLeft.Y);
+            var bottomLeft = new Point(topLeft.X, bottomRight.Y);
+
+            AddComponent(new Line(topLeft, topRight));
+            AddComponent(new Line(topRight, bottomRight));
+            AddComponent(new Line(bottomRight, bottomLeft));
+            AddComponent(new Line(bottomLeft, topLeft));
+        }
+
+    }
+}
---
+++ solutions/30-jubair/Age of Villagers/Shape/MyTriangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Age_of_Villagers.Shape
+{
+    class MyTriangle : CompositeShape
+    {
+        public MyTriangle(Point top, Point baseLeft, Point baseRight)
+        {
+            AddComponent(new Line(top, baseLeft));
+            AddComponent(new Line(baseLeft, baseRight));
+            AddComponent(new Line(baseRight, top));
+        }
+    }
+}
