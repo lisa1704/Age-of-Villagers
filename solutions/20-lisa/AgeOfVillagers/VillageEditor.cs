1be32e541875ccb670294c740d741fea22df2c2f
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-15 01:54:02 +0600

    #4 created village editor window

--- solutions/20-lisa/AgeOfVillagers/VillageEditor.cs
+++ solutions/20-lisa/AgeOfVillagers/VillageEditor.cs
@@ -10,12 +10,16 @@ using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
-    public partial class Form1 : Form
+    public partial class VillageEditor : Form
     {
-        public Form1()
+        public VillageEditor()
         {
             InitializeComponent();
         }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
