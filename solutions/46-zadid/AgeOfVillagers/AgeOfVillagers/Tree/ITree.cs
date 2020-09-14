55d53a1eb6adab5305a2831bd3abb0137350a457
Zadid Bin Azad <43434510+Zenhance@users.noreply.github.com>
2020-09-14 23:10:39 +0600

    #9 #10 #11 #12 Interface for tree drawing

---
+++ solutions/46-zadid/AgeOfVillagers/AgeOfVillagers/Tree/ITree.cs
@@ -0,0 +1,13 @@
+﻿using AgeOfVillagers.Data_Objects;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.Tree
+{
+    public interface ITree
+    {
+        public void Draw(ElementSize sz, Graphics g, Point p);
+    }
+}
