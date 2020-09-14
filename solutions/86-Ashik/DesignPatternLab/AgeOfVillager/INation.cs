d3cc95c9035f520be2f475b02c8566176512301e
86-Ashik <nusayerashik@iut-dhaka.edu>
2020-09-14 20:51:06 +0600

    #8 interface for nation created

---
+++ solutions/86-Ashik/DesignPatternLab/AgeOfVillager/INation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillager
+{
+    interface INation
+    {
+        //A nation has a terrain, house, tree, water resource and so on
+    }
+}
