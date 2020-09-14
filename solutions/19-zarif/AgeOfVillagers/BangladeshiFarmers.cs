8c6d6926c3e2ed4d9824a9d79259f8db68e56d44
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-14 23:11:09 +0600

    #9 Implemented methods

--- solutions/19-zarif/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/19-zarif/AgeOfVillagers/BangladeshiFarmers.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    class BangladeshiFarmers
+    public class BangladeshiFarmers : INation
     {
+        public void DrawHouse(Graphics g, int X, int Y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTrees(Graphics g, int X, int Y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSources(Graphics g, int X, int Y)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
