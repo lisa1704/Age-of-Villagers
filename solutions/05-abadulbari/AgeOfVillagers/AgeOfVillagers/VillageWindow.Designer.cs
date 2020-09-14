fe675e6f5a4a2166d247a99db51fb92eda66595a
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-14 17:05:49 +0600

    #4 VillageWindow modified

--- solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillageWindow.Designer.cs
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillageWindow.Designer.cs
@@ -53,6 +53,7 @@
             // 
             this.toolPanel.AllowDrop = true;
             this.toolPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.toolPanel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.toolPanel.Controls.Add(this.openButton);
             this.toolPanel.Controls.Add(this.newButton);
             this.toolPanel.Controls.Add(this.saveButton);
@@ -69,32 +70,35 @@
             // 
             // openButton
             // 
+            this.openButton.BackColor = System.Drawing.Color.WhiteSmoke;
             this.openButton.Location = new System.Drawing.Point(38, 354);
             this.openButton.Name = "openButton";
             this.openButton.Size = new System.Drawing.Size(133, 34);
             this.openButton.TabIndex = 4;
             this.openButton.Text = "Open Village";
-            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.UseVisualStyleBackColor = false;
             this.openButton.Click += new System.EventHandler(this.openButton_Click);
             // 
             // newButton
             // 
+            this.newButton.BackColor = System.Drawing.Color.WhiteSmoke;
             this.newButton.Location = new System.Drawing.Point(39, 314);
             this.newButton.Name = "newButton";
             this.newButton.Size = new System.Drawing.Size(132, 34);
             this.newButton.TabIndex = 4;
             this.newButton.Text = "New Village";
-            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.UseVisualStyleBackColor = false;
             this.newButton.Click += new System.EventHandler(this.newButton_Click);
             // 
             // saveButton
             // 
+            this.saveButton.BackColor = System.Drawing.Color.WhiteSmoke;
             this.saveButton.Location = new System.Drawing.Point(38, 274);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(133, 34);
             this.saveButton.TabIndex = 4;
             this.saveButton.Text = "Save Village";
-            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.UseVisualStyleBackColor = false;
             this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // waterRadioBtn
@@ -135,37 +139,39 @@
             // 
             // nationLabel
             // 
-            this.nationLabel.AutoSize = true;
-            this.nationLabel.Location = new System.Drawing.Point(47, 88);
+            this.nationLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.nationLabel.Location = new System.Drawing.Point(2, 88);
             this.nationLabel.Name = "nationLabel";
-            this.nationLabel.Size = new System.Drawing.Size(117, 25);
+            this.nationLabel.Size = new System.Drawing.Size(196, 28);
             this.nationLabel.TabIndex = 2;
             this.nationLabel.Text = "Nation Name";
+            this.nationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.nationLabel.Click += new System.EventHandler(this.nationLabel_Click);
             // 
             // villageLabel
             // 
             this.villageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.villageLabel.AutoSize = true;
-            this.villageLabel.Location = new System.Drawing.Point(48, 45);
+            this.villageLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.villageLabel.Location = new System.Drawing.Point(2, 45);
             this.villageLabel.Name = "villageLabel";
-            this.villageLabel.Size = new System.Drawing.Size(116, 25);
+            this.villageLabel.Size = new System.Drawing.Size(196, 28);
             this.villageLabel.TabIndex = 1;
             this.villageLabel.Text = "Village Name";
+            this.villageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.villageLabel.Click += new System.EventHandler(this.villageLabel_Click);
             // 
             // gameLabel
             // 
             this.gameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gameLabel.AutoSize = true;
-            this.gameLabel.Font = new System.Drawing.Font("Algerian", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.gameLabel.Location = new System.Drawing.Point(16, 9);
+            this.gameLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gameLabel.Font = new System.Drawing.Font("Algerian", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gameLabel.Location = new System.Drawing.Point(2, 9);
             this.gameLabel.Name = "gameLabel";
-            this.gameLabel.Size = new System.Drawing.Size(172, 20);
+            this.gameLabel.Size = new System.Drawing.Size(196, 30);
             this.gameLabel.TabIndex = 0;
             this.gameLabel.Text = "Age Of Villagers";
-            this.gameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.gameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.gameLabel.Click += new System.EventHandler(this.gameLabel_Click);
             // 
             // VillageWindow
