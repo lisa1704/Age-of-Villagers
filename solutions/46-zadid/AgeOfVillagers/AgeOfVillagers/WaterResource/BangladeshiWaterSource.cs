3c75001efaea9581d60b9a27acebb7f261c32a67
Zadid Bin Azad <43434510+Zenhance@users.noreply.github.com>
2020-09-15 00:41:34 +0600

    #9 #7 Tree placing implemented.

--- solutions/46-zadid/AgeOfVillagers/AgeOfVillagers/WaterResource/BangladeshiWaterSource.cs
+++ solutions/46-zadid/AgeOfVillagers/AgeOfVillagers/WaterResource/BangladeshiWaterSource.cs
@@ -8,7 +8,13 @@ namespace AgeOfVillagers.WaterResource
     {
         public void Draw(ElementSize sz, Graphics g, Point p)
         {
-           
+            Pen pen = new Pen(Color.Black);
+
+            g.DrawLine(pen, p.X, p.Y, p.X + sz.width / 3, p.Y - sz.height / 3);
+            g.DrawLine(pen, p.X + sz.width / 3, p.Y - sz.height / 3, p.X + sz.width / 8, p.Y + sz.height + 2);
+            g.DrawLine(pen, p.X + sz.width / 8, p.Y + sz.height + 2, p.X + sz.width / 4, sz.height / 7);
+            g.DrawLine(pen, p.X + sz.width / 4, sz.height / 7, p.X - sz.width / 5, p.Y - sz.height / 6);
+            g.DrawLine(pen, p.X - sz.width / 5, p.Y - sz.height / 6, p.X, p.Y);
         }
     }
 }
