8c3d7001aa04039c557a17de95b841af232dd414
Muhaimin <47084807+Muhai10@users.noreply.github.com>
2020-09-14 23:05:51 +0600

    #4 #1 Changed the name form1 to villageeditor

--- solutions/10-muhaimin/Age of Villagers/Form1.Designer.cs
+++ solutions/10-muhaimin/Age of Villagers/Form1.Designer.cs
@@ -153,7 +153,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "VillageEditor";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
