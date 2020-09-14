1e47d0849a221e9cf5456b8369cee2c75a198d62
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-14 23:07:32 +0600

    #4 #5 #6 #7 Null Value Handler Initialized

--- solutions/32-zahin/AgeOfVillagers/NullValuesHandler.cs
+++ solutions/32-zahin/AgeOfVillagers/NullValuesHandler.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
     class NullValuesHandler
     {
+        public void execute()
+        {
+            MessageBox.Show("Invalid!!");
+        }
     }
 }
