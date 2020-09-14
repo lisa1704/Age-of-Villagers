bdd7f1c762dba045316af03435294f650a3f08b4
30-jubair <jobairt03@gmail.com>
2020-09-14 20:31:58 +0600

    #8 #9 #10 #11 #12 Line Shape added for Drawing a Line

---
+++ solutions/30-jubair/Age of Villagers/Shape/Line.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers.Shape
+{
+    public class Line : IShape
+    {
+        private readonly Point _point1;
+        private readonly Point _point2;
+
+        public Line(Point point1, Point point2)
+        {
+            _point1 = point1;
+            _point2 = point2;
+        }
+
+        public void Paint(Graphics graphics)
+        {
+            var pen = new Pen(Color.Red, 2);
+            graphics.DrawLine(pen, _point1, _point2);
+        }
+
+        // GetBoundingBox()
+        // {
+
+        // }
+
+    }
+}
