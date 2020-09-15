dfbdabdbbf9f81c24a1b566df5a53cc98d272faf
musfiksifat <musfiksifat766@gmail.com>
2020-09-15 21:29:06 +0600

    #11 12*12px  EgyptianKings_WaterResource can be drawn

--- solutions/88-musfik/AgeOfVillagers/EgyptianKings.cs
+++ solutions/88-musfik/AgeOfVillagers/EgyptianKings.cs
@@ -77,8 +77,12 @@ namespace AgeOfVillagers
 
     public class EgyptianKings_WaterResource : CompositeShape
     {
-        public EgyptianKings_WaterResource(Point root)
+        public EgyptianKings_WaterResource(Point center)
         {
+            Point Circle_topLeft = new Point(center.X - 6, center.Y - 6);
+            Point Circle_bottomRight = new Point(center.X + 6, center.Y + 6);
+
+            AddComponent(new Arc(Circle_topLeft, Circle_bottomRight, 0, 360));
         }
     }
 }
