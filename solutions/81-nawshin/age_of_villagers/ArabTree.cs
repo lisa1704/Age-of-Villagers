c3fa5646f370b2bcf7d50ab2af84713ee74b66a7
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-16 00:40:03 +0600

    #7 #10 class created ArabTree

---
+++ solutions/81-nawshin/age_of_villagers/ArabTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace age_of_villagers
+{
+    public class ArabTree:CompositeShape
+    {
+        public ArabTree(Point pt)
+        {
+
+        }
+    }
+}
