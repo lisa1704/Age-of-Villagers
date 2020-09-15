ff64a11c51c7a9f80a5773cf97c764c818e46e93
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-15 17:56:32 +0600

    #7 Started adding the buttons to the panel.

--- solutions/48-zoyee/Age Of Villagers/Village_Window.cs
+++ solutions/48-zoyee/Age Of Villagers/Village_Window.cs
@@ -12,18 +12,74 @@ namespace Age_Of_Villagers
 {
     public partial class Village_Window : Form
     {
+        int x;
+        int y;
+        Graphics g;
+        string text = "";
+        string village_name = "";
+        List<Point> tree_point = new List<Point>();
+        List<Point> house_point = new List<Point>();
+        List<Point> waterSource_point = new List<Point>();
         public Village_Window()
         {
             InitializeComponent();
         }
 
-        private void InitializeComponent()
+        private void CreateNewVillage_Load(object sender, EventArgs e)
         {
-            //throw new NotImplementedException();
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void AgeOfVillagers_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void villageName_Click(object sender, EventArgs e)
+        {
+            string text1 = villageName.Text;
+            village_name = text1;
+        }
+
+        private void nation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeButton_CheckedChanged(object sender, EventArgs e)
+        {
+            text = "Tree";
+        }
+
+        private void houseButton_CheckedChanged(object sender, EventArgs e)
+        {
+            text = "House";
+        }
+
+        private void waterSourceButton_CheckedChanged(object sender, EventArgs e)
+        {
+            text = "Water Source";
+        }
+
+        private void saveVillagebutton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newVillagebutton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openVillagebutton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = drawingPanel.CreateGraphics();
+            Pen p = new Pen(Color.Black);
 
         }
     }
