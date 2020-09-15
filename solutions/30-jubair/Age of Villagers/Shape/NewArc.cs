8c5547c28a09135d59a4b181bcf5c08a97968fd7
30-jubair <jobairt03@gmail.com>
2020-09-15 14:28:11 +0600

    #9 #12 Arc and Ellipse Composite Shape Implemented for the ellipse shpaed items of different nation

---
+++ solutions/30-jubair/Age of Villagers/Shape/NewArc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Age_of_Villagers.Shape
+{
+    public class NewArc : IShape
+    {
+        private readonly float x;
+        private readonly float y;
+        private readonly float width;
+        private readonly float height;
+        private readonly float startAngle;
+        private readonly float sweepAngle;
+
+        public NewArc(float x, float y, float width, float height, float startAngle, float sweepAngle)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.startAngle = startAngle;
+            this.sweepAngle = sweepAngle;
+
+
+           
+        }
+        public void Paint(Graphics graphics)
+        {
+            var pen = new Pen(Color.Black, 1);
+            graphics.DrawArc(pen, x, y, width, height, startAngle, sweepAngle);
+        }
+
+    }
+}
---
+++ solutions/30-jubair/Age of Villagers/Shape/NewEllipse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+namespace Age_of_Villagers.Shape
+{
+    public class NewEllipse : CompositeShape
+    {
+        private readonly Point topLeft;
+        private readonly Point bottomRight;
+
+        public NewEllipse(Point _topleft,Point _bottomright)
+        {
+            this.topLeft = _topleft;
+            this.bottomRight = _bottomright;
+            AddComponent(new NewArc(topLeft.X, topLeft.Y, bottomRight.X - topLeft.X, bottomRight.Y - topLeft.Y, 0, 360));
+        }
+
+
+    }
+}
