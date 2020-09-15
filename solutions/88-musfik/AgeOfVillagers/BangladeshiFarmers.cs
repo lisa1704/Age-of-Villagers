2fd736c1bf14e8eb059e601a1f8137c052da0907
musfiksifat <musfiksifat766@gmail.com>
2020-09-15 17:13:53 +0600

    #9 16*24px BangladeshiTree can be drawn

--- solutions/88-musfik/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/88-musfik/AgeOfVillagers/BangladeshiFarmers.cs
@@ -56,8 +56,14 @@ namespace AgeOfVillagers
     public class BangladeshiTree : CompositeShape
     {
         public BangladeshiTree(Point point)
-        {
-            
+        {  
+            Point TopLeft = new Point(point.X - 1, point.Y + 4);
+            Point BottomRight = new Point(point.X + 1, point.Y + 16);
+            Point Circle_topLeft = new Point(point.X - 8, point.Y - 8);
+            Point Circle_bottomRight = new Point(point.X + 8, point.Y + 8);
+
+            AddComponent(new Arc(Circle_topLeft, Circle_bottomRight, 0, 360));
+            AddComponent(new Rectangle(TopLeft, BottomRight));
         }
     }
 }
--- solutions/88-musfik/AgeOfVillagers/VillageWindow.cs
+++ solutions/88-musfik/AgeOfVillagers/VillageWindow.cs
@@ -36,11 +36,11 @@ namespace AgeOfVillagers
             //Point point2 = new Point(e.X+30, e.Y+60);
 
             Point point = new Point(e.X, e.Y);
-            //IShape bShape = new BangladeshiHouse(new Point(point.X+20, point.Y));
+            IShape bShape = new BangladeshiHouse(new Point(point.X+20, point.Y));
             //IShape aShape = new ArabBedouinHouse(new Point(point.X - 20, point.Y));
-            IShape iShape = new InuitHuntersHouse(point);
+            IShape iShape = new BangladeshiTree(point);
 
-            //bShape.Draw(g, p);
+            bShape.Draw(g, p);
             //aShape.Draw(g, p);
             iShape.Draw(g, p);
 
