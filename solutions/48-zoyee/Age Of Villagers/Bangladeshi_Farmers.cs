36abcc648b0d8bb9ab70fb538590c753cf1fd511
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-15 22:44:15 +0600

    #7 #8 #9 INation interface impplemented in Bangladeshi Farmers class.

--- solutions/48-zoyee/Age Of Villagers/Bangladeshi_Farmers.cs
+++ solutions/48-zoyee/Age Of Villagers/Bangladeshi_Farmers.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Age_Of_Villagers
 {
-    class Bangladeshi_Farmers
+    class Bangladeshi_Farmers : INation
     {
+        public void Draw_House()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_Tree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_WaterSource()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
