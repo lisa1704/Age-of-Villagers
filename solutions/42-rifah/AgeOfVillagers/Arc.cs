8fbc8bcd6d42d534811131b93eb7475b05a851a3
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-15 21:34:15 +0600

    #7 arc class implemented

--- solutions/42-rifah/AgeOfVillagers/Arc.cs
+++ solutions/42-rifah/AgeOfVillagers/Arc.cs
@@ -7,9 +7,19 @@ namespace AgeOfVillagers
 {
     class Arc : IShape
     {
+        private readonly Point point1;
+        private readonly Point point2;
+        public int x, y, w, h, a1, a2;
+        public Arc(Point point1, Point point2)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+        }
+
         public void Draw(Graphics g)
         {
-            throw new NotImplementedException();
+            var pen = new Pen(Color.Black, 1);
+            g.DrawArc(pen, x, y, w, h, a1, a2);
         }
     }
 }
