b6d03f335734e4dee8ffe63c43ccdf406b3d96f8
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-15 21:05:07 +0600

    #7 Line, Rectangle and Arc class created and IShape interface implemented

---
+++ solutions/42-rifah/AgeOfVillagers/Arc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class Arc : IShape
+    {
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ solutions/42-rifah/AgeOfVillagers/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
