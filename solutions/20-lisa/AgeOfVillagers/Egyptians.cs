8c453d9efdd8a57f8c12193f9028128faa5462ab
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-16 00:54:17 +0600

    #8 #9 #10 #11 #12 Different Nations created implementing Nation interface

---
+++ solutions/20-lisa/AgeOfVillagers/Egyptians.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class Egyptians : INation
+    {
+        public void DrawHouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTerrain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
