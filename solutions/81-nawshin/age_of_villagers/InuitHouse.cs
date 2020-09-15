f39073ffdb5f6042ca0ab67f2f7e3a459cf31725
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-16 00:45:24 +0600

    #7 #12 class created for Inuit House

---
+++ solutions/81-nawshin/age_of_villagers/InuitHouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace age_of_villagers
+{
+    public class InuitHouse:CompositeShape
+    {
+        public InuitHouse(Point pt)
+        {
+
+        }
+    }
+}
