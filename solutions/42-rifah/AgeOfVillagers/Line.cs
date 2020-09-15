44eee713e3300e09278898338f66c81d7d865189
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-15 21:13:45 +0600

    #7 Line class implemented

--- solutions/42-rifah/AgeOfVillagers/Line.cs
+++ solutions/42-rifah/AgeOfVillagers/Line.cs
@@ -7,9 +7,19 @@ namespace AgeOfVillagers
 {
     class Line : IShape
     {
+        private readonly Point point_1;
+        private readonly Point point_2;
+
+        public Line(Point point1, Point point2)
+        {
+            this.point_1 = point1;
+            this.point_2 = point2;
+        }
+
         public void Draw(Graphics g)
         {
-            throw new NotImplementedException();
+            var my_pen = new Pen(Color.Black, 1);
+            g.DrawLine(my_pen, point_1, point_2);
         }
     }
 }
