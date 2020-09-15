2410420b2af3c4d5dd3bbe384016b96f4bcc73bf
Maksuda Islam <maksudalima321@outlook.com>
2020-09-15 13:47:23 +0600

    #8 #9 #10 #11 #12 INation is created and updated

--- solutions/63-lima/AgeOfVillagers/AgeOfVillagers/INation.cs
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/INation.cs
@@ -6,6 +6,6 @@ public interface INation
     public void drawHouse(Graphics g, int X, int Y);
     public void drawTree(Graphics g, int X, int Y);
     public void drawWaterSource(Graphics g, int X, int Y);
-    public void paintTerrain();
+    public void paintTerrain(); //for different color of drawing_panel for different nations
 
 }
