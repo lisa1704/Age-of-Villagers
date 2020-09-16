2bbe7df5b428ccec876d5af753c1e229165f56bd
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-16 20:32:51 +0600

    #7 #12 can draw Inuit house

--- solutions/81-nawshin/age_of_villagers/InuitHouse.cs
+++ solutions/81-nawshin/age_of_villagers/InuitHouse.cs
@@ -11,7 +11,14 @@ namespace age_of_villagers
     {
         public InuitHouse(Point pt)
         {
+            Point left = new Point(pt.X - 20, pt.Y);
+            Point right = new Point(pt.X + 20, pt.Y);
+            Point hc = new Point(pt.X + 5, pt.Y);
+            Point inner= new Point(right.X-3 , pt.Y+15);
 
+            AddComponent(new Line(left, right));
+            AddComponent(new Arc(hc, 40, 40, -192, 208));
+            AddComponent(new Arc(inner, 15, 15, -192, 208));
         }
     }
 }
