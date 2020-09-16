1cf29d779dc2e5b4660a15b7319a31b1b70dc704
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-17 00:04:41 +0600

    #1 bd farmer tree refactored

--- solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/BdFarmerTree.cs
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/BdFarmerTree.cs
@@ -12,8 +12,17 @@ namespace AgeOfVillagers
         public BdFarmerTree(Point e)
         {
             this.e = e;
+
+            Point point1 = new Point(e.X - 1, e.Y);
+            Point point2 = new Point(e.X + 1, e.Y);
+            Point point3 = new Point(e.X + 1, e.Y+16);
+            Point point4 = new Point(e.X - 1, e.Y+16);
+            Point point5 = new Point(e.X - 8, e.Y-8);
+
+            addComponent(new Quadrilateral(point1, point2, point3, point4));
+            addComponent(new Circle(point5, 16, 16));
         }
-        public void drawComponent(Point e, Graphics graphics, Pen pen)
+        /*public void drawComponent(Point e, Graphics graphics, Pen pen)
         {
             int x = e.X;
             int y = e.Y;
@@ -22,7 +31,7 @@ namespace AgeOfVillagers
             graphics.DrawLine(pen, x + 1, y, x + 1, y + 16);
             graphics.DrawLine(pen, x + 1, y + 16, x - 1, y + 16);
             graphics.DrawLine(pen, x - 1, y + 16, x - 1, y);
-        }
+        }*/
 
         public override Point getPoint()
         {
---
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public class Circle : IShape
+    {
+        public Point point;
+        public int height;
+        public int width;
+        public Circle(Point point, int height, int width)
+        {
+            this.point = point;
+            this.height = height;
+            this.width = width;
+        }
+        public void drawComponent(Point e, Graphics graphics, Pen pen)
+        {
+            graphics.DrawArc(pen, point.X, point.Y, width, height, 0, 360);
+        }
+    }
+}
