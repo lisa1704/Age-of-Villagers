e19a1038cb6ca6b8f7aeb7a68577bb59e89b72d7
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-16 13:38:51 +0600

    #8 IunitHunters added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/InuitHunters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class InuitHunters : INation
+    {
+        public string DrawHouse()
+        {
+            return "Igloo";
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
