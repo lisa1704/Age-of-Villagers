cc7f4743f5a50ee41ad7c490b03576b3c1cf5397
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-17 03:00:14 +0600

    #9 bangladeshi house, tree, waterSource class created

---
+++ solutions/34-meem/Age_Of_Villagers/BangladeshiTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_Of_Villagers
+{
+    class BangladeshiTree : IVillageItem
+    {
+        public void Draw(Point e, Graphics g, Pen p)
+        {
+            
+        }
+    }
+}
---
+++ solutions/34-meem/Age_Of_Villagers/BangladeshiWaterSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    class BangladeshiWaterSource : IVillageItem
+    {
+        public void Draw(Point e, Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
