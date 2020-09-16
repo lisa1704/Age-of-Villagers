8a8254feca58158a37490423147a329dd074cb51
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-16 13:34:23 +0600

    #8 ArabBedouin added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/ArabBedouin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class ArabBedouin : INation
+    {
+        public string DrawHouse()
+        {
+            return "Stonehouse";
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
