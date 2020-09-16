2790dcf9a103ebc28f5c03f2431a02e9ee1bb947
musfiksifat <musfiksifat766@gmail.com>
2020-09-16 21:07:57 +0600

    #12 InuitHunters class fully implemented

--- solutions/88-musfik/AgeOfVillagers/InuitHunters.cs
+++ solutions/88-musfik/AgeOfVillagers/InuitHunters.cs
@@ -16,7 +16,7 @@ namespace AgeOfVillagers
 
         public object getHouse(Point point)
         {
-            throw new NotImplementedException();
+            return new InuitHuntersHouse(point);
         }
 
         public string getNationName()
@@ -31,12 +31,12 @@ namespace AgeOfVillagers
 
         public object getTree(Point point)
         {
-            throw new NotImplementedException();
+            return new NullShape(point);
         }
 
         public object getWaterResource(Point point)
         {
-            throw new NotImplementedException();
+            return new NullShape(point);
         }
     }
 
