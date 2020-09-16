5d4546336c8db19b67377dd92777863350955534
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-16 19:49:53 +0600

    #7 Rectangle class implementation done.

--- solutions/48-zoyee/Age Of Villagers/Rectangle.cs
+++ solutions/48-zoyee/Age Of Villagers/Rectangle.cs
@@ -7,9 +7,20 @@ namespace Age_Of_Villagers
 {
     class Rectangle : IShape
     {
+        private readonly Point TopLeft;
+        private readonly Point BottomRight;
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            this.TopLeft = topLeft;
+            this.BottomRight = bottomRight;
+        }
         public void Draw(Graphics g)
         {
-            throw new NotImplementedException();
+            var pen = new Pen(Color.Black, 1);
+            g.DrawLine(pen, TopLeft.X, TopLeft.Y, BottomRight.X, TopLeft.Y);
+            g.DrawLine(pen, TopLeft.X, TopLeft.Y, TopLeft.X, BottomRight.Y);
+            g.DrawLine(pen, TopLeft.X, BottomRight.Y, BottomRight.X, BottomRight.Y);
+            g.DrawLine(pen, BottomRight.X, TopLeft.Y, BottomRight.X, BottomRight.Y);
         }
     }
 }
