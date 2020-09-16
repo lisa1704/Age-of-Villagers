fbab279cc4815e55bf84dc5a2b052849f33346a5
30-jubair <jobairt03@gmail.com>
2020-09-17 04:21:40 +0600

    #1 #8  Factory Method Pattern implied and Refactored

---
+++ solutions/30-jubair/Age of Villagers/NationFactory/NationFactoryApplication.cs
@@ -0,0 +1,35 @@
+﻿using Age_of_Villagers.Nation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_Villagers.NationFactory
+{
+    public class NationFactoryApplication
+    {
+        public AbstractNationCreator GetNation(string nation_name)
+        {
+            if(nation_name == "Bangladeshi Farmers")
+            {
+                return new BangladeshiNationCreator();
+            }
+
+            else if (nation_name == "Arab Bedouin")
+            {
+                return new ArabNationCreator();
+            }
+
+            else if (nation_name == "Egyptian King")
+            {
+                return new EgyptianNationCreator();
+            }
+
+            else if (nation_name == "Inuit Hunter")
+            {
+                return new InuitNationCreator();
+            }
+
+            return null;
+        }
+    }
+}
