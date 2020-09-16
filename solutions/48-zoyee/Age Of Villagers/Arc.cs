e77e557e11044d1c8457cc726103f866ce540098
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-16 20:59:11 +0600

    #1 #4 #7 Arc class edited.

--- solutions/48-zoyee/Age Of Villagers/Arc.cs
+++ solutions/48-zoyee/Age Of Villagers/Arc.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Age_Of_Villagers
 {
     class Arc : IShape
     {
-        public float x, y, width, height, angle_1, angle_2;
+        public float x, y, width, height, i_angle, s_angle;
+        private readonly Point point1;
+        private readonly Point point2;
 
-        public Arc(float x, float y, float width, float height, float angle_1, float angle_2)
+
+        public Arc(Point point1, Point point2)
         {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.angle_1 = angle_1;
-            this.angle_2 = angle_2;
+            this.point1 = point1;
+            this.point2 = point2;
         }
-            public void Draw(Graphics g)
+        public void Draw(Graphics g)
         {
             var pen = new Pen(Color.Black, 1);
-            g.DrawArc(pen, x, y, width, height, angle_1, angle_2);
+            g.DrawArc(pen, x, y, width, height, i_angle, s_angle);
         }
     }
 }
