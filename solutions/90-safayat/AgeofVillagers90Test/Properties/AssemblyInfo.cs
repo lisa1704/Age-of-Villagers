66f6b01630efe84c2f0c535d048320a608ed4210
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-16 15:10:28 +0600

    #1 #15  Test created.

---
+++ solutions/90-safayat/AgeofVillagers90Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AgeofVillagers90Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AgeofVillagers90Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b89f507b-b4e0-46ad-b27e-e8495b39a270")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ solutions/90-safayat/AgeofVillagers90Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgeOfVillagers90;
+
+namespace AgeofVillagers90Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BangladeshiFarmers bdfarmerTest = new BangladeshiFarmers();
+            string s = bdfarmerTest.BangladeshiFarmerTest();
+            Assert.AreEqual("Bangladeshi", s);
+            
+        }
+    }
+}
