8ee98efc2b0a239407fb809d4fa37001a669aebe
30-jubair <jobairt03@gmail.com>
2020-09-16 22:10:28 +0600

    #1  Null Pattern Implemented for null village component

---
+++ solutions/30-jubair/Age of Villagers/Shape/NullVillageComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Age_of_Villagers.Shape
+{
+    public class NullVillageComponent : CompositeShape
+    {
+        private readonly Point _point;
+
+        public NullVillageComponent(Point point)
+        {
+            _point = point;
+        }
+
+
+    }
+}
