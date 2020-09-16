30c9bf85c5b95b83e23b9043a6cb96bfdc8604f6
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-16 13:32:41 +0600

    #8 BangladeshiFarmers added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/BangladeshiFarmers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class BangladeshiFarmers : INation
+    {
+        public string DrawHouse()
+        {
+            return "KureGhor";
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
