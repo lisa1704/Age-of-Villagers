dcf1774a638ed13a458eb02e7d951ac8c9ba49e9
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-17 04:03:04 +0600

    #9 functionality to draw water source added

--- solutions/34-meem/Age_Of_Villagers/BangladeshiWaterSource.cs
+++ solutions/34-meem/Age_Of_Villagers/BangladeshiWaterSource.cs
@@ -11,7 +11,17 @@ namespace Age_Of_Villagers
     {
         public void Draw(Point e, Graphics g, Pen p)
         {
-            throw new NotImplementedException();
+            int x = e.X;
+            int y = e.Y;
+            g.DrawLine(p, x, y, x +16, y+12);
+            g.DrawLine(p, x + 16, y + 12, x + 24, y);
+            g.DrawLine(p, x + 24, y, x+40, y +10);
+            g.DrawLine(p, x + 40, y + 10, x +48, y);
+            g.DrawLine(p, x + 48, y, x + 40, y - 10);
+            g.DrawLine(p, x + 40, y - 10, x + 20, y - 6);
+            g.DrawLine(p, x + 20, y - 6, x + 22, y - 10);
+            g.DrawLine(p, x + 22, y - 10, x + 16, y - 8);
+            g.DrawLine(p, x + 16, y - 8, x , y );
         }
     }
 }
