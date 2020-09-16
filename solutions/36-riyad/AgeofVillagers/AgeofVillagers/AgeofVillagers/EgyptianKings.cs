78c96b1434c7698ec21443334b16c785737a5f2d
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-16 13:35:56 +0600

    #8 EgyptianKings added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/EgyptianKings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class EgyptianKings : INation
+    {
+        public string DrawHouse()
+        {
+            return "Piramid";
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
