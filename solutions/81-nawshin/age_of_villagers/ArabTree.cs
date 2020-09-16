450a18687d7afc62405e86bdf9b85502e22bb583
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-16 19:43:44 +0600

    #7  #10 can draw Arab Tree

--- solutions/81-nawshin/age_of_villagers/ArabTree.cs
+++ solutions/81-nawshin/age_of_villagers/ArabTree.cs
@@ -11,7 +11,20 @@ namespace age_of_villagers
     {
         public ArabTree(Point pt)
         {
+            Point bottomRight = new Point(pt.X - 2, pt.Y - 20);
+            Point mid = new Point(pt.X - 1, pt.Y - 20);
+            Point top = new Point(mid.X, mid.Y - 26);
+            Point rmid = new Point(pt.X + 14, mid.Y - 20);
+            Point lmid = new Point(pt.X - 14, mid.Y - 20);
+            Point rright = new Point(pt.X + 20, mid.Y - 10);
+            Point lleft = new Point(pt.X - 20, mid.Y - 10);
 
+            AddComponent(new Rectangle(pt, bottomRight));
+            AddComponent(new Line(mid, top));
+            AddComponent(new Line(mid, lmid));
+            AddComponent(new Line(mid, rmid));
+            AddComponent(new Line(mid, rright));
+            AddComponent(new Line(mid, lleft));
         }
     }
 }
