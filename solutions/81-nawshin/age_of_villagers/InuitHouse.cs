9826d2bd57b08bb169c15a6c64fa52113a8d050d
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-16 20:34:22 +0600

    #1 variable name change

--- solutions/81-nawshin/age_of_villagers/InuitHouse.cs
+++ solutions/81-nawshin/age_of_villagers/InuitHouse.cs
@@ -13,12 +13,12 @@ namespace age_of_villagers
         {
             Point left = new Point(pt.X - 20, pt.Y);
             Point right = new Point(pt.X + 20, pt.Y);
-            Point hc = new Point(pt.X + 5, pt.Y);
-            Point inner= new Point(right.X-3 , pt.Y+15);
+            Point outcircle = new Point(pt.X + 5, pt.Y);
+            Point incircle= new Point(right.X-3 , pt.Y+15);
 
             AddComponent(new Line(left, right));
-            AddComponent(new Arc(hc, 40, 40, -192, 208));
-            AddComponent(new Arc(inner, 15, 15, -192, 208));
+            AddComponent(new Arc(outcircle, 40, 40, -192, 208));
+            AddComponent(new Arc(incircle, 15, 15, -192, 208));
         }
     }
 }
