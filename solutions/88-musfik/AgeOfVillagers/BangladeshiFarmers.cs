2f22d6e6874fd945b3d3f5358d9dc6f675f90013
musfiksifat <musfiksifat766@gmail.com>
2020-09-16 20:20:41 +0600

    #9 BangladeshiFarmers class fully implemented

--- solutions/88-musfik/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/88-musfik/AgeOfVillagers/BangladeshiFarmers.cs
@@ -16,7 +16,7 @@ namespace AgeOfVillagers
 
         public object getHouse(Point point)
         {
-            throw new NotImplementedException();
+            return new BangladeshiHouse(point);
         }
 
         public string getNationName()
@@ -31,12 +31,12 @@ namespace AgeOfVillagers
 
         public object getTree(Point point)
         {
-            throw new NotImplementedException();
+            return new BangladeshiTree(point);
         }
 
         public object getWaterResource(Point point)
         {
-            throw new NotImplementedException();
+            return new Bangladeshi_WaterSource(point);
         }
     }
 
