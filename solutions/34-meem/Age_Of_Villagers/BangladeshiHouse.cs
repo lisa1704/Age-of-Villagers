ad1fe976f49bbf28b8b5c17290494a6124cc07fe
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-17 03:02:27 +0600

    #9 functionality added to draw bangladeshi house

--- solutions/34-meem/Age_Of_Villagers/BangladeshiHouse.cs
+++ solutions/34-meem/Age_Of_Villagers/BangladeshiHouse.cs
@@ -14,7 +14,15 @@ namespace Age_Of_Villagers
 
         public void Draw(Point e, Graphics g, Pen p)
         {
-            
+            int x = e.X;
+            int y = e.Y;
+            g.DrawLine(p, x, y, x + 50, y);
+            g.DrawLine(p, x + 50, y, x + 50, y + 25);
+            g.DrawLine(p, x + 50, y + 25, x, y + 25);
+            g.DrawLine(p, x, y + 25, x, y);
+            g.DrawLine(p, x, y, x + 25, y - 25);
+            g.DrawLine(p, x + 50, y, x + 25, y - 25);
+
         }
     }
 }
