b6c90a999c975b9ee94b2a66cd4e6114b8f30582
Asif Aziz <asifaziz2061@gmail.com>
2020-09-16 23:26:47 +0600

    #1 Draw method edited

--- solutions/31-aziz/empty_project/empty_project/Arc.cs
+++ solutions/31-aziz/empty_project/empty_project/Arc.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace empty_project
 {
     class Arc : IShapeElement
     {
-        void IShapeElement.Draw()
+        public void Draw(Graphics graphics)
         {
             throw new NotImplementedException();
         }
