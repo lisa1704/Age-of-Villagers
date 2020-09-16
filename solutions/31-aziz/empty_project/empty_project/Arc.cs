4863a845b36c6d0c33f1ce33345e3efa5167117b
Asif Aziz <asifaziz2061@gmail.com>
2020-09-16 23:59:15 +0600

    #7 Arc Shape class implemented

--- solutions/31-aziz/empty_project/empty_project/Arc.cs
+++ solutions/31-aziz/empty_project/empty_project/Arc.cs
@@ -7,9 +7,20 @@ namespace empty_project
 {
     class Arc : IShapeElement
     {
+        int x_coordinate, y_coordinate, width, height, startAngle, arcAngle;
+        public Arc(int x_coordinate, int y_coordinate, int width, int height, int startAngle, int arcAngle)
+        {
+            this.x_coordinate = x_coordinate;
+            this.y_coordinate = y_coordinate;
+            this.width = width;
+            this.height = height;
+            this.startAngle = startAngle;
+            this.arcAngle = arcAngle;
+        }
         public void Draw(Graphics graphics)
         {
-            throw new NotImplementedException();
+            var pen = new Pen(Color.Black, 1);
+            graphics.DrawArc(pen, x_coordinate, y_coordinate, width, height, startAngle, arcAngle);
         }
     }
 }
