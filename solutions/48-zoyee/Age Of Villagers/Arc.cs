0841f835f680c220e0ad6cf6bc455d100d3e1975
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-16 19:50:11 +0600

    #7 Arc class implementation done.

--- solutions/48-zoyee/Age Of Villagers/Arc.cs
+++ solutions/48-zoyee/Age Of Villagers/Arc.cs
@@ -7,9 +7,21 @@ namespace Age_Of_Villagers
 {
     class Arc : IShape
     {
-        public void Draw(Graphics g)
+        public float x, y, width, height, angle_1, angle_2;
+
+        public Arc(float x, float y, float width, float height, float angle_1, float angle_2)
         {
-            throw new NotImplementedException();
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.angle_1 = angle_1;
+            this.angle_2 = angle_2;
+        }
+            public void Draw(Graphics g)
+        {
+            var pen = new Pen(Color.Black, 1);
+            g.DrawArc(pen, x, y, width, height, angle_1, angle_2);
         }
     }
 }
