539b6933405d53a36b155828907c9936fd6412b8
Asif Aziz <asifaziz2061@gmail.com>
2020-09-16 23:42:14 +0600

    #7 Rectangle Shape class implemented

--- solutions/31-aziz/empty_project/empty_project/Rectangle.cs
+++ solutions/31-aziz/empty_project/empty_project/Rectangle.cs
@@ -7,9 +7,20 @@ namespace empty_project
 {
     class Rectangle : IShapeElement
     {
+        Point TopLeft_point;
+        Point BottomRight_point;
+        public Rectangle(Point TopLeft_pont,Point BottomRight_point)
+        {
+            this.TopLeft_point =TopLeft_pont ;
+            this.BottomRight_point = BottomRight_point;
+        }
         public void Draw(Graphics graphics)
         {
-            throw new NotImplementedException();
+            var pen = new Pen(Color.Black, 1);
+            graphics.DrawLine(pen, TopLeft_point.X, TopLeft_point.Y, BottomRight_point.X, TopLeft_point.Y);
+            graphics.DrawLine(pen, TopLeft_point.X, TopLeft_point.Y, TopLeft_point.X, BottomRight_point.Y);
+            graphics.DrawLine(pen, TopLeft_point.X, BottomRight_point.Y, BottomRight_point.X, BottomRight_point.Y);
+            graphics.DrawLine(pen, BottomRight_point.X, TopLeft_point.Y, BottomRight_point.X, BottomRight_point.Y);
         }
     }
 }
