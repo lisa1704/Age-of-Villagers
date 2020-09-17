01f5b3e7918b1f40081b06d457c08e0643a4d38b
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-18 01:07:01 +0600

    #7 Line shape added

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/Shapes/Line.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace AgeOfVillagers.Shapes
+{
+    public class Line : IShape
+    {
+        private Point point1, point2;
+        public Line(Point point1, Point point2)
+        {
+            this.point1 = point1;
+            this.point1 = point2;
+        }
+        public void draw(Graphics g, Pen p)
+        {
+            g.DrawLine(p, point1, point2);
+        }
+    }
+}
