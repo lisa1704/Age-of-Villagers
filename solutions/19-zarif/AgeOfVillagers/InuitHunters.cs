89aa3f7923015cc7c787167f4e9551a23d6a1e45
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-18 03:56:59 +0600

    #7 #12 Created InuitHunters and House

---
+++ solutions/19-zarif/AgeOfVillagers/InuitHunters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public class InuitHunters : INation
+    {
+        public IGraphicShape GetHouse(Point point)
+        {
+            return new InuitHuntersHouse(point);
+        }
+
+        public IGraphicShape GetTrees(Point point)
+        {
+            return new NullHandler();
+        }
+
+        public IGraphicShape GetWaterSources(Point point)
+        {
+            return new NullHandler();
+        }
+    }
+}
---
+++ solutions/19-zarif/AgeOfVillagers/InuitHuntersHouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public class InuitHuntersHouse : CompositeShape
+    {
+        private Point point;
+
+        public InuitHuntersHouse(Point point)
+        {
+            this.point = point;
+        }
+    }
+}
