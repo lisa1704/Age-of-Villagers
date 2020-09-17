29ddfe09e67a0984e60e52c9cf3336eaf7afd4e4
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-18 04:27:45 +0600

    #7 #8 #12 Created Base Arc for inuithouse

---
+++ solutions/19-zarif/AgeOfVillagers/BaseArc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public class BaseArc : IGraphicShape
+    {
+        private Point  _point;
+        private int _h, _w;
+        public BaseArc(Point point, int h, int w)
+        {
+            _point = point;
+            _h = h;
+            _w = w;
+        }
+        public void draw(Graphics g, Pen pen)
+        {
+            Rectangle shape = new Rectangle(_point.X, _point.Y, _h, _w);
+            g.DrawArc(pen, shape, 0, -180);
+        }
+    }
+}
