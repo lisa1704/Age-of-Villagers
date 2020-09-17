2c28cfc7e6e11f1d6e51ff87850e2b4ef389560e
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-17 08:47:01 +0600

    #1 egyptian king river refactored

--- solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/EgyptianKingRiver.cs
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/EgyptianKingRiver.cs
@@ -12,13 +12,19 @@ namespace AgeOfVillagers
         public EgyptianKingRiver(Point e)
         {
             this.e = e;
+
+            Point point = new Point(e.X - 6, e.Y - 6);
+            int width = 12;
+            int height = 12;
+
+            addComponent(new Circle(point, height, width));
         }
-        public void drawComponent(Point e, Graphics graphics, Pen pen)
+        /*public void drawComponent(Point e, Graphics graphics, Pen pen)
         {
             int x = e.X;
             int y = e.Y;
             graphics.DrawArc(pen, x - 6, y - 6, 12, 12, 0, 360);
-        }
+        }*/
 
         public override Point getPoint()
         {
