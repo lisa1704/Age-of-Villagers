fb2a9593e6d510a9193a1829d9fc5718c085f06c
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-09-18 01:13:08 +0600

    #1 moved drawing to another class

--- solutions/72-rafidhaque/src/VillageWindow.java
+++ solutions/72-rafidhaque/src/VillageWindow.java
@@ -14,65 +14,15 @@ public class VillageWindow extends JPanel {
         this.nationName = nationName;
     }
 
+    WindowDesign windowDesign = new WindowDesign(f, p, villageName, nationName);
+
     public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        this.setBackground(Color.WHITE);
-
-        g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
-        g.setColor(Color.BLACK);
-        g.drawString(this.gameName, 20, 50);
-
-        g.setFont(new Font("TimesRoman", Font.PLAIN, 13));
-        g.drawString(this.villageName, 20, 90);
-        g.drawString(this.nationName, 20, 130);
-
-        JButton treebutton=new JButton("Tree");
-        treebutton.setFont(new Font("Arial", Font.PLAIN, 10));
-        treebutton.setBounds(620,160,70,30);
-        f.add(treebutton);
-
-        JButton housebutton=new JButton("House");
-        housebutton.setFont(new Font("Arial", Font.PLAIN, 10));
-        housebutton.setBounds(700,160,70,30);
-        f.add(housebutton);
-
-        JButton waterbutton=new JButton("Water Source");
-        waterbutton.setFont(new Font("Arial", Font.PLAIN, 10));
-        waterbutton.setBounds(625,195,140,30);
-        f.add(waterbutton);
-
-        JButton savebutton=new JButton("Save Village");
-        savebutton.setFont(new Font("Arial", Font.PLAIN, 10));
-        savebutton.setBounds(625,260,140,30);
-        f.add(savebutton);
-
-        JButton newbuttton=new JButton("New\nVillage");
-        newbuttton.setFont(new Font("Arial", Font.PLAIN, 10));
-        newbuttton.setMargin(new Insets(1,1,1,1));
-        newbuttton.setBounds(620,300,70,30);
-        f.add(newbuttton);
-
-        JButton openbutton=new JButton("Open\nVillage");
-        openbutton.setFont(new Font("Arial", Font.PLAIN, 10));
-        openbutton.setMargin(new Insets(1,1,1,1));
-        openbutton.setBounds(700,300,70,30);
-        f.add(openbutton);
-
-        g.setColor(Color.BLACK);
-        g.drawLine(600, 0, 600, 400);
-        g.drawLine(0, 0, 0, 400);
-        g.drawLine(0, 0, 800, 0);
-        g.drawLine(800, 0, 800, 400);
-        g.drawLine(0, 400, 800, 400);
-
-
+        windowDesign.paintComponent(g);
     }
 
     public void showWindow() {
-        p.setPreferredSize(new Dimension(600, 400));
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.add(this);
-        f.add(this.p, BorderLayout.WEST);
         f.setSize(800, 400);
         f.setVisible(true);
     }
