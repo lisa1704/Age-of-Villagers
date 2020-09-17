e316c05651a5420c7cb8cac53e8dda8ae562edd6
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-17 18:09:12 +0600

    #9#10#11#12 creating abstract factory class to create different items

---
+++ solutions/34-meem/Age_Of_Villagers/ItemFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    public abstract class ItemFactory
+    {
+        public abstract IVillageItem getItem(string button);
+    }
+}
