e68beacd2e7d1a01d7cab35c323b6cc1bbe1051a
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-09-18 04:26:23 +0600

    #7 Created DrawHouse, DrawTree, DrawWaterSource
    didn't tag the commit before, that's why committing again.

--- solutions/72-rafidhaque/src/DrawWaterSource.java
+++ solutions/72-rafidhaque/src/DrawWaterSource.java
@@ -5,7 +5,6 @@ public class DrawWaterSource extends DrawComponent {
     public DrawWaterSource(JPanel p, int x, int y) {
         super(p, x, y);
     }
-
     @Override
     public void drawComponent() {
 
