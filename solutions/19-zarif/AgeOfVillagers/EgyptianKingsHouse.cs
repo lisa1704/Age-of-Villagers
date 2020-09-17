7743a969be65989f6f69f8fb7ba10385778b2e24
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-18 02:32:44 +0600

    #1 Refactored EgyptianKings House

--- solutions/19-zarif/AgeOfVillagers/EgyptianKingsHouse.cs
+++ solutions/19-zarif/AgeOfVillagers/EgyptianKingsHouse.cs
@@ -13,10 +13,11 @@ namespace AgeOfVillagers
             Point _topLeft = new Point(_toppoint.X - 8, _toppoint.Y + 24);
             Point _bottomRight = new Point(_toppoint.X + 8, _toppoint.Y + 16);
             Point _topRight = new Point(_bottomRight.X, _topLeft.Y);
-
+            Point _bottombehind = new Point(_topRight.X+4, _topRight.Y - 16);
 
             addShape(new BaseTriangle(_toppoint, _topLeft, _topRight));
-          
+            addShape(new BaseLine(_toppoint,_bottombehind));
+            addShape(new BaseLine(_topRight,_bottombehind));
         }
     }
 }
