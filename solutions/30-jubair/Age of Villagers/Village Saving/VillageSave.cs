baff35d8eb0e19e498b181b1756e526491368b32
30-jubair <jobairt03@gmail.com>
2020-09-18 01:43:41 +0600

    #5  Saving the current state of a village

---
+++ solutions/30-jubair/Age of Villagers/Village Saving/VillageSave.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Age_of_Villagers.Nation;
+
+namespace Age_of_Villagers.Village_Saving
+{
+    public class VillageSave
+    
+    {
+        public List<Point> houseList = new List<Point>();
+        public List<Point> treeList = new List<Point>();
+        public List<Point> waterResourceList = new List<Point>();
+
+        public string villageName = "";
+
+        public string GetVillageName()
+        {
+            return villageName;
+        }
+        public void SetVillageName(string name)
+        {
+            villageName = name;
+        }
+
+        public void AddHouse(Graphics graphics, Point house, INation ination)
+        
+        {
+            houseList.Add(house);
+            ination.GetHouse(graphics,house);
+        }
+
+
+        public void AddTree(Graphics graphics, Point tree, INation ination)
+
+        {
+            treeList.Add(tree);
+            ination.GetTree(graphics, tree);
+        }
+
+
+        public void AddWaterResource(Graphics graphics, Point waterResource, INation ination)
+
+        {
+            waterResourceList.Add(waterResource);
+            ination.GetWaterResource(graphics, waterResource);
+        }
+
+
+
+
+
+    }
+}
