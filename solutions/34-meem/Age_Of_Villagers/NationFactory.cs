5e2db3bada9b8c1cc793402e8fe8c1c5986d5250
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-17 18:11:23 +0600

    #9#10#11#12 factory class created to create different nations

---
+++ solutions/34-meem/Age_Of_Villagers/NationFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    class NationFactory
+    {
+        public static ItemFactory getNation(string nation)
+        {
+            if(nation== "Bangladeshi")
+            {
+                return new BangladeshiItemFactory();
+            }
+            return null;
+        }
+
+    }
+}
