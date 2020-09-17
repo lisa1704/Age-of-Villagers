b3958c61c25c944801fe0bf8d6a727bad32d14eb
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-18 02:18:19 +0600

    #11 EgyptianHouse implemented

--- solutions/19-zarif/AgeOfVillagers/EgyptianKingsHouse.cs
+++ solutions/19-zarif/AgeOfVillagers/EgyptianKingsHouse.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    class EgyptianKingsHouse
+    public class EgyptianKingsHouse : CompositeShape
     {
+        public EgyptianKingsHouse(Point point)
+        {
+            Point _toppoint = point;
+            Point _topLeft = new Point(_toppoint.X - 8, _toppoint.Y + 24);
+            Point _bottomRight = new Point(_toppoint.X + 8, _toppoint.Y + 16);
+            Point _topRight = new Point(_bottomRight.X, _topLeft.Y);
+
+
+            addShape(new BaseTriangle(_toppoint, _topLeft, _topRight));
+          
+        }
     }
 }
