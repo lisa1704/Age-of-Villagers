f233cb0c8477ec30a4dcd89ce3e3e0e62c66dc4a
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-18 03:27:50 +0600

    #11 EgyptianKings Tree Implemented

--- solutions/19-zarif/AgeOfVillagers/EgyptianKingsTree.cs
+++ solutions/19-zarif/AgeOfVillagers/EgyptianKingsTree.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    class EgyptianKingsTree
+    public class EgyptianKingsTree : CompositeShape
     {
+        public EgyptianKingsTree(Point point)
+        {
+            Point _upbranchright = new Point(point.X + 16, point.Y - 18);
+            Point _upbranchleft = new Point(point.X - 16, point.Y - 18);
+            addShape(new BaseLine(point,_upbranchright));
+            addShape(new BaseLine(point, _upbranchleft));
+            addShape(new BaseLine(_upbranchright,new Point(_upbranchright.X+12,_upbranchright.Y-8)));
+            addShape(new BaseLine(_upbranchright, new Point(_upbranchright.X - 12, _upbranchright.Y - 8)));
+            addShape(new BaseLine(_upbranchright, new Point(_upbranchright.X, _upbranchright.Y - 12)));
+
+            addShape(new BaseLine(_upbranchleft, new Point(_upbranchleft.X + 12, _upbranchleft.Y - 8)));
+            addShape(new BaseLine(_upbranchleft, new Point(_upbranchleft.X - 12, _upbranchleft.Y - 8)));
+            addShape(new BaseLine(_upbranchleft, new Point(_upbranchleft.X, _upbranchleft.Y - 12)));
+
+
+
+        }
     }
 }
