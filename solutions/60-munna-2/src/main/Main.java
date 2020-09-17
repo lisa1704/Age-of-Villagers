85a683f7973f360bcf05d88c0d511f9ea36a0c98
sakibul-munnaa <sakibulislam1@iut-dhaka.edu>
2020-09-17 22:06:45 +0600

    #4 Label for country name added

--- solutions/60-munna-2/src/main/Main.java
+++ solutions/60-munna-2/src/main/Main.java
@@ -6,13 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Age Of Villagers");
-        primaryStage.setScene(new Scene(root, 1000 , 500));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Age of Villagers");
+        primaryStage.setScene(new Scene(root, 1000, 500));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
