51f5da96dc60e3e7a37f7a648be1cfa14c44c57f
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-17 08:57:44 +0600

    #1 egyptian king tree refactored

--- solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/EgyptianKingTree.cs
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/EgyptianKingTree.cs
@@ -12,8 +12,25 @@ namespace AgeOfVillagers
         public EgyptianKingTree(Point e)
         {
             this.e = e;
+
+            Point point1 = new Point(e.X, e.Y);
+            Point point2 = new Point(e.X+3, e.Y-12);
+            Point point3 = new Point(e.X-2, e.Y-12);
+            Point point4 = new Point(e.X+8, e.Y-24);
+            Point point5 = new Point(e.X+5, e.Y-24);
+            Point point6 = new Point(e.X+2, e.Y-24);
+            Point point7 = new Point(e.X-1, e.Y-24);
+            Point point8 = new Point(e.X-4, e.Y-24);
+            Point point9 = new Point(e.X-8, e.Y-24);
+
+            addComponent(new Line(point1, point5));
+            addComponent(new Line(point1, point8));
+            addComponent(new Line(point2, point4));
+            addComponent(new Line(point2, point6));
+            addComponent(new Line(point3, point7));
+            addComponent(new Line(point3, point9));
         }
-        public void drawComponent(Point e, Graphics graphics, Pen pen)
+        /*public void drawComponent(Point e, Graphics graphics, Pen pen)
         {
             int x = e.X;
             int y = e.Y;
@@ -25,7 +42,7 @@ namespace AgeOfVillagers
             graphics.DrawLine(pen, x - 2, y - 12, x - 8, y - 24);
             graphics.DrawLine(pen, x - 2, y - 12, x - 4, y - 24);
             graphics.DrawLine(pen, x - 2, y - 12, x - 1, y - 24);
-        }
+        }*/
 
         public override Point getPoint()
         {
