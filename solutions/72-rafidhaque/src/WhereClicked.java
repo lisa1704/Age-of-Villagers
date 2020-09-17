f19b28b133654b7488c06778c47028e3a97f3c5f
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-09-17 23:58:42 +0600

    #7 Created WhereClicked
    To find Co ordinates on the panel to start drawing.

---
+++ solutions/72-rafidhaque/src/WhereClicked.java
@@ -0,0 +1,21 @@
+import java.awt.event.MouseEvent;
+
+public class WhereClicked {
+    MouseEvent e;
+
+    public WhereClicked(MouseEvent e, int x, int y) {
+        this.e = e;
+        x = mouseClickedx();
+        y = mouseClickedy();
+    }
+
+    public int mouseClickedx() {
+        int x=this.e.getX();
+        return x;
+    }
+
+    public int mouseClickedy() {
+        int y=this.e.getY();
+        return y;
+    }
+}
