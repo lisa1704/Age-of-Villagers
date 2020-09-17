55b908ca3f991f0a1f04792303453d89970e2512
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-18 01:38:27 +0600

    #8 #1  added InuitEnvironment (in Environment Extending classes named folder,extending Environment class

---
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/Environment extending Classes/InuitEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers.Environment_extending_Classes
+{
+    class InuitEnvironment : Environment
+    {
+        public override void InvalidateOtherNations()
+        {
+            
+        }
+
+        public override void setTerrainColor()
+        {
+            
+        }
+
+        public override void showNationName()
+        {
+            
+        }
+    }
+}
