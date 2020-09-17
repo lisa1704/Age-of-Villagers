70792bb091cea6e7878194e7dddf56850300fd0a
Rizwan Khan <rizwanulhaque19@gmail.com>
2020-09-17 17:12:24 +0600

    #4 drawing panel added

--- solutions/78-rizwan/AgeofVillagers/AgeofVillagers/Form1.Designer.cs
+++ solutions/78-rizwan/AgeofVillagers/AgeofVillagers/Form1.Designer.cs
@@ -39,28 +39,29 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(0, 0);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(600, 400);
-            this.splitter1.TabIndex = 1;
+            this.splitter1.Size = new System.Drawing.Size(3, 736);
+            this.splitter1.TabIndex = 14;
             this.splitter1.TabStop = false;
             // 
             // splitter2
             // 
-            this.splitter2.Location = new System.Drawing.Point(600, 0);
+            this.splitter2.Location = new System.Drawing.Point(3, 0);
             this.splitter2.Name = "splitter2";
-            this.splitter2.Size = new System.Drawing.Size(6, 400);
-            this.splitter2.TabIndex = 2;
+            this.splitter2.Size = new System.Drawing.Size(3, 736);
+            this.splitter2.TabIndex = 13;
             this.splitter2.TabStop = false;
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(1159, 124);
+            this.button2.Location = new System.Drawing.Point(1382, 223);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(123, 47);
             this.button2.TabIndex = 3;
@@ -69,17 +70,18 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(852, 148);
+            this.button3.Location = new System.Drawing.Point(1074, 300);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(176, 37);
             this.button3.TabIndex = 4;
             this.button3.Text = "Water Source";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button4.Location = new System.Drawing.Point(606, 124);
+            this.button4.Location = new System.Drawing.Point(871, 231);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 32);
             this.button4.TabIndex = 5;
@@ -88,7 +90,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(852, 211);
+            this.button5.Location = new System.Drawing.Point(1342, 560);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(163, 43);
             this.button5.TabIndex = 6;
@@ -97,7 +99,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(1138, 268);
+            this.button6.Location = new System.Drawing.Point(1085, 499);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(165, 39);
             this.button6.TabIndex = 7;
@@ -107,7 +109,7 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(612, 278);
+            this.button7.Location = new System.Drawing.Point(852, 560);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(158, 39);
             this.button7.TabIndex = 8;
@@ -116,7 +118,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(861, 48);
+            this.textBox2.Location = new System.Drawing.Point(1094, 75);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(156, 39);
             this.textBox2.TabIndex = 10;
@@ -124,7 +126,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(861, 93);
+            this.textBox3.Location = new System.Drawing.Point(1094, 138);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(156, 39);
             this.textBox3.TabIndex = 11;
@@ -133,17 +135,25 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(852, 13);
+            this.label1.Location = new System.Drawing.Point(1074, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(182, 32);
             this.label1.TabIndex = 12;
             this.label1.Text = "Age of Villagers";
             // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(144, 138);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 400);
+            this.panel1.TabIndex = 15;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1318, 400);
+            this.ClientSize = new System.Drawing.Size(1581, 736);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -174,6 +184,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- solutions/78-rizwan/AgeofVillagers/AgeofVillagers/Form1.cs
+++ solutions/78-rizwan/AgeofVillagers/AgeofVillagers/Form1.cs
@@ -26,5 +26,10 @@ namespace AgeofVillagers
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
