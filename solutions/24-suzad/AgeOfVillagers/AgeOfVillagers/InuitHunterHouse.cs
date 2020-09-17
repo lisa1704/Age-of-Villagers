dcb4cb575abf243cd443b907f6231a12550a8d76
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-17 09:18:44 +0600

    #1 inuit hunter house refactored

--- solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/InuitHunterHouse.cs
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/InuitHunterHouse.cs
@@ -12,15 +12,30 @@ namespace AgeOfVillagers
         public InuitHunterHouse(Point e)
         {
             this.e = e;
+
+            Point linePoint1 = new Point(e.X, e.Y);
+            Point linePoint2 = new Point(e.X+16, e.Y);
+            Point arcPoint1 = new Point(e.X, e.Y-16);
+            int height1 = 32;
+            int width1 = 16;
+
+            Point linePoint3 = new Point(e.X+5, e.Y);
+            Point linePoint4 = new Point(e.X + 12, e.Y);
+            Point arcPoint2 = new Point(e.X+5, e.Y - 6);
+            int height2 = 12;
+            int width2 = 6;
+
+            addComponent(new SemiCircle(arcPoint1, linePoint1, linePoint2, height1, width1));
+            addComponent(new SemiCircle(arcPoint2, linePoint3, linePoint4, height2, width2));
         }
-        public void drawComponent(Point e, Graphics graphics, Pen pen)
+        /*public void drawComponent(Point e, Graphics graphics, Pen pen)
         {
             int x = e.X;
             int y = e.Y;
             graphics.DrawLine(pen, x, y, x + 16, y);
             graphics.DrawArc(pen, x, y - 16, 16, 32, 180, 180);
             graphics.DrawArc(pen, x + 5, y - 6, 6, 12, 180, 180);
-        }
+        }*/
 
         public override Point getPoint()
         {
---
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/SemiCircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public class SemiCircle : IShape
+    {
+        public Point arcPoint;
+        public Point linePoint1;
+        public Point linePoint2;
+        public int height;
+        public int width;
+
+        public SemiCircle(Point arcPoint, Point linePoint1, Point linePoint2, int height, int width)
+        {
+            this.arcPoint = arcPoint;
+            this.linePoint1 = linePoint1;
+            this.linePoint2 = linePoint2;
+            this.height = height;
+            this.width = width;
+        }
+        public void drawComponent(Point e, Graphics graphics, Pen pen)
+        {
+            graphics.DrawLine(pen, linePoint1, linePoint2);
+            graphics.DrawArc(pen, arcPoint.X, arcPoint.Y, width, height, 180, 180);
+        }
+    }
+}
