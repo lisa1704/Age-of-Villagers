49e6e1339908f759d7d502af1a27d56b3a8de136
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-18 02:40:19 +0600

    # 1#11  Egyptianking Refactored

--- solutions/19-zarif/AgeOfVillagers/EgyptianKings.cs
+++ solutions/19-zarif/AgeOfVillagers/EgyptianKings.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    class EgyptianKings
+    public class EgyptianKings : INation
     {
+        public IGraphicShape GetHouse(Point point)
+        {
+            return new EgyptianKingsHouse(point);
+        }
+
+        public IGraphicShape GetTrees(Point point)
+        {
+            return new EgyptianKingsTree(point);
+        }
+
+        public IGraphicShape GetWaterSources(Point point)
+        {
+            return new EgyptianKingsWaterSource(point);
+        }
     }
 }
