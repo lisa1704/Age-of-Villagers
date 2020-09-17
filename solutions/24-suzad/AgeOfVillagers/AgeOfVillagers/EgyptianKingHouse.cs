36a9d377228b6ef7ebfa6e298e7efd5b7c91beec
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-17 08:43:26 +0600

    #1 egyptian king house refactored

--- solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/EgyptianKingHouse.cs
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers/EgyptianKingHouse.cs
@@ -12,8 +12,16 @@ namespace AgeOfVillagers
         public EgyptianKingHouse(Point e)
         {
             this.e = e;
+
+            Point point1 = new Point(e.X, e.Y);
+            Point point2 = new Point(e.X+6, e.Y-7);
+            Point point3 = new Point(e.X-3, e.Y-16);
+            Point point4 = new Point(e.X-10, e.Y-4);
+
+            addComponent(new Triangle(point1, point2, point3));
+            addComponent(new Triangle(point1, point3, point4));
         }
-        public void drawComponent(Point e, Graphics graphics, Pen pen)
+        /*public void drawComponent(Point e, Graphics graphics, Pen pen)
         {
             int x = e.X;
             int y = e.Y;
@@ -22,7 +30,7 @@ namespace AgeOfVillagers
             graphics.DrawLine(pen, x - 3, y - 16, x - 10, y - 4);
             graphics.DrawLine(pen, x - 10, y - 4, x, y);
             graphics.DrawLine(pen, x - 3, y - 16, x, y);
-        }
+        }*/
 
         public override Point getPoint()
         {
