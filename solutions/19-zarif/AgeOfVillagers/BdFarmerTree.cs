55c82cae9fc3ba48e9bafaee38b3789727e7448a
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-17 21:56:20 +0600

    #1 BangladeshiTree refactored

--- solutions/19-zarif/AgeOfVillagers/BdFarmerTree.cs
+++ solutions/19-zarif/AgeOfVillagers/BdFarmerTree.cs
@@ -9,13 +9,14 @@ namespace AgeOfVillagers
     {
         public BdFarmerTree(Point point)
         {
+            Point exlocation = point;
+            Point _toppoint = new Point(exlocation.X + 6, exlocation.Y+10);
 
-            Point _toppoint = point;
-            Point _topRight = new Point(_toppoint.X + 16, _toppoint.Y);
-            Point _bottomLeft = new Point(_toppoint.X, _toppoint.Y + 8);
-            Point _bottomRight = new Point(_toppoint.X + 16, _toppoint.Y + 8);
+            Point _topRight = new Point(_toppoint.X + 3, _toppoint.Y);
+            Point _bottomLeft = new Point(_toppoint.X, _toppoint.Y + 12);
+            Point _bottomRight = new Point(_toppoint.X + 3, _toppoint.Y + 12);
             
-            addShape(new BaseCircle(_toppoint,12,12));
+            addShape(new BaseCircle(exlocation,16,16));
 
             addShape(new BaseRectangle(_toppoint,_bottomLeft,_bottomRight,_topRight));
 
