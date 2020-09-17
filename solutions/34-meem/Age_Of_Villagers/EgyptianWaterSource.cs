63d40449dfd612874580781c6dd66c3df5a61142
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-18 02:59:23 +0600

    #11 functionality to draw egyptian water source added

--- solutions/34-meem/Age_Of_Villagers/EgyptianWaterSource.cs
+++ solutions/34-meem/Age_Of_Villagers/EgyptianWaterSource.cs
@@ -6,7 +6,9 @@ namespace Age_Of_Villagers
     {
         public void Draw(Point e, Graphics g, Pen p)
         {
-            throw new System.NotImplementedException();
+            int x = e.X;
+            int y = e.Y;
+            g.DrawArc(p, x - 40, y - 50, 16, 16, 0, 360);
         }
     }
 }
