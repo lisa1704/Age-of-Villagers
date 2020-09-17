b674b0da276b04980072614c9c2c124b60b68288
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-18 04:28:25 +0600

    #8 Created Nullhandler For the Nations

---
+++ solutions/19-zarif/AgeOfVillagers/NullHandlingNation.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace AgeOfVillagers
+{
+    internal class NullHandlingNation : INation
+    {
+        public IGraphicShape GetHouse(Point point)
+        {
+            return new NullHandler();
+        }
+
+        public IGraphicShape GetTrees(Point point)
+        {
+            return new NullHandler();
+        }
+
+        public IGraphicShape GetWaterSources(Point point)
+        {
+            return new NullHandler();
+        }
+    }
+}
