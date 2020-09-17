d937ba6f22ec2014f333580d1134a44235872877
sakibul-munnaa <sakibulislam1@iut-dhaka.edu>
2020-09-17 23:27:59 +0600

    #4 A new fxml file and its controller class added to take village name and country name as input.

--- solutions/60-munna-2/src/main/Controller.java
+++ solutions/60-munna-2/src/main/Controller.java
@@ -1,5 +1,57 @@
 package main;
 
+import com.sun.prism.shader.Solid_ImagePattern_Loader;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML
+    private Pane main_pane;
+
+    @FXML
+    private AnchorPane drawing_space;
+
+    @FXML
+    private Button newVillage_Button;
+
+    @FXML
+    private Button openVillage_Button;
+
+    @FXML
+    private Button saveVillage_Button;
+
+    @FXML
+    private RadioButton water_RadioButton;
+
+    @FXML
+    private RadioButton tree_RadioButton;
+
+    @FXML
+    private RadioButton house_RadioButton;
+
+    @FXML
+    private Label country_name;
+
+    @FXML
+    private Label village_name;
+
+    @FXML
+    void newVillage_Button_Clicked(ActionEvent event) {
+        Stage stage = (Stage) newVillage_Button.getScene().getWindow();
+
+
+
+
+
+
+    }
+
 
 }
--- solutions/60-munna-2/src/main/Main.java
+++ solutions/60-munna-2/src/main/Main.java
@@ -6,13 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.URL;
-
 public class Main extends Application {
+    public Scene Input;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("main_page.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("Age of Villagers");
         primaryStage.setScene(new Scene(root, 1000, 500));
