3c0b5fc49d46a99571b6fb8483be711bbedaf4a7
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-18 01:30:51 +0600

    #8 #1  added ArabianEnvironment (in Environment Extending classes named folder,extending Environment class

--- solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/AbstractClass/Environment.cs
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/AbstractClass/Environment.cs
@@ -18,5 +18,7 @@ namespace AgeOfVillagers
             setTerrainColor();
         }
 
+
+
     }
 }
---
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/Environment extending Classes/ArabianEnvironmet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers.Environment_extending_Classes
+{
+    class ArabianEnvironmet
+    {
+    }
+}
