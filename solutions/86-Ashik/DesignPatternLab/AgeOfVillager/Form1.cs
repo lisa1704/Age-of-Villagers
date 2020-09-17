a02e7bd13536dab1abb4acd07c366a05b592b3a3
86-Ashik <nusayerashik@iut-dhaka.edu>
2020-09-17 23:58:10 +0600

    #8 #7 an object can be drawn based on selected nation and object

--- solutions/86-Ashik/DesignPatternLab/AgeOfVillager/Form1.cs
+++ solutions/86-Ashik/DesignPatternLab/AgeOfVillager/Form1.cs
@@ -12,28 +12,25 @@ namespace AgeOfVillager
 {
     public partial class Form1 : Form
     {
-        int x, y,h,w;
-        Pen myPen = new Pen(Color.Black);
-        Brush myBrush = new SolidBrush(Color.Blue);
-        //EgyptianKingsTree et = new EgyptianKingsTree();
-
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-            x = e.X;
-            y = e.Y;
-            h = 50;
-            w = 75;
             Graphics g = this.CreateGraphics();
-            Rectangle rect = new Rectangle(x, y, h, w);
+            string selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
             //tree
             if (radioButton1.Checked)
             {
                 //EgyptianKingsTree et = new EgyptianKingsTree(g);
                 //et.draw(e);
-                //BDFarmersTree bdt = new BDFarmersTree(g);
-                //bdt.draw(e);
-                ArabBedouinsTree abt = new ArabBedouinsTree(g);
-                abt.draw(e);
+                if (selected == "Bangladeshi Farmers")
+                {
+                    BDFarmersTree bdt = new BDFarmersTree(g);
+                    bdt.draw(e);
+                }
+                else
+                {
+                    ArabBedouinsTree abt = new ArabBedouinsTree(g);
+                    abt.draw(e);
+                }
             }
             //house
             else if (radioButton2.Checked)
