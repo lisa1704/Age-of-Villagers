6c5ffae97976bce55264e9f47f7274bbe4e8d48b
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2020-09-18 09:17:39 +0600

    #9 #10 Bangladeshi and Arab tree fully completed

--- solutions/57-monisha/AgeOfVillagers/ArabTree.cs
+++ solutions/57-monisha/AgeOfVillagers/ArabTree.cs
@@ -9,11 +9,10 @@ namespace AgeOfVillagers
         public class BDTree : CompositeShape
         {
             public BDTree(Point MPt)
-            {
-                Point topRight = new Point(MPt.X + 20, MPt.Y);
-                Point bottomRight = new Point(MPt.X + 25, MPt.Y + 50);
-                Point topPt = new Point(MPt.X + 40, MPt.Y + 40);
-                Point RTopLeft = new Point(MPt.X + 20, MPt.Y + 20);
+            {               
+                Point bottomRight = new Point(MPt.X + 24, MPt.Y + 65);
+                Point topPt = new Point(MPt.X + 40, MPt.Y + 45);
+                Point RTopLeft = new Point(MPt.X + 20, MPt.Y+25);
 
                 AddComp(new ShapeRectangle(RTopLeft, bottomRight));
                 AddComp(new ShapeEllipse(MPt, topPt));
@@ -22,8 +21,7 @@ namespace AgeOfVillagers
         public class EgyptianTree : CompositeShape
         {
             public EgyptianTree(Point MPt)
-            {
-               
+            {               
                 Point L1topPt = new Point(MPt.X +25, MPt.Y - 50);
                 Point L2topPt = new Point(MPt.X - 25, MPt.Y - 50);
              
@@ -42,7 +40,6 @@ namespace AgeOfVillagers
                 AddComp(new Line(URpoint6, URpoint7));
                 AddComp(new Line(URpoint6, URpoint8));
 
-
             }
         }
         public class ArabTree : CompositeShape
@@ -50,16 +47,21 @@ namespace AgeOfVillagers
             public ArabTree(Point MPt)
             {
                 Point topRight = new Point(MPt.X + 22, MPt.Y+20);
-                Point bottomRight = new Point(MPt.X + 25, MPt.Y + 50);
-                Point L1topPt = new Point(MPt.X-5 , MPt.Y -35);
-                Point L2topPt = new Point(MPt.X +50, MPt.Y - 35);
-                Point L3topPt = new Point(MPt.X + 20, MPt.Y - 35);
+                Point bottomRight = new Point(MPt.X + 25, MPt.Y + 65);
+                Point L1topPt = new Point(MPt.X-12 , MPt.Y -25);
+                Point L2topPt = new Point(MPt.X +57, MPt.Y - 25);
+                Point L3topPt = new Point(topRight.X, MPt.Y - 35);
+                Point L4topPt = new Point(MPt.X + 78, MPt.Y - 10);
+                Point L5topPt = new Point(MPt.X - 35, MPt.Y - 8);
                 Point RTopLeft = new Point(MPt.X + 20, MPt.Y + 20);
+               // Point RTopLeft = new Point(MPt.X , MPt.Y );
 
                 AddComp(new ShapeRectangle(RTopLeft, bottomRight));
                 AddComp(new Line(RTopLeft, L1topPt));
                 AddComp(new Line(topRight, L2topPt));
                 AddComp(new Line(topRight, L3topPt));
+                AddComp(new Line(topRight, L4topPt));
+                AddComp(new Line(topRight, L5topPt));
             }
         }
 
