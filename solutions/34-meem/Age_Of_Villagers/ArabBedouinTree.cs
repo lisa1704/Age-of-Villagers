c177577cf35e5690f993713c33fc3a07e3def62f
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-18 19:53:25 +0600

    #10 arabian nation tree class created

---
+++ solutions/34-meem/Age_Of_Villagers/ArabBedouinTree.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Age_Of_Villagers
+{
+    internal class ArabBedouinTree : IVillageItem
+    {
+        public void Draw(Point e, Graphics g, Pen p)
+        {
+            
+        }
+    }
+}
