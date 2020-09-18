466cdbfdd6725888d2eb3b9a7a273945d9268443
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-18 18:30:16 +0600

    #1 #7 #9 Resized Bangladesh watersource into 24px * 16px

--- solutions/81-nawshin/age_of_villagers/BDWatersource.cs
+++ solutions/81-nawshin/age_of_villagers/BDWatersource.cs
@@ -11,25 +11,24 @@ namespace age_of_villagers
     {
         public BDWatersource(Point pt)
         {
-            Point p1 = new Point(pt.X - 25, pt.Y + 50);
-            Point p2 = new Point(pt.X, pt.Y + 75);
-            Point p3 = new Point(pt.X +25, pt.Y + 50);
-            Point p4 = new Point(pt.X +50, pt.Y + 75);
-            Point p5 = new Point(pt.X +75, pt.Y + 25);
-            Point p6 = new Point(pt.X +50, pt.Y);
-            Point p7 = new Point(pt.X+25, pt.Y + 25);
-            Point p8 = new Point(pt.X + 25, pt.Y);
-
-            AddComponent(new Line(pt,p1));
-            AddComponent(new Line(p1,p2));
-            AddComponent(new Line(p2,p3));
-            AddComponent(new Line(p3,p4));
-            AddComponent(new Line(p4,p5));
-            AddComponent(new Line(p5,p6));
-            AddComponent(new Line(p6,p7));
-            AddComponent(new Line(p7,p8));
-            AddComponent(new Line(p8,pt));
-
+            Point p1 = new Point(pt.X - 6, pt.Y );
+            Point p2 = new Point(pt.X - 12, pt.Y + 9);
+            Point p3 = new Point(pt.X - 5, pt.Y + 15);
+            Point p4 = new Point(pt.X + 1, pt.Y + 10);
+            Point p5 = new Point(pt.X + 7, pt.Y + 13);
+            Point p6 = new Point(pt.X + 12, pt.Y + 5);
+            Point p7 = new Point(pt.X + 6, pt.Y - 1);
+            Point p8 = new Point(pt.X, pt.Y + 4);
+            
+            AddComponent(new Line(pt, p1));
+            AddComponent(new Line(p1, p2));
+            AddComponent(new Line(p2, p3));
+            AddComponent(new Line(p3, p4));
+            AddComponent(new Line(p4, p5));
+            AddComponent(new Line(p5, p6));
+            AddComponent(new Line(p6, p7));
+            AddComponent(new Line(p7, p8));
+            AddComponent(new Line(pt, p8));
         }
     }
 }
