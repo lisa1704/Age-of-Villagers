ccd056cc1f97b34c2862284f648916e6142e373a
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-19 02:25:32 +0600

    #10 Null nation implemented.

--- solutions/48-zoyee/Age Of Villagers/Null_Nation.cs
+++ solutions/48-zoyee/Age Of Villagers/Null_Nation.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_Of_Villagers
 {
-    class Null_Nation
+    class Null_Nation : INation
     {
+        public Color BackGroundColor()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Draw_House(Graphics g, Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_Tree(Graphics g, Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_WaterSource(Graphics g, Point point)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
