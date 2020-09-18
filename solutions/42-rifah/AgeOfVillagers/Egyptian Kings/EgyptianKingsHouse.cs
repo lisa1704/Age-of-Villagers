daa70b498ac7d2f83a6a4f69504ebc30187782c4
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-19 04:30:37 +0600

    #7 #11 Egyptian house implemented

--- solutions/42-rifah/AgeOfVillagers/Egyptian Kings/EgyptianKingsHouse.cs
+++ solutions/42-rifah/AgeOfVillagers/Egyptian Kings/EgyptianKingsHouse.cs
@@ -7,11 +7,15 @@ namespace AgeOfVillagers.Egyptian_Kings
 {
     class EgyptianKingsHouse : Composite
     {
-        private Point point;
 
         public EgyptianKingsHouse(Point point)
         {
-            this.point = point;
+            Point point1 = new Point(point.X - 25, point.Y + 45);
+            Point point2 = new Point(point.X + 20, point.Y + 50);
+            Point point3 = new Point(point.X + 30, point.Y + 25);
+
+            AddComponent(new Triangle(point2, point1, point));
+            AddComponent(new Triangle(point3, point2, point));
         }
     }
 }
