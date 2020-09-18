55e3943d17ec8cea202f0e3c5ce5680806fb7283
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-18 17:03:58 +0600

    #1 #9 #10 #11 #12 Specified Null Values Handler for Nations
    Have to add other conditions in respective methods later. but it's probably a better idea to create seperate null handlers

--- solutions/32-zahin/AgeOfVillagers/NullValuesHandler.cs
+++ solutions/32-zahin/AgeOfVillagers/NullValuesHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,21 @@ using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
-    class NullValuesHandler
+    class NullValuesHandler : INation
     {
-        public void execute()
+        public void DrawHouse(Graphics g, Point point)
         {
-            MessageBox.Show("Invalid!!");
+            MessageBox.Show("Can not Draw House for the chosen Nation!!");
+        }
+
+        public void DrawTree(Graphics g, Point point)
+        {
+            MessageBox.Show("Can not Draw Tree for the Chosen Nation!!");
+        }
+
+        public void DrawWaterSource(Graphics g, Point point)
+        {
+            MessageBox.Show("Can not Draw Water for the Chosen Nation!!");
         }
     }
 }
