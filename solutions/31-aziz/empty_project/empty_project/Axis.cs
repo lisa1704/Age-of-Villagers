eb4840a4098fa262197ee79a186369508725c06a
Asif Aziz <asifaziz2061@gmail.com>
2020-09-18 19:25:32 +0600

    #9 #10 #11 #12 Axis class implemented

--- solutions/31-aziz/empty_project/empty_project/Axis.cs
+++ solutions/31-aziz/empty_project/empty_project/Axis.cs
@@ -6,6 +6,13 @@ namespace empty_project
 {
     class Axis
     {
+        public Axis(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
 
+        public int X { get; }
+        public int Y { get; }
     }
 }
