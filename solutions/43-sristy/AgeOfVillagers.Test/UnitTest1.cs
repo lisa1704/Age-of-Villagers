fff1eae293deacd83bfb110d8c3b8d0737e2aa2b
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-18 21:58:46 +0600

    #10 #11 #12 test background color of Arab, Egypt, Inuit

--- solutions/43-sristy/AgeOfVillagers.Test/UnitTest1.cs
+++ solutions/43-sristy/AgeOfVillagers.Test/UnitTest1.cs
@@ -23,6 +23,32 @@ namespace AgeOfVillagers.Test
             Color actual = nation.get_backcolor();
             Assert.Equal(expected,actual);
         }
+        [Fact]
+        public void Test3()
+        {
+            INation nation = new Egyptian();
+            Color expected = Color.LightYellow;
+            Color actual = nation.get_backcolor();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test4()
+        {
+            INation nation = new Arab();
+            Color expected = Color.PaleGoldenrod;
+            Color actual = nation.get_backcolor();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test5()
+        {
+            INation nation = new Inuit();
+            Color expected = Color.Snow;
+            Color actual = nation.get_backcolor();
+            Assert.Equal(expected, actual);
+        }
+
+
 
     }
 
