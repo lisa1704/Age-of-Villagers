9cc7a57063912389e3fdf6fd63c720e424385860
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-09-18 19:24:55 +0600

    #10

--- solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/ArabTree.cs
+++ solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/ArabTree.cs
@@ -9,9 +9,15 @@ namespace AgeOfVillagers
     class ArabTree : IShape
     {
         private Panel drawingBoard;
-        public void draw(Point p, Panel drawingBoard)
+
+        public ArabTree(Panel drawingBoard)
         {
             this.drawingBoard = drawingBoard;
+        }
+        public void draw(Point p, Panel drawingBoard)
+        {
+            Graphics g = drawingBoard.CreateGraphics();
+            Pen p = new Pen(Color.Green);
            
         }
     }
