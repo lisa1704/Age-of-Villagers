3f35f5c788a97a7f4b390b06259129bdeb8354d7
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-18 15:18:33 +0600

    #1 Refactored BaseArc Class

--- solutions/19-zarif/AgeOfVillagers/BaseArc.cs
+++ solutions/19-zarif/AgeOfVillagers/BaseArc.cs
@@ -8,17 +8,23 @@ namespace AgeOfVillagers
     public class BaseArc : IGraphicShape
     {
         private Point  _point;
-        private int _h, _w;
-        public BaseArc(Point point, int h, int w)
+        private int _h, _w,_initial,_angle;
+        public BaseArc(Point point, int h, int w,int initial,int angle)
         {
             _point = point;
             _h = h;
             _w = w;
+            _initial = initial;
+            _angle = angle;
+
+
         }
         public void draw(Graphics g, Pen pen)
         {
             Rectangle shape = new Rectangle(_point.X, _point.Y, _h, _w);
-            g.DrawArc(pen, shape, 0, -180);
+            Rectangle shape_1 = new Rectangle(_point.X+10, _point.Y+10, _h-20, _w-20);
+            g.DrawArc(pen, shape, _initial, _angle);
+            g.DrawArc(pen, shape_1, _initial, _angle);
         }
     }
 }
