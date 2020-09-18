1f01373fde0b9487219e7092de6daa66f0f99e46
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-19 03:48:15 +0600

    #7 BangladeshiWaterSource implemented

--- solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillegeItems/BangladeshiWaterSource.cs
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillegeItems/BangladeshiWaterSource.cs
@@ -8,9 +8,29 @@ namespace AgeOfVillagers.VillegeItems
 {
     class BangladeshiWaterSource : IShape
     {
+        private readonly Point point1, point2, point3, point4, point5, point6, point7, point8;
+
+        public BangladeshiWaterSource(Point point)
+        {
+            point1 = point;
+            point2 = new Point(point.X, point.Y+20);
+            point3 = new Point(point.X+30, point.Y);
+            point4 = new Point(point.X+50, point.Y+20);
+            point5 = new Point(point.X+45, point.Y+45);
+            point6 = new Point(point.X+25, point.Y+33);
+            point7 = new Point(point.X, point.Y+45);
+            point8 = new Point(point.X-30, point.Y+33);
+        }
         public void draw(Graphics g, Pen p)
         {
-            throw new NotImplementedException();
+            new Line(point1, point2).draw(g, p);
+            new Line(point2, point3).draw(g, p);
+            new Line(point3, point4).draw(g, p);
+            new Line(point4, point5).draw(g, p);
+            new Line(point5, point6).draw(g, p);
+            new Line(point6, point7).draw(g, p);
+            new Line(point7, point8).draw(g, p);
+            new Line(point8, point1).draw(g, p);
         }
     }
 }
