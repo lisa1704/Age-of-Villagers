548e88694cd30b71fb0b18e950d3d829be78324f
Zadid Bin Azad <43434510+Zenhance@users.noreply.github.com>
2020-09-18 12:18:14 +0600

    #11 #7  Egyptian House drawn.

--- solutions/46-zadid/AgeOfVillagers/AgeOfVillagers/House/EgyptianHouse.cs
+++ solutions/46-zadid/AgeOfVillagers/AgeOfVillagers/House/EgyptianHouse.cs
@@ -7,7 +7,13 @@ namespace AgeOfVillagers.House
     {
         public void Draw(ElementSize sz, Graphics g, Point p)
         {
-            
+            Pen pen = new Pen(Color.Black);
+
+            g.DrawLine(pen, p.X, p.Y, p.X - sz.width / 2, p.Y - sz.height / 4);
+            g.DrawLine(pen, p.X, p.Y, p.X + sz.width / 2, p.Y - sz.height / 3);
+            g.DrawLine(pen, p.X, p.Y, p.X, p.Y - sz.height);
+            g.DrawLine(pen, p.X, p.Y - sz.height, p.X - sz.width / 2, p.Y - sz.height / 4);
+            g.DrawLine(pen, p.X, p.Y - sz.height, p.X + sz.width / 2, p.Y - sz.height / 3);
         }
     }
 }
