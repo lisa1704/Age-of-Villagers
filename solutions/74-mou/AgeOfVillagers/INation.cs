365b280d6be87b794e122424b9d89c4ef77877c7
sharminnaj@iut-dhaka.edu <sharminnaj@iut-dhaka.edu>
2020-09-18 12:08:02 +0600

    #9 method implemented

--- solutions/74-mou/AgeOfVillagers/INation.cs
+++ solutions/74-mou/AgeOfVillagers/INation.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
     interface INation
     {
-        void draw_house()
-        {
-
-        }
-        void draw_tree()
-        {
-
-        }
-        void draw_watercource()
-        {
-
-        }
+        void set_graphics(Graphics gr);
+        void set_pen(Pen pen);
+        void set_villagename(string vname);
+        string get_villagename();
+        void draw_house(Point p);
+        void draw_tree(Point p);
+        void draw_watercource(Point p);
     }
 }
