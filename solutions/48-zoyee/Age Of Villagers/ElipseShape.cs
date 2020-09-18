08efe01d62411fe88fe1e5898744105acccbb6f4
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-18 08:09:28 +0600

    #7 Elipse shape class added.

---
+++ solutions/48-zoyee/Age Of Villagers/ElipseShape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_Of_Villagers
+{
+    class ElipseShape : CompositeShape
+    {
+        private readonly Point topleft;
+        private readonly Point bottomright;
+
+        public ElipseShape(Point topleft, Point bottomright)
+        {
+            this.topleft = topleft;
+            this.bottomright = bottomright;
+            AddComponent(new Arc(topleft.X, topleft.Y, bottomright.X - topleft.X, bottomright.Y - topleft.Y, 0, 360));
+        }
+    }
+}
