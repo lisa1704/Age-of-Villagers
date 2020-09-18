e9ea83309c43020cdfca4d0c78b187975dfe514e
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-19 01:33:25 +0600

    Triangle class implemented

--- solutions/42-rifah/AgeOfVillagers/Triangle.cs
+++ solutions/42-rifah/AgeOfVillagers/Triangle.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
     class Triangle : Composite
     {
+        public Triangle(Point point_1, Point point_2, Point point_3)
+        {
+            AddComponent(new Line(point_1, point_2));
+            AddComponent(new Line(point_2, point_3));
+            AddComponent(new Line(point_3, point_1));
+        }
     }
 }
