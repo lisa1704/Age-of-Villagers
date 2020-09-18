94e91d64de3b2b9a8049c69df0c31120d6d9d7b9
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-18 21:28:02 +0600

    #10 house class created for arab bedouins

---
+++ solutions/34-meem/Age_Of_Villagers/ArabBedouinHouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace Age_Of_Villagers
+{
+    internal class ArabBedouinHouse : IVillageItem
+    {
+        public void Draw(Point e, Graphics g, Pen p)
+        {
+            
+
+
+        }
+    }
+}
