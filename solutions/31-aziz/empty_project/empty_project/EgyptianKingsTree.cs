6463d8ed691339daeafd2b3da6e041d40b74df4c
Asif Aziz <asifaziz2061@gmail.com>
2020-09-18 21:00:33 +0600

    #11 Draw tree class implemented in Egyptian Kings Tree class

--- solutions/31-aziz/empty_project/empty_project/EgyptianKingsTree.cs
+++ solutions/31-aziz/empty_project/empty_project/EgyptianKingsTree.cs
@@ -8,5 +8,21 @@ namespace empty_project
 {
     class EgyptianKingsTree : ITree
     {
+        public void drawTree(Axis axis, Panel panel)
+        {
+            Pen pen = new Pen(Color.Black);
+            Graphics graphics = panel.CreateGraphics();
+
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X + 4, axis.Y - 20));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 4, axis.Y - 20));
+
+            graphics.DrawLine(pen, new PointF(axis.X + 4, axis.Y - 20), new PointF(axis.X + 4, axis.Y - 24));
+            graphics.DrawLine(pen, new PointF(axis.X + 4, axis.Y - 20), new PointF(axis.X + 8, axis.Y - 20));
+            graphics.DrawLine(pen, new PointF(axis.X + 4, axis.Y - 20), new PointF(axis.X + 8, axis.Y - 24));
+
+            graphics.DrawLine(pen, new PointF(axis.X - 4, axis.Y - 20), new PointF(axis.X - 4, axis.Y - 24));
+            graphics.DrawLine(pen, new PointF(axis.X - 4, axis.Y - 20), new PointF(axis.X - 8, axis.Y - 20));
+            graphics.DrawLine(pen, new PointF(axis.X - 4, axis.Y - 20), new PointF(axis.X - 8, axis.Y - 24));
+        }
     }
 }
