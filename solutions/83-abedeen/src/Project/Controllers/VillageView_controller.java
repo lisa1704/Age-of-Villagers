152f3996712adcca2d9e895c6d5cfc7e29df3d2c
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-19 01:25:24 +0600

    #5 File Save Implemented

--- solutions/83-abedeen/src/Project/Controllers/VillageView_controller.java
+++ solutions/83-abedeen/src/Project/Controllers/VillageView_controller.java
@@ -3,15 +3,23 @@ package Project.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
+import org.json.simple.JSONObject;
 
-import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class VillageView_controller implements Initializable {
-    @FXML private TextField village_name;
+    @FXML public ChoiceBox NationList;
+    @FXML public TextField VillageNameField;
     @FXML private Pane DrawingCanvas;
 
     @Override
@@ -19,9 +27,33 @@ public class VillageView_controller implements Initializable {
 
     }
 
-    public void TreePressed(ActionEvent actionEvent) {
+    public void TreePressed(ActionEvent actionEvent){
 
         Line simpleLine = new Line(25,25,65,65);
         DrawingCanvas.getChildren().add(simpleLine);
+        System.out.println(NationList.getValue());
+        System.out.println(VillageNameField.getText());
+    }
+
+    public void SaveVillage() throws IOException {
+        String FileName = VillageNameField.getText() + ".aov";
+        JSONObject CurrentVillage = new JSONObject();
+        CurrentVillage.put("villageName", VillageNameField.getText());
+        CurrentVillage.put("nation", NationList.getValue());
+
+        try {
+            FileOutputStream file = new FileOutputStream("SavedFiles/"+FileName);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+            out.writeObject(CurrentVillage.toJSONString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void save_village(ActionEvent actionEvent) throws IOException {
+        SaveVillage();
+        System.out.println("File Write done");
     }
 }
