907b60e57a98bb0ecfde956125bfdd93ba0a8674
Asif Aziz <asifaziz2061@gmail.com>
2020-09-18 20:52:39 +0600

    #10 Draw Tree method implemented in Arab Bedouin Tree class

--- solutions/31-aziz/empty_project/empty_project/ArabBedouinTree.cs
+++ solutions/31-aziz/empty_project/empty_project/ArabBedouinTree.cs
@@ -8,5 +8,20 @@ namespace empty_project
 {
     class ArabBedouinTree : ITree
     {
+        public void drawTree(Axis axis, Panel panel)
+        {
+            Pen pen = new Pen(Color.Black);
+            Graphics graphics = panel.CreateGraphics();
+
+            graphics.DrawRectangle(pen, axis.X, axis.Y, 2, 15);
+
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X + 1, axis.Y - 15));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X + 4, axis.Y - 15));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X + 6, axis.Y - 15));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X + 8, axis.Y - 15));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X - 3, axis.Y - 15));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X - 5, axis.Y - 15));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X - 7, axis.Y - 15));
+        }
     }
 }
