757f9348943dce14ac1d7c0987a78b6f9bbde120
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-18 12:35:10 +0600

    #5 #6 #1 added VillageItems Class in IStated Implementinf Folder

---
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/IStateElements implimentinf folder/VillageItems.cs
@@ -0,0 +1,21 @@
+﻿using AgeOfVillagers.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers.IStateElements_implimentinf_folder
+{
+    class VillageItems : IStateElements
+    {
+        List<IItem> itemList;
+        public VillageItems() {
+            itemList = new List<IItem>();
+        }
+
+        
+        public void displayElements()
+        {
+            
+        }
+    }
+}
