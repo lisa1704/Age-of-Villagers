393fe1e9fc426b80713b93c55cbb7c0aabe38083
MD ASHIQUR RAHMAN JOY <ashiqurrahman85@iut-dhaka.edu>
2020-09-19 00:05:06 +0600

    #4 User Now can give a name of his/her Village

--- solutions/85-joy/src/project/Controllers/VillageViewController.java
+++ solutions/85-joy/src/project/Controllers/VillageViewController.java
@@ -1,5 +1,6 @@
 package project.Controllers;
 
+import javafx.collections.ObservableArray;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +15,7 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -23,11 +25,10 @@ import javafx.scene.shape.Line;
 
 
 public class VillageViewController implements Initializable {
-    @FXML
-    private TextField Village_Name;
+    @FXML public ChoiceBox NationDropDown;
+    @FXML private TextField Village_Name;
     @FXML private Button WaterSource = new Button();
     @FXML private Pane DrawPane;
-    private double x1,y1,x2,y2;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -39,7 +40,8 @@ public class VillageViewController implements Initializable {
 
             Circle circle = new Circle(500,200,10,Color.BLACK);
             DrawPane.getChildren().add(circle);
-
+            System.out.println(NationDropDown.getValue());
+            System.out.println(Village_Name.getText());
 
         }
         catch (Exception e){
