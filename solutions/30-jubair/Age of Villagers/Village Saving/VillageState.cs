4457f50dc1a2c4b57eb2fb34c789e3eef053eefa
30-jubair <jobairt03@gmail.com>
2020-09-18 22:08:08 +0600

    #1 Decoupled the Village State logic from the GUI

---
+++ solutions/30-jubair/Age of Villagers/Village Saving/VillageState.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Age_of_Villagers.Nation;
+
+namespace Age_of_Villagers.Village_Saving
+{
+    public class VillageState
+    
+    {
+        public List<Point> houseList = new List<Point>();
+        public List<Point> treeList = new List<Point>();
+        public List<Point> waterResourceList = new List<Point>();
+
+        public string villageName = "";
+
+        public string GetVillageName()
+        {
+            return villageName;
+        }
+        public void SetVillageName(string name)
+        {
+            villageName = name;
+        }
+
+        public void AddHouse(Graphics graphics, Point house, INation ination)
+        
+        {
+            houseList.Add(house);
+            ination.GetHouse(graphics,house);
+
+            //houseList.Add(house);
+
+        }
+
+
+        public void AddTree(Graphics graphics, Point tree, INation ination)
+
+        {
+           treeList.Add(tree);
+            ination.GetTree(graphics, tree);
+           // treeList.Add(tree);
+        }
+
+
+        public void AddWaterResource(Graphics graphics, Point waterResource, INation ination)
+
+        {
+           waterResourceList.Add(waterResource);
+            ination.GetWaterResource(graphics, waterResource);
+
+            //waterResourceList.Add(waterResource);
+        }
+
+    }
+}
