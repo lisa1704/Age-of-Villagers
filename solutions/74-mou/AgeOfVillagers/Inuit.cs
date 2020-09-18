f66e54bae78334910df78e34eac14ec6a9e9a0a1
sharminnaj@iut-dhaka.edu <sharminnaj@iut-dhaka.edu>
2020-09-18 12:36:45 +0600

    #12 Inuit method implemented

--- solutions/74-mou/AgeOfVillagers/Inuit.cs
+++ solutions/74-mou/AgeOfVillagers/Inuit.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
     class Inuit : INation
     {
-
+        private string vname;
+        private Graphics gr;
+        private Pen pen;
+        void set_graphics(Graphics gr)
+        {
+            this.gr = gr;
+        }
+        void set_pen(Pen pen)
+        {
+            this.pen = pen;
+        }
+        void set_villagename(string vname)
+        {
+            this.vname = vname;
+        }
+        string get_villagename()
+        {
+            return vname;
+        }
         void draw_house()
         {
 
