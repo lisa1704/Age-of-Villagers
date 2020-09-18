e40aab0a22cab4a32122948216c1c0aa2eb87425
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-18 23:00:24 +0600

    #12 house can be drawn for inuit hunters

--- solutions/34-meem/Age_Of_Villagers/InuitHunterHouse.cs
+++ solutions/34-meem/Age_Of_Villagers/InuitHunterHouse.cs
@@ -1,6 +1,18 @@
-﻿namespace Age_Of_Villagers
+﻿using System.Drawing;
+
+namespace Age_Of_Villagers
 {
     internal class InuitHunterHouse : IVillageItem
     {
+        public void Draw(Point e, Graphics g, Pen p)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            
+            g.DrawLine(p, x, y, x + 20, y);
+            g.DrawArc(p, x, y - 15, 20, 30, 180, 180);
+            g.DrawArc(p, x + 6, y - 6, 8, 15, 180, 180);
+        }
     }
 }
