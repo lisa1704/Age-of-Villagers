227eafb9b4620244648075837b80977fd1a305e0
30-jubair <jobairt03@gmail.com>
2020-09-19 04:42:49 +0600

    #5 #8 Nation Information won't be saved

--- solutions/30-jubair/Age of Villagers/Command/VillageSave.cs
+++ solutions/30-jubair/Age of Villagers/Command/VillageSave.cs
@@ -2,16 +2,50 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Windows.Forms;
+
 
 
 namespace Age_of_Villagers.Village_Saving
 {
     public class VillageSave : ICommand
     {
-        
+
+        SaveFileDialog sfd = new SaveFileDialog();
+        VillageEdit newVillageEdit;
+        string path;
+        string villageSerialized;
+      
+        public VillageSave(VillageEdit villageEdit)
+        {
+            newVillageEdit = villageEdit;
+        }
+
+
         public void ExecuteCommand()
         {
-        }            
+        }
+
+        public void SaveVillage(VillageEdit village)
+        {
+            newVillageEdit = village;
+            villageSerialized = JsonConvert.SerializeObject(newVillageEdit);
+            sfd.Filter = ".aov|*.aov";
+            sfd.Title = "Save AOV File";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                path = sfd.FileName;
+                using Stream aovStream = File.Open(path, FileMode.Create);
+                using StreamWriter stream_writer = new StreamWriter(aovStream);
+                stream_writer.Write(villageSerialized);
+            }
+            else
+            {
+                return;
+            }
+        }
 
     }
 }
