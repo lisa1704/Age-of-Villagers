0aada38fcbd822c2155e0b1f4ff3dec7242e655a
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-19 03:48:08 +0600

    #4 Two Panels are Added

--- solutions/04-nafis/Age of Villagers/Age of Villagers/VillageWindow.Designer.cs
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/VillageWindow.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.VillageDrawing = new System.Windows.Forms.Panel();
+            this.MenuBar = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // VillageDrawing
+            // 
+            this.VillageDrawing.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.VillageDrawing.Location = new System.Drawing.Point(1, 0);
+            this.VillageDrawing.Name = "VillageDrawing";
+            this.VillageDrawing.Size = new System.Drawing.Size(450, 400);
+            this.VillageDrawing.TabIndex = 0;
+            // 
+            // MenuBar
+            // 
+            this.MenuBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MenuBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MenuBar.Location = new System.Drawing.Point(452, 0);
+            this.MenuBar.Margin = new System.Windows.Forms.Padding(0);
+            this.MenuBar.Name = "MenuBar";
+            this.MenuBar.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.MenuBar.Size = new System.Drawing.Size(150, 400);
+            this.MenuBar.TabIndex = 1;
+            // 
+            // VillageWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600,400);
+            this.ClientSize = new System.Drawing.Size(602, 408);
+            this.Controls.Add(this.MenuBar);
+            this.Controls.Add(this.VillageDrawing);
+            this.Name = "VillageWindow";
             this.Text = "Age of Villagers";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel VillageDrawing;
+        private System.Windows.Forms.Panel MenuBar;
     }
 }
 
