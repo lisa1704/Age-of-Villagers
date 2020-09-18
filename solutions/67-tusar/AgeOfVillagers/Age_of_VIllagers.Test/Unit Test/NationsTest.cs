c72bf4f79f428c2aa8d355fdf1492ed99afabd37
67-tusar <zubairrahman@iut-dhaka.edu>
2020-09-18 22:13:29 +0600

    test added to check nation tree type

--- solutions/67-tusar/AgeOfVillagers/Age_of_VIllagers.Test/Unit Test/NationsTest.cs
+++ solutions/67-tusar/AgeOfVillagers/Age_of_VIllagers.Test/Unit Test/NationsTest.cs
@@ -17,8 +17,33 @@ namespace Age_of_VIllagers.Test.Unit_Test
         [TestMethod]
         public void TestNationHouse()
         {
-            AbstractPainter painter = new BangladeshiPainter(new Panel());
-            Assert.AreEqual(painter.getHouseType().GetType(), new BangladeshiHouse().GetType());
+            AbstractPainter bdpainter = new BangladeshiPainter(new Panel());
+            Assert.AreEqual(bdpainter.getHouseType().GetType(), new BangladeshiHouse().GetType());
+
+            AbstractPainter arpainter = new ArabPainter(new Panel());
+            Assert.AreEqual(arpainter.getHouseType().GetType(), new ArabHouse().GetType());
+
+            AbstractPainter egpainter = new EgyptianPainter(new Panel());
+            Assert.AreEqual(egpainter.getHouseType().GetType(), new EgyptianHouse().GetType());
+
+            AbstractPainter inpainter = new InuitPainter(new Panel());
+            Assert.AreEqual(inpainter.getHouseType().GetType(), new InuitHouse().GetType());
+        }
+
+        [TestMethod]
+        public void TestNationTree()
+        {
+            AbstractPainter bdpainter = new BangladeshiPainter(new Panel());
+            Assert.AreEqual(bdpainter.getTreeType().GetType(), new BangladeshiTree().GetType());
+
+            AbstractPainter arpainter = new ArabPainter(new Panel());
+            Assert.AreEqual(arpainter.getTreeType().GetType(), new ArabTree().GetType());
+
+            AbstractPainter egpainter = new EgyptianPainter(new Panel());
+            Assert.AreEqual(egpainter.getTreeType().GetType(), new EgyptianTree().GetType());
+
+            AbstractPainter inpainter = new InuitPainter(new Panel());
+            Assert.AreEqual(inpainter.getTreeType().GetType(), new NullTree().GetType());
         }
     }
 }
