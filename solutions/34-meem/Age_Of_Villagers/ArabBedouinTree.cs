dd819a64a26f26517d9ced7aaca028df9f85c56a
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-18 19:59:35 +0600

    #10 tree class implemented

--- solutions/34-meem/Age_Of_Villagers/ArabBedouinTree.cs
+++ solutions/34-meem/Age_Of_Villagers/ArabBedouinTree.cs
@@ -6,6 +6,19 @@ namespace Age_Of_Villagers
     {
         public void Draw(Point e, Graphics g, Pen p)
         {
+            int x = e.X;
+            int y = e.Y;
+
+            g.DrawLine(p, x - 2, y, x + 2, y);
+            g.DrawLine(p, x + 2, y, x + 2, y + 20);
+            g.DrawLine(p, x - 2, y, x - 2, y + 20);
+            g.DrawLine(p, x - 2, y + 20, x + 2, y + 20);
+
+            g.DrawLine(p, x , y-15, x , y );
+            g.DrawLine(p, x + 15, y - 10, x, y);
+            g.DrawLine(p, x - 15, y - 10, x, y);
+            g.DrawLine(p, x + 17, y - 3, x, y);
+            g.DrawLine(p, x - 17, y - 3, x, y);
             
         }
     }
