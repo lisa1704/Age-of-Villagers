cf179bdf0353ec403ae40d223317f8e14f62f4e3
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-19 01:55:51 +0600

    #1 #7 #8 #9 BDFarmersHouse class implemented

--- solutions/42-rifah/AgeOfVillagers/BDFarmersHouse.cs
+++ solutions/42-rifah/AgeOfVillagers/BDFarmersHouse.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
     class BDFarmersHouse : Composite
     {
+        public BDFarmersHouse(Point Top, Point TopLeft, Point BottomRight)
+        {
+            AddComponent(new Rectangle(TopLeft, BottomRight));
+            AddComponent(new Triangle(Top, TopLeft, new Point(BottomRight.X, TopLeft.Y)));
+        }
     }
 }
