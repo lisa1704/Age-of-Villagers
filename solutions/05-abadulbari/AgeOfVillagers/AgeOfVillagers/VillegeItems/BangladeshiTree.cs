338823765612585cf1abd16bd998a71a68505093
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-19 03:09:49 +0600

    #7 BangladeshiTree created and implemented

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillegeItems/BangladeshiTree.cs
@@ -0,0 +1,29 @@
+﻿using AgeOfVillagers.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Rectangle = AgeOfVillagers.Shapes.Rectangle;
+
+namespace AgeOfVillagers.VillegeItems
+{
+    class BangladeshiTree : IShape
+    {
+        private readonly Point topLeft, bottomRight, centre;
+
+        public BangladeshiTree(Point point)
+        {
+            centre = point;
+            topLeft.X = point.X - 20;
+            topLeft.Y = point.Y - 20;
+            bottomRight.X = point.X + 20;
+            bottomRight.Y = point.Y + 20;
+        }
+
+        public void draw(Graphics g, Pen p)
+        {
+            new Circle(topLeft, bottomRight).draw(g, p);
+            new Rectangle(new Point(centre.X - 2, centre.Y), new Point(centre.X + 2, centre.Y + 40)).draw(g, p);
+        }
+    }
+}
