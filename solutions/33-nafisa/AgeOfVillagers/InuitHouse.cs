e4e7cee7c44a04cdf9ca0fc5385e3d53a7d588f9
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-18 11:57:38 +0600

    #12 InuitHouse class created

---
+++ solutions/33-nafisa/AgeOfVillagers/InuitHouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    class InuitHouse : IShape
+    {
+        public void Draw(Point point, Panel Drawingpanel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
