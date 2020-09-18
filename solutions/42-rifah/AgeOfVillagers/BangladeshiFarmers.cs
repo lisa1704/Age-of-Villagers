e5fd6a9aed61b0d6b1b4ca5dc01fcad6b1a96a8e
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-19 01:27:00 +0600

    #1 #8 #9 New Triangle class created and changes made in Bangladeshi Farmers class

--- solutions/42-rifah/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/42-rifah/AgeOfVillagers/BangladeshiFarmers.cs
@@ -15,6 +15,8 @@ namespace AgeOfVillagers
         public void DrawHouse(Graphics g, Point point)
         {
             g.DrawLine(p, point.X, point.Y, 100, 100);
+            Rectangle rectangle = new Rectangle(new Point(point.X, point.Y), new Point(point.X + 15, point.Y + 25));
+            rectangle.Draw(g);
         }
         public void DrawWaterSource(Graphics g, Point point)
         {
