41a8c0f15cdeabdb0ceca98b8755cd6dd5f991a6
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-18 20:50:12 +0600

    #6 added OpenVillage class ,implementing GameControlCommand

---
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/OpenVillage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class OpenVillage : GameControlCommand
+    {
+        public void execute()
+        {
+            
+        }
+    }
+}
