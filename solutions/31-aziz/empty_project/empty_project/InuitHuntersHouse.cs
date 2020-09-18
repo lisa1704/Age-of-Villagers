0db44368617f5fe76098af87bd89941a664504d7
Asif Aziz <asifaziz2061@gmail.com>
2020-09-18 20:21:51 +0600

    #12 Draw House method implementeed in Inuit Hunters House class

--- solutions/31-aziz/empty_project/empty_project/InuitHuntersHouse.cs
+++ solutions/31-aziz/empty_project/empty_project/InuitHuntersHouse.cs
@@ -8,5 +8,17 @@ namespace empty_project
 {
     class InuitHuntersHouse : IHouse
     {
+        public void drawHouse(Axis axis, Panel panel)
+        {
+            Pen pen = new Pen(Color.Black);
+            Graphics graphics = panel.CreateGraphics();
+
+            float startAngle = 0.0F;
+            float sweepAngle = -180.0F;
+
+            graphics.DrawArc(pen, axis.X, axis.Y, 20, 20, startAngle, sweepAngle);
+            graphics.DrawArc(pen, axis.X + 5, axis.Y + 5,10,10, startAngle, sweepAngle);
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y + 10), new PointF(axis.X + 20, axis.Y + 10));
+        }
     }
 }
