d682e9f177fc689899cd31cbcc594e68c497ceca
musfiksifat <musfiksifat766@gmail.com>
2020-09-18 20:12:38 +0600

    #7 #9 #10 #11 #12 all necessary child class under House implemented

--- solutions/88-musfik/AgeOfVillagers/VillageItem/IVillageItem.cs
+++ solutions/88-musfik/AgeOfVillagers/VillageItem/IVillageItem.cs
@@ -27,5 +27,56 @@ namespace AgeOfVillagers
         }
         public abstract CompositeShape getItemShape(Point point);
     }
+    public class ArabHouse : House
+    {
+        public ArabHouse(Point point) : base(point)
+        {
+        }
+
+        public override CompositeShape getItemShape(Point point)
+        {
+            return new ArabHouseShape(point);
+        }
+    }
+
+    public class BangladeshiHouse : House
+    {
+        public BangladeshiHouse(Point point) : base(point)
+        {
+        }
+
+        public override CompositeShape getItemShape(Point point)
+        {
+            return new BangladeshiHouseShape(point);
+        }
+    }
+
+    public class EgyptianHouse : House
+    {
+        public EgyptianHouse(Point point) : base(point)
+        {
+        }
+
+        public override CompositeShape getItemShape(Point point)
+        {
+            return new EgyptianHouseShape(point);
+        }
+    }
+
+    public class InuitHuntersHouse : House
+    {
+        public InuitHuntersHouse(Point point) : base(point)
+        {
+        }
+
+        public override CompositeShape getItemShape(Point point)
+        {
+            return new InuitHuntersHouseShape(point);
+        }
+    }
+
+    
+    
+
 
 }
