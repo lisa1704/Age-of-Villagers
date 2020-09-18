2fc811381b785dc72e8f6dfe70aede878feacce7
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-18 20:08:58 +0600

    #4 Created the basic UI without the functionalites

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/Form1.Designer.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/Form1.Designer.cs
@@ -42,7 +42,8 @@
             // 
             // DrawingPanel
             // 
-            this.DrawingPanel.Location = new System.Drawing.Point(0, 28);
+            this.DrawingPanel.Location = new System.Drawing.Point(11, 12);
+            this.DrawingPanel.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.DrawingPanel.Name = "DrawingPanel";
             this.DrawingPanel.Size = new System.Drawing.Size(600, 400);
             this.DrawingPanel.TabIndex = 0;
@@ -51,32 +52,47 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(639, 28);
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(639, 24);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(183, 27);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(185, 23);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Age of Villagers";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox2
             // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Location = new System.Drawing.Point(664, 81);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(132, 27);
+            this.textBox2.Size = new System.Drawing.Size(132, 18);
             this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Katakhali";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox3
             // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Location = new System.Drawing.Point(639, 128);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(183, 27);
+            this.textBox3.Size = new System.Drawing.Size(183, 18);
             this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Bangladeshi Farmers";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(630, 200);
+            this.radioButton1.Location = new System.Drawing.Point(630, 195);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(58, 24);
+            this.radioButton1.Size = new System.Drawing.Size(58, 21);
             this.radioButton1.TabIndex = 4;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Tree";
@@ -85,9 +101,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(750, 200);
+            this.radioButton2.Location = new System.Drawing.Point(751, 195);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(72, 24);
+            this.radioButton2.Size = new System.Drawing.Size(71, 21);
             this.radioButton2.TabIndex = 5;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "House";
@@ -96,9 +113,10 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(664, 230);
+            this.radioButton3.Location = new System.Drawing.Point(664, 222);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(118, 24);
+            this.radioButton3.Size = new System.Drawing.Size(119, 21);
             this.radioButton3.TabIndex = 6;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Water Source";
@@ -115,7 +133,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(615, 383);
+            this.button2.Location = new System.Drawing.Point(615, 366);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(108, 36);
             this.button2.TabIndex = 8;
@@ -124,7 +142,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(729, 383);
+            this.button3.Location = new System.Drawing.Point(729, 366);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(113, 36);
             this.button3.TabIndex = 9;
@@ -133,9 +151,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(852, 483);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(845, 431);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -146,6 +165,8 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.DrawingPanel);
+            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
