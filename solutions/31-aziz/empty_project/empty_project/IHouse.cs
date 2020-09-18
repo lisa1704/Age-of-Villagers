0155be13c57b2f00a1f1084133226829c172c828
Asif Aziz <asifaziz2061@gmail.com>
2020-09-18 19:39:08 +0600

    #7 DrawHouse method added in IHouse interface

--- solutions/31-aziz/empty_project/empty_project/IHouse.cs
+++ solutions/31-aziz/empty_project/empty_project/IHouse.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 namespace empty_project
 {
     interface IHouse
     {
+        void DrawHouse(Axis axis, Panel panel);
     }
 }
