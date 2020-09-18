6ab2d4cc667e08ef039168de32103471067fa7ff
Asif Aziz <asifaziz2061@gmail.com>
2020-09-18 21:56:44 +0600

    #9 Draw Water Source method implemented in Bangladeshi Farmers Water Source class

--- solutions/31-aziz/empty_project/empty_project/BangladeshiFarmersWaterSource.cs
+++ solutions/31-aziz/empty_project/empty_project/BangladeshiFarmersWaterSource.cs
@@ -8,5 +8,18 @@ namespace empty_project
 {
     class BangladeshiFarmersWaterSource : IWaterSource
     {
+        public void drawWaterSource(Axis axis, Panel panel)
+        {
+            Pen pen = new Pen(Color.Black);
+            Graphics graphics = panel.CreateGraphics();
+
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 3, axis.Y - 3));
+            graphics.DrawLine(pen, new PointF(axis.X - 3, axis.Y - 3), new PointF(axis.X - 6, axis.Y + 5));
+            graphics.DrawLine(pen, new PointF(axis.X - 6, axis.Y + 5), new PointF(axis.X - 12, axis.Y - 1));
+            graphics.DrawLine(pen, new PointF(axis.X - 12, axis.Y - 1), new PointF(axis.X - 24, axis.Y + 6));
+            graphics.DrawLine(pen, new PointF(axis.X - 24, axis.Y + 6), new PointF(axis.X - 13, axis.Y + 24));
+            graphics.DrawLine(pen, new PointF(axis.X - 13, axis.Y + 24), new PointF(axis.X, axis.Y + 10));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y + 10), new PointF(axis.X, axis.Y));
+        }
     }
 }
