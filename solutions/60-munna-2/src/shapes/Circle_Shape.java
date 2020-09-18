35b47c85e382e5ce5bcc68d57047237a58fda301
sakibul-munnaa <sakibulislam1@iut-dhaka.edu>
2020-09-19 01:08:06 +0600

    #7 Circle_Shape class added to draw circles. No functionality added yet.

---
+++ solutions/60-munna-2/src/shapes/Circle_Shape.java
@@ -0,0 +1,12 @@
+package shapes;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.ArcType;
+
+public class Circle_Shape implements IShape{
+
+    @Override
+    public void draw() {
+        
+    }
+}
