fedc6aa67f36d441bc022d7bdef62fab0873d36b
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-18 18:44:19 +0600

    #1 #7 #11 egyptian trees reshaped

--- solutions/90-safayat/AgeOfVillagers90/EgyptianTree.cs
+++ solutions/90-safayat/AgeOfVillagers90/EgyptianTree.cs
@@ -6,21 +6,22 @@ namespace AgeOfVillagers90
     {
         public EgyptianTree(Point mainPoint)
         {
-            Point point1 = new Point(mainPoint.X + 25, mainPoint.Y - 50);
-            Point point2 = new Point(mainPoint.X - 25, mainPoint.Y - 50);
-            Point point3 = new Point(mainPoint.X + 15, mainPoint.Y - 30);
-            Point point4 = new Point(mainPoint.X + 40, mainPoint.Y - 45);
-            Point point5 = new Point(mainPoint.X + 10, mainPoint.Y - 50);
-            Point point6 = new Point(mainPoint.X - 16, mainPoint.Y - 30);
-            Point point7 = new Point(mainPoint.X - 10, mainPoint.Y - 50);
-            Point point8 = new Point(mainPoint.X - 35, mainPoint.Y - 50);
+            
+            Point point1 = new Point(mainPoint.X - 5, mainPoint.Y - 24);
+            Point point2 = new Point(mainPoint.X + 5, mainPoint.Y - 24);
+            Point point3 = new Point(mainPoint.X - 2, mainPoint.Y - 24);
+            Point point4 = new Point(mainPoint.X + 2, mainPoint.Y - 24);
+            Point point5 = new Point(mainPoint.X + 8, mainPoint.Y - 24);
+            Point point6 = new Point(mainPoint.X - 8, mainPoint.Y - 24);
+            Point point7 = new Point(mainPoint.X - 3, mainPoint.Y - 12);
+            Point point8 = new Point(mainPoint.X + 3, mainPoint.Y - 12);
 
             AddComponent(new Line(mainPoint, point1));
             AddComponent(new Line(mainPoint, point2));
-            AddComponent(new Line(point3, point4));
-            AddComponent(new Line(point3, point5));
+            AddComponent(new Line(point3, point7));
             AddComponent(new Line(point6, point7));
-            AddComponent(new Line(point6, point8));
+           AddComponent(new Line(point4, point8));
+           AddComponent(new Line(point5, point8));
         }
     }
 }
