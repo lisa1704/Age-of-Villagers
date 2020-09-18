c1d013cbb23b92f53c773ebf76d6096afe8d3011
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-18 12:28:29 +0600

    #7 #12 Inuit house can be drawn

--- solutions/33-nafisa/AgeOfVillagers/InuitHouse.cs
+++ solutions/33-nafisa/AgeOfVillagers/InuitHouse.cs
@@ -18,7 +18,11 @@ namespace AgeOfVillagers
         {
             Graphics g = Drawingpanel.CreateGraphics();
             Pen p = new Pen(Color.Black);
-            //g.DrawArc(p, point.X, point.Y, 12, 12, 45, 360);
+           
+            g.DrawArc(p, point.X, point.Y, 16, 16, 0, -180);
+            g.DrawArc(p, point.X + 4, point.Y + 4, 8, 8, 0, -180);
+            g.DrawLine(p, point.X, point.Y + 8, point.X + 16, point.Y+8);
+            
         }
     }
 }
