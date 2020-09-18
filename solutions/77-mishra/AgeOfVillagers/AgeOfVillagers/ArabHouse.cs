9ea965acd4b89babd6a0ac35f9fbe9e4f3c214c3
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-09-18 22:03:08 +0600

    #10 Arab House class created

--- solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/ArabHouse.cs
+++ solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/ArabHouse.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
+using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
-    class ArabHouse
+    class ArabHouse : IShape
     {
+        private Panel drawingBoard;
+
+        public ArabHouse(Panel drawingBoard)
+        {
+            this.drawingBoard = drawingBoard;
+        }
+        public void draw(Point p, Panel drawingBoard)
+        {
+            Graphics g = drawingBoard.CreateGraphics();
+            Pen pen = new Pen(Color.Brown);
+        }
     }
 }
