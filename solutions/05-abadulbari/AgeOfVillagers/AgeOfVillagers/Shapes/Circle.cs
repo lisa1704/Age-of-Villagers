2bea40c57f60da9a06277b40965bbadd6eaacade
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-19 02:19:20 +0600

    #7 Circle Shape created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/Shapes/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.Shapes
+{
+    class Circle : IShape
+    {
+        private readonly Point point;
+        private float x, y, width, height, startAngle, sweepAngle;
+
+        public Circle(Point topLeft, Point bottomRight)
+        {
+            x = topLeft.X;
+            y = topLeft.Y;
+            width = bottomRight.X - topLeft.X;
+            height = bottomRight.Y - topLeft.Y;
+            startAngle = 0;
+            sweepAngle = 360;
+        }
+        public void draw(Graphics g, Pen p)
+        {
+            g.DrawArc(p, x, y, width, height, startAngle, sweepAngle);
+        }
+    }
+}
