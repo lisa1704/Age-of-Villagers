4bcfdedf4bcdd23d616691e95e3d74458af98654
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-18 08:12:02 +0600

    #7 #9 Bangladesh farmer tree class created and implemented.

---
+++ solutions/48-zoyee/Age Of Villagers/BangladeshiFarmer_Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    class BangladeshFarmer_Tree : CompositeShape
+    {
+        public BangladeshFarmer_Tree(Point MainPoint)
+        {
+            Point TopRight = new Point(MainPoint.X + 20, MainPoint.Y);
+            Point BottomRight = new Point(MainPoint.X + 25, MainPoint.Y + 50);
+            Point TopPoint = new Point(MainPoint.X + 40, MainPoint.Y + 40);
+            Point TopLeft = new Point(MainPoint.X + 20, MainPoint.Y + 20);
+
+            AddComponent(new Rectangle(TopLeft, BottomRight));
+            AddComponent(new ElipseShape(MainPoint, TopPoint));
+        }
+    }
+}
