eb0e71248ea7db8155ceaf20445aa69f01aedaeb
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-18 17:48:31 +0600

    #7 #4 Triangle shape added

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/Shapes/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace AgeOfVillagers.Shapes
+{
+    public class Triangle : IShape
+    {
+        private readonly Point top;
+        private readonly Point bottomLeft;
+        private readonly Point bottomRight;
+
+        public Triangle(Point top, Point bottomLeft, Point bottomRight)
+        {
+            this.top = top;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+        }
+        public void draw(Graphics g, Pen p)
+        {
+            new Line(top, bottomLeft).draw(g, p);
+            new Line(top, bottomRight).draw(g, p);
+            new Line(bottomLeft, bottomRight).draw(g, p);
+        }
+    }
+}
