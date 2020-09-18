e7deeeaf9c960fbe301e3199c0614995a7b9937e
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-18 07:44:51 +0600

    #7 #9 Bangladeshi House edited

--- solutions/33-nafisa/AgeOfVillagers/BangladeshiHouse.cs
+++ solutions/33-nafisa/AgeOfVillagers/BangladeshiHouse.cs
@@ -21,9 +21,11 @@ namespace AgeOfVillagers
             Graphics g = Drawingpanel.CreateGraphics();
             Pen p = new Pen(Color.Black);
             g.DrawLine(p, point.X, point.Y, point.X + 24, point.Y);
-            g.DrawLine(p, point.X + 24, point.Y, point.X + 24, point.Y - 16);
-            g.DrawLine(p, point.X, point.Y - 16, point.X + 24, point.Y - 16);
-            g.DrawLine(p, point.X, point.Y, point.X, point.Y - 16);
+            g.DrawLine(p, point.X + 24, point.Y, point.X + 24, point.Y + 16);
+            g.DrawLine(p, point.X, point.Y + 16, point.X + 24, point.Y + 16);
+            g.DrawLine(p, point.X, point.Y, point.X, point.Y + 16);
+            g.DrawLine(p, point.X, point.Y, point.X + 12, point.Y-8);
+            g.DrawLine(p, point.X + 24, point.Y, point.X + 12, point.Y - 8);
         }
     }
 }
