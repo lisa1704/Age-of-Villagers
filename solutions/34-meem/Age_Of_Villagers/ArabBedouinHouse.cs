da9838afc316cb914c82d1b8d6e8b741dc13fde4
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-18 21:28:45 +0600

    #10 house can be drawn

--- solutions/34-meem/Age_Of_Villagers/ArabBedouinHouse.cs
+++ solutions/34-meem/Age_Of_Villagers/ArabBedouinHouse.cs
@@ -6,7 +6,16 @@ namespace Age_Of_Villagers
     {
         public void Draw(Point e, Graphics g, Pen p)
         {
-            
+            int x = e.X;
+            int y = e.Y;
+
+            g.DrawLine(p, x - 10, y, x + 10, y);
+            g.DrawLine(p, x, y - 10, x - 10, y);
+            g.DrawLine(p, x, y - 10, x + 10, y);
+
+            g.DrawLine(p, x, y - 10, x + 10, y - 15);
+            g.DrawLine(p, x + 10, y, x + 20, y - 5);
+            g.DrawLine(p, x + 10, y - 15, x + 20, y - 5);
 
 
         }
