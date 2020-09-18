f131ec74ac1f5f07a273242d52ed803e4b2a2623
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-19 01:58:32 +0600

    #10 Arab bedouin class created.

---
+++ solutions/48-zoyee/Age Of Villagers/Arab_Bedouin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_Of_Villagers
+{
+    class Arab_Bedouin : INation
+    {
+        public void Draw_House(Graphics g, Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_Tree(Graphics g, Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_WaterSource(Graphics g, Point point)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
