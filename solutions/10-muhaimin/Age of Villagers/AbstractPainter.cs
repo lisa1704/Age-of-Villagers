826af0dca46d2663b3253d5fe6484c7ee972d848
Muhaimin <47084807+Muhai10@users.noreply.github.com>
2020-09-18 17:07:07 +0600

    #8 #9 #10 #11 #12 made painter hierarchy for nations

---
+++ solutions/10-muhaimin/Age of Villagers/AbstractPainter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_Villagers
+{
+    abstract class AbstractPainter : Ipainter
+    {
+        List<IElement> elements = new List<IElement>();
+        public void draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
