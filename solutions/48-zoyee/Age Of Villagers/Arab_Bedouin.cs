e8f6cb3066d0e14597aac17f45c96a1e74908d0c
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-19 02:34:55 +0600

    #7 #10 #12 Null implemented in Arab bedouin class

--- solutions/48-zoyee/Age Of Villagers/Arab_Bedouin.cs
+++ solutions/48-zoyee/Age Of Villagers/Arab_Bedouin.cs
@@ -8,21 +8,30 @@ namespace Age_Of_Villagers
     class Arab_Bedouin : INation
     {
         Pen p = new Pen(Color.Violet);
+
+        public Color BackGroundColor()
+        {
+            return Color.Yellow;
+        }
+
         public void Draw_House(Graphics g, Point point)
         {
-            g.DrawLine(p, point.X, point.Y, 100, 100);
+            ArabBedouin_House ab_h = new ArabBedouin_House(point);
+            ab_h.Draw(g);
         }
 
         public void Draw_Tree(Graphics g, Point point)
         {
-            g.DrawLine(p, point.X, point.Y, 100, 100);
             ArabBedouin_Tree ab_tr = new ArabBedouin_Tree(point);
             ab_tr.Draw(g);
         }
 
         public void Draw_WaterSource(Graphics g, Point point)
         {
-            throw new NotImplementedException();
+            NullVillage_Item ab_wsNULL = new NullVillage_Item();
+            ab_wsNULL.paint();
         }
+
+
     }
 }
