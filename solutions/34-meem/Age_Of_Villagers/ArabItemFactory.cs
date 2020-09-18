9e0becd42891f98dc5e8a1126680acb85fa5e373
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-18 19:39:09 +0600

    #10 concrete factory class created for arab bedouin village items

---
+++ solutions/34-meem/Age_Of_Villagers/ArabItemFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    class ArabItemFactory : ItemFactory
+    {
+        public override IVillageItem getItem(string button)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
