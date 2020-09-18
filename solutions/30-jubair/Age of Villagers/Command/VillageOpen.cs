3bcb5ef6e4b87f5edbe3c472b7fadc26f6264fae
30-jubair <jobairt03@gmail.com>
2020-09-19 02:22:25 +0600

    #1  #5  #6   Command Pattern Implied & Save and Open Commands implemented Command Interface

---
+++ solutions/30-jubair/Age of Villagers/Command/VillageOpen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_Villagers.Village_Saving
+{
+    public class VillageOpen : ICommand
+    {     
+
+        public void ExecuteCommand()
+        {
+        }
+    }
+}
---
+++ solutions/30-jubair/Age of Villagers/Command/VillageSave.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Age_of_Villagers.Village_Saving
+{
+    public class VillageSave : ICommand
+    {
+        
+        public void ExecuteCommand()
+        {
+        }            
+
+    }
+}
