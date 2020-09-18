7bd8a578b778c03a859298df9194fa573bc133a3
30-jubair <jobairt03@gmail.com>
2020-09-19 04:40:40 +0600

    #1 Decoupled the Village State from the Village Edit

---
+++ solutions/30-jubair/Age of Villagers/Command/VillageEdit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Age_of_Villagers.Nation;
+
+namespace Age_of_Villagers.Village_Saving
+{
+    public class VillageEdit : VillageState
+    
+    {
+
+        public string villageName ;
+
+        public VillageEdit(string villageName)
+        {
+            this.villageName = villageName;
+
+        }
+
+
+        public void ReDrawVillage(Graphics graphics, INation ination)
+        {
+            foreach (Point house in houseList)
+            {
+                ination.GetHouse(graphics, house);
+            }
+
+            foreach (Point tree in treeList)
+            {
+                ination.GetTree(graphics, tree);
+            }
+
+            foreach (Point waterResource in waterResourceList)
+            {
+                ination.GetWaterResource(graphics, waterResource);
+            }
+        }
+
+    }
+}
--- solutions/30-jubair/Age of Villagers/Command/VillageState.cs
+++ solutions/30-jubair/Age of Villagers/Command/VillageState.cs
@@ -7,42 +7,31 @@ using Age_of_Villagers.Nation;
 namespace Age_of_Villagers.Village_Saving
 {
     public class VillageState
-    
     {
+
         public List<Point> houseList = new List<Point>();
         public List<Point> treeList = new List<Point>();
         public List<Point> waterResourceList = new List<Point>();
 
-        public string villageName ;
-
-        public VillageState(string villageName)
-        {
-            this.villageName = villageName;
-
-        }
-
         public void AddHouse(Graphics graphics, Point house, INation ination)
-        
+
         {
             houseList.Add(house);
-            ination.GetHouse(graphics,house);
-
+            ination.GetHouse(graphics, house);
         }
-
 
         public void AddTree(Graphics graphics, Point tree, INation ination)
 
         {
-           treeList.Add(tree);
+            treeList.Add(tree);
             ination.GetTree(graphics, tree);
         }
-
 
         public void AddWaterResource(Graphics graphics, Point waterResource, INation ination)
 
         {
-           waterResourceList.Add(waterResource);
-           ination.GetWaterResource(graphics, waterResource);
+            waterResourceList.Add(waterResource);
+            ination.GetWaterResource(graphics, waterResource);
         }
 
     }
