117b1e3a6f5dfc0f20b4dcf7ae0d399059252886
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-18 11:17:15 +0600

    #11 EgyptianHouse, EgyptianTree and EgyptianWatersource class created

---
+++ solutions/33-nafisa/AgeOfVillagers/EgyptianHouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    class EgyptianHouse : IShape
+    {
+        public void Draw(Point point, Panel Drawingpanel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
