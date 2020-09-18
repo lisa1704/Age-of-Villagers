5dafd65eeaa26c944cff29e596c051179ecca326
shadidintisar <shadidintisar@gmail.com>
2020-09-18 21:53:26 +0600

    #4 Created 600*400 px box for Drawing

--- solutions/59-shadid/Village_Formation/Form1.cs
+++ solutions/59-shadid/Village_Formation/Form1.cs
@@ -26,5 +26,30 @@ namespace Village_Formation
         {
 
         }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
