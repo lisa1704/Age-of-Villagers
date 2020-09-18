0466b514e073dee316404f54d6e7fe281381e01e
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-19 03:13:55 +0600

    #7 BangaldeshiWaterSource class created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillegeItems/BangladeshiWaterSource.cs
@@ -0,0 +1,16 @@
+﻿using AgeOfVillagers.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.VillegeItems
+{
+    class BangladeshiWaterSource : IShape
+    {
+        public void draw(Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
