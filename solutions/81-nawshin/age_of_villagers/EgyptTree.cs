8af5571e34103ea6d2116d5c9e84c6d72b362cad
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-18 12:57:46 +0600

    #1 #7 #11 Resized Egypt tree into 16px * 24px

--- solutions/81-nawshin/age_of_villagers/EgyptTree.cs
+++ solutions/81-nawshin/age_of_villagers/EgyptTree.cs
@@ -20,16 +20,6 @@ namespace age_of_villagers
             Point lr = new Point(pt.X - 2, pt.Y - 22);
             Point ll = new Point(pt.X - 8, pt.Y - 18);
 
-            /*Point p1 = new Point(pt.X + 5, pt.Y - 23);
-            Point p2 = new Point(pt.X - 7, pt.Y - 24);
-            Point p3 = new Point(pt.X + 2, pt.Y - 9);
-            Point p4 = new Point(pt.X + 8, pt.Y - 18);
-            Point p5 = new Point(pt.X + 1, pt.Y - 22);
-            Point p6 = new Point(pt.X - 4, pt.Y - 11);
-            Point p7 = new Point(pt.X - 3, pt.Y - 12);
-            Point p8 = new Point(pt.X - 8, pt.Y - 10);*/
-
-
             AddComponent(new Line(pt, rm));
             AddComponent(new Line(rm, rt));
             AddComponent(new Line(rm, rr));
@@ -39,12 +29,6 @@ namespace age_of_villagers
             AddComponent(new Line(lm, lr));
             AddComponent(new Line(lm, ll));
 
-            //AddComponent(new Line(pt, p1));
-            //AddComponent(new Line(pt, p2));
-            //AddComponent(new Line(p3, p4));
-            //AddComponent(new Line(p3, p5));
-            //AddComponent(new Line(p6, p7));
-            //AddComponent(new Line(p6, p8));
         }
     }
 }
