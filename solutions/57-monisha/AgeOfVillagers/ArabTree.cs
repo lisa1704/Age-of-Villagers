bf04735cce8ac905352a3210783476360be9e667
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2020-09-18 13:02:21 +0600

    #1 #7 #10 Arab tree sized fixed

--- solutions/57-monisha/AgeOfVillagers/ArabTree.cs
+++ solutions/57-monisha/AgeOfVillagers/ArabTree.cs
@@ -46,22 +46,30 @@ namespace AgeOfVillagers
         {
             public ArabTree(Point MPt)
             {
-                Point topRight = new Point(MPt.X + 22, MPt.Y+20);
-                Point bottomRight = new Point(MPt.X + 25, MPt.Y + 65);
-                Point L1topPt = new Point(MPt.X-12 , MPt.Y -25);
-                Point L2topPt = new Point(MPt.X +57, MPt.Y - 25);
+               /* Point topRight = new Point(MPt.X + 22, MPt.Y+20);
+                //Point bottomRight = new Point(MPt.X +22, MPt.Y + 40);
+                Point L1topPt = new Point(MPt.X - 12, MPt.Y - 25);
+                Point L2topPt = new Point(MPt.X + 57, MPt.Y - 25);
                 Point L3topPt = new Point(topRight.X, MPt.Y - 35);
                 Point L4topPt = new Point(MPt.X + 78, MPt.Y - 10);
                 Point L5topPt = new Point(MPt.X - 35, MPt.Y - 8);
-                Point RTopLeft = new Point(MPt.X + 20, MPt.Y + 20);
-               // Point RTopLeft = new Point(MPt.X , MPt.Y );
-
+                Point RTopLeft = new Point(MPt.X + 20, MPt.Y + 20);*/
+                Point RTopLeft = new Point(MPt.X , MPt.Y );
+                Point bottomRight = new Point(MPt.X + 2, MPt.Y + 12);
+                Point RecMid = new Point(MPt.X + 1, MPt.Y );
+                Point L1topPt = new Point(RecMid.X - 5, MPt.Y - 9);
+                Point L2topPt = new Point(RecMid.X + 5, MPt.Y - 9);
+                Point L3topPt = new Point(RecMid.X , MPt.Y - 12);
+                Point L4topPt = new Point(RecMid.X + 8, MPt.Y - 5);
+                Point L5topPt = new Point(RecMid.X - 8, MPt.Y - 5);
+             
                 AddComp(new ShapeRectangle(RTopLeft, bottomRight));
-                AddComp(new Line(RTopLeft, L1topPt));
-                AddComp(new Line(topRight, L2topPt));
-                AddComp(new Line(topRight, L3topPt));
-                AddComp(new Line(topRight, L4topPt));
-                AddComp(new Line(topRight, L5topPt));
+                AddComp(new Line(RecMid, L3topPt));
+                AddComp(new Line(RecMid, L1topPt));
+                AddComp(new Line(RecMid, L2topPt));
+                AddComp(new Line(RecMid, L3topPt));
+                AddComp(new Line(RecMid, L4topPt));
+                AddComp(new Line(RecMid, L5topPt));
             }
         }
 
