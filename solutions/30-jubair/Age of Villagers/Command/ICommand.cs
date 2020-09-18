d02f6804dbf0ae38d7cf2b8a897568def4bf110b
30-jubair <jobairt03@gmail.com>
2020-09-19 02:20:36 +0600

    #1 Implemented the Command Interface for implementing multiple commands

---
+++ solutions/30-jubair/Age of Villagers/Command/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_Villagers.Village_Saving
+{
+    public interface ICommand
+    {
+        void ExecuteCommand();
+    }
+}
