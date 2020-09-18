0204a9be36eebab1b85ac1d1c72de38dbb51c5c6
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-18 07:41:27 +0600

    #1#7 Triangle class created and implemented.

---
+++ solutions/48-zoyee/Age Of Villagers/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_Of_Villagers
+{
+    class Triangle : CompositeShape
+    {
+        public Triangle(Point pt1, Point pt2, Point pt3)
+        {
+            AddComponent(new Line(pt1, pt2));
+            AddComponent(new Line(pt2, pt3));
+            AddComponent(new Line(pt3, pt1));
+        }
+    }
+}
