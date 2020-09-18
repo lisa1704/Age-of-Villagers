26868da521ed82d5cd12a5ea62e9cbb30b21c521
MD ASHIQUR RAHMAN JOY <ashiqurrahman85@iut-dhaka.edu>
2020-09-18 23:21:12 +0600

    #4 basic shape drawing implemented in the Drawing Canvas with minor tweaks in the UI

--- solutions/85-joy/src/project/Controllers/VillageViewController.java
+++ solutions/85-joy/src/project/Controllers/VillageViewController.java
@@ -9,14 +9,22 @@ import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 
 public class VillageViewController implements Initializable {
     @FXML
     private TextField Village_Name;
     @FXML private Button WaterSource = new Button();
+    @FXML private Pane DrawPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -27,6 +35,16 @@ public class VillageViewController implements Initializable {
         try{
 
             System.out.println("NIGGA!");
+//            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("project/Views/VillageView.fxml"));
+//            fxmlLoader.setRoot(this);
+//            fxmlLoader.setController(this);
+//            Canvas canvas = new Canvas(600,400);
+            Circle circle = new Circle(200,200,10,Color.BLACK);
+            DrawPane.getChildren().add(circle);
+
+
+//            g.drawLine(100,200,300,400);
+
         }
         catch (Exception e){
             e.printStackTrace();
