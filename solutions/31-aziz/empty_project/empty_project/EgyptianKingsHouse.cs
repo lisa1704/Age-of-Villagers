18b30b6efb5f5d31dcfc4adc6c5c9bdd702cc191
Asif Aziz <asifaziz2061@gmail.com>
2020-09-18 20:15:28 +0600

    #11 Darw House method implemented in Egyptian Kings House class

--- solutions/31-aziz/empty_project/empty_project/EgyptianKingsHouse.cs
+++ solutions/31-aziz/empty_project/empty_project/EgyptianKingsHouse.cs
@@ -8,5 +8,18 @@ namespace empty_project
 {
     class EgyptianKingsHouse : IHouse
     {
+        public void drawHouse(Axis axis, Panel panel)
+        {
+            Pen pen = new Pen(Color.Black);
+            Graphics graphics = panel.CreateGraphics();
+
+            graphics.DrawLine(pen, new PointF(axis.X,axis.Y), new PointF(axis.X +10, axis.Y + 15));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X -10, axis.Y +11));
+
+            graphics.DrawLine(pen, new PointF(axis.X +10, axis.Y + 15), new PointF(axis.X - 4, axis.Y + 20));
+            graphics.DrawLine(pen, new PointF(axis.X -10, axis.Y +11), new PointF(axis.X - 4, axis.Y + 20));
+
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 4, axis.Y + 20));
+        }
     }
 }
