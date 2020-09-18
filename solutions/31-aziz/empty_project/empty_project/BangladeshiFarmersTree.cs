130f75adcf5e019b3cc47de74a67cd4751674d93
Asif Aziz <asifaziz2061@gmail.com>
2020-09-18 20:44:55 +0600

    #9 Draw Tree method implemented in Bangladeshi Farmers Tree class

--- solutions/31-aziz/empty_project/empty_project/BangladeshiFarmersTree.cs
+++ solutions/31-aziz/empty_project/empty_project/BangladeshiFarmersTree.cs
@@ -8,5 +8,13 @@ namespace empty_project
 {
     class BangladeshiFarmersTree : ITree
     {
+        public void drawTree(Axis axis, Panel panel)
+        {
+            Pen pen = new Pen(Color.Black);
+            Graphics graphics = panel.CreateGraphics();
+
+            graphics.DrawEllipse(pen, axis.X, axis.Y, 20, 20);
+            graphics.DrawEllipse(pen, axis.X + 8, axis.Y + 20, 4, 10);
+        }
     }
 }
