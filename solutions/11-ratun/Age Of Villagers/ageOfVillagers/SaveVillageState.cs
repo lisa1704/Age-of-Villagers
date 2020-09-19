c4f3dddd1797f9da49244d7cfc36408a6f4cbd7a
Ratun <www.ratunrahman@gmail.com>
2020-09-19 19:31:20 +0600

    #5 Create save method

---
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/SaveVillageState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class SaveVillageState
+{
+	MenuForm menuForm = new MenuForm();
+	private string nation;
+	private string villagename;
+	private panel drawingPanel;
+
+	public void setVillage()
+    {
+		nation = menuForm.Nation.Text;
+		villagename = menuForm.VillageName.Text;
+		drawingPanel = menuForm.panel1;
+	}
+
+	public string save()
+    {
+		SaveVillageState state = new SaveVillageState();
+		state.getState(villagename);
+		return "Saved !";
+    }
+}
