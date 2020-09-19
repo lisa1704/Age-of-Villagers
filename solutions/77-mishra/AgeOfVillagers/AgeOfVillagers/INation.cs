2464700a44bfee8ff9212e4bad5ad994a1a72443
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-09-19 17:45:52 +0600

    #9 Interface Ination updated, and BDFarmer class created

--- solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/INation.cs
+++ solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/INation.cs
@@ -6,7 +6,6 @@ namespace AgeOfVillagers
 {
 	public interface INation
 	{
-		
 		public void DrawHouse(Point p, Panel drawingBoard);
 		public void DrawTree(Point p, Panel drawingBoard);
 		public void DrawWaterSource(Point p, Panel drawingBoard);
