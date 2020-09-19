0d2bf2cc864726d04dc03230a0e2d0da3d7b5dc9
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-19 23:47:34 +0600

    committing separate folder

---
+++ solutions/34-meem/Age_Of_Villagers/Factories/BangladeshiItemFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+
+    class BangladeshiItemFactory : ItemFactory
+    {
+        public override IVillageItem getItem(string button)
+        {
+            if (button=="House")
+            {
+                return new BangladeshiHouse();
+            }
+            else if(button == "Tree")
+            {
+                return new BangladeshiTree();
+            }
+            else if (button == "WaterSource")
+            {
+                return new BangladeshiWaterSource();
+            }
+
+            return new NullItem();
+
+        }
+    }
+}
---
+++ solutions/34-meem/Age_Of_Villagers/Factories/InuitItemFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Age_Of_Villagers
+{
+    internal class InuitItemFactory : ItemFactory
+    {
+        public override IVillageItem getItem(string button)
+        {
+            if (button == "House")
+            {
+                return new InuitHunterHouse();
+            }
+            return new NullItem(); ;
+        }
+    }
+}
