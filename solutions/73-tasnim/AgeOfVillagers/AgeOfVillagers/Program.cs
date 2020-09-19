7b94fc8a143cbb4ba83856770c3cd5524628609a
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-19 19:11:57 +0600

    #1 Removed Form1 folder

--- solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Program.cs
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Program.cs
@@ -16,7 +16,6 @@ namespace AgeOfVillagers
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
             Application.Run(new CreateNewVillage());
 
         }
