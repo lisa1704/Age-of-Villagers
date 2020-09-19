8bf128bc81315865e8f9ce1c4c11d0a0000b87ec
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-20 02:49:36 +0600

    #5 VillageSaver class created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillagerSaver.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    class VillagerSaver
+    {
+        private readonly Village village;
+
+        public VillagerSaver(Village village)
+        {
+            this.village = village;
+        }
+        public void saveVillage()
+        {
+        }
+    }
+}
