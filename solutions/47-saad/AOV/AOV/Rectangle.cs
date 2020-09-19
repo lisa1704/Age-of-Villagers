08f04bbd5ca51b1f9d7465533a565ae8acfc87e0
Saad <saadbinjohir@gmail.com>
2020-09-20 03:19:23 +0600

    #2 #7 Rectangle

--- solutions/47-saad/AOV/AOV/Rectangle.cs
+++ solutions/47-saad/AOV/AOV/Rectangle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,18 @@ namespace AOV
 {
     class Rectangle : CompoundShape
     {
-        public Rectangle()
+        private Point a;
+        private Point b;
+        private Point c;
+        private Point d;
+
+        public Rectangle(Point a, Point b, Point c, Point d)
         {
+            addShape(new DrawLine(a,d));
+            addShape(new DrawLine(a, b));
+            addShape(new DrawLine(c, d));
+            addShape(new DrawLine(b, c));
         }
+
     }
 }
