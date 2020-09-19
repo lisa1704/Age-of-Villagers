5dcfe117b5d7b470c923f6609370143dfc02381a
Saad <saadbinjohir@gmail.com>
2020-09-19 22:04:20 +0600

    #4 #8 Nation Factory constructed

---
+++ solutions/47-saad/AOV/AOV/NationFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOV
+{
+    class NationFactory
+    {
+        public INation SelectNation(string typeOfNation)
+        {
+            if(typeOfNation== "Arab Bedouin")
+            {
+                return new ArabNation();
+            }
+            else if (typeOfNation == "Egyptian Kings")
+            {
+                return new EgyptianNation();
+            }
+            else if (typeOfNation == "Bangladeshi Farmers")
+            {
+                return new BangladeshiNation();
+            }
+            else
+            {
+                return new InuitNation();
+            }
+        }
+    }
+}
