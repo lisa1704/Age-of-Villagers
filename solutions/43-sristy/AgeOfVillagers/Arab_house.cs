d2fd7d3e7053e1e78fc95986df5cb9189a1d8fb5
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-19 22:18:53 +0600

    #10 restore the size of arab house and tree

--- solutions/43-sristy/AgeOfVillagers/Arab_house.cs
+++ solutions/43-sristy/AgeOfVillagers/Arab_house.cs
@@ -11,10 +11,10 @@ namespace Age_of_villagers
     {
         public Arab_house(Point mptop)
         {
-            Point rptop = new Point(mptop.X +25, mptop.Y+50);
-            Point lptop = new Point(mptop.X-25, mptop.Y + 50);
-            Point mpback = new Point(mptop.X + 50, mptop.Y - 25);
-            Point rpback = new Point(mptop.X + 75, mptop.Y + 25);
+            Point rptop = new Point(mptop.X +8, mptop.Y+16);
+            Point lptop = new Point(mptop.X-8, mptop.Y + 16);
+            Point mpback = new Point(mptop.X + 16, mptop.Y - 8);
+            Point rpback = new Point(mptop.X + 24, mptop.Y + 8);
             AddComponent(new obj_triangle(lptop, rptop, mptop));
             AddComponent(new obj_rectangle(mptop, mpback, rptop, rpback));
         }
@@ -24,15 +24,15 @@ namespace Age_of_villagers
     {
         public Arab_tree(Point mptop)
         {
-            Point lptop = new Point(mptop.X - 2, mptop.Y);
-            Point rptop = new Point(mptop.X + 2, mptop.Y);
-            Point lpbottom = new Point(mptop.X - 2, mptop.Y + 50);
-            Point rpbottom = new Point(mptop.X + 2, mptop.Y + 50);
-            Point p1 = new Point(mptop.X, mptop.Y - 35);
-            Point p2 = new Point(mptop.X - 25, mptop.Y - 25);
-            Point p3 = new Point(mptop.X + 25, mptop.Y - 25);
-            Point p4 = new Point(mptop.X - 35, mptop.Y - 10);
-            Point p5 = new Point(mptop.X + 35, mptop.Y - 10);
+            Point lptop = new Point(mptop.X - 1, mptop.Y);
+            Point rptop = new Point(mptop.X + 1, mptop.Y);
+            Point lpbottom = new Point(mptop.X - 1, mptop.Y + 16);
+            Point rpbottom = new Point(mptop.X + 1, mptop.Y + 16);
+            Point p1 = new Point(mptop.X, mptop.Y - 11);
+            Point p2 = new Point(mptop.X - 8, mptop.Y - 8);
+            Point p3 = new Point(mptop.X + 8, mptop.Y - 8);
+            Point p4 = new Point(mptop.X - 11, mptop.Y - 3);
+            Point p5 = new Point(mptop.X + 11, mptop.Y - 3);
             AddComponent(new obj_rectangle(lptop, rptop, lpbottom, rpbottom));
             AddComponent(new obj_line(mptop, p1));
             AddComponent(new obj_line(mptop, p2));
