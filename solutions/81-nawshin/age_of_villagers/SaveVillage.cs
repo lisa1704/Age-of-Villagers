9bd2305e7d1cefd9815fa0b98dd982cf36f98506
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-19 10:23:00 +0600

    #5 SaveVillage class is created

---
+++ solutions/81-nawshin/age_of_villagers/SaveVillage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace age_of_villagers
+{
+    public class SaveVillage
+    {
+
+    }
+    }
+    
+
