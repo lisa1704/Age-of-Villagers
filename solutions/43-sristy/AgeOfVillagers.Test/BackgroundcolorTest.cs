b7ed4d34fd39d893c20f3324d3a55a3591bf304a
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 00:42:03 +0600

    #1 refactoring

---
+++ solutions/43-sristy/AgeOfVillagers.Test/BackgroundcolorTest.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using Age_of_villagers;
+using System.Drawing;
+
+namespace AgeOfVillagers.Test
+{
+    public class BackgroundcolorTest
+    {
+        [Fact]
+        public void Bangladeshibackgroundcolor()
+        {
+            INation nation = new Bangladeshi();
+            Color expected = Color.LightGreen;
+            Color actual = nation.get_backcolor();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Egyptianbackgroundcolor()
+        {
+            INation nation = new Egyptian();
+            Color expected = Color.LightYellow;
+            Color actual = nation.get_backcolor();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Arabianbackgroundcolor()
+        {
+            INation nation = new Arab();
+            Color expected = Color.PaleGoldenrod;
+            Color actual = nation.get_backcolor();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Inuitianbackgroundcolor()
+        {
+            INation nation = new Inuit();
+            Color expected = Color.Snow;
+            Color actual = nation.get_backcolor();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- solutions/43-sristy/AgeOfVillagers.Test/UnitTest1.cs
+++ solutions/43-sristy/AgeOfVillagers.Test/UnitTest1.cs
@@ -2,7 +2,6 @@ using Xunit;
 using Age_of_villagers;
 using System.Drawing;
 
-
 namespace AgeOfVillagers.Test
 {
     public class UnitTest1
@@ -13,42 +12,7 @@ namespace AgeOfVillagers.Test
             int var = 2;
             var expectedValue = 2;
             Assert.Equal(expectedValue, var);
-        }
-
-        [Fact]
-        public void Test2()
-        {
-            INation nation = new Bangladeshi();
-            Color expected = Color.LightGreen;
-            Color actual = nation.get_backcolor();
-            Assert.Equal(expected,actual);
-        }
-        [Fact]
-        public void Test3()
-        {
-            INation nation = new Egyptian();
-            Color expected = Color.LightYellow;
-            Color actual = nation.get_backcolor();
-            Assert.Equal(expected, actual);
-        }
-        [Fact]
-        public void Test4()
-        {
-            INation nation = new Arab();
-            Color expected = Color.PaleGoldenrod;
-            Color actual = nation.get_backcolor();
-            Assert.Equal(expected, actual);
-        }
-        [Fact]
-        public void Test5()
-        {
-            INation nation = new Inuit();
-            Color expected = Color.Snow;
-            Color actual = nation.get_backcolor();
-            Assert.Equal(expected, actual);
-        }
-
-
+        }        
 
     }
 
