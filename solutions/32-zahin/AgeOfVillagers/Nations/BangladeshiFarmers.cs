82ae8754baf233ab68a2213ec8a08de2055eb504
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-19 16:09:01 +0600

    #9 water resource can be drawn

--- solutions/32-zahin/AgeOfVillagers/Nations/BangladeshiFarmers.cs
+++ solutions/32-zahin/AgeOfVillagers/Nations/BangladeshiFarmers.cs
@@ -43,8 +43,23 @@ namespace AgeOfVillagers
 
         public void DrawWaterSource(Graphics g, Point point)
         {
-            var message = new StringBuilder();
-            MessageBox.Show("Drawing Water for Bangladeshi Farmers");
+            Point p1 = new Point(point.X - 25, point.Y + 50);
+            Point p2 = new Point(point.X, point.Y + 75);
+            Point p3 = new Point(point.X + 25, point.Y + 50);
+            Point p4 = new Point(point.X + 50, point.Y + 75);
+            Point p5 = new Point(point.X + 75, point.Y + 25);
+            Point p6 = new Point(point.X + 50, point.Y);
+            Point p7 = new Point(point.X + 25, point.Y + 25);
+            Point p8 = new Point(point.X + 25, point.Y);
+            g.DrawLine(pen, point, p1);
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p2, p3);
+            g.DrawLine(pen, p3, p4);
+            g.DrawLine(pen, p4, p5);
+            g.DrawLine(pen, p5, p6);
+            g.DrawLine(pen, p6, p7);
+            g.DrawLine(pen, p7, p8);
+            g.DrawLine(pen, p8, point);
         }
     }
 }
