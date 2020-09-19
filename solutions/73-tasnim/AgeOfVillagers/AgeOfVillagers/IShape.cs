0b293e7da52486ae8b88a3647a8e14f4674932e7
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-20 01:10:18 +0600

    #7 Interface for the shape of the  objects are created

---
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/IShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    interface IShape
+    {
+        void Draw_Shape(Graphics g);
+    }
+}
