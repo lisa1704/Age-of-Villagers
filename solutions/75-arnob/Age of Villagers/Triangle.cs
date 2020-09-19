5c23d32e5677f6334bf87938cc23ddfa33a84fbe
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 04:22:40 +0600

    #9 #10 #11 triangle class created

---
+++ solutions/75-arnob/Age of Villagers/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_Villagers
+{
+    class Triangle : CompositeShaping
+    {
+        public Triangle(Point top, Point baseLeft, Point baseRight)
+        {
+            AddComponent(new Line(top, baseLeft));
+            AddComponent(new Line(baseLeft, baseRight));
+            AddComponent(new Line(baseRight, top));
+        }
+    }
+}
