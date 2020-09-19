2a1e658395ebbd5b88a250c3a166f2ca09f5e70a
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 00:43:11 +0600

    #9 bangladeshi nation creation unit test added

---
+++ solutions/43-sristy/AgeOfVillagers.Test/NationTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Age_of_villagers;
+using System.Drawing;
+
+namespace AgeOfVillagers.Test
+{
+    public class NationTest
+    {
+        Nationfactory nationfactory = new Nationfactory();
+        [Fact]
+        public void BangladeshinationTest()
+        {
+            INation actual = nationfactory.GetNation("Bangladeshi");
+            INation expected = new Bangladeshi();
+            actual.Equals(expected);
+        }
+    }
+}
