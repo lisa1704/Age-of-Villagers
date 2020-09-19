7f2a054c9dd197418364c32e6e05d0afe98f409a
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-19 13:55:05 +0600

    #9#10#11#12 a factory class created to create nation with specific property

---
+++ solutions/34-meem/Age_Of_Villagers/NationPropertyFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    class NationPropertyFactory
+    {
+        public  INation getNation(string nation)
+        {
+            if (nation == "Bangladeshi Farmers")
+            {
+                return new BangladeshiNation();
+            }
+            
+            else if (nation == "Arab Bedouins")
+            {
+                return new ArabBedouinNation();
+            }
+            else if (nation == "Egyptian King")
+            {
+                return new EgyptianKingNation();
+            }
+
+            else if (nation == "Inuit Hunters")
+            {
+                return new InuitHunterNation();
+            }
+
+
+
+
+
+            return null;
+        }
+    }
+}
