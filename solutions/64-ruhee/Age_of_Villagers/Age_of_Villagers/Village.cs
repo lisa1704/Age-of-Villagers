d0c4c3af0f0bb77c2c065a98bb532403eff3e8bd
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-19 19:01:53 +0600

    #7 created interfaces for houses, trees and water sources. #4 village class will collect name of the village and elements.

--- solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/Village.cs
+++ solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/Village.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Microsoft.Office.Core;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Age_of_Villagers.Age_of_Villagers
 {
     class Village
     {
+        public string villageName = "untitled";
+        public List<Axes> houses = new List<Axes>();
+        public List<Axes> trees = new List<Axes>();
+        public List<Axes> waterS = new List<Axes>();
     }
 }
