4f9f86139e39b42a7eaeafa3fe57a2dd725a88d4
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 03:50:48 +0600

    #1 #9 #10 #11 #12 line class updated and refactored

--- solutions/75-arnob/Age of Villagers/Line.cs
+++ solutions/75-arnob/Age of Villagers/Line.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace Age_of_Villagers
 {
     class Line
     {
+        private readonly Point _point1;
+        private readonly Point _point2;
+
+        public Line(Point point1, Point point2)
+        {
+            _point1 = point1;
+            _point2 = point2;
+        }
+
+        public void Paint(Graphics graphics)
+        {
+            var pen = new Pen(Color.Black, 1);
+            graphics.DrawLine(pen, _point1, _point2);
+        }
     }
 }
