215bd34360daf410db6897e2a1f9ecb8acdb2bf9
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-20 04:40:27 +0600

    #10 added functionality of makeShape() method and drawSHape() method in Paralellogram class

--- solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/Drawable Shape Extending Classes/Paralellogram.cs
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/Drawable Shape Extending Classes/Paralellogram.cs
@@ -8,18 +8,22 @@ namespace AgeOfVillagers.Drawable_Shape_Extending_Classes
 {
     class Paralellogram : DrawableShapes
     {
-        private Graphics g;
+        private Graphics graphics;
         private Pen pen;
         private Point topLeftPoint;
         private Point bottomLeftPoint;
         private Point topRightPoint;
         private Point bottomRightPoint;
         private int width;
+        private DrawableShapes rightLine;
+        private DrawableShapes leftLine;
+        private DrawableShapes topLine;
+        private DrawableShapes bottomLine;
 
-     
-        public Paralellogram(Graphics g, Pen pen, Point topLeftPoint, Point bottomLeftPoint, int width)
+
+        public Paralellogram(Graphics graphics, Pen pen, Point topLeftPoint, Point bottomLeftPoint, int width)
         {
-            this.g = g;
+            this.graphics = graphics;
             this.pen = pen;
             this.topLeftPoint = topLeftPoint;
             this.bottomLeftPoint = bottomLeftPoint;
@@ -29,12 +33,23 @@ namespace AgeOfVillagers.Drawable_Shape_Extending_Classes
 
         public override void drawShape()
         {
-            
+            topLine.drawShape();
+            bottomLine.drawShape();
+            leftLine.drawShape();
+            rightLine.drawShape();
         }
 
         public override void makeShape()
         {
-            
+            topRightPoint = new Point(topLeftPoint.X + width, topLeftPoint.Y);
+            bottomRightPoint= new Point(bottomLeftPoint.X + width, bottomLeftPoint.Y);
+
+            topLine = base.getLines(topLeftPoint, topRightPoint, graphics, pen);
+            leftLine= base.getLines(topLeftPoint, bottomLeftPoint, graphics, pen);
+            rightLine= base.getLines(topRightPoint, bottomRightPoint, graphics, pen);
+            bottomLine= base.getLines(bottomLeftPoint, bottomRightPoint, graphics, pen);
+            drawShape();
+
         }
     }
 }
