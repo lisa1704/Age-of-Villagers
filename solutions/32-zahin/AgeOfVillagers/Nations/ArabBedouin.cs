527abb7c840bc1af884a49f977efc8d616ebad61
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-19 16:24:26 +0600

    #10 tree for Arab

--- solutions/32-zahin/AgeOfVillagers/Nations/ArabBedouin.cs
+++ solutions/32-zahin/AgeOfVillagers/Nations/ArabBedouin.cs
@@ -32,7 +32,22 @@ namespace AgeOfVillagers
 
         public void DrawTree(Graphics g, Point point)
         {
-            MessageBox.Show("Drawing Tree for Arab Bedouin");
+            Point rectanglpoint1 = new Point(point.X - 2, point.Y);
+            Point rectanglepoint2 = new Point(point.X + 2, point.Y);
+            Point rectanglepoint3 = new Point(point.X - 2, point.Y + 50);
+            Point rectanglepoint4 = new Point(point.X + 2, point.Y + 50);
+            Point p1 = new Point(point.X, point.Y - 35);
+            Point p2 = new Point(point.X - 25, point.Y - 25);
+            Point p3 = new Point(point.X + 25, point.Y - 25);
+            Point p4 = new Point(point.X - 35, point.Y - 10);
+            Point p5 = new Point(point.X + 35, point.Y - 10);
+            Rectangles rectangle = new Rectangles(g, pen, rectanglpoint1, rectanglepoint2, rectanglepoint3, rectanglepoint4);
+            rectangle.DrawShape();
+            g.DrawLine(pen, point, p1);
+            g.DrawLine(pen, point, p2);
+            g.DrawLine(pen, point, p3);
+            g.DrawLine(pen, point, p4);
+            g.DrawLine(pen, point, p5);
         }
         public void DrawWaterSource(Graphics g, Point point)
         {
