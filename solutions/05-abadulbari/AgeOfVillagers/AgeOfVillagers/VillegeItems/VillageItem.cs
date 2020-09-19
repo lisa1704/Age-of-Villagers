bfaa301936f943a49c2e5674a1bf53d1a08f557c
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-20 00:53:15 +0600

    #7 #5 abstract VillageItem created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillegeItems/VillageItem.cs
@@ -0,0 +1,26 @@
+﻿using AgeOfVillagers.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.VillegeItems
+{
+    public abstract class VillageItem : IShape
+    {
+        private readonly List<IShape> items;
+        public VillageItem()
+        {
+            items = new List<IShape>();
+        }
+
+        public void addItem(IShape shape)
+        {
+            items.Add(shape);
+        }
+        public void draw(Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
