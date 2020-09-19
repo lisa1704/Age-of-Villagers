a6238981a267b4899bbd1be643cc341b68953bac
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-19 23:21:13 +0600

    #8 #9 #10 #11 #12  factory Nation class created

---
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/FactoryNation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class FactoryNation
+    {
+        public INation NationType(string Inputnation)
+        {
+            if (Inputnation == "Bangladeshi Farmers")
+            {
+                return new BangladeshiFarmers();
+            }
+            else if (Inputnation == "Arab Bedouin")
+            {
+                return new ArabBedouin();
+            }
+            else if (Inputnation == "Egyptian Kings")
+            {
+                return new EgyptianKings();
+            }
+            else if (Inputnation == "Inuit Hunters")
+            {
+                return new InuitHunters();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
