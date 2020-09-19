fe19c8c95496b45ea0ac2561877df37698a1705a
86-Ashik <nusayerashik@iut-dhaka.edu>
2020-09-19 18:34:26 +0600

    #10 arab bedouins tree implemented with constraints

--- solutions/86-Ashik/DesignPatternLab/AgeOfVillager/ArabBedouinsTree.cs
+++ solutions/86-Ashik/DesignPatternLab/AgeOfVillager/ArabBedouinsTree.cs
@@ -19,8 +19,11 @@ namespace AgeOfVillager
         {
             x = e.X;
             y = e.Y;
-            Rectangle rect = new Rectangle(x, y, 4, 24);
-            g.DrawRectangle(myPen, rect);
+            g.DrawLine(myPen, x, y, x + 4, y);
+            g.DrawLine(myPen, x, y, x, y+24);
+            g.DrawLine(myPen, x, y+24, x + 4, y+24);
+            g.DrawLine(myPen, x+4, y, x + 4, y+24);
+            
             g.DrawLine(myPen, x, y, x - 24, y - 24);
             g.DrawLine(myPen, x+1, y, x - 12, y - 24);
             g.DrawLine(myPen, x+2, y, x + 12, y - 24);
