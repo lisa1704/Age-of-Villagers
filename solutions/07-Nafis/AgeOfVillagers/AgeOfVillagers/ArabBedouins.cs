35953ec69eae80c37c85cc7d56ddb5ea274d7097
nafis10670 <nafissazad@gmail.com>
2020-09-20 03:31:05 +0600

    #10 Implemented Arab Bedouins.

--- solutions/07-Nafis/AgeOfVillagers/AgeOfVillagers/ArabBedouins.cs
+++ solutions/07-Nafis/AgeOfVillagers/AgeOfVillagers/ArabBedouins.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace AgeOfVillagers
@@ -12,19 +13,29 @@ namespace AgeOfVillagers
         
         public void DrawHouse(int x, int y, Graphics g)
         {
-            Point[] points = { new Point(x, y), new Point(x + 15, y + 6), new Point(x + 20, y - 3), new Point(x + 9, y - 12) };
-            g.DrawPolygon(pen, points);
-            g.DrawLine(pen, new Point(x + 15, y + 6), new Point(x + 9, y - 12));
+            Point[] triangle = { new Point(x, y), new Point(x - 20, y + 25), new Point(x + 20, y + 25) };
+            //Point[] rectangle = { new Point(x, y), new Point(x + 14, y + 14), new Point(x + 24, y + 29), new Point(x + 20, y + 25) };
+            g.DrawPolygon(pen, triangle);
+            g.DrawLine(pen, new Point(x, y), new Point(x + 12, y - 12));
+            g.DrawLine(pen, new Point(x + 12, y - 12), new Point(x + 32, y + 13));
+            g.DrawLine(pen, new Point(x + 20, y + 25), new Point(x + 32, y + 13));
         }
 
         public void DrawTree(int x, int y, Graphics g)
         {
-            throw new NotImplementedException();
+            g.DrawLine(pen, new Point(x, y), new Point(x, y - 30)); //mid
+            g.DrawLine(pen, new Point(x, y), new Point(x - 13, y - 22)); //mid left
+            g.DrawLine(pen, new Point(x, y), new Point(x - 18, y - 13)); //left
+            g.DrawLine(pen, new Point(x, y), new Point(x + 11, y - 22)); //mid right
+            g.DrawLine(pen, new Point(x, y), new Point(x + 25, y - 22)); //right
+
+            g.DrawRectangle(pen, x - 2, y, 4, 30);
+
         }
 
         public void DrawWater(int x, int y, Graphics g)
         {
-            throw new NotImplementedException();
+            g.DrawLine(pen, new Point(10, 10), new Point(10, 10));
         }
     }
 }
