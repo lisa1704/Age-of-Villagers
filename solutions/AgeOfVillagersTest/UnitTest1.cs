ba2a5f94be6999e1a13045170013d05cc6de19f0
Maysha Afrin Munia <mayshaafrin@iut-dhaka.edu>
2020-09-19 20:02:29 +0600

    # Test project created

---
+++ solutions/AgeOfVillagersTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgeOfVillagersTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
