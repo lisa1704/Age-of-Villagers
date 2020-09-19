eae96bbe2d3dd12f738b660cd651f918ff4a4d94
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-19 06:43:56 +0600

    #4 #7 #8 #9 #10 #11 #12  Nation Interface created

---
+++ solutions/75-arnob/Age of Villagers/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_Villagers
+{
+    public interface INation
+    {
+        String getName();
+        Colour getTerrainColour();
+        void GetTree(Graphics graphics, Point _point);
+        void GetHouse(Graphics graphics, Point _point);
+        void GetWaterSource(Graphics graphics, Point _point);
+    }
+}
