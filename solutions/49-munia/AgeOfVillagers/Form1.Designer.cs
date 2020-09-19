e1c8a3f243a1eda3755d40575a58338ce6c90a4c
Maysha Afrin Munia <mayshaafrin@iut-dhaka.edu>
2020-09-19 23:47:52 +0600

    #4 iten buttons placed

--- solutions/49-munia/AgeOfVillagers/Form1.Designer.cs
+++ solutions/49-munia/AgeOfVillagers/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(748, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tree";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(842, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "House";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(772, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Water Source";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 362);
+            this.ClientSize = new System.Drawing.Size(972, 548);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +76,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
