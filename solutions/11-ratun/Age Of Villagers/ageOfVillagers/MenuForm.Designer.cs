18fa532f11c1d182ace557c0f1d29242a8d1b965
Ratun <www.ratunrahman@gmail.com>
2020-09-19 14:14:59 +0600

    #4 create buttons and panels

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/MenuForm.Designer.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/MenuForm.Designer.cs
@@ -28,19 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.Tree = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ageOfVillagers = new System.Windows.Forms.Label();
+            this.SaveVillage = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.VillageName = new System.Windows.Forms.TextBox();
+            this.Nation = new System.Windows.Forms.TextBox();
+            this.House = new System.Windows.Forms.Button();
+            this.Water = new System.Windows.Forms.Button();
+            this.NewVillage = new System.Windows.Forms.Button();
+            this.OpenVillage = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 400);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // Tree
+            // 
+            this.Tree.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Tree.Location = new System.Drawing.Point(35, 187);
+            this.Tree.Name = "Tree";
+            this.Tree.Size = new System.Drawing.Size(75, 23);
+            this.Tree.TabIndex = 1;
+            this.Tree.Text = "Tree";
+            this.Tree.UseVisualStyleBackColor = true;
+            this.Tree.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.OpenVillage);
+            this.panel1.Controls.Add(this.NewVillage);
+            this.panel1.Controls.Add(this.Water);
+            this.panel1.Controls.Add(this.House);
+            this.panel1.Controls.Add(this.Nation);
+            this.panel1.Controls.Add(this.VillageName);
+            this.panel1.Controls.Add(this.SaveVillage);
+            this.panel1.Controls.Add(this.ageOfVillagers);
+            this.panel1.Controls.Add(this.Tree);
+            this.panel1.Location = new System.Drawing.Point(601, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 400);
+            this.panel1.TabIndex = 2;
+            // 
+            // ageOfVillagers
+            // 
+            this.ageOfVillagers.AutoSize = true;
+            this.ageOfVillagers.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageOfVillagers.Location = new System.Drawing.Point(43, 29);
+            this.ageOfVillagers.Name = "ageOfVillagers";
+            this.ageOfVillagers.Size = new System.Drawing.Size(162, 24);
+            this.ageOfVillagers.TabIndex = 0;
+            this.ageOfVillagers.Text = "Age Of Villagers";
+            this.ageOfVillagers.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // SaveVillage
+            // 
+            this.SaveVillage.Location = new System.Drawing.Point(74, 327);
+            this.SaveVillage.Name = "SaveVillage";
+            this.SaveVillage.Size = new System.Drawing.Size(124, 23);
+            this.SaveVillage.TabIndex = 1;
+            this.SaveVillage.Text = "Save Village";
+            this.SaveVillage.UseVisualStyleBackColor = true;
+            this.SaveVillage.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 400);
+            this.panel2.TabIndex = 3;
+            // 
+            // VillageName
+            // 
+            this.VillageName.Location = new System.Drawing.Point(83, 76);
+            this.VillageName.Name = "VillageName";
+            this.VillageName.Size = new System.Drawing.Size(100, 22);
+            this.VillageName.TabIndex = 2;
+            // 
+            // Nation
+            // 
+            this.Nation.Location = new System.Drawing.Point(47, 105);
+            this.Nation.Name = "Nation";
+            this.Nation.Size = new System.Drawing.Size(186, 22);
+            this.Nation.TabIndex = 3;
+            // 
+            // House
+            // 
+            this.House.Location = new System.Drawing.Point(144, 186);
+            this.House.Name = "House";
+            this.House.Size = new System.Drawing.Size(75, 23);
+            this.House.TabIndex = 4;
+            this.House.Text = "House";
+            this.House.UseVisualStyleBackColor = true;
+            // 
+            // Water
+            // 
+            this.Water.Location = new System.Drawing.Point(74, 216);
+            this.Water.Name = "Water";
+            this.Water.Size = new System.Drawing.Size(109, 23);
+            this.Water.TabIndex = 5;
+            this.Water.Text = "Water Source";
+            this.Water.UseVisualStyleBackColor = true;
+            // 
+            // NewVillage
+            // 
+            this.NewVillage.Location = new System.Drawing.Point(16, 365);
+            this.NewVillage.Name = "NewVillage";
+            this.NewVillage.Size = new System.Drawing.Size(94, 23);
+            this.NewVillage.TabIndex = 6;
+            this.NewVillage.Text = "New Village";
+            this.NewVillage.UseVisualStyleBackColor = true;
+            // 
+            // OpenVillage
+            // 
+            this.OpenVillage.Location = new System.Drawing.Point(142, 365);
+            this.OpenVillage.Name = "OpenVillage";
+            this.OpenVillage.Size = new System.Drawing.Size(105, 23);
+            this.OpenVillage.TabIndex = 7;
+            this.OpenVillage.Text = "Open Village";
+            this.OpenVillage.UseVisualStyleBackColor = true;
             // 
             // MenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(992, 515);
+            this.ClientSize = new System.Drawing.Size(860, 400);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitter1);
             this.Name = "MenuForm";
             this.Text = "MenuForm";
+            this.Load += new System.EventHandler(this.MenuForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Button Tree;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label ageOfVillagers;
+        private System.Windows.Forms.Button SaveVillage;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox Nation;
+        private System.Windows.Forms.TextBox VillageName;
+        private System.Windows.Forms.Button Water;
+        private System.Windows.Forms.Button House;
+        private System.Windows.Forms.Button OpenVillage;
+        private System.Windows.Forms.Button NewVillage;
     }
 }
--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/MenuForm.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/MenuForm.cs
@@ -16,5 +16,25 @@ namespace ageOfVillagers
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            global::ageOfVillagers.save();
+        }
+
+        private void MenuForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
