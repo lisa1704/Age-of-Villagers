c0c0793edcceec65841e692e3f1ea0433a15563a
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-19 23:10:26 +0600

    #7 Rectangle created to draw.

--- solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/Rectangle.cs
+++ solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/Rectangle.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Age_of_Villagers
 {
-    class Rectangle
+    class Rectangle: CShape
     {
+        public Rectangle(Point pt1, Point pt2, Point pt3,Point pt4)
+        {
+            AddComponent(new Line(pt1, pt2));
+            AddComponent(new Line(pt2, pt3));
+            AddComponent(new Line(pt3, pt4));
+            AddComponent(new Line(pt4, pt1));
+        }
     }
 }
