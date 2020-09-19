78064fb45a9956d4babb78914c432dfa93911584
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-20 01:17:54 +0600

    #8 #9 #10 #11 #12 Factory implemented

--- solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/INation.cs
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/INation.cs
@@ -10,4 +10,30 @@ namespace AgeofVillagers
         void DrawTree();
         void DrawWaterSource();
     }
+
+    internal class FactoryPattern
+    {
+        public INation GetNation(string type)
+        {
+            if (type == "BangladeshiFarmers")
+            {
+                return new BangladeshiFarmers();
+            }
+            else if (type == "EgyptianKings")
+            {
+                return new EgyptianKings();
+            }
+
+            else if (type == "ArabBedouin")
+            {
+                return new ArabBedouin();
+            }
+
+            else if (type == "InuitHunters")
+            {
+                return new InuitHunters();
+            }
+            throw new Exception("No nation selected!");
+        }
+    }
 }
