497ce15b8160d2e90f0e13bc7a01b4678b326989
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 03:35:00 +0600

    #9 bangladeshi house unit test

--- solutions/43-sristy/AgeOfVillagers.Test/Bangladeshiobjecttesting.cs
+++ solutions/43-sristy/AgeOfVillagers.Test/Bangladeshiobjecttesting.cs
@@ -6,11 +6,13 @@ namespace AgeOfVillagers.Test
 {
     public class Bangladeshiobjecttesting
     {
+        Nationfactory nationfactory = new Nationfactory();
         [Fact]
         public void BangladeshinationTest()
         {
             Point lptop = new Point(200, 100);
-            IObject actual = new Bangladeshi_house(lptop);
+            IObject actual = nationfactory.GetNation("Bangladeshi").Draw_house(lptop);
+            IObject expected = new Bangladeshi_house(lptop);
             actual.Equals(expected);
         }
     }
