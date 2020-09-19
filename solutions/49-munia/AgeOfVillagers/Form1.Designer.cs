90b18dd57a25f02dc73c2348e1e47f4cf8633630
Maysha Afrin Munia <mayshaafrin@iut-dhaka.edu>
2020-09-19 23:58:53 +0600

    #4 All 6 buttons placed

--- solutions/49-munia/AgeOfVillagers/Form1.Designer.cs
+++ solutions/49-munia/AgeOfVillagers/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -51,21 +54,54 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "House";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(772, 276);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(129, 23);
+            this.button3.Size = new System.Drawing.Size(129, 31);
             this.button3.TabIndex = 2;
             this.button3.Text = "Water Source";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(772, 335);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 33);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Save Village";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(734, 388);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 34);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "New Village";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(847, 388);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 34);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Open Village";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 548);
+            this.ClientSize = new System.Drawing.Size(982, 453);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -80,6 +116,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- solutions/49-munia/AgeOfVillagers/Form1.cs
+++ solutions/49-munia/AgeOfVillagers/Form1.cs
@@ -21,5 +21,20 @@ namespace AgeOfVillagers
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
