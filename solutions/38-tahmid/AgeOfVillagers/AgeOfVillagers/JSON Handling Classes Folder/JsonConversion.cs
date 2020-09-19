052ed697023e11fe7e57a52871f0c585095f25b8
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-19 15:52:39 +0600

    #1 #5 #6 added JsonConversion class containing two methods name serialize() and deserialize() in JSON Handling Classes Folder

---
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/JSON Handling Classes Folder/JsonConversion.cs
@@ -0,0 +1,30 @@
+﻿using AgeOfVillagers.Model_Class_Folder;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers.JSON_Handling_Classes_Folder
+{
+    class JsonConversion
+    {
+        public string serialize(StateModel currentState)
+        {
+            string jsonData = JsonConvert.SerializeObject(currentState);
+            return jsonData;
+        }
+
+        public StateModel deserialize(string jsonData)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+
+
+            StateModel  previouslySavedState = JsonConvert.DeserializeObject<StateModel>(jsonData, settings);
+            return previouslySavedState;
+        }
+    }
+}
