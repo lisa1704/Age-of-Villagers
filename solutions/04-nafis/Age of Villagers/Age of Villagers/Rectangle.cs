dbc97100e16b22da041394afd17ddb7ccdf37ce6
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-20 04:15:15 +0600

    #9 #10 #11 #12 Rectangle Drawing is completed

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    public class Rectangle : CompositeShape
+    {
+        private readonly Point point1;
+        private readonly Point point2;
+        private readonly Point point3;
+        private readonly Point point4;
+
+        public Rectangle(Point point1, Point point2, Point point3,Point point4)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            this.point3 = point3;
+            this.point4 = point4;
+
+            ShapePush(new Line(point1, point2));
+            ShapePush(new Line(point2, point3));
+            ShapePush(new Line(point3, point4));
+            ShapePush(new Line(point4, point1));
+        }
+    }
+}
