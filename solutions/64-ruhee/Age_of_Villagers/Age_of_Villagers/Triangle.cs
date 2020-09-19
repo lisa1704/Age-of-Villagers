319edfb7372dde667f2e3a1b06119f6a9b37628a
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-19 23:13:05 +0600

    #7 triangle created to draw.

--- solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/Triangle.cs
+++ solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/Triangle.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Age_of_Villagers
 {
-    class triangle
+    class Triangle : CShape
     {
+        public Triangle(Point pt1, Point pt2, Point pt3)
+        {
+            AddComponent(new Line(pt1, pt2));
+            AddComponent(new Line(pt2, pt3));
+            AddComponent(new Line(pt3, pt1));
+        }
     }
 }
