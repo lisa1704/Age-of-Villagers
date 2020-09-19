6922ccea51942c369566996d41c95625e9e730fc
Ratun <www.ratunrahman@gmail.com>
2020-09-19 18:59:30 +0600

    #5 create class village

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/SaveState.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/SaveState.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 
-public class Class1
+public class SaveState
 {
-	public Class1()
-	{
-	}
+	
 }
--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Village.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Village.cs
@@ -2,5 +2,11 @@
 
 public class Village
 {
-	
+    private Nation nation;
+	private string villagename;
+	public Village(Nation nation, string villagename)
+    {
+        this.nation = nation;
+        this.villagename = villagename;
+    }
 }
