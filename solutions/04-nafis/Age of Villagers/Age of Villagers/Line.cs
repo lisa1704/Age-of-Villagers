99f3765b494de6bcf7c061c0c1e72e517b0d5504
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-20 01:28:34 +0600

    #9 #10 #11 #12 Line Class Error is fixed

--- solutions/04-nafis/Age of Villagers/Age of Villagers/Line.cs
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/Line.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers
 {
-    class Line
+    class Line : IShape
     {
+        private readonly Point point1;
+        private readonly Point point2;
+
+        public Line(Point x1, Point x2)
+        {
+            this.point1 = x1;
+            this.point2 = x2;
+        }
+
+        public void draw(Graphics graphics, Pen pen)
+        {
+            graphics.DrawLine(pen,point1,point2);
+        }
     }
 }
