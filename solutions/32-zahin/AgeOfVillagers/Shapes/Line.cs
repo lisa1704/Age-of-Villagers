3a83886c976e0cc1f575f5596cab053e26e7a1b4
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-19 15:58:54 +0600

    #7 line defined

--- solutions/32-zahin/AgeOfVillagers/Shapes/Line.cs
+++ solutions/32-zahin/AgeOfVillagers/Shapes/Line.cs
@@ -9,9 +9,20 @@ namespace AgeOfVillagers.Shapes
 {
     class Line : IShape
     {
+        Point p1;
+        Point p2;
+        Pen pen;
+        Graphics g;
+        public Line(Graphics g, Pen pen, Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.pen = pen;
+            this.g = g;
+        }
         public void DrawShape()
         {
-            //g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p1, p2);
         }
     }
 }
