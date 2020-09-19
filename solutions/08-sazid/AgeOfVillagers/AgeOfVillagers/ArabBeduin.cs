a128d7f7cb3d118fdd7f36a10ba019f56135395f
ESD <ESD@DESKTOP-SNRCVRJ>
2020-09-19 16:34:51 +0600

    #10 Arab beduin nation added

---
+++ solutions/08-sazid/AgeOfVillagers/AgeOfVillagers/ArabBeduin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class ArabBeduin : INation
+    {
+        public void DrawHouse(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterource(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
