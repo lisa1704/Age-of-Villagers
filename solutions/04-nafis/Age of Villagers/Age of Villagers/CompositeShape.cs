b3f87d7e5d33c8f144636f1ed5dff97567dd9378
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-20 03:54:38 +0600

    #9 #10 #11 #12 Composite Pattern Implemented

--- solutions/04-nafis/Age of Villagers/Age of Villagers/CompositeShape.cs
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/CompositeShape.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers
 {
-    class CompositeShape
+    public abstract class CompositeShape : IShape
     {
+        private List<IShape> ShapeStack = new List<IShape>();
+
+        public void ShapePush(IShape shape)
+        {
+            ShapeStack.Add(shape);
+        }
+        public void draw(Graphics graphics, Pen pen)
+        {
+            foreach( var shape in ShapeStack)
+            {
+                shape.draw(graphics, pen);
+            }
+        }
     }
 }
