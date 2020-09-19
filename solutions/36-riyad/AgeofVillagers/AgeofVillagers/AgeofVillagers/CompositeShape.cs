3742357cab9d4d611d08493455326ce3e0b1cfc7
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-20 03:03:31 +0600

    #1 #7 Composite Shape implemented

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/CompositeShape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class CompositeShape : IShapeItem
+    {
+        private List<IShapeItem> Components;
+
+        public CompositeShape()
+        {
+            Components = new List<IShapeItem>();
+        }
+
+        public void AddComponents(IShapeItem shapeItem)
+        {
+            Components.Add(shapeItem);
+        }
+
+        public void Paint(Graphics g, Pen p)
+        {
+            foreach (var component in Components)
+                component.Paint(g, p);
+        }
+    }
+}
