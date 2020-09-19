a20426565e9f663bff2e8302936ef543f8661ef9
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-19 21:25:55 +0600

    #1 composite implemented IShape.

--- solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/CShape.cs
+++ solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/CShape.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Age_of_Villagers
 {
-    class CShape
+    class CShape : IShape
     {
+        protected List<IShape> My_components = new List<IShape>();
+
+        protected void AddComponent(IShape myshape)
+        {
+            My_components.Add(myshape);
+        }
+        public void Draw(Graphics g)
+        {
+            foreach (var component in My_components)
+            {
+                component.Draw(g);
+            }
+        }
 
     }
 }
