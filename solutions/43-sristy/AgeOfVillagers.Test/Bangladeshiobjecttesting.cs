0864e2bc37547231e78a79da4c0a6bd5067ed9b8
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 03:38:42 +0600

    #9 bangladeshi tree & watersource unit test added

--- solutions/43-sristy/AgeOfVillagers.Test/Bangladeshiobjecttesting.cs
+++ solutions/43-sristy/AgeOfVillagers.Test/Bangladeshiobjecttesting.cs
@@ -7,12 +7,26 @@ namespace AgeOfVillagers.Test
     public class Bangladeshiobjecttesting
     {
         Nationfactory nationfactory = new Nationfactory();
+        Point lptop = new Point(200, 100);
         [Fact]
-        public void BangladeshinationTest()
+        public void BangladeshihouseTest()
         {
-            Point lptop = new Point(200, 100);
             IObject actual = nationfactory.GetNation("Bangladeshi").Draw_house(lptop);
             IObject expected = new Bangladeshi_house(lptop);
+            actual.Equals(expected);
+        }
+        [Fact]
+        public void BangladeshitreeTest()
+        {
+            IObject actual = nationfactory.GetNation("Bangladeshi").Draw_tree(lptop);
+            IObject expected = new Bangladeshi_tree(lptop);
+            actual.Equals(expected);
+        }
+        [Fact]
+        public void BangladeshiwatersourceTest()
+        {
+            IObject actual = nationfactory.GetNation("Bangladeshi").Draw_watersource(lptop);
+            IObject expected = new Bangladeshi_watersource(lptop);
             actual.Equals(expected);
         }
     }
