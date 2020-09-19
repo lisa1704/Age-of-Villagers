cb4db6c68c086272e896d19bcb5c7af5daae2663
nafis10670 <nafissazad@gmail.com>
2020-09-19 06:09:52 +0600

    #9 #10 #11 #12 Created classes for each of the different nations. They implement the INations interface.

---
+++ solutions/07-Nafis/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class BangladeshiFarmers : INations
+    {
+        public void DrawHouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWater()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ solutions/07-Nafis/AgeOfVillagers/AgeOfVillagers/InuitHunters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class InuitHunters : INations
+    {
+        public void DrawHouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWater()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
