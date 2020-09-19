8a2cf069e3b49d8b90bc14f9fb7f989d4df1bc21
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-19 11:14:42 +0600

    #6 Openvillage class created

--- solutions/81-nawshin/age_of_villagers/Openvillage.cs
+++ solutions/81-nawshin/age_of_villagers/Openvillage.cs
@@ -6,24 +6,6 @@ namespace age_of_villagers
 {
     public class Openvillage 
     {
-        SaveVillage village;
-        public void execute()
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                Stream fileStream = ofd.OpenFile();
-                using (StreamReader sr = new StreamReader(fileStream))
-                {
-                    string json = sr.ReadToEnd();
-                    village = JsonConvert.DeserializeObject<SaveVillage>(json);
-                }
-            }
-        }
-
-        public SaveVillage get_village()
-        {
-            return this.village;
-        }
+        
     }
 }
