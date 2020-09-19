b9d59c0ba81d5f3a2414d5efec0c7f8be08f7455
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-19 21:53:18 +0600

    #10 Created inheritence with abstract nation factory class

--- solutions/75-arnob/Age of Villagers/Arab_Creator.cs
+++ solutions/75-arnob/Age of Villagers/Arab_Creator.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class Arab_Creator
+    class Arab_Creator : AbstractNationFactoryCreator
     {
+        private String nationName = "Arab Bedouin";
+
+        public string GetNationName
+        {
+            get
+            {
+                return nationName;
+            }
+
+            set
+            {
+                if (value == "Arab Bedouin")
+                    nationName = value;
+            }
+        }
+
+        public override INation CreateNation()
+        {
+            return new Arab_Bedouin(nationName);
+            
+        }
     }
 }
