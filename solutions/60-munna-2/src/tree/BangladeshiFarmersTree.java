41db74d6c1e3b34bb229acc88d1ffb0df75c9cd6
sakibul-munnaa <sakibulislam1@iut-dhaka.edu>
2020-09-19 23:14:12 +0600

    #7 #9 BangladeshiFarmersTree class added with some basic structure.

---
+++ solutions/60-munna-2/src/tree/BangladeshiFarmersTree.java
@@ -0,0 +1,24 @@
+package tree;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import shapes.Point;
+
+public class BangladeshiFarmersTree implements ITree {
+    private Canvas t_canvas;
+    private Point centre,bottomLeft,topLeft, bottomRight, topRight;
+
+    private GraphicsContext graphicsContext;
+
+
+
+    @Override
+    public void drawTree() {
+
+    }
+
+    @Override
+    public void getCanvas() {
+
+    }
+}
--- solutions/60-munna-2/src/tree/ITree.java
+++ solutions/60-munna-2/src/tree/ITree.java
@@ -4,7 +4,7 @@ public interface ITree {
     void drawTree();
 
     void getCanvas();
-    
+
 
 
 }
