60b153904162f0412a88781edbb5a34d110302bf
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-20 01:30:53 +0600

    #4 Village class created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/Village.cs
@@ -0,0 +1,23 @@
+﻿using AgeOfVillagers.Nation;
+using AgeOfVillagers.VillegeItems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class Village
+    {
+        private INation nation;
+        private string villageName;
+        private List<VillageItem> trees, houses, waterSources;
+        public Village(INation nation, string villageName)
+        {
+            this.nation = nation;
+            this.villageName = villageName;
+            trees = new List<VillageItem>();
+            houses = new List<VillageItem>();
+            waterSources = new List<VillageItem>();
+        }
+    }
+}
