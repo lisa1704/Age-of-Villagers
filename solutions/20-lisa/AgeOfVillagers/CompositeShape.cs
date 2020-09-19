0a99773fb712f15dee58be049e1af2839e6b4137
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-19 19:57:09 +0600

    #7 Composte shape class added and implemented

---
+++ solutions/20-lisa/AgeOfVillagers/CompositeShape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class CompositeShape : IShape
+    {
+        protected List<IShape> components = new List<IShape>();
+        protected void AddComponent(IShape shape)
+        {
+            components.Add(shape);
+        }
+        public void Draw(Graphics graphics)
+        {
+            foreach(var c in components)
+            {
+                c.Draw(graphics);
+            }
+        }
+    }
+}
