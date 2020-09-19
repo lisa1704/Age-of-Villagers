87eb73ad31fd245ecc6b81fc6b79fe7d35d3ea82
Saad <saadbinjohir@gmail.com>
2020-09-20 03:09:16 +0600

    #2 #7 Triangle

--- solutions/47-saad/AOV/AOV/Triangle.cs
+++ solutions/47-saad/AOV/AOV/Triangle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace AOV
 {
     class Triangle : CompoundShape
     {
-        public Triangle()
+        public Triangle(Point x, Point y, Point z)
         {
+            addShape(new DrawLine(x, y));
+            addShape(new DrawLine(y, z));
+            addShape(new DrawLine(z, x));
         }
     }
 }
