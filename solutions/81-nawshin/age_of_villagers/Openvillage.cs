baa1d44db549bf7aaca6e5f226e1d9c004b2bfa7
nawshin <nawshinulfat@iut-dhaka.edu>
2020-09-19 10:26:05 +0600

    #1 #5 points passing through the constructor

---
+++ solutions/81-nawshin/age_of_villagers/Openvillage.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace age_of_villagers
+{
+    public class Openvillage 
+    {
+        SaveVillage village;
+        public void execute()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Stream fileStream = ofd.OpenFile();
+                using (StreamReader sr = new StreamReader(fileStream))
+                {
+                    string json = sr.ReadToEnd();
+                    village = JsonConvert.DeserializeObject<SaveVillage>(json);
+                }
+            }
+        }
+
+        public SaveVillage get_village()
+        {
+            return this.village;
+        }
+    }
+}
