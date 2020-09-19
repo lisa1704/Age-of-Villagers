8b84ff0d5a38d28a80ab4cc7bb38407a0987d454
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-20 03:15:11 +0600

    #7 Rectangle shape added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/RectangleShape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class RectangleShape : IShapeItem
+    {
+        private Point leftTop;
+        private Point RightBottom;
+
+        public RectangleShape(Point leftTop, Point RightBottom)
+        {
+            this.leftTop = leftTop;
+            this.RightBottom = RightBottom;
+        }
+
+        public void Paint(Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
