28f97b754c9a5f916bb64b70908cecbb7fdd5efa
Maysha Afrin Munia <mayshaafrin@iut-dhaka.edu>
2020-09-20 01:36:25 +0600

    #4 Label Added

--- solutions/49-munia/AgeOfVillagers/Form1.Designer.cs
+++ solutions/49-munia/AgeOfVillagers/Form1.Designer.cs
@@ -35,14 +35,15 @@
             this.button6 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.button2.Location = new System.Drawing.Point(868, 211);
+            this.button2.Location = new System.Drawing.Point(846, 203);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(79, 28);
+            this.button2.Size = new System.Drawing.Size(79, 36);
             this.button2.TabIndex = 1;
             this.button2.Text = "House";
             this.button2.UseVisualStyleBackColor = false;
@@ -51,9 +52,9 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Azure;
-            this.button3.Location = new System.Drawing.Point(795, 257);
+            this.button3.Location = new System.Drawing.Point(760, 261);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(129, 31);
+            this.button3.Size = new System.Drawing.Size(129, 40);
             this.button3.TabIndex = 2;
             this.button3.Text = "Water Source";
             this.button3.UseVisualStyleBackColor = false;
@@ -61,7 +62,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(795, 328);
+            this.button4.Location = new System.Drawing.Point(760, 333);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(129, 33);
             this.button4.TabIndex = 3;
@@ -71,7 +72,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(760, 388);
+            this.button5.Location = new System.Drawing.Point(715, 388);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(93, 34);
             this.button5.TabIndex = 4;
@@ -80,7 +81,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(868, 388);
+            this.button6.Location = new System.Drawing.Point(832, 388);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(102, 34);
             this.button6.TabIndex = 5;
@@ -90,25 +91,38 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.button1.Location = new System.Drawing.Point(772, 211);
+            this.button1.Location = new System.Drawing.Point(715, 203);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 28);
+            this.button1.Size = new System.Drawing.Size(81, 36);
             this.button1.TabIndex = 6;
             this.button1.Text = "Tree";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.panel1.Location = new System.Drawing.Point(38, 33);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(600, 400);
             this.panel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(676, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 46);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Age of Villagers";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(988, 488);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button6);
@@ -120,6 +134,7 @@
             this.Text = "Village Template";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -131,6 +146,7 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- solutions/49-munia/AgeOfVillagers/Form1.cs
+++ solutions/49-munia/AgeOfVillagers/Form1.cs
@@ -41,5 +41,10 @@ namespace AgeOfVillagers
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
