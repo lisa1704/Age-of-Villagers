1c1ba0d8dc9a686c1d4cb609668749b7313005e0
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-20 01:05:24 +0600

    #7 Arc shape Added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/Arc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class Arc : IShapeItem
+    {
+        private Point point_1;
+        private Point point_2;
+
+        public int x, y, height, width, start, sweep;
+
+        public Arc()
+        {
+
+        }
+
+        public void Paint(Graphics g, Pen p)
+        {
+            var pen = new Pen(Color.Black, 1);
+            g.DrawArc(pen, x, y, width, height, start, sweep);
+        }
+    }
+}
