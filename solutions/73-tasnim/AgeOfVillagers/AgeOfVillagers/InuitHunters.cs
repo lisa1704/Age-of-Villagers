c784181cb4fa76bb658e6b2e66272c02ff1f7660
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-19 22:18:34 +0600

    #4 #8 #12 InuitHunters class created

---
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/InuitHunters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class InuitHunters : INation
+    {
+        public Color Color_Terrain()
+        {
+            return Color.White;
+        }
+
+        public void Draw_House(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_Tree(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_WaterSource(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
