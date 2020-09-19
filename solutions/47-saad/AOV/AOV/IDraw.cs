988152a680fc28f0a3f7ad21949037adb3b30d9e
Saad <saadbinjohir@gmail.com>
2020-09-20 02:02:24 +0600

    Shape Basic created

---
+++ solutions/47-saad/AOV/AOV/IDraw.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOV
+{
+    interface IDraw
+    {
+        void draw(Graphics g);
+    }
+}
