70d66d33067c89ec121efb5d16f97b1de7ca28a9
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-19 15:58:44 +0600

    #7 circle defined

--- solutions/32-zahin/AgeOfVillagers/Shapes/Circle.cs
+++ solutions/32-zahin/AgeOfVillagers/Shapes/Circle.cs
@@ -9,9 +9,23 @@ namespace AgeOfVillagers.Shapes
 {
     class Circle : IShape
     {
+        private Point x;
+        private int y, width, height, radius;
+        Graphics g;
+        Pen pen;
+        public Circle(Graphics g, Pen pen, Point x, int y, int width, int height, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.radius = radius;
+            this.g = g;
+            this.pen = pen;
+        }
         public void DrawShape()
         {
-            throw new NotImplementedException();
+            g.DrawArc(pen, x.X - 25, x.Y - 25, y, width, height, radius);
         }
     }
 }
