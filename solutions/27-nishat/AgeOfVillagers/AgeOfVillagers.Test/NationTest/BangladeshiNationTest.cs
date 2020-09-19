04cb0a66849886f419ac84b604772b7078679e38
mdnishat27 <59754386+mdnishat27@users.noreply.github.com>
2020-09-19 16:37:16 +0600

    #9 bangladeshi nation name test

---
+++ solutions/27-nishat/AgeOfVillagers/AgeOfVillagers.Test/NationTest/BangladeshiNationTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace AgeOfVillagers.Test
+{
+    public class BangladeshiNationTest
+    {
+
+        BangladeshiFarmers bangladeshiFarmers = new BangladeshiFarmers("Bangladeshi Farmers");
+
+        [Fact]
+        public void BangladeshiNationNameTest()
+        {
+            Assert.Equal("Bangladeshi Farmers", bangladeshiFarmers.GetNationName());
+        }
+    }
+}
