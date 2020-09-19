d3103b7a1cf1270b8ac54b9efc4902b851aa227d
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-19 15:24:41 +0600

    New Village board created

--- solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Program.cs
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Program.cs
@@ -17,6 +17,8 @@ namespace AgeOfVillagers
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new CreateNewVillage());
+
         }
     }
 }
