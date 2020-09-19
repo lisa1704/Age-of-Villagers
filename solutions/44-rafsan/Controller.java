5de0ba9fcbac496639f3eb388b815cd831b6233d
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-09-20 01:43:54 +0600

    #1 Buttons and canvas add

--- solutions/44-rafsan/Controller.java
+++ solutions/44-rafsan/Controller.java
@@ -1,4 +1,51 @@
 package sample;
-
+import javafx.scene.control.Button;
 public class Controller {
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+
+    public Button button8;
+
+    public void b1(){
+        button1.setText("button1");
+    }
+
+
+    public void b2(){
+        button2.setText("button2");
+    }
+
+    public void b3(){
+        button3.setText("button3");
+    }
+
+    public void b4(){
+        button4.setText("button4");
+    }
+
+    public void b5(){
+        button5.setText("button5");
+    }
+
+    public void b6(){
+        button6.setText("button6");
+    }
+
+
+    public void b7(){
+        button7.setText("button7");
+    }
+
+
+    public void b8(){
+        button8.setText("button8");
+    }
+
+
+
 }
