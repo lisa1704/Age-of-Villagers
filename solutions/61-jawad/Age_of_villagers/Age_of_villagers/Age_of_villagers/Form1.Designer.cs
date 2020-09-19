614d37e3510c859dc3655bcce176911cd79421da
Jawad Zamal <jawadzamal@iut-dhaka.edu>
2020-09-20 01:05:16 +0600

    #4 buttons created

---
+++ solutions/61-jawad/Age_of_villagers/Age_of_villagers/Age_of_villagers/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Age_of_villagers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveVillage = new System.Windows.Forms.Button();
+            this.NewVillage = new System.Windows.Forms.Button();
+            this.OpenVillage = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Tree = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // SaveVillage
+            // 
+            this.SaveVillage.Location = new System.Drawing.Point(799, 418);
+            this.SaveVillage.Name = "SaveVillage";
+            this.SaveVillage.Size = new System.Drawing.Size(121, 28);
+            this.SaveVillage.TabIndex = 1;
+            this.SaveVillage.Text = "Save Village";
+            this.SaveVillage.UseVisualStyleBackColor = true;
+            this.SaveVillage.Click += new System.EventHandler(this.SaveVillage_Click);
+            // 
+            // NewVillage
+            // 
+            this.NewVillage.Location = new System.Drawing.Point(730, 452);
+            this.NewVillage.Name = "NewVillage";
+            this.NewVillage.Size = new System.Drawing.Size(117, 29);
+            this.NewVillage.TabIndex = 2;
+            this.NewVillage.Text = "New Village";
+            this.NewVillage.UseVisualStyleBackColor = true;
+            this.NewVillage.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // OpenVillage
+            // 
+            this.OpenVillage.Location = new System.Drawing.Point(875, 452);
+            this.OpenVillage.Name = "OpenVillage";
+            this.OpenVillage.Size = new System.Drawing.Size(117, 29);
+            this.OpenVillage.TabIndex = 3;
+            this.OpenVillage.Text = "Open Village";
+            this.OpenVillage.UseVisualStyleBackColor = true;
+            this.OpenVillage.Click += new System.EventHandler(this.OpenVillage_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(776, 106);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Tree
+            // 
+            this.Tree.AutoSize = true;
+            this.Tree.Location = new System.Drawing.Point(809, 218);
+            this.Tree.Name = "Tree";
+            this.Tree.Size = new System.Drawing.Size(58, 24);
+            this.Tree.TabIndex = 0;
+            this.Tree.TabStop = true;
+            this.Tree.Text = "Tree\r\n";
+            this.Tree.UseVisualStyleBackColor = true;
+            this.Tree.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(809, 248);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 24);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "House";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(809, 278);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(118, 24);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Water Source";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 594);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.OpenVillage);
+            this.Controls.Add(this.NewVillage);
+            this.Controls.Add(this.SaveVillage);
+            this.Controls.Add(this.Tree);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SaveVillage;
+        private System.Windows.Forms.Button NewVillage;
+        private System.Windows.Forms.Button OpenVillage;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton Tree;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ solutions/61-jawad/Age_of_villagers/Age_of_villagers/Age_of_villagers/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_of_villagers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveVillage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenVillage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
