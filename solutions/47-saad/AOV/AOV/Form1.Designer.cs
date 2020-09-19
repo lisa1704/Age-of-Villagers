9edce5f292b3ac065a56d448fe94ed520fe38be7
Saad <saadbinjohir@gmail.com>
2020-09-19 19:38:20 +0600

    Variable name modified

--- solutions/47-saad/AOV/AOV/Form1.Designer.cs
+++ solutions/47-saad/AOV/AOV/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AOV
 {
-    partial class Form1
+    partial class AgeOfVillagers
     {
         /// <summary>
         /// Required designer variable.
@@ -28,59 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.NameOfVillage = new System.Windows.Forms.TextBox();
+            this.SaveVillage = new System.Windows.Forms.Button();
+            this.OpenVillage = new System.Windows.Forms.Button();
+            this.NewVillage = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.Label = new System.Windows.Forms.Label();
+            this.Nation = new System.Windows.Forms.ComboBox();
+            this.Tree = new System.Windows.Forms.Button();
+            this.WaterSource = new System.Windows.Forms.Button();
+            this.House = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // NameOfVillage
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(616, 62);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 22);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Name your village";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.NameOfVillage.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.NameOfVillage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameOfVillage.Location = new System.Drawing.Point(616, 62);
+            this.NameOfVillage.Name = "NameOfVillage";
+            this.NameOfVillage.Size = new System.Drawing.Size(169, 22);
+            this.NameOfVillage.TabIndex = 0;
+            this.NameOfVillage.Text = "Name your village";
+            this.NameOfVillage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // button3
+            // SaveVillage
             // 
-            this.button3.BackColor = System.Drawing.Color.LimeGreen;
-            this.button3.Location = new System.Drawing.Point(661, 346);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 31);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Save Village";
-            this.button3.UseVisualStyleBackColor = false;
+            this.SaveVillage.BackColor = System.Drawing.Color.LimeGreen;
+            this.SaveVillage.Location = new System.Drawing.Point(661, 346);
+            this.SaveVillage.Name = "SaveVillage";
+            this.SaveVillage.Size = new System.Drawing.Size(99, 31);
+            this.SaveVillage.TabIndex = 3;
+            this.SaveVillage.Text = "Save Village";
+            this.SaveVillage.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // OpenVillage
             // 
-            this.button5.BackColor = System.Drawing.Color.Yellow;
-            this.button5.Location = new System.Drawing.Point(713, 383);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(84, 31);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Open Village";
-            this.button5.UseVisualStyleBackColor = false;
+            this.OpenVillage.BackColor = System.Drawing.Color.Yellow;
+            this.OpenVillage.Location = new System.Drawing.Point(713, 383);
+            this.OpenVillage.Name = "OpenVillage";
+            this.OpenVillage.Size = new System.Drawing.Size(84, 31);
+            this.OpenVillage.TabIndex = 5;
+            this.OpenVillage.Text = "Open Village";
+            this.OpenVillage.UseVisualStyleBackColor = false;
             // 
-            // button6
+            // NewVillage
             // 
-            this.button6.BackColor = System.Drawing.Color.IndianRed;
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(616, 383);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(91, 31);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "New village";
-            this.button6.UseVisualStyleBackColor = false;
+            this.NewVillage.BackColor = System.Drawing.Color.IndianRed;
+            this.NewVillage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewVillage.Location = new System.Drawing.Point(616, 383);
+            this.NewVillage.Name = "NewVillage";
+            this.NewVillage.Size = new System.Drawing.Size(91, 31);
+            this.NewVillage.TabIndex = 6;
+            this.NewVillage.Text = "New village";
+            this.NewVillage.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
@@ -90,82 +90,81 @@
             this.panel1.Size = new System.Drawing.Size(600, 400);
             this.panel1.TabIndex = 7;
             // 
-            // label1
+            // Label
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(635, 14);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(10);
-            this.label1.Size = new System.Drawing.Size(139, 36);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Age of Villagers";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Label.AutoSize = true;
+            this.Label.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label.Location = new System.Drawing.Point(635, 14);
+            this.Label.Name = "Label";
+            this.Label.Padding = new System.Windows.Forms.Padding(10);
+            this.Label.Size = new System.Drawing.Size(139, 36);
+            this.Label.TabIndex = 9;
+            this.Label.Text = "Age of Villagers";
+            this.Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // comboBox1
+            // Nation
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.Nation.FormattingEnabled = true;
+            this.Nation.Items.AddRange(new object[] {
             "Bangladeshi Farmers",
             "Arab Bedouin",
             "Egyptian Kings",
             "Inuit Hunters"});
-            this.comboBox1.Location = new System.Drawing.Point(616, 101);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(169, 21);
-            this.comboBox1.TabIndex = 11;
-            this.comboBox1.Text = "Select nation";
+            this.Nation.Location = new System.Drawing.Point(616, 101);
+            this.Nation.Name = "Nation";
+            this.Nation.Size = new System.Drawing.Size(169, 21);
+            this.Nation.TabIndex = 11;
+            this.Nation.Text = "Select nation";
             // 
-            // button1
+            // Tree
             // 
-            this.button1.BackColor = System.Drawing.Color.LightSeaGreen;
-            this.button1.Location = new System.Drawing.Point(617, 175);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 31);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Tree";
-            this.button1.UseVisualStyleBackColor = false;
+            this.Tree.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.Tree.Location = new System.Drawing.Point(617, 175);
+            this.Tree.Name = "Tree";
+            this.Tree.Size = new System.Drawing.Size(91, 31);
+            this.Tree.TabIndex = 12;
+            this.Tree.Text = "Tree";
+            this.Tree.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // WaterSource
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button2.Location = new System.Drawing.Point(647, 211);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 31);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Water Source";
-            this.button2.UseVisualStyleBackColor = false;
+            this.WaterSource.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.WaterSource.Location = new System.Drawing.Point(647, 211);
+            this.WaterSource.Name = "WaterSource";
+            this.WaterSource.Size = new System.Drawing.Size(113, 31);
+            this.WaterSource.TabIndex = 13;
+            this.WaterSource.Text = "Water Source";
+            this.WaterSource.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // House
             // 
-            this.button4.BackColor = System.Drawing.Color.Goldenrod;
-            this.button4.Location = new System.Drawing.Point(713, 175);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(82, 31);
-            this.button4.TabIndex = 14;
-            this.button4.Text = "House";
-            this.button4.UseVisualStyleBackColor = false;
+            this.House.BackColor = System.Drawing.Color.Goldenrod;
+            this.House.Location = new System.Drawing.Point(713, 175);
+            this.House.Name = "House";
+            this.House.Size = new System.Drawing.Size(82, 31);
+            this.House.TabIndex = 14;
+            this.House.Text = "House";
+            this.House.UseVisualStyleBackColor = false;
             // 
-            // Form1
+            // AgeOfVillagers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.House);
+            this.Controls.Add(this.WaterSource);
+            this.Controls.Add(this.Tree);
+            this.Controls.Add(this.Nation);
+            this.Controls.Add(this.Label);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
+            this.Controls.Add(this.NewVillage);
+            this.Controls.Add(this.OpenVillage);
+            this.Controls.Add(this.SaveVillage);
+            this.Controls.Add(this.NameOfVillage);
+            this.Name = "AgeOfVillagers";
             this.Text = "Age of Villagers";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,16 +172,16 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox NameOfVillage;
+        private System.Windows.Forms.Button SaveVillage;
+        private System.Windows.Forms.Button OpenVillage;
+        private System.Windows.Forms.Button NewVillage;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.ComboBox Nation;
+        private System.Windows.Forms.Button Tree;
+        private System.Windows.Forms.Button WaterSource;
+        private System.Windows.Forms.Button House;
     }
 }
 
--- solutions/47-saad/AOV/AOV/Form1.cs
+++ solutions/47-saad/AOV/AOV/Form1.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace AOV
 {
-    public partial class Form1 : Form
+    public partial class AgeOfVillagers : Form
     {
-        public Form1()
+        public AgeOfVillagers()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+        }        
     }
 }
