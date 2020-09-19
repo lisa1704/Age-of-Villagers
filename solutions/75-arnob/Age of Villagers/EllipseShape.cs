9b06fe9d79a1aa7a844d931c10d1ce651b368f82
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 04:32:27 +0600

    implemented the ellipse shape by compositing it with arc shape

--- solutions/75-arnob/Age of Villagers/EllipseShape.cs
+++ solutions/75-arnob/Age of Villagers/EllipseShape.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class EllipseShape : IShape
+    class EllipseShape : CompositeShaping
     {
+        private readonly Point topLeft;
+        private readonly Point bottomRight;
+
+        public EllipseShape(Point _topleft, Point _bottomright)
+        {
+            this.topLeft = _topleft;
+            this.bottomRight = _bottomright;
+            AddComponent(new ArcShape(topLeft.X, topLeft.Y, bottomRight.X - topLeft.X, bottomRight.Y - topLeft.Y, 0, 360));
+        }
 
     }
 }
