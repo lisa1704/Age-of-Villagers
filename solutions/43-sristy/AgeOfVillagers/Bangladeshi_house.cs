9fc556c5fe722e829cc80e744933c1466bafb4fb
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-19 22:37:48 +0600

    #9 restore the size of bangladeshi waterresource

--- solutions/43-sristy/AgeOfVillagers/Bangladeshi_house.cs
+++ solutions/43-sristy/AgeOfVillagers/Bangladeshi_house.cs
@@ -33,14 +33,14 @@ namespace Age_of_villagers
     {
         public Bangladeshi_watersource(Point p1)
         {
-            Point p2 = new Point(p1.X - 25, p1.Y + 50);
-            Point p3 = new Point(p1.X, p1.Y + 75);
-            Point p4 = new Point(p1.X + 25, p1.Y + 50);
-            Point p5 = new Point(p1.X + 50, p1.Y + 75);
-            Point p6 = new Point(p1.X + 75, p1.Y + 25);
-            Point p7 = new Point(p1.X + 50, p1.Y);
-            Point p8 = new Point(p1.X + 25, p1.Y + 25);
-            Point p9 = new Point(p1.X + 25, p1.Y);
+            Point p2 = new Point(p1.X - 6, p1.Y + 11);
+            Point p3 = new Point(p1.X, p1.Y + 16);
+            Point p4 = new Point(p1.X + 6, p1.Y + 11);
+            Point p5 = new Point(p1.X + 6, p1.Y + 16);
+            Point p6 = new Point(p1.X + 16, p1.Y + 5);
+            Point p7 = new Point(p1.X + 12, p1.Y);
+            Point p8 = new Point(p1.X + 6, p1.Y + 5);
+            Point p9 = new Point(p1.X + 6, p1.Y);
             AddComponent(new obj_line(p1, p2));
             AddComponent(new obj_line(p2, p3));
             AddComponent(new obj_line(p3, p4));
