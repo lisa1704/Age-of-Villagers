0999fb064e93f696da60e7cb90d62a44de894346
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-20 00:44:12 +0600

    #12 Null implemented

--- solutions/48-zoyee/Age Of Villagers/Inuit_Hunters.cs
+++ solutions/48-zoyee/Age Of Villagers/Inuit_Hunters.cs
@@ -15,17 +15,20 @@ namespace Age_Of_Villagers
         }
         public void Draw_House(Graphics g, Point point)
         {
-            
+            Inuit_House inhouse = new Inuit_House(point);
+            inhouse.Draw(g);
         }
 
         public void Draw_Tree(Graphics g, Point point)
         {
-            throw new NotImplementedException();
+            NullVillage_Item NullNationtree = new NullVillage_Item();
+            NullNationtree.paint();
         }
 
         public void Draw_WaterSource(Graphics g, Point point)
         {
-            throw new NotImplementedException();
+            NullVillage_Item NullNationws = new NullVillage_Item();
+            NullNationws.paint();
         }
     }
 }
