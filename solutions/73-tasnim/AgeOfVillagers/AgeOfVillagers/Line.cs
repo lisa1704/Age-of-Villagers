700af2bfe723b0be9baae6a813ceb264d950dd25
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-20 01:59:29 +0600

    #7 Line class implemented

--- solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Line.cs
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Line.cs
@@ -9,9 +9,19 @@ namespace AgeOfVillagers
 {
     class Line : IShape
     {
+        private Point P1;
+        private Point P2;
+
+        public Line(Point P1, Point P2)
+        {
+            this.P1 = P1;
+            this.P2 = P2;
+        }
         public void Draw_Shape(Graphics g)
         {
-            throw new NotImplementedException();
+            var pen = new Pen(Color.Black, 1);
+            g.DrawLine(pen, P1, P2);
         }
     }
+
 }
