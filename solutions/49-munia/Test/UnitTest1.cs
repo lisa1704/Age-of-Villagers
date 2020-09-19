5ce96e70eb44f0fd401c3867b5c5281e6f740c47
Maysha Afrin Munia <mayshaafrin@iut-dhaka.edu>
2020-09-19 20:23:32 +0600

    $15 Sample test written

--- solutions/49-munia/Test/UnitTest1.cs
+++ solutions/49-munia/Test/UnitTest1.cs
@@ -8,7 +8,12 @@ namespace Test
         [Fact]
         public void Test1()
         {
+            int value1 = 2;
+            int value2 = 2;
 
+            var result = value1 + value2;
+
+            Assert.Equal(4, result);
         }
     }
 }
