997260aa7a0f159033303758ff0e7bf76590c9a4
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-19 15:39:59 +0600

    #7 rectangle defined

---
+++ solutions/32-zahin/AgeOfVillagers/Shapes/Rectangles.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AgeOfVillagers.Shapes
+{
+    class Rectangles : IShape
+    {
+
+        Point p1;
+        Point p2;
+        Point p3;
+        Point p4;
+        Pen pen;
+        Graphics g;
+        public Rectangles(Graphics g, Pen pen, Point p1, Point p2, Point p3, Point p4)
+        {
+
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+            this.p4 = p4;
+            this.pen = pen;
+            this.g = g;
+        }
+        public void DrawShape()
+        {
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p1, p3);
+            g.DrawLine(pen, p3, p4);
+            g.DrawLine(pen, p2, p4);
+        }
+    }
+}
