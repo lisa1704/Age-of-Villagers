013b9e762bdbbed6f2d0dc5a40c624565a5f5ff8
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-20 02:42:30 +0600

    #7 Rectangle class implemented

--- solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Rectangle.cs
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Rectangle.cs
@@ -9,6 +9,18 @@ namespace AgeOfVillagers
 {
     class Rectangle : AbstractShape
     {
-        
-    }
+        private Point TopLeft;
+        private Point BottomRight;
+        private Point TopRight;
+        private Point BottomLeft;
+        public Rectangle(Point topLeft, Point BottomRight)
+        {
+            TopRight = new Point(BottomRight.X, topLeft.Y);
+            BottomLeft = new Point(topLeft.X, BottomRight.Y);
+
+            addShape(new Line(topLeft, BottomLeft));
+            addShape(new Line(topLeft, TopRight));
+            addShape(new Line(BottomLeft, BottomRight));
+            addShape(new Line(BottomRight, TopRight));
+        }
 }
