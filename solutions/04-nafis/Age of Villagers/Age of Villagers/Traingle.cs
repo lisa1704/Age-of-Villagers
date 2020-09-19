fff7cc2e09ecda56ac16f598dd9b653ff9d3b470
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-20 04:09:49 +0600

    #9 #10 #11 #12 Triangle Drawing is completed

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/Traingle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    public class Traingle : CompositeShape
+    {
+        private readonly Point point1;
+        private readonly Point point2;
+        private readonly Point point3;
+
+        public Traingle(Point point1, Point point2, Point point3)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            this.point3 = point3;
+
+            ShapePush(new Line(point1,point2));
+            ShapePush(new Line(point1,point3));
+            ShapePush(new Line(point2,point3));
+        }
+    }
+}
