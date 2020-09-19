87efc67b84576d32971ce53f0b3163f1546a0853
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-19 13:25:09 +0600

    #9#10#11#12 creating Ination interface to set properties

---
+++ solutions/34-meem/Age_Of_Villagers/INation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Of_Villagers
+{
+    public interface INation
+    {
+         void setTerrain(VillageEditorWindow villageEditor);
+         void setRadiobutton(VillageEditorWindow villageEditor);
+
+    }
+
+    
+
+
+
+}
