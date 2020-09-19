a1d7be397e2702fbcad2fb2c34180d8ff2c158e4
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-20 05:07:21 +0600

    #5 Implemented Save village

--- solutions/19-zarif/AgeOfVillagers/SaveVillageState.cs
+++ solutions/19-zarif/AgeOfVillagers/SaveVillageState.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
+using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace AgeOfVillagers
 {
-    class SaveVillageState
+    public class SaveVillageState
     {
+        VillageState village;
+
+        public SaveVillageState(VillageState village)
+        {
+            this.village = village;
+        }
+
+        public void execute()
+        {
+            SaveFileDialog _savefiledg = new SaveFileDialog();
+            _savefiledg.InitialDirectory = @"E:\Dp_Assignment_Age_of_villagers\save\";
+            _savefiledg.RestoreDirectory = true;
+            _savefiledg.FileName = "*.aov";
+            _savefiledg.DefaultExt = "aov";
+            _savefiledg.Filter = "AoV file(*.aov)| *.aov";
+            if (_savefiledg.ShowDialog() == DialogResult.OK)
+            {
+                Stream fileStream = _savefiledg.OpenFile();
+                using (StreamWriter _sw = new StreamWriter(fileStream))
+                {
+                    Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                    serializer.Serialize(_sw, village);
+                    _sw.Close();
+                }
+                fileStream.Close();
+            }
+
+        }
     }
 }
--- solutions/19-zarif/AgeOfVillagers/VillageState.cs
+++ solutions/19-zarif/AgeOfVillagers/VillageState.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    class VillageState
+    public class VillageState
     {
+        
+        public string _name;
+        public List<Point> _housePoints, _treePoints, _WaterScPoints;
+      
+
+        public VillageState(string name, List<Point> housePoints, List<Point> treePoints, List<Point> waterScPoints)
+        {
+            _name = name;
+            _housePoints = housePoints;
+            _treePoints = treePoints;
+            _WaterScPoints = waterScPoints;
+        }
     }
 }
+
