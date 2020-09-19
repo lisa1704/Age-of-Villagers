64b2e9cf099751d20f07701c4d1d0138efa69211
Saad <saadbinjohir@gmail.com>
2020-09-20 02:51:15 +0600

    #2 #7 Compound Shape created

--- solutions/47-saad/AOV/AOV/DrawArc.cs
+++ solutions/47-saad/AOV/AOV/DrawArc.cs
@@ -9,9 +9,26 @@ namespace AOV
 {
     class DrawArc : IDraw
     {
+        private float x;
+        private float y;
+        private float height;
+        private float width;
+        private float startAngle;
+        private float sweepAngle;
+
+        public DrawArc(float x, float y, float height, float width, float startAngle, float sweepAngle)
+        {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+            this.width = width;
+            this.startAngle = startAngle;
+            this.sweepAngle = sweepAngle;
+        }
         public void draw(Graphics g)
         {
-            throw new NotImplementedException();
+            Pen blackPen = new Pen(Color.Black, 1);
+            g.DrawArc(blackPen, x, y, height, width, startAngle, sweepAngle);
         }
     }
 }
--- solutions/47-saad/AOV/AOV/DrawLine.cs
+++ solutions/47-saad/AOV/AOV/DrawLine.cs
@@ -9,9 +9,18 @@ namespace AOV
 {
     class DrawLine : IDraw
     {
+        private Point x;
+        private Point y;
+
+        public DrawLine(Point x, Point y)
+        {
+            this.x = x;
+            this.y = y;
+        }
         public void draw(Graphics g)
         {
-            throw new NotImplementedException();
+            Pen blackPen = new Pen(Color.Black, 1);
+            g.DrawLine(blackPen, x, y);
         }
     }
 }
