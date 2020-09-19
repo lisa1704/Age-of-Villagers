053783f837731730c04c8703b3597e4a9b1dfb3a
Asif Aziz <asifaziz2061@gmail.com>
2020-09-19 22:40:49 +0600

    #9 #10 #11 #12 Abstract class implemented

--- solutions/31-aziz/empty_project/empty_project/AbstractNation.cs
+++ solutions/31-aziz/empty_project/empty_project/AbstractNation.cs
@@ -8,5 +8,28 @@ namespace empty_project
 {
     abstract class AbstractNation : INation
     {
+        IHouse house;
+        ITree tree;
+        IWaterSource waterSource;
+        Panel panel;
+
+        public AbstractNation(Panel panel)
+        {
+            this.panel = panel;
+        }
+        public void drawHouse(Axis axis)
+        {
+            house.drawHouse(axis, panel);
+        }
+
+        public void drawTree(Axis axis)
+        {
+            tree.drawTree(axis, panel);
+        }
+
+        public void drawWaterSource(Axis axis)
+        {
+            waterSource.drawWaterSource(axis, panel);
+        }
     }
 }
