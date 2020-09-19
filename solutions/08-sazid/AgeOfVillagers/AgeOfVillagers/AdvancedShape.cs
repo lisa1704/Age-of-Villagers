2d30ab285251d34070435f0aed0a4aab7d962232
ESD <ESD@DESKTOP-SNRCVRJ>
2020-09-19 22:18:31 +0600

    #7 Classes for drawing different shapes added

---
+++ solutions/08-sazid/AgeOfVillagers/AgeOfVillagers/AdvancedShape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    public abstract class AdvancedShape : IShape
+    {
+        List<IShape> shapeElements;
+
+        public AdvancedShape()
+        {
+            shapeElements = new List<IShape>();
+        }
+
+        public void addShape(IShape shape)
+        {
+            shapeElements.Add(shape);
+        }
+        public void draw(Graphics g)
+        {
+            foreach(var shapes in shapeElements)
+            {
+                shapes.draw(g);
+            }
+        }
+    }
+}
