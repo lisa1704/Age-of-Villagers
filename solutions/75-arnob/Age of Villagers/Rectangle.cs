93cf470ec1fc5ada8800999610b136d11a8eb9f3
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 04:12:30 +0600

    #9 #10 #11 #12 Rectangle class created

---
+++ solutions/75-arnob/Age of Villagers/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_Villagers
+{
+    class Rectangle : CompositeShaping
+    {
+        public MyRectangle(Point topLeft, Point bottomRight)
+        {
+            var topRight = new Point(bottomRight.X, topLeft.Y);
+            var bottomLeft = new Point(topLeft.X, bottomRight.Y);
+
+            AddComponent(new Line(topLeft, topRight));
+            AddComponent(new Line(topRight, bottomRight));
+            AddComponent(new Line(bottomRight, bottomLeft));
+            AddComponent(new Line(bottomLeft, topLeft));
+        }
+    }
+}
