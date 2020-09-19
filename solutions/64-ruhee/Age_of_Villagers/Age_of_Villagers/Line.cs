484e975a4f2e1d8411ed0e8f7cbca39b12a79de2
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-19 21:37:02 +0600

    #7 Line created to draw.

--- solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/Line.cs
+++ solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/Line.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Age_of_Villagers
 {
-    class Line
+    class Line : IShape
     {
+        private Point point_1;
+        private Point point_2;
+
+        public Line(Point point1, Point point2)
+        {
+            this.point_1 = point1;
+            this.point_2 = point2;
+        }
+
+        public void Draw(Graphics g)
+        {
+            var my_pen = new Pen(Color.Black, 1);
+            g.DrawLine(my_pen, point_1, point_2);
+        }
 
     }
 }
