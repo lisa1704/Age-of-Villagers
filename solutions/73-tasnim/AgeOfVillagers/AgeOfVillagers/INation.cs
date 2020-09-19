a83ea94d36003b3cd4ae54ca6a325082d3d8ccc5
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-19 20:21:44 +0600

    #8 INation interface created

---
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/INation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AgeOfVillagers
+{
+    interface INation
+    {
+        void Draw_House(Graphics g, int x, int y);
+        void Draw_Tree(Graphics g, int x, int y);
+        void Draw_WaterSource(Graphics g, int x, int y);
+        void Paint_Terrain();
+    }
+}
+
+
+
