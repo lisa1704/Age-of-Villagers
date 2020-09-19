7bf0765a04c471bdb782e64dfa449ae841e9c46d
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-20 02:53:22 +0600

    #5 VillageSaver implemented

--- solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillagerSaver.cs
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillagerSaver.cs
@@ -16,6 +16,19 @@ namespace AgeOfVillagers
         }
         public void saveVillage()
         {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "AgeOfVillagers|*.aov";
+            saveFileDialog.Title = "Save Village";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                var settings = new JsonSerializerSettings()
+                {
+                    TypeNameHandling = TypeNameHandling.All
+                };
+                var dataString = JsonConvert.SerializeObject(village, Formatting.Indented, settings);
+                System.IO.File.WriteAllText(saveFileDialog.FileName, dataString);
+            }
         }
     }
 }
