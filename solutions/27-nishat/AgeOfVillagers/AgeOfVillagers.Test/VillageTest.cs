a08271e6c33aae62da81c234526af02bed3c5999
mdnishat27 <59754386+mdnishat27@users.noreply.github.com>
2020-09-19 19:21:49 +0600

    #4 village test class created

---
+++ solutions/27-nishat/AgeOfVillagers/AgeOfVillagers.Test/VillageTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Xunit;
+
+namespace AgeOfVillagers.Test
+{
+    public class VillageTest
+    {
+        string villagename = "my village";
+        BangladeshiFarmers bangladeshiFarmers = new BangladeshiFarmers("Bangladeshi Farmers");
+
+        [Fact]
+        public void VillageCreateTest()
+        {
+            Village village = new Village(bangladeshiFarmers, villagename);
+
+            Assert.IsType<Village>(village);
+        }
+    }
+}
