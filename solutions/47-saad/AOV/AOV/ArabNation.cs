e8084b64cf2f26af8b6a776e46ea5f028b532f32
Saad <saadbinjohir@gmail.com>
2020-09-19 21:45:31 +0600

    #10 Arab Nation implemented

--- solutions/47-saad/AOV/AOV/ArabNation.cs
+++ solutions/47-saad/AOV/AOV/ArabNation.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AOV
 {
-    class ArabNation
+    class ArabNation : INation
     {
+        public void DrawHouse(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color GetColor()
+        {
+            return Color.LightYellow;
+        }
     }
 }
