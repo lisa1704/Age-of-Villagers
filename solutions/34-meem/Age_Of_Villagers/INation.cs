e62571df059944160c94a427d29bb72d2f5e61a8
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-19 13:29:47 +0600

    #9 bangladeshi terrain and item property added

--- solutions/34-meem/Age_Of_Villagers/INation.cs
+++ solutions/34-meem/Age_Of_Villagers/INation.cs
@@ -4,17 +4,32 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Age_Of_Villagers
 {
     public interface INation
     {
-         void setTerrain(VillageEditorWindow villageEditor);
-         void setRadiobutton(VillageEditorWindow villageEditor);
+        void setTerrain(VillageEditorWindow villageEditor);
+        void setRadiobutton(VillageEditorWindow villageEditor);
 
     }
 
-    
+    public class BangladeshiNation : INation
+    {
+        public void setRadiobutton(VillageEditorWindow villageEditor)
+        {
+            villageEditor.rdButtonHouse.Enabled = true;
+            villageEditor.rdButtonTree.Enabled = true;
+            villageEditor.rdButtonWaterSrc.Enabled = true;
+
+        }
+
+        public void setTerrain(VillageEditorWindow villageEditor)
+        {
+            villageEditor.Panel1.BackColor = Color.Green;
+        }
+    }
 
 
 
