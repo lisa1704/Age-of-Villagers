80156b9c286720ccadc2e465e6d5d92d5a5614a8
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-19 22:08:51 +0600

    #4 #8 #11 Egyptian kings class created

---
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/EgyptianKings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class EgyptianKings : INation
+    {
+        public Color Color_Terrain()
+        {
+            return Color.Yellow;
+        }
+
+        public void Draw_House(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_Tree(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_WaterSource(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
