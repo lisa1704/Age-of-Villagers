4cd1ae8ed8a0ccd2ace5b0f028816bb2a568e852
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 02:36:27 +0600

    #1 change compositeobject to Iobject

--- solutions/43-sristy/AgeOfVillagers/Bangladeshi.cs
+++ solutions/43-sristy/AgeOfVillagers/Bangladeshi.cs
@@ -5,17 +5,17 @@ namespace Age_of_villagers
 {
     public class Bangladeshi : INation
     {        
-        public Compositeobj Draw_house(Point lptop)
+        public IObject Draw_house(Point lptop)
         {
             Compositeobj house = new Bangladeshi_house(lptop);
             return house;
         }
-        public Compositeobj Draw_tree(Point mptop)
+        public IObject Draw_tree(Point mptop)
         {
             Compositeobj tree = new Bangladeshi_tree(mptop);
             return tree;
         }
-        public Compositeobj Draw_watersource(Point p1)
+        public IObject Draw_watersource(Point p1)
         {
             Compositeobj watersource = new Bangladeshi_watersource(p1);
             return watersource;
--- solutions/43-sristy/AgeOfVillagers/INation.cs
+++ solutions/43-sristy/AgeOfVillagers/INation.cs
@@ -4,8 +4,8 @@ namespace Age_of_villagers
     public interface INation
     {
         Color get_backcolor();
-        Compositeobj Draw_house(Point p);
-        Compositeobj Draw_tree(Point p);
-        Compositeobj Draw_watersource(Point p);
+        IObject Draw_house(Point p);
+        IObject Draw_tree(Point p);
+        IObject Draw_watersource(Point p);
     }
 }
