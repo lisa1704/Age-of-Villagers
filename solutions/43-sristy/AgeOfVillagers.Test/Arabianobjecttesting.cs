08fd090378e52eb8cfb89527fa517fd0a7bc65c8
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 04:58:44 +0600

    #10 arab house and tree unit test

---
+++ solutions/43-sristy/AgeOfVillagers.Test/Arabianobjecttesting.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Age_of_villagers;
+using System.Drawing;
+
+namespace AgeOfVillagers.Test
+{
+    public class Arabianobjecttesting
+    {
+        Nationfactory nationfactory = new Nationfactory();
+        Point lptop = new Point(200, 100);
+        [Fact]
+        public void ArabhouseTest()
+        {
+            IObject actual = nationfactory.GetNation("Arab").Draw_house(lptop);
+            IObject expected = new Arab_house(lptop);
+            actual.Equals(expected);
+        }
+        [Fact]
+        public void ArabtreeTest()
+        {
+            IObject actual = nationfactory.GetNation("Arab").Draw_tree(lptop);
+            IObject expected = new Arab_tree(lptop);
+            actual.Equals(expected);
+        }
+    }
+}
