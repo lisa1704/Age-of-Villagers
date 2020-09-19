5fe7fb61b4082455a181c9f1e0527e890de8a780
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-09-19 22:42:27 +0600

    #11 #12 egyptianking and inuit hunter class added

---
+++ solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/InuitHunter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    public class InuitHunter : INation
+    {
+        public void DrawHouse(Point p, Panel drawingBoard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree(Point p, Panel drawingBoard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource(Point p, Panel drawingBoard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
