93a1871d587c5f084d06a503bd792575860063a2
sakibul-munnaa <sakibulislam1@iut-dhaka.edu>
2020-09-20 01:43:44 +0600

    #1 #7 Some Refactoring done on calculating_points method.

--- solutions/60-munna-2/src/house/BangladeshiFarmersHouse.java
+++ solutions/60-munna-2/src/house/BangladeshiFarmersHouse.java
@@ -7,7 +7,6 @@ import javafx.scene.input.MouseEvent;
 import shapes.Point;
 
 public class BangladeshiFarmersHouse implements IHouse {
-    private Canvas canvas;
 
     private Point top_center;
     private Point top_left;
@@ -15,35 +14,34 @@ public class BangladeshiFarmersHouse implements IHouse {
     private Point bottom_left;
     private Point bottom_right;
 
-
     @Override
-    public void draw_house() {
+    public void draw_house(Canvas canvas) {
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent event) {
-                            calculating_points(new Point((int) event.getX(), (int) event.getY()));
-                            graphicsContext.strokeLine(top_center.x, top_center.y, top_left.x, top_left.y);
-                            graphicsContext.strokeLine(top_center.x, top_center.y, top_right.x, top_right.y);
-                            graphicsContext.strokeLine(top_left.x, top_left.y, top_right.x, top_right.y);
-                            graphicsContext.strokeLine(top_left.x, top_left.y, bottom_left.x, bottom_left.y);
-                            graphicsContext.strokeLine(bottom_left.x, bottom_left.y, bottom_right.x, bottom_right.y);
-                            graphicsContext.strokeLine(bottom_right.x, bottom_right.y, top_right.x, top_right.y);
-
-                    }
-                });
+            @Override
+            public void handle(MouseEvent event) {
+                calculating_points(new Point((int) event.getX(), (int) event.getY()));
+                graphicsContext.strokeLine(top_center.x, top_center.y, top_left.x, top_left.y);
+                graphicsContext.strokeLine(top_center.x, top_center.y, top_right.x, top_right.y);
+                graphicsContext.strokeLine(top_left.x, top_left.y, top_right.x, top_right.y);
+                graphicsContext.strokeLine(top_left.x, top_left.y, bottom_left.x, bottom_left.y);
+                graphicsContext.strokeLine(bottom_left.x, bottom_left.y, bottom_right.x, bottom_right.y);
+                graphicsContext.strokeLine(bottom_right.x, bottom_right.y, top_right.x, top_right.y);
+            }
+        });
     }
-    
 
     @Override
     public void calculating_points(Point main_point) {
-        top_center = new Point(main_point.x, main_point.y + 6);
-        bottom_right = new Point(main_point.x + 6, main_point.y - 6);
-        top_left = new Point(main_point.x - 6, main_point.y );
-        top_right = new Point(main_point.x + 6, main_point.y);
-        bottom_left = new Point(main_point.x - 6, main_point.y - 6);
+        top_center = new Point(main_point.x, main_point.y - 10);
+        top_left = new Point(main_point.x + 10, main_point.y );
+        top_right = new Point(main_point.x - 10, main_point.y);
+        bottom_left = new Point(main_point.x + 10, main_point.y +10);
+        bottom_right = new Point(main_point.x - 10, main_point.y +10);
     }
+
+
 
 
 }
