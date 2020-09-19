f65d99ed0004daf2a3bb46e462a3f36ca43b2e30
sakibul-munnaa <sakibulislam1@iut-dhaka.edu>
2020-09-19 22:41:39 +0600

    #9 BangladeshiFarmer Class created and it will be implementing INation interface.

---
+++ solutions/60-munna-2/src/nations/BangladeshiFarmer.java
@@ -0,0 +1,23 @@
+package nations;
+
+public class BangladeshiFarmer implements INation{
+    @Override
+    public void drawHouse() {
+
+    }
+
+    @Override
+    public void drawTree() {
+
+    }
+
+    @Override
+    public void drawWaterSource() {
+
+    }
+
+    @Override
+    public void drawTerrain() {
+
+    }
+}
