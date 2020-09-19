02ea24b46ce615bbd720b5fb174d6dc1ef863573
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-20 04:50:49 +0600

    #5 #6 interface and save village

--- solutions/19-zarif/AgeOfVillagers/SaveVillageState.cs
+++ solutions/19-zarif/AgeOfVillagers/SaveVillageState.cs
@@ -4,8 +4,7 @@ using System.Text;
 
 namespace AgeOfVillagers
 {
-   public interface ICommandVillage
+    class SaveVillageState
     {
-        void Execute();
     }
 }
--- solutions/19-zarif/AgeOfVillagers/VillageState.cs
+++ solutions/19-zarif/AgeOfVillagers/VillageState.cs
@@ -4,8 +4,7 @@ using System.Text;
 
 namespace AgeOfVillagers
 {
-   public interface ICommandVillage
+    class VillageState
     {
-        void Execute();
     }
 }
