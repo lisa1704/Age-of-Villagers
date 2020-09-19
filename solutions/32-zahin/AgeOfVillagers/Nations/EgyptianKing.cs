879ab34ecb43dc4e6fc2264904f72b2c5194a8f4
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-19 17:00:13 +0600

    #11 tree for Egyptian

--- solutions/32-zahin/AgeOfVillagers/Nations/EgyptianKing.cs
+++ solutions/32-zahin/AgeOfVillagers/Nations/EgyptianKing.cs
@@ -30,7 +30,22 @@ namespace AgeOfVillagers
 
         public void DrawTree(Graphics g, Point point)
         {
-            MessageBox.Show("Drawing Tree for Egyptian Kings");
+            Point p1 = new Point(point.X + 25, point.Y - 25);
+            Point p2 = new Point(point.X - 25, point.Y - 25);
+            Point p3 = new Point(p1.X + 25, p1.Y - 25);
+            Point p4 = new Point(p1.X, p1.Y - 25);
+            Point p5 = new Point(p1.X + 25, point.Y - 25);
+            Point p6 = new Point(p2.X + 25, p2.Y - 25);
+            Point p7 = new Point(p2.X - 5, p2.Y - 25);
+            Point p8 = new Point(p2.X -25, p2.Y - 25);
+            g.DrawLine(pen, point, p1);
+            g.DrawLine(pen, point, p2);
+            g.DrawLine(pen, p1, p3);
+            g.DrawLine(pen, p1, p4);
+            g.DrawLine(pen, p1, p5);
+            g.DrawLine(pen, p2, p6);
+            g.DrawLine(pen, p2, p7);
+            g.DrawLine(pen, p2, p8); ;
         }
 
         public void DrawWaterSource(Graphics g, Point point)
