f74572c832fde54896b73f5e82f104380019fc32
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 04:09:17 +0600

    Added methods in the composite shaping class

--- solutions/75-arnob/Age of Villagers/CompositeShaping.cs
+++ solutions/75-arnob/Age of Villagers/CompositeShaping.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class CompositeShaping
+    public abstract class CompositeShaping : IShape
     {
+        private List<IShape> _components;
+
+        protected CompositeShaping()
+        {
+            _components = new List<IShape>();
+        }
+
+        protected void AddComponent(IShape shape)
+        {
+            _components.Add(shape);
+        }
+        public void Paint(Graphics graphics)
+        {
+            foreach (var component in _components)
+            {
+                component.Paint(graphics);
+            }
+        }
     }
 }
