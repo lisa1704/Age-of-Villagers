7cdde07867028d877b237059de9bfd7a06009a0d
Tasnuva12 <47001609+Tasnuva12@users.noreply.github.com>
2020-09-20 01:29:05 +0600

    #7 NullItem class completed

--- solutions/37-tasnuva/AgeOfVillegers/NullItem.cs
+++ solutions/37-tasnuva/AgeOfVillegers/NullItem.cs
@@ -5,5 +5,9 @@ namespace AgeOfVillegers
 {
 	internal class NullItem : IVillageItem
 	{
+		public void Draw(Point e, Graphics g, Pen p)
+		{
+			MessageBox.Show("please select a nation and an item first!");
+		}
 	}
 }
