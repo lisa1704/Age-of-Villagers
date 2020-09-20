c8dcd9c1e5f240bd9626a01d9d13bc7a565ba1d9
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 22:02:21 +0600

    #1 Method Arc and Triangle refactored

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Arc.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Arc.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Shape
 {
-    class Arc
+    class Arc : IShape
     {
+        public void Draw(Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Triangle.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Triangle.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Shape
 {
     class Triangle : IShape
     {
-
+        public void Draw(Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
