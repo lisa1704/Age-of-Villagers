022f87f499458e3cd7b7e6e0e46841474ee8cb61
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 17:41:25 +0600

    #9 BD farmer tree implemented

---
+++ solutions/75-arnob/Age of Villagers/Bangladeshi Farmer/BDfarmer_tree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_Villagers.Bangladeshi_Farmer
+{
+    class BDfarmer_tree
+    {
+        public BDfarmer_tree(Point point)
+        {
+            Point TopPoint = new Point(point.X + 60, point.Y + 70);
+            Point BottomRight = new Point(point.X + 28, point.Y + 100);
+            Point TopLeft = new Point(point.X + 33, point.Y + 45);
+
+            AddComponent(new Rectangle(TopLeft, BottomRight));
+            AddComponent(new EllipseShape(point, TopPoint));
+        }
+    }
+}
