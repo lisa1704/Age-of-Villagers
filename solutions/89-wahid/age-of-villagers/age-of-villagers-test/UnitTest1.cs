84b938316d8bc4326b6c5e3087289bfb372a3bc0
Md Wahid Shikder <www.wahid16@gmail.com>
2020-09-20 13:15:16 +0600

    #15 new test framework created

---
+++ solutions/89-wahid/age-of-villagers/age-of-villagers-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace age_of_villagers_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(89, 89);
+
+        }
+    }
+}
