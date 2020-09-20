99885c60d0172b71c1ed9e646afec1f99fbeab7a
Muhaimin <47084807+Muhai10@users.noreply.github.com>
2020-09-20 20:38:55 +0600

    #1 #10 bangladeshi water Source added as documented

--- solutions/10-muhaimin/Age of Villagers/BangladeshiWaterSource.cs
+++ solutions/10-muhaimin/Age of Villagers/BangladeshiWaterSource.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,18 @@ namespace Age_of_Villagers
     {
         public void drawWaterSource(Axis axes, Panel drawingSpace)
         {
-            throw new NotImplementedException();
+            Pen pen = new Pen(Color.Black);
+            Graphics elem = drawingSpace.CreateGraphics();
+
+            elem.DrawLine(pen, new PointF(axes.X , axes.Y - 6), new PointF(axes.X + 6, axes.Y - 12));
+            elem.DrawLine(pen, new PointF(axes.X + 6, axes.Y - 12), new PointF(axes.X + 8, axes.Y - 6));
+            elem.DrawLine(pen, new PointF(axes.X + 8, axes.Y - 6), new PointF(axes.X + 11, axes.Y - 12));
+            elem.DrawLine(pen, new PointF(axes.X + 11, axes.Y - 12), new PointF(axes.X + 14, axes.Y - 12));
+            elem.DrawLine(pen, new PointF(axes.X + 14, axes.Y - 12), new PointF(axes.X + 16, axes.Y - 6));
+            elem.DrawLine(pen, new PointF(axes.X + 16, axes.Y - 6), new PointF(axes.X + 11, axes.Y));
+            elem.DrawLine(pen, new PointF(axes.X + 11, axes.Y), new PointF(axes.X + 7, axes.Y - 2));
+            elem.DrawLine(pen, new PointF(axes.X + 7, axes.Y - 2), new PointF(axes.X + 3, axes.Y));
+            elem.DrawLine(pen, new PointF(axes.X + 3, axes.Y), new PointF(axes.X, axes.Y - 6));
         }
     }
 }
