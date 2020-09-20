52bb63619d46d62d9f0a1096184ef82db7a2ce39
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-21 00:42:28 +0600

    #9 #10 #11 #12 Interface for different village items created.

--- solutions/53-silvi/AgeOfVillagers/IVillageItem.cs
+++ solutions/53-silvi/AgeOfVillagers/IVillageItem.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AgeOfVillagers
 {
-	public interface IVillageItem
-	{
-		void Draw(Point e, Graphics g, Pen p);
-	}
+    interface IVillageItem
+    {
+
+    }
 }
