a0a10775e0a6f44d73d3dfbc6e21bb9013c2931b
Jawad Zamal <jawadzamal@iut-dhaka.edu>
2020-09-20 22:46:30 +0600

    #7 #8 interface INation created

---
+++ solutions/61-jawad/Age_of_villagers/Age_of_villagers/Age_of_villagers/Nations/INation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_villagers.Nations
+{
+    interface INation
+    {
+        string DrawHouse(Graphics g, Point pt);
+        void DrawTree();
+        void DrawWaterSource();
+    }
+}
