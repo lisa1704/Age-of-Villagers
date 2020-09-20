9733e559e5be216d0faeca8fe42b935a34d81a4f
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-20 22:23:12 +0600

    #9 tried drawing house

--- solutions/55-wasif/age-of-villagers/Bangladeshi_Farmers.cs
+++ solutions/55-wasif/age-of-villagers/Bangladeshi_Farmers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,23 @@ namespace age_of_villagers
 {
     class Bangladeshi_Farmers : INations
     {
+        Pen pen = new Pen(Color.Black);
+        Graphics gp;
         public void draw_house()
         {
-            throw new NotImplementedException();
+            
+            Point p = new Point(50, 50);
+            Point p1 = new Point(p.X + 25, p.Y - 25);
+            Point p2 = new Point(p.X + 50, p.Y);
+            Point p3 = new Point(p.X, p.Y + 25);
+            Point p4 = new Point(p2.X, p2.Y + 25);
+
+            gp.DrawLine(pen, p, p1);
+            gp.DrawLine(pen, p1, p2);
+            gp.DrawLine(pen, p2, p);
+            gp.DrawLine(pen, p, p2);
+            gp.DrawLine(pen, p, p3);
+            gp.DrawLine(pen, p3, p4);
         }
 
         public void draw_tree()
