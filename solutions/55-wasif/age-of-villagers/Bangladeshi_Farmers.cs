313b599006be56a7a0c4332ed973dcf09b9bc37d
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-20 22:33:13 +0600

    #9 tried drawing water source

--- solutions/55-wasif/age-of-villagers/Bangladeshi_Farmers.cs
+++ solutions/55-wasif/age-of-villagers/Bangladeshi_Farmers.cs
@@ -45,7 +45,24 @@ namespace age_of_villagers
 
         public void draw_watersource()
         {
-            throw new NotImplementedException();
+            Point point = new Point(50, 50);
+            Point p1 = new Point(point.X - 25, point.Y + 50);
+            Point p2 = new Point(point.X, point.Y + 75);
+            Point p3 = new Point(point.X + 25, point.Y + 50);
+            Point p4 = new Point(point.X + 50, point.Y + 75);
+            Point p5 = new Point(point.X + 75, point.Y + 25);
+            Point p6 = new Point(point.X + 50, point.Y);
+            Point p7 = new Point(point.X + 25, point.Y + 25);
+            Point p8 = new Point(point.X + 25, point.Y);
+            gp.DrawLine(pen, point, p1);
+            gp.DrawLine(pen, p1, p2);
+            gp.DrawLine(pen, p2, p3);
+            gp.DrawLine(pen, p3, p4);
+            gp.DrawLine(pen, p4, p5);
+            gp.DrawLine(pen, p5, p6);
+            gp.DrawLine(pen, p6, p7);
+            gp.DrawLine(pen, p7, p8);
+            gp.DrawLine(pen, p8, point);
         }
     }
 }
