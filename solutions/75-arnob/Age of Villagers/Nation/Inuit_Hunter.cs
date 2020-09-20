f163af3155f6b7f9d18c8924afb95812f5c15102
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-21 04:40:42 +0600

    #12 house implemented in nation

--- solutions/75-arnob/Age of Villagers/Nation/Inuit_Hunter.cs
+++ solutions/75-arnob/Age of Villagers/Nation/Inuit_Hunter.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class Inuit_Hunter
+    public class Inuit_Hunter : INation
     {
+        public void GetHouse(Graphics graphics, Point _point)
+        {
+            InuitHouse inuit_house = new InuitHouse(new Point(_point.X, _point.Y), new Point(_point.X + 16, _point.Y));
+            inuit_house.Paint(graphics);
+        }
+
+        string name;
+
+        public Inuit_Hunter(string name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return name;
+        }
+
+        public Color getTerrainColour()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetTree(Graphics graphics, Point _point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetWaterSource(Graphics graphics, Point _point)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
