94631b30398558b04cd26aaf9920df594a75d0f2
akib <akibahmed@iut-dhaka.edu>
2020-09-20 14:10:13 +0600

    #9 Details added for Bangladeshi_Farmers Class

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Bangladeshi_Farmer.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Bangladeshi_Farmer.cs
@@ -4,5 +4,27 @@ namespace AgeOfVillagers
 
 public class Bangladeshi_Farmer : Nation
 {
-	
+    private int x_water = 24, y_water = 16;
+
+	public I_Shape Draw_Tree(int x, int y)
+    {
+        tree = new Bangladeshi_Tree(x, y);
+        return tree;
+    }
+    public I_Shape Draw_House(int x, int y)
+    {
+        house = new Bangladeshi_House(x, y);
+        return house;
+    }
+    public I_Shape Draw_WaterRes(int x_water, int y_water)
+    {
+        water_res = new Bangladeshi_WaterRes(x_water, y_water);
+        return water_res;
+    }
+
+    public string BG_Color()
+    {
+        return "Plain Green color";
+    }
+
 }
---
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/I_Shape.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AgeOfVillagers
+
+public class I_Shape
+{
+	I_Shape Get_Shape(string shape);
+}
