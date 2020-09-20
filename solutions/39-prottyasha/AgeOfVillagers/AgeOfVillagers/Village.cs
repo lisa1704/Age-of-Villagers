e7f964345df52e2f7735366ffa08b3ca78cb23d3
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-21 02:31:10 +0600

    #5 nation name added in village info class

--- solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Village.cs
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Village.cs
@@ -9,5 +9,6 @@ namespace AgeOfVillagers
         public List<Point> trees_drawn;
         public List<Point> houses_drawn;
         public List<Point> rivers_drawn;
+        public string nation;
     }
 }
