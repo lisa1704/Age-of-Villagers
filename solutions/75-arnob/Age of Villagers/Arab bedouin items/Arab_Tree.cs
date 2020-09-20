dd08b9e6d8af2c83dff194112fd7646a6f2ec320
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-21 04:21:56 +0600

    #1 #10 edited code in arab tree

--- solutions/75-arnob/Age of Villagers/Arab bedouin items/Arab_Tree.cs
+++ solutions/75-arnob/Age of Villagers/Arab bedouin items/Arab_Tree.cs
@@ -7,24 +7,22 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers.Arab_bedouin
 {
-    class Arab_Tree : CompositeShaping
+    public class Arab_Tree : CompositeShaping
     {
-        public Arab_Tree(Point point)
-        {
-            Point point1 = new Point(point.X + 30, point.Y - 10);
-            Point point2 = new Point(point.X - 30, point.Y - 10);
-            Point point3 = new Point(point.X + 25, point.Y - 25);
-            Point point4 = new Point(point.X - 25, point.Y - 25);
-            Point point5 = new Point(point.X, point.Y - 30);
-            Point TopLeft = new Point(point.X - 3, point.Y);
-            Point RightBottom = new Point(point.X + 3, point.Y + 50);
+        
+        
+            public Arab_Tree(Point startPoint, Point point1, Point point2, Point point3, Point point4, Point point5, Point topLeftRectangle, Point bottomRightRectangle)
+            {
+                AddComponent(new Line(startPoint, point1));
+                AddComponent(new Line(startPoint, point2));
+                AddComponent(new Line(startPoint, point3));
+                AddComponent(new Line(startPoint, point4));
+                AddComponent(new Line(startPoint, point5));
 
-            AddComponent(new Rectangle(TopLeft, RightBottom));
-            AddComponent(new Line(point, point1));
-            AddComponent(new Line(point, point2));
-            AddComponent(new Line(point, point3));
-            AddComponent(new Line(point, point4));
-            AddComponent(new Line(point, point5));
-        }
+                // Rectangle
+                AddComponent(new Rectangle(topLeftRectangle, bottomRightRectangle));
+
+            }
+        
     }
 }
--- solutions/75-arnob/Age of Villagers/Nation/Arab_Bedouin.cs
+++ solutions/75-arnob/Age of Villagers/Nation/Arab_Bedouin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Age_of_Villagers.Arab_bedouin;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
