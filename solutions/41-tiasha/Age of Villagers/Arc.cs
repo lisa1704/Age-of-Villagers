267716b25a06ba21a260ab5f577e581a2fb0be4a
tiashaa <hriditatiasha@gmail.com>
2020-09-20 23:54:09 +0600

    creating shapes.

---
+++ solutions/41-tiasha/Age of Villagers/Arc.cs
@@ -0,0 +1,27 @@
+﻿using Age_of_Villagers;
+using System.Drawing;
+
+namespace AgeOfVillagers
+{
+    public class Arc : IComponent
+    {
+        public int x, y, width, height, startAngle, sweepAngle;
+
+        public Arc(int x, int y, int width, int height, int startAngle, int sweepAngle)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.startAngle = startAngle;
+            this.sweepAngle = sweepAngle;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+
+            g.DrawArc(pen, x, y, width, height, startAngle, sweepAngle);
+        }
+    }
+}
