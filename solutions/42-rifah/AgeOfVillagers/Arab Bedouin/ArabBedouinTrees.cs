3b70e514b4921b2d1254b774fcb9cfe14a2e02ee
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-20 08:12:42 +0600

    #7 #10 Arab Bedouin Tree implemented

--- solutions/42-rifah/AgeOfVillagers/Arab Bedouin/ArabBedouinTrees.cs
+++ solutions/42-rifah/AgeOfVillagers/Arab Bedouin/ArabBedouinTrees.cs
@@ -7,11 +7,23 @@ namespace AgeOfVillagers.Arab_Bedouin
 {
     class ArabBedouinTrees : Composite
     {
-        private Point point;
 
         public ArabBedouinTrees(Point point)
         {
-            this.point = point;
+            Point point1 = new Point(point.X + 30, point.Y - 10);
+            Point point2 = new Point(point.X - 30, point.Y - 10);
+            Point point3 = new Point(point.X + 25, point.Y - 20);
+            Point point4 = new Point(point.X - 25, point.Y - 20);
+            Point point5 = new Point(point.X, point.Y - 30);
+            Point TopLeft = new Point(point.X - 3, point.Y);
+            Point RightBottom = new Point(point.X + 3, point.Y + 50);
+
+            AddComponent(new Rectangle(TopLeft, RightBottom));
+            AddComponent(new Line(point, point1));
+            AddComponent(new Line(point, point2));
+            AddComponent(new Line(point, point3));
+            AddComponent(new Line(point, point4));
+            AddComponent(new Line(point, point5));
         }
     }
 }
