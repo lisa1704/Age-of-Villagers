81403110b10b2f92729de622d22ba0943f74b715
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-20 13:21:40 +0600

    #12 inuit house.

--- solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/InuitHunters.cs
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/InuitHunters.cs
@@ -9,22 +9,21 @@ namespace AgeOfVillagers
     {
         public void drawhouse(Point p, Graphics g, Pen pen)
         {
-            
-        }
-
-        public void drawtree(Point p, Graphics g)
-        {
-            throw new NotImplementedException();
+            g.DrawArc(pen, p.X, p.Y, 8, 8, 180, 180);
+            g.DrawArc(pen, p.X, p.Y, 3, 3, 180, 180);
+            Point p1 = new Point(p.X - 8, p.Y);
+            Point p2 = new Point(p.X + 8, p.Y);
+            g.DrawLine(pen, p1, p2);
         }
 
         public void drawtree(Point p, Graphics g, Pen pen)
         {
-            
+            //do nothing
         }
 
         public void drawwatersource(Point p, Graphics g, Pen pen)
         {
-            
+            //do nothing
         }
     }
 }
