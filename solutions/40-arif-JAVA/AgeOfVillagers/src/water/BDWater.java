b37f872c14b32db34af8e68d0ce140ff4830c735
Md. Ariful Islam, 170042040 <arifulislam40@iut-dhaka.edu>
2020-09-20 15:15:35 +0600

    #1 #9 bd water updated

--- solutions/40-arif-JAVA/AgeOfVillagers/src/water/BDWater.java
+++ solutions/40-arif-JAVA/AgeOfVillagers/src/water/BDWater.java
@@ -4,15 +4,17 @@ import canvas.MyCanvas;
 import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.RadioButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.ArcType;
 
 public class BDWater implements IWater {
+    boolean active = true;
     public BDWater(){
     }
 
     @Override
-    public void draw(Canvas canvas, String item) {
+    public void draw(Canvas canvas, RadioButton radioButton) {
 
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
@@ -20,15 +22,16 @@ public class BDWater implements IWater {
             @Override
             public void handle(MouseEvent mouseEvent) {
 
-                if(item.equals("water")){
+                if(active){
                     double x= mouseEvent.getX();
                     double y= mouseEvent.getY();
                     graphicsContext.strokeArc(x, y,40,50,200,360, ArcType.OPEN);
                     graphicsContext.lineTo(x,y);
                     graphicsContext.lineTo(x+10,y+20);
                     graphicsContext.strokeText("bd water", mouseEvent.getX(),mouseEvent.getY());
+                    radioButton.setSelected(false);
+                    active = false;
                 }
-//                System.out.println("kjdklsjflksdjf");
             }
         });
     }
