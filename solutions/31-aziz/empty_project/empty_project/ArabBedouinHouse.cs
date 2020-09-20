009f2d18d7304677945e66ba8331c80508776481
Asif Aziz <asifaziz2061@gmail.com>
2020-09-21 00:17:35 +0600

    #1 Refactored Arab Bedouin House Shape

--- solutions/31-aziz/empty_project/empty_project/ArabBedouinHouse.cs
+++ solutions/31-aziz/empty_project/empty_project/ArabBedouinHouse.cs
@@ -13,13 +13,13 @@ namespace empty_project
             Pen pen = new Pen(Color.Black);
             Graphics graphics = panel.CreateGraphics();
 
-            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 5, axis.Y -10));
-            graphics.DrawLine(pen, new PointF(axis.X - 5, axis.Y -10), new PointF(axis.X -10, axis.Y));
-            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X -10, axis.Y));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 24, axis.Y + 50));
+            graphics.DrawLine(pen, new PointF(axis.X - 24, axis.Y + 50), new PointF(axis.X + 16, axis.Y + 50));
+            graphics.DrawLine(pen, new PointF(axis.X + 16, axis.Y + 50), new PointF(axis.X, axis.Y));
 
-            graphics.DrawLine(pen, new PointF(axis.X - 5, axis.Y -10), new PointF(axis.X -10,axis.Y -10));
-            graphics.DrawLine(pen, new PointF(axis.X -10, axis.Y), new PointF(axis.X - 15, axis.Y));
-            graphics.DrawLine(pen, new PointF(axis.X -10, axis.Y -10), new PointF(axis.X - 15, axis.Y));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X + 25,axis.Y - 30));
+            graphics.DrawLine(pen, new PointF(axis.X + 25, axis.Y - 30), new PointF(axis.X + 41, axis.Y + 20));
+            graphics.DrawLine(pen, new PointF(axis.X + 41, axis.Y + 20), new PointF(axis.X + 16, axis.Y + 50));
         }
     }
 }
