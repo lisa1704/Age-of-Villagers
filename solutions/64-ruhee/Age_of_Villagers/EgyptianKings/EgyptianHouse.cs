18e42bc32520397c23821f438da7f29faa2a9d5e
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-21 00:57:36 +0600

    #1 #7 #8 #11 updated egyptian house.

--- solutions/64-ruhee/Age_of_Villagers/EgyptianKings/EgyptianHouse.cs
+++ solutions/64-ruhee/Age_of_Villagers/EgyptianKings/EgyptianHouse.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Age_of_Villagers.Age_of_Villagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.EgyptianKings
 {
-    class EgyptianHouse
+    class EgyptianHouse:CShape
     {
+        public EgyptianHouse(Point pt)
+        {
+            Point pt1 = new Point(pt.X - 25, pt.Y + 45);
+            Point pt2 = new Point(pt.X + 20, pt.Y + 50);
+            Point pt3 = new Point(pt.X + 30, pt.Y + 25);
+
+            AddComponent(new Triangle(pt2, pt1, pt));
+            AddComponent(new Triangle(pt3, pt2, pt));
+        }
     }
 }
