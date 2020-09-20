1bdf923568a5c8dbb2e925cc9c12f9127163df8f
Ratun <www.ratunrahman@gmail.com>
2020-09-20 20:29:43 +0600

    #11 implement EgyptianKings class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptianKings.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptianKings.cs
@@ -2,5 +2,27 @@
 
 public class EgyptianKings : Nation
 {
-	
+    private int waterX = 12, waterY = 12;
+    public IShape draw_tree(int x, int y)
+    {
+        tree = new Egypttree(x, y);
+        return tree;
+        //return "Drawing a tree. ";
+    }
+    public IShape draw_house(int x, int y)
+    {
+        house = new Egypthouse(x, y);
+        return house;
+        //return "Drawing a tree. ";
+    }
+    public IShape draw_water(int waterX, int waterY)
+    {
+        water = new Egyptwater(waterX, waterY);
+        return water;
+        //return "Drawing a tree. ";
+    }
+    public string backgroundColor()
+    {
+        return "plain yellow colour";
+    }
 }
