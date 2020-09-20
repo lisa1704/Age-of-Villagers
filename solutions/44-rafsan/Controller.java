75f2b60782dce8d15d49ce50acaa722e8a5fa5a5
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-09-20 20:29:42 +0600

    #4 added house button and its activity

--- solutions/44-rafsan/Controller.java
+++ solutions/44-rafsan/Controller.java
@@ -1,51 +1,30 @@
 package sample;
 import javafx.scene.control.Button;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+
+import java.io.File;
 public class Controller {
-    public Button button1;
-    public Button button2;
-    public Button button3;
-    public Button button4;
-    public Button button5;
-    public Button button6;
-    public Button button7;
 
-    public Button button8;
+    private CurrentState currentState = CurrentState.getInstance();
 
-    public void b1(){
-        button1.setText("button1");
+    private Canvas canvas;
+    private NationManager nationManager;
+    private IHouse iHouse;
+    @FXML
+    private RadioButton house;
+
+    @FXML
+    void selectHouse(ActionEvent event) {
+        if (house.isSelected()) {
+            iHouse = nationManager.getHouse();
+            iHouse.getCanvas();
+            iHouse.canvasController();
+        } else {
+            iHouse.releaseCanvas();
+        }
     }
-
-
-    public void b2(){
-        button2.setText("button2");
-    }
-
-    public void b3(){
-        button3.setText("button3");
-    }
-
-    public void b4(){
-        button4.setText("button4");
-    }
-
-    public void b5(){
-        button5.setText("button5");
-    }
-
-    public void b6(){
-        button6.setText("button6");
-    }
-
-
-    public void b7(){
-        button7.setText("button7");
-    }
-
-
-    public void b8(){
-        button8.setText("button8");
-    }
-
-
-
 }
