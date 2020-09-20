d2510d636107b74bfff67f4dc205c451c5428d7e
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-20 15:25:31 +0600

    #8 GetNations method added to NationFactory class

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/NationFactory.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/NationFactory.cs
@@ -6,5 +6,22 @@ namespace AgeOfVillagers
 {
     class NationFactory
     {
+        public INation GetNations(string nation)
+        {
+            if (nation == "Arab Bedouins")
+            {
+                return new ArabBedouins();
+            }
+            if (nation == "Bangladeshi Farmers")
+            {
+                return new BangladeshiFarmers();
+            }
+            if (nation == "Egyptian Kings")
+            {
+                return new EgyptianKings();
+            }
+            else
+                return new InuitHunters();
+        }
     }
 }
