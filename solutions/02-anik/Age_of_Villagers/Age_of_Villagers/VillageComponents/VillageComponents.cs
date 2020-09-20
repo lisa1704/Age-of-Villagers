467ed208af0fb2ec0440a68640d4c4e3a5590be4
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-21 03:37:45 +0600

    # 7 Village Components added

---
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/VillageComponents/VillageComponents.cs
@@ -0,0 +1,28 @@
+﻿using Age_of_Villagers.Shape;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers.VillageComponents
+{
+    
+       
+        public abstract class VillageComponents : IShape
+        {
+            private readonly List<IShape> Components;
+            public VillageComponents()
+            {
+            Components = new List<IShape>();
+            }
+            public void Draw(Graphics g, Pen p)
+            {
+                foreach (IShape Component in Components )
+                {
+                    Component.Draw(g, p);
+                }
+            }
+
+        }
+    }
+
