b609523d46549ef074efa6beb29dba2519d7d366
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 20:57:39 +0600

    #12 house item created

--- solutions/75-arnob/Age of Villagers/Inuit Hunter Items/Inuit_House.cs
+++ solutions/75-arnob/Age of Villagers/Inuit Hunter Items/Inuit_House.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers.Inuit_Hunter
 {
-    class Inuit_House
+    class Inuit_House : CompositeShaping
     {
+        public Inuit_House(Point startPoint, Point pt1)
+        {
+            AddComponent(new Line(startPoint, pt1));
+            AddComponent(new ArcShape(startPoint.X, startPoint.Y - 16, 16, 32, 180, 180));
+            AddComponent(new ArcShape(startPoint.X + 5, startPoint.Y - 6, 6, 12, 180, 180));
+
+        }
     }
 }
