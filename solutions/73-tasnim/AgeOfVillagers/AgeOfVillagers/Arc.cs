f91b7ce92ccd1ef39e635fe717621f788b7e62d2
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-20 21:57:45 +0600

    #7 Arc class added

---
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Arc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class Arc : IShape
+    {
+        public float x, y, width, heigth, initialangle, sweepAngle;
+        public Arc(float x, float y, float width, float heigth, float initialangle, float sweepAngle)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.heigth = heigth;
+            this.initialangle = initialangle;
+            this.sweepAngle = sweepAngle;
+        }
+
+        public void Draw_Shape(Graphics g)
+        {
+            var pen = new Pen(Color.Black, 1);
+            g.DrawArc(pen, x, y, width, heigth, initialangle, sweepAngle);
+        }
+    }
+}
