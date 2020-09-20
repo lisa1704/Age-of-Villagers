44f8cb14685a40701e9255a35096fc08bc26986e
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-20 20:22:47 +0600

    #7 #8 #11 EgyptianKings draw house implemented

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/EgyptianKings.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/EgyptianKings.cs
@@ -9,7 +9,15 @@ namespace AgeOfVillagers
     {
         public void drawhouse(Point p, Graphics g, Pen pen)
         {
-            throw new NotImplementedException();
+            Point p1 = new Point(p.X + 4, p.Y + 16);
+            Point p2 = new Point(p.X - 8, p.Y + 13);
+            Point p3 = new Point(p.X + 8, p.Y + 9);
+
+            g.DrawLine(pen, p, p1);
+            g.DrawLine(pen, p, p2);
+            g.DrawLine(pen, p, p3);
+            g.DrawLine(pen, p3, p1);
+            g.DrawLine(pen, p2, p1);
         }
 
         public void drawtree(Point p, Graphics g, Pen pen)
