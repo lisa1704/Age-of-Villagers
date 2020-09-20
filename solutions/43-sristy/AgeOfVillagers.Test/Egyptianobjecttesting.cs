43a5aa8d8511816ed5888e928ac9175b95f07a27
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 16:06:12 +0600

    #11 Egyptian house object creation unit test

---
+++ solutions/43-sristy/AgeOfVillagers.Test/Egyptianobjecttesting.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Age_of_villagers;
+using System.Drawing;
+
+namespace AgeOfVillagers.Test
+{
+    public class Egyptianobjecttesting
+    {
+        Nationfactory nationfactory = new Nationfactory();
+        Point lptop = new Point(200, 100);
+        [Fact]
+        public void EgypthouseTest()
+        {
+            IObject actual = nationfactory.GetNation("Egyptian").Draw_house(lptop);
+            IObject expected = new Egyptian_house(lptop);
+            actual.Equals(expected);
+        }
+    }
+}
