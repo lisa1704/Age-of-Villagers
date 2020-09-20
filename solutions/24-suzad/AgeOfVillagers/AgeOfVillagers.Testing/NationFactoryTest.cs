5688af17d9adcbe8698762338860918a1dc299a2
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-20 09:11:05 +0600

    #9 #10 #11 #12 nation factory testing

---
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers.Testing/NationFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AgeOfVillagers.Testing
+{
+    public class NationFactoryTest
+    {
+        [Fact]
+        public void NationAbstractFactoryTesting()
+        {
+            NationFactory nationFactory = new NationFactory();
+            var actual = nationFactory.getNation("Bangladeshi Farmer");
+            var expected = typeof(BdFarmerNation);
+            Assert.IsType(expected, actual);
+        }
+    }
+}
