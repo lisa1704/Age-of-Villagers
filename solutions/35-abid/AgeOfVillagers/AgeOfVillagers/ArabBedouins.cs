3779027d45c4bdadd77eebe3df8458ed03f3c99d
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-20 19:11:38 +0600

    #7 #8 #10 Arab Bedouin draw house implemented

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/ArabBedouins.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/ArabBedouins.cs
@@ -9,7 +9,16 @@ namespace AgeOfVillagers
     {
         public void drawhouse(Point p, Graphics g, Pen pen)
         {
-            throw new NotImplementedException();
+            Point p1 = new Point(p.X - 8, p.Y + 16);
+            Point p2 = new Point(p1.X + 16, p1.Y);
+            Point p3 = new Point(p.X + 8, p.Y - 3);
+            Point p4 = new Point(p2.X + 8, p3.Y + 16);
+            g.DrawLine(pen, p, p1);
+            g.DrawLine(pen, p, p2);
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p, p3);
+            g.DrawLine(pen, p3, p4);
+            g.DrawLine(pen, p2, p4);
         }
 
         public void drawtree(Point p, Graphics g, Pen pen)
