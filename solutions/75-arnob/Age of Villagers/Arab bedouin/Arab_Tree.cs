8b11d353ddafc80ff7e61cffc5d601374f4ccf0f
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 17:49:38 +0600

    #10 Arab tree implemented

--- solutions/75-arnob/Age of Villagers/Arab bedouin/Arab_Tree.cs
+++ solutions/75-arnob/Age of Villagers/Arab bedouin/Arab_Tree.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers.Arab_bedouin
 {
-    class Arab_Tree
+    class Arab_Tree : CompositeShaping
     {
+        public Arab_Tree(Point point)
+        {
+            Point point1 = new Point(point.X + 30, point.Y - 10);
+            Point point2 = new Point(point.X - 30, point.Y - 10);
+            Point point3 = new Point(point.X + 25, point.Y - 25);
+            Point point4 = new Point(point.X - 25, point.Y - 25);
+            Point point5 = new Point(point.X, point.Y - 30);
+            Point TopLeft = new Point(point.X - 3, point.Y);
+            Point RightBottom = new Point(point.X + 3, point.Y + 50);
+
+            AddComponent(new Rectangle(TopLeft, RightBottom));
+            AddComponent(new Line(point, point1));
+            AddComponent(new Line(point, point2));
+            AddComponent(new Line(point, point3));
+            AddComponent(new Line(point, point4));
+            AddComponent(new Line(point, point5));
+        }
     }
 }
