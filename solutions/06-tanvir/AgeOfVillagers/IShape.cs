a6a7d3018110e916c8c307fdd3d42432b0287bd9
Tanvirus06 <tanvirrahman3344@gmail.com>
2020-09-21 05:06:30 +0600

    Create IShape.cs

---
+++ solutions/06-tanvir/AgeOfVillagers/IShape.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AgeOfVillagers.Shapes
+{
+    interface IShape
+    {
+       void CreateShape();
+    }
+}
