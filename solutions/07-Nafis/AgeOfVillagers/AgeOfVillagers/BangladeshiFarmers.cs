2642dc99e3293d5d48036087e3d60dc661d7c5bb
nafis10670 <nafissazad@gmail.com>
2020-09-20 16:20:04 +0600

    #9 Changed Bangladeshi Farmers to only use drawLine and drawArc methods.

--- solutions/07-Nafis/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/07-Nafis/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
@@ -12,29 +12,36 @@ namespace AgeOfVillagers
 
         public void DrawHouse(int x, int y, Graphics g)
         {
-
-            Point[] points = { new Point(x - 1, y), new Point(x + 15, y - 10), new Point(x + 30, y) };
-            g.DrawRectangle(pen, x, y, 30, 15);
-            g.DrawPolygon(pen, points);
+            g.DrawLine(pen, new Point(x, y), new Point(x + 15, y - 10));
+            g.DrawLine(pen, new Point(x, y), new Point(x + 30, y));
+            g.DrawLine(pen, new Point(x + 15, y - 10), new Point(x + 30, y));
+            g.DrawLine(pen, new Point(x, y), new Point(x, y + 15));
+            g.DrawLine(pen, new Point(x + 30, y), new Point(x + 30, y + 15));
+            g.DrawLine(pen, new Point(x, y + 15), new Point(x + 30, y + 15));
         }
 
         public void DrawTree(int x, int y, Graphics g)
         {
-            g.DrawEllipse(pen, x - 15, y - 20, 30, 40);
-            g.DrawRectangle(pen, x, y, 2, 30);
-
+            g.DrawArc(pen, x - 15, y - 20, 30, 40, 0, 360);
+            g.DrawLine(pen, new Point(x - 1, y), new Point(x + 1, y));
+            g.DrawLine(pen, new Point(x - 1, y), new Point(x - 1, y + 30));
+            g.DrawLine(pen, new Point(x + 1, y), new Point(x + 1, y + 30));
+            g.DrawLine(pen, new Point(x - 1, y + 30), new Point(x + 1, y + 30));
         }
 
         public void DrawWater(int x, int y, Graphics g)
         {
-            Point[] points = { new Point(x, y), new Point(x + 8, y + 6), new Point(x + 16, y), new Point(x + 21, y + 5), new Point(x + 24, y - 4), new Point(x + 18, y - 10), new Point(x + 13, y - 4), new Point(x + 12, y - 9), new Point(x + 9, y - 8) };
-            g.DrawPolygon(pen, points);
+            g.DrawLine(pen, new Point(x, y), new Point(x + 8, y + 6));
+            g.DrawLine(pen, new Point(x + 8, y + 6), new Point(x + 16, y));
+            g.DrawLine(pen, new Point(x + 16, y), new Point(x + 21, y + 5));
+            g.DrawLine(pen, new Point(x + 21, y + 5), new Point(x + 24, y - 4));
+            g.DrawLine(pen, new Point(x + 24, y - 4), new Point(x + 18, y - 10));
+            g.DrawLine(pen, new Point(x + 18, y - 10), new Point(x + 13, y - 4));
+            g.DrawLine(pen, new Point(x + 13, y - 4), new Point(x + 12, y - 9));
+            g.DrawLine(pen, new Point(x + 12, y - 9), new Point(x + 9, y - 8));
+            g.DrawLine(pen, new Point(x + 9, y - 8), new Point(x, y));
+
         }
 
-        
-        
-        
-
-        
     }
 }
