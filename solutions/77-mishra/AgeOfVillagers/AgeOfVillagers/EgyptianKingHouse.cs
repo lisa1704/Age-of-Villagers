628d8f9cac82769e05ae8e9be51dd0d797277e93
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-09-20 07:43:57 +0600

    #1 #11 interfaces for nation and shapes applied, Egyptian functionalities created

---
+++ solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/EgyptianKingHouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    public class EgyptianKingHouse : IShape
+    {
+        public void draw(Point p, Panel drawingBoard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
