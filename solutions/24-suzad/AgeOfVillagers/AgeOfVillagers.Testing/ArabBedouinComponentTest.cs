1c34c060314322c0291e9abd26cec506139a47f4
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-20 07:58:42 +0600

    #10 arab house testing

---
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers.Testing/ArabBedouinComponentTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Xunit;
+
+namespace AgeOfVillagers.Testing
+{
+    public class ArabBedouinComponentTest
+    {
+        [Fact]
+        public void ArabBedouinHouseTest()
+        {
+            Point p = new Point(10, 10);
+            ArabBedouinHouse obj = new ArabBedouinHouse(p);
+            Point actual = obj.getPoint();
+            Assert.Equal(actual, p);
+        }
+    }
+}
