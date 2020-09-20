7724189ef399bb3320715fff63ad19b5a618bc4c
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-20 19:11:43 +0600

    #5 Json package installed and VillageStateSaver class updated

---
+++ solutions/33-nafisa/AgeOfVillagers/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    interface ICommand
+    {
+        public void execute();
+    }
+}
---
+++ solutions/33-nafisa/AgeOfVillagers/VillageStateSaver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    class VillageStateSaver : ICommand
+    {
+        private VillageState village;
+        SaveFileDialog imgDialog;
+        public VillageStateSaver(VillageState village)
+        {
+            this.village = village;
+        }
+        public void execute()
+        {
+            this.imgDialog = new SaveFileDialog();
+           // imgDialog.InitialDirectory = "";
+            imgDialog.FileName = "*.aov";
+            imgDialog.DefaultExt = "aov";
+            imgDialog.Filter = "Aov Files | *.aov";
+            if (imgDialog.ShowDialog()== DialogResult.OK)
+            {
+                Stream filestream = imgDialog.OpenFile();
+                using (StreamWriter _sw = new StreamWriter(filestream))
+                {
+                    Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                    serializer.Serialize(_sw, village);
+                    _sw.Close();
+                }
+                filestream.Close();
+            }
+        }
+    }
+}
