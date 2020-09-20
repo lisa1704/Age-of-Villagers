953d12a87191bb8903132455a4e7b95158c8fb31
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-20 20:29:48 +0600

    #11 egyptian king class implemented

--- solutions/09-rafid/age of villagers/age of villagers/age of villagers/Egyptian_Kings.cs
+++ solutions/09-rafid/age of villagers/age of villagers/age of villagers/Egyptian_Kings.cs
@@ -2,5 +2,28 @@
 
 public class Egyptians_kings : Nation
 {
-	
+    private int waterX = 12, waterY = 12;
+    public Ishape draw_tree(int x, int y)
+    {
+        tree = new Egypttree(x, y);
+        return tree;
+    }
+
+    public IShape draw_house(int x, int y)
+    {
+        house = new Egypthouse(x, y);
+        return house;
+    }
+
+    public IShape draw_water(int waterX, int waterY)
+    {
+        water = new Egyptwater(waterX, waterY);
+        return water;
+    }
+
+    public string backgroundColor()
+    {
+        return "plain green colour";
+    }
+
 }
