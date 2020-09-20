8fb1d9822deb67acd3c4d6cee97e5338209ca374
akib <akibahmed@iut-dhaka.edu>
2020-09-20 14:21:42 +0600

    #10 Arab_Bedouin Class Implementation

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Arab_Bedouin.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Arab_Bedouin.cs
@@ -2,7 +2,25 @@
 
 namespace AgeOfVillagers
 
-public class Arab_Bedouin
+public class Arab_Bedouin : Nation
 {
-	
+    public I_Shape Draw_Tree(int x, int y)
+    {
+        tree = new Arab_Tree(x, y);
+        return tree;
+    }
+    public I_Shape Draw_House(int x, int y)
+    {
+        house = new Arab_House(x, y);
+        return house;
+    }
+    public I_Shape Draw_WaterRes(int x_water, int y_water)
+    {
+        return NotSupportedException;
+    }
+
+    public string BG_Color()
+    {
+        return "Yellowish";
+    }
 }
