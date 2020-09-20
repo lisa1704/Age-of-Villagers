be85f9cb75c4ec1ce30bd75b792553a76d50a4c1
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 20:43:27 +0600

    #9 water source implemented

---
+++ solutions/75-arnob/Age of Villagers/Bangladeshi Farmer Items/BDfarmer_WaterSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_Villagers.Bangladeshi_Farmer
+{
+    class BDfarmer_WaterSource : CompositeShaping
+    {
+        public BDfarmer_WaterSource (Point startPoint, Point pt1, Point pt2, Point pt3, Point pt4, Point pt5, Point pt6, Point pt7, Point pt8)
+        {
+            AddComponent(new Line(startPoint, pt1));
+            AddComponent(new Line(pt1, pt2));
+            AddComponent(new Line(pt2, pt3));
+            AddComponent(new Line(pt3, pt4));
+            AddComponent(new Line(pt4, pt5));
+            AddComponent(new Line(pt5, pt6));
+            AddComponent(new Line(pt6, pt7));
+            AddComponent(new Line(pt7, pt8));
+            AddComponent(new Line(pt8, startPoint));
+        }
+
+    }
+}
