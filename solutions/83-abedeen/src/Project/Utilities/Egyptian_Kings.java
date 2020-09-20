a30019561c875079c6eed93c0acbafac04d64cbf
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-21 00:54:17 +0600

    #7 #11 water source Draw implemented for Egyptian Kings

--- solutions/83-abedeen/src/Project/Utilities/Egyptian_Kings.java
+++ solutions/83-abedeen/src/Project/Utilities/Egyptian_Kings.java
@@ -1,5 +1,8 @@
 package Project.Utilities;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
@@ -48,7 +51,20 @@ public class Egyptian_Kings implements INations{
 
     @Override
     public ArrayList<Shape> drawWaterBody(double x, double y) {
-        return null;
+        ArrayList<Shape> WaterSource = new ArrayList<Shape>();
+        Arc arc = new Arc();
+        arc.setCenterX(x);
+        arc.setCenterY(y);
+        arc.setRadiusX(WaterBodyWidth/2);
+        arc.setRadiusY(WaterBodyWidth/2);
+        arc.setType(ArcType.OPEN);
+        arc.setStroke(Color.BLACK);
+        arc.setStrokeWidth(2);
+        arc.setFill(Color.TRANSPARENT);
+        arc.setStartAngle(0.0);
+        arc.setLength(1000.0);
+        WaterSource.add(arc);
+        return WaterSource;
     }
 
     @Override
