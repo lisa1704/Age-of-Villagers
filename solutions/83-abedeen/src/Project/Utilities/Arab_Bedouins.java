473dc8e1e1a0425c806311ac94121d8825c602bb
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-20 20:34:05 +0600

    #7 #8 #10 Tree Drawn for Arab Bedouins

--- solutions/83-abedeen/src/Project/Utilities/Arab_Bedouins.java
+++ solutions/83-abedeen/src/Project/Utilities/Arab_Bedouins.java
@@ -2,6 +2,7 @@ package Project.Utilities;
 
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
+import javafx.scene.transform.Rotate;
 
 import java.util.ArrayList;
 
@@ -29,7 +30,23 @@ public class Arab_Bedouins implements INations{
 
     @Override
     public ArrayList<Shape> drawTree(double x, double y) {
-        return null;
+        ArrayList<Shape> tree = new ArrayList<Shape>();
+        double trunk_height = TreeHeight - (TreeWidth/2);
+        double half_width = TreeWidth/2;
+        //Rectangle
+        tree.add(new Line(x-1,y,x+1,y));
+        tree.add(new Line(x-1,y+trunk_height,x+1,y+trunk_height));
+        tree.add(new Line(x-1,y,x-1,y+trunk_height));
+        tree.add(new Line(x+1,y,x+1,y+trunk_height));
+
+        //Lines
+        Line l1 = new Line(x,y,x+(half_width),y-half_width/3); tree.add(l1);
+        Line l2 = new Line(x,y,x-(half_width),y-half_width/3); tree.add(l2);
+        Line l3 = new Line(x,y,x+(3*half_width/4),y-(2*half_width/3)); tree.add(l3);
+        Line l4 = new Line(x,y,x-(3*half_width/4),y-(2*half_width/3)); tree.add(l4);
+        Line l5 = new Line(x,y,x,y-half_width); tree.add(l5);
+
+        return tree;
     }
 
     @Override
