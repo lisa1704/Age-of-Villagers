80ae5eedf21a64d594945ae3b1d55ace9d4335e3
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-20 20:54:34 +0600

    #7 house created

---
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/House.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ageofvillagers
+{
+    internal abstract class House
+    {
+        int Xaxis = 16;
+        int Yaxis = 16;
+
+        public abstract void CreateTree();
+    }
+}
