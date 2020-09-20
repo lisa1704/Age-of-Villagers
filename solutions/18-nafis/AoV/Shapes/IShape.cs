6e0970e0eb7da65dec34e624832746ace034c542
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2020-09-21 01:37:19 +0600

    Shape interface and Line Added

---
+++ solutions/18-nafis/AoV/Shapes/IShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoV.Shapes
+{
+    interface IShape
+    {
+        void Draw(Graphics g);
+    }
+}
---
+++ solutions/18-nafis/AoV/Shapes/Line.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoV.Shapes
+{
+    class Line : IShape
+    {
+        private Point p1, p2;
+        public Line(Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+        public void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 1);
+            g.DrawLine(pen, p1, p2);
+        }
+    }
+}
