631e8597e6c2011f1dd08c57c7f60ff72b39fb60
Asif Aziz <asifaziz2061@gmail.com>
2020-09-20 16:22:58 +0600

    #5 #6 installed dependency

--- solutions/31-aziz/empty_project/empty_project/VillageSaveOpen.cs
+++ solutions/31-aziz/empty_project/empty_project/VillageSaveOpen.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
 using System.Drawing;
+using Newtonsoft.Json;
 
 namespace empty_project
 {
     class VillageSaveOpen
     {
+        protected Village villageState;
+        protected string villageSerialized;
+
+        public VillageSaveOpen(Village village)
+        {
+            villageState = village;
+        }
+        public void saveVillage(Village village)
+        {
+            villageState = village;
+            villageSerialized = JsonConvert.SerializeObject(villageState);
+        }
     }
 }
