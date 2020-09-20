3e2db825ed5f0f526e55a7ae37e41cbe7f2a2eb6
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-20 08:40:10 +0600

    #10 arab bedouin component factory testing

---
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers.Testing/NationComponentFactoryTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Xunit;
+
+namespace AgeOfVillagers.Testing
+{
+    public class NationComponentFactoryTest
+    {
+        [Fact]
+        public void ArabBedouinComponentFactoryTesting()
+        {
+            Point p = new Point(100, 100);
+            ArabBedouinComponentFactory arabBedouinComponentFactory = new ArabBedouinComponentFactory();
+            var expected = typeof(ArabBedouinHouse);
+            Assert.IsType(expected, arabBedouinComponentFactory.getComponent("House", p));
+        }
+    }
+}
