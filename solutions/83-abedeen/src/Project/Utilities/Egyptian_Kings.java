1a6bae43415b5c7f595c4e742a192be1f57e99de
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-21 00:46:47 +0600

    #7 #11 Draw tree implemented for Egyptian Kings

--- solutions/83-abedeen/src/Project/Utilities/Egyptian_Kings.java
+++ solutions/83-abedeen/src/Project/Utilities/Egyptian_Kings.java
@@ -35,7 +35,15 @@ public class Egyptian_Kings implements INations{
 
     @Override
     public ArrayList<Shape> drawTree(double x, double y) {
-        return null;
+        double half_width = TreeWidth/2;
+        ArrayList<Shape> tree = new ArrayList<>();
+        tree.add(new Line(x,y,x-half_width+2,y-TreeHeight));
+        tree.add(new Line(x,y,x+half_width-2,y-TreeHeight));
+        tree.add(new Line(x-half_width/2,y-TreeHeight/2,x-2,y-TreeHeight));
+        tree.add(new Line(x-half_width/2,y-TreeHeight/2,x-half_width,y-TreeHeight*2/3));
+        tree.add(new Line(x+half_width/3,y-TreeHeight/3,x+2,y-TreeHeight));
+        tree.add(new Line(x+half_width/3,y-TreeHeight/3,x+half_width,y-TreeHeight*2/3));
+        return tree;
     }
 
     @Override
