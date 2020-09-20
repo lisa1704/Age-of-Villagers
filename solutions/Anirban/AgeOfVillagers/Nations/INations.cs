8353d9dbfba3da62aab6d5507a3c4de1047486aa
Anirban Azad <azadarannya@gmail.com>
2020-09-21 00:52:53 +0600

    #8 nations created

---
+++ solutions/Anirban/AgeOfVillagers/Nations/INations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.Nations
+{
+    public interface INations
+    {
+        void Draw_Tree();
+        void Draw_House();
+        void Draw_Water();
+
+        void terrain_Color();
+    }
+}
