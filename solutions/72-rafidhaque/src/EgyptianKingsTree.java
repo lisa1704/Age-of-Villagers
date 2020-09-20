636d09795117843cfb42bf23a7d96e004eda1cea
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-09-20 06:11:32 +0600

    #11 Drawn Trees for Egyptian Kings

--- solutions/72-rafidhaque/src/EgyptianKingsTree.java
+++ solutions/72-rafidhaque/src/EgyptianKingsTree.java
@@ -1,4 +1,5 @@
 import javafx.scene.Group;
+import javafx.scene.shape.Line;
 
 public class EgyptianKingsTree extends DrawTree {
     public EgyptianKingsTree(Group g) {
@@ -7,6 +8,12 @@ public class EgyptianKingsTree extends DrawTree {
 
     @Override
     public void draw() {
-
+        Line line1 = new Line(x, y, x + 8, y + 12);
+        Line line2 = new Line(x + 16, y, x + 8, y + 12);
+        Line line3 = new Line(x + 4, y + 6, x + 6, y);
+        Line line4 = new Line(x + 12, y + 6, x + 9, y);
+        Line line5 = new Line( x + 8, y + 12, x + 8, y + 24);
+        g.getChildren().addAll(line1, line2, line3, line4, line5);
+        drawOuterLayer();
     }
 }
