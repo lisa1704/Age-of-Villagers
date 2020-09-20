857573e674992b374b513b477529af75f268a364
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-21 01:08:36 +0600

    #1changed code in BD_tree item

--- solutions/75-arnob/Age of Villagers/Bangladeshi Farmer Items/BDfarmer_tree.cs
+++ solutions/75-arnob/Age of Villagers/Bangladeshi Farmer Items/BDfarmer_tree.cs
@@ -11,12 +11,11 @@ namespace Age_of_Villagers.Bangladeshi_Farmer
     {
         public BDfarmer_Tree(Point point, Point point1, Point point2)
         {
-            Point TopPoint = new Point(point.X + 60, point.Y + 70);
-            Point BottomRight = new Point(point.X + 28, point.Y + 100);
-            Point TopLeft = new Point(point.X + 33, point.Y + 45);
+    }
 
-            AddComponent(new Rectangle(TopLeft, BottomRight));
-            AddComponent(new EllipseShape(point, TopPoint));
+        public BDfarmer_Tree(Point mainPoint, Point bottomRight, Point topPoint, Point topLeft)
+        {
+            AddComponent(new Rectangle(topLeft, bottomRight));
+            AddComponent(new EllipseShape(mainPoint, topPoint));
         }
     }
-}
