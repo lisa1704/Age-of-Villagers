4ab85b4899dac177803910bb10cfbdf81d743ccb
Anirban Azad <azadarannya@gmail.com>
2020-09-21 01:18:28 +0600

    #11 Egyptian Kings created

---
+++ solutions/Anirban/AgeOfVillagers/Nations/Egyptian_kings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.Nations
+{
+    class Egyptian_kings : INations
+    {
+        public void Draw_House()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_Tree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw_Water()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void terrain_Color()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
