acec5560f335192cdf628e5773e9ed591f43f795
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 22:24:31 +0600

    #7 Rectangle funtionality added

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Rectangle.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Rectangle.cs
@@ -7,12 +7,29 @@ namespace Age_of_Villagers.Shape
 {
     class Rectangle : IShape
     {
-       
-            public void Draw(Graphics g, Pen p)
+        private readonly Point tl;
+        private readonly Point tr;
+        private readonly Point bl;
+        private readonly Point br;
+
+        public Rectangle(Point topLeft, Point bottomRight)
         {
-            throw new NotImplementedException();
+            this.tl = tl;
+            this.tr = new Point(br.X, tl.Y);
+            this.bl = new Point(tl.X, br.Y);
+            this.br = br;
         }
 
-        
+        public void Draw(Graphics g, Pen p)
+        {
+            new Line(tl, tr).Draw(g, p);
+            new Line(tl, bl).Draw(g, p);
+            new Line(bl, br).Draw(g, p);
+            new Line(br, tr).Draw(g, p);
+        }
+
+
     }
 }
+
+  
