f75db2e961eb712348b67ef519ba9b191bc246fe
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 19:23:14 +0600

    #11 built egyptian tree

--- solutions/75-arnob/Age of Villagers/Egyptian King/Egyptian_Tree.cs
+++ solutions/75-arnob/Age of Villagers/Egyptian King/Egyptian_Tree.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers.Egyptian_King
 {
-    class Egyptian_Tree
+    class Egyptian_Tree : CompositeShaping
     {
+        public Egyptian_Tree(Point startPoint, Point pt1, Point pt2, Point pt3, Point pt4, Point pt5, Point pt6, Point pt7, Point pt8)
+        {
+            AddComponent(new Line(startPoint, pt1));
+            AddComponent(new Line(startPoint, pt2));
+            AddComponent(new Line(pt1, pt3));
+            AddComponent(new Line(pt2, pt4));
+            AddComponent(new Line(pt1, pt5));
+            AddComponent(new Line(pt1, pt6));
+            AddComponent(new Line(pt2, pt7));
+            AddComponent(new Line(pt2, pt8));
+
+        }
     }
 }
