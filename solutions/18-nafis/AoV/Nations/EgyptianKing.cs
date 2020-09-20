f61f201cc898677702cd55a7239c7231f48da728
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2020-09-21 00:16:46 +0600

    #11 Egyptian Kings Added

---
+++ solutions/18-nafis/AoV/Nations/EgyptianKing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoV.Nations
+{
+    class EgyptianKing : INation
+    {
+        public void ColorTerrain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawHouse(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
