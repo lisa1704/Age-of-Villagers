cbd05075d87ef4a6b9f231bfb49ac90906c565e1
Ratun <www.ratunrahman@gmail.com>
2020-09-20 20:36:37 +0600

    #12 implement Inuit Hunters

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHunters.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHunters.cs
@@ -2,5 +2,22 @@
 
 public class InuitHunters : Nation
 {
-	
+    public IShape draw_tree()
+    {
+        return NotSupportedException;
+    }
+    public IShape draw_house(int x, int y)
+    {
+        house = new Inuithouse(x, y);
+        return house;
+        //return "Drawing a tree. ";
+    }
+    public IShape draw_water()
+    {
+        return NotSupportedException;
+    }
+    public string backgroundColor()
+    {
+        return "pure white";
+    }
 }
