b0abc5ce95284c7d3312858749e4791c68c3e739
Md Wahid Shikder <www.wahid16@gmail.com>
2020-09-21 01:44:13 +0600

    #9 method created in Ination and bangladeshi farmers

--- solutions/89-wahid/age-of-villagers/age-of-villagers/nation/Ination.cs
+++ solutions/89-wahid/age-of-villagers/age-of-villagers/nation/Ination.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace age_of_villagers.nation
 {
     interface Ination
     {
+        string getnationname();
+        Color getterraintcolor();
     }
 }
--- solutions/89-wahid/age-of-villagers/age-of-villagers/nation/bangladeshifarmers.cs
+++ solutions/89-wahid/age-of-villagers/age-of-villagers/nation/bangladeshifarmers.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace age_of_villagers.nation
 {
     class bangladeshifarmers : Ination
     {
+        public string getnationname()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color getterraintcolor()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
