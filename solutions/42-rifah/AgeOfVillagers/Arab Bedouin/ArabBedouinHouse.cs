eca8ea250732b8861840199bef8ef4bf7abefe0d
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-20 07:59:22 +0600

    #7 #10 ArabBedouinHouse implemented

--- solutions/42-rifah/AgeOfVillagers/Arab Bedouin/ArabBedouinHouse.cs
+++ solutions/42-rifah/AgeOfVillagers/Arab Bedouin/ArabBedouinHouse.cs
@@ -9,7 +9,16 @@ namespace AgeOfVillagers.Arab_Bedouin
     {
         public ArabBedouinHouse(Point point)
         {
-          
+            Point point1 = new Point(point.X - 30, point.Y + 50);
+            Point point2 = new Point(point.X + 30, point.Y + 50);
+            Point point3 = new Point(point.X + 38, point.Y - 18);
+            Point point4 = new Point(point2.X + 35, point2.Y - 20);
+
+
+            AddComponent(new Triangle(point, point1, point2));
+            AddComponent(new Line(point, point3));
+            AddComponent(new Line(point2, point4));
+            AddComponent(new Line(point3, point4));
 
         }
     }
