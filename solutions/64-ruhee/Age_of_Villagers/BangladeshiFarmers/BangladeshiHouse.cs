a5035f9f8395ccf4e0ea6fa6142b37c1a0811acf
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 23:20:54 +0600

    #1 #7 #8 #9 bangladeshi house created.

---
+++ solutions/64-ruhee/Age_of_Villagers/BangladeshiFarmers/BangladeshiHouse.cs
@@ -0,0 +1,22 @@
+﻿using Age_of_Villagers.Age_of_Villagers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers.BangladeshiFarmers
+{
+    class BangladeshiHouse:CShape
+    {
+        public BangladeshiHouse(Point pt)
+        {
+            Point Top = new Point(pt.X, pt.Y);
+            Point TopLeft = new Point(pt.X - 50, pt.Y + 30);
+            Point BottomRight = new Point(pt.X + 50, pt.Y + 80);
+            Point TopRight = new Point(BottomRight.X, TopLeft.Y);
+
+            AddComponent(new Age_of_Villagers.Rectangle(TopLeft, BottomRight));
+            AddComponent(new Triangle(Top, TopLeft, TopRight));
+        }
+    }
+}
