5341ec6d41c91aee4a7ea7147969b73a210af4ad
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-20 18:05:38 +0600

    #7 #8 #9 BangladeshiFarmers draw tree implemented

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
@@ -20,10 +20,17 @@ namespace AgeOfVillagers
             g.DrawLine(pen, p2, p4);
             g.DrawLine(pen, p3, p4);
         }
-
         public void drawtree(Point p, Graphics g, Pen pen)
         {
-            throw new NotImplementedException();
+            g.DrawArc(pen, p.X, p.Y, 16, 16, 0, 360);
+            Point p1 = new Point(p.X + 7, p.Y + 8);
+            Point p2 = new Point(p.X + 9, p.Y + 8);
+            Point p3 = new Point(p1.X, p1.Y + 13);
+            Point p4 = new Point(p2.X, p2.Y + 13);
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p1, p3);
+            g.DrawLine(pen, p4, p2);
+            g.DrawLine(pen, p3, p4);
         }
 
         public void drawwatersrc(Point p, Graphics g, Pen pen)
