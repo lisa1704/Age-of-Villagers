a24a7a82017fa0448b3ad004b843530f3247a79c
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 15:32:45 +0600

    #12 #1 refactored again

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Nation/InunitHunters.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Nation/InunitHunters.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Nation
 {
     class InunitHunters : INation
     {
+        private Color Colorbackground = Color.White;
+
 
     }
 
