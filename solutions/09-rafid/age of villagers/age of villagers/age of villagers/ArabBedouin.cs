87899e000752d9034d2d56183bf64b4908fa5e49
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-20 20:22:19 +0600

    #10 arab bedouin class implemented

--- solutions/09-rafid/age of villagers/age of villagers/age of villagers/ArabBedouin.cs
+++ solutions/09-rafid/age of villagers/age of villagers/age of villagers/ArabBedouin.cs
@@ -1,6 +1,29 @@
 ﻿using System;
 
-public class ArabBedouin
+public class ArabBedouin : Nation
 {
-	
+    
+    public Ishape draw_tree(int x, int y)
+    {
+        tree = new Arabtree(x, y);
+        return tree;
+    }
+
+    public IShape draw_house(int x, int y)
+    {
+        house = new Arabhouse(x, y);
+        return house;
+    }
+    public IShape draw_water()
+    {
+       
+        return unavailable;
+    }
+
+    public string backgroundColor()
+    {
+        return "yellowish";
+    }
+
+
 }
