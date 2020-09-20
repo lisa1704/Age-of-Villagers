d34b60ac4e494be55b71322485263f14c3b8f772
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-21 01:43:53 +0600

    #9 Class BangladeshiTree updated.

--- solutions/53-silvi/AgeOfVillagers/BangladeshiTree.cs
+++ solutions/53-silvi/AgeOfVillagers/BangladeshiTree.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using AgeOfVillagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace AgeOfVillagers
+namespace Age_Of_Villagers
 {
-    class BangladeshiTree
+    class BangladeshiTree : IVillageItem
     {
+    public void Draw(Point e, Graphics g, Pen p)
+        {
+            int x = e.X;
+            int y = e.Y;
+            g.DrawLine(p, x - 3, y, x + 3, y);
+            g.DrawLine(p, x + 3, y, x + 3, y + 50);
+            g.DrawLine(p, x + 3, y + 50, x - 3, y + 50);
+            g.DrawLine(p, x - 3, y, x - 3, y + 50);
+            g.DrawArc(p, x - 40, y - 50, 80, 80, 0, 360);
+        }
     }
+    
 }
