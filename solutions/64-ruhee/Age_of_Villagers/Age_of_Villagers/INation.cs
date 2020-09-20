b25225b79e2f6b5cfb488064101c2e22cdb5eaed
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 18:23:23 +0600

    #1 #8 Updated Interface INation.

--- solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/INation.cs
+++ solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/INation.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Age_of_Villagers
 {
-    interface INation
+    public interface INation
     {
-
+        void DrawTree(Graphics g, Point point);
+        void DrawHouse(Graphics g, Point point);
+        void DrawWaterSource(Graphics g, Point point);
+        Color BackgroundColor();
     }
 }
