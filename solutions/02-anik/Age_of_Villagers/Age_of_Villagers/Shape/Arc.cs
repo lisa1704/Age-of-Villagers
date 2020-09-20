b912a62936435a4a994fe51f9ff1d2bec7051d4a
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 22:30:15 +0600

    #7 Arc class implemented

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Arc.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Arc.cs
@@ -7,9 +7,21 @@ namespace Age_of_Villagers.Shape
 {
     class Arc : IShape
     {
+        private readonly Point point;
+        private float x, y, width, height, startAngle, sweepAngle;
+
+        public Arc(Point topLeft, Point bottomRight)
+        {
+            x = topLeft.X;
+            y = topLeft.Y;
+            width = bottomRight.X - topLeft.X;
+            height = bottomRight.Y - topLeft.Y;
+            startAngle = 0;
+            sweepAngle = 180;
+        }
         public void Draw(Graphics g, Pen p)
         {
-            throw new NotImplementedException();
+            g.DrawArc(p, x, y, width, height, startAngle, sweepAngle);
         }
     }
 }
