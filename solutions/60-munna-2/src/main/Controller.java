47c1d69e7cb89051dffe3bfdc1c8cd873fe84549
sakibul-munnaa <sakibulislam1@iut-dhaka.edu>
2020-09-20 23:35:57 +0600

    #7 #9 Bangladeshi House, Tree, Pond can be drawn on the drawing space now.
    At first I am targeting to draw shapes for Bangladeshi Farmers only. Other nations options have not structured yet.

--- solutions/60-munna-2/src/main/Controller.java
+++ solutions/60-munna-2/src/main/Controller.java
@@ -1,6 +1,8 @@
 package main;
 
 import house.BangladeshiFarmersHouse;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,12 +10,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import nations.BangladeshiFarmer;
 import tree.BangladeshiFarmersTree;
 import waterSource.Pond;
 
@@ -22,6 +23,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private ToggleGroup toggleGroup;
+
     @FXML
     private Pane main_pane;
 
@@ -55,7 +60,9 @@ public class Controller implements Initializable {
     public static Label vn_label,cn_label;
 
     @FXML
-    public Canvas drawing_space_canvas;
+    private Canvas drawing_space_canvas;
+
+    public static Canvas d1,d2,d3;
 
 
     @FXML
@@ -68,30 +75,86 @@ public class Controller implements Initializable {
         stage.setResizable(false);
         stage.show();
     }
-
+    private BangladeshiFarmer bangladeshiFarmer;
+    private BangladeshiFarmersHouse bangladeshiFarmersHouse;
+    private BangladeshiFarmersTree bangladeshiFarmersTree;
+    private Pond pond;
+    private Canvas canvas;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         cn_label = country_name;
         vn_label = village_name;
+        toggleGroup = new ToggleGroup();
+        bangladeshiFarmer = new BangladeshiFarmer();
+        bangladeshiFarmersHouse = new BangladeshiFarmersHouse();
+        bangladeshiFarmersTree = new BangladeshiFarmersTree();
+        pond = new Pond();
+        MainCanvas mainCanvas = MainCanvas.getInstance();
+        canvas = mainCanvas.getCanvas();
+        drawing_space.getChildren().add(canvas);
+    }
+
+    public void RadioSelect(ActionEvent actionEvent)
+    {
+        if (house_RadioButton.isSelected())
+        {
+            bangladeshiFarmersHouse.Draw();
+            bangladeshiFarmersHouse.draw_house();
+            bangladeshiFarmersTree.Dont_Draw();
+            pond.Dont_Draw();
+        }
+        else if (tree_RadioButton.isSelected())
+        {
+            bangladeshiFarmersTree.Draw();
+            bangladeshiFarmersTree.draw_Tree();
+            bangladeshiFarmersHouse.Dont_Draw();
+            pond.Dont_Draw();
+        }
+        else if(water_RadioButton.isSelected())
+        {
+            pond.Draw();
+            pond.draw_water_resource();
+            bangladeshiFarmersHouse.Dont_Draw();
+            bangladeshiFarmersTree.Dont_Draw();
+        }
+
+    }
+
+    /*@FXML
+    public void House_Button_Clicked (ActionEvent event){
+        if (house_RadioButton.isSelected()) {
+            bangladeshiFarmersHouse.Draw();
+            bangladeshiFarmersHouse.draw_house();
+            bangladeshiFarmersHouse.Dont_Draw();
+        }
+        else {
+            bangladeshiFarmersHouse.Dont_Draw();
+        }
     }
 
     @FXML
-    public void House_Button_Clicked(ActionEvent actionEvent) {
-        BangladeshiFarmersHouse bangladeshiFarmersHouse = new BangladeshiFarmersHouse();
-        bangladeshiFarmersHouse.draw_house(drawing_space_canvas);
+    void Tree_Button_Clicked (ActionEvent event){
+        if (tree_RadioButton.isSelected()) {
+            bangladeshiFarmersTree.Draw();
+            bangladeshiFarmersTree.draw_Tree();
+        }
+        else {
+            bangladeshiFarmersTree.Dont_Draw();
+        }
     }
 
     @FXML
-    void Tree_Button_Clicked(ActionEvent actionEvent) {
-        BangladeshiFarmersTree bangladeshiFarmersTree = new BangladeshiFarmersTree();
-        bangladeshiFarmersTree.draw_Tree(drawing_space_canvas);
-    }
+    void WaterSource_Button_Clicked (ActionEvent event){
+        if (water_RadioButton.isSelected()) {
+            pond.Draw();
+            pond.draw_water_resource();
+        }
+        else {
+            pond.Dont_Draw();
+        }
+    }*/
 
-    @FXML
-    void WaterSource_Button_Clicked(ActionEvent event) {
-        Pond pond = new Pond();
-        pond.draw_water_resource(drawing_space_canvas);
-
-    }
 }
+
+
