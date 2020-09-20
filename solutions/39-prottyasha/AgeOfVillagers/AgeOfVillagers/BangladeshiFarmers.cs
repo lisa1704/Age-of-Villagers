dd1bf7aa8ec6eca04d4646fab54ea75cf9c514d8
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-20 07:40:14 +0600

    #10 #7 arab nation done. pen set.

--- solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
     class BangladeshiFarmers : INations
     {
+        public void drawhouse(Point p, Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawtree(Point p, Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawwatersource(Point p, Graphics g)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
