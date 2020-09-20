110b8776a994ec24f74378d5a0a8f188a8aadde3
Suzad <61018495+Suzad@users.noreply.github.com>
2020-09-20 09:39:15 +0600

    #9 #10 #11 #12 null village component testing

---
+++ solutions/24-suzad/AgeOfVillagers/AgeOfVillagers.Testing/NullVillageComponentTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Xunit;
+
+namespace AgeOfVillagers.Testing
+{
+    public class NullVillageComponentTest
+    {
+        [Fact]
+        public void NullVillageComponentTesting()
+        {
+            Point p = new Point();
+            NullVillageComponent obj = new NullVillageComponent();
+            Point actual = obj.getPoint();
+            Point expected = p;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
