7f022bdd6e6fcef42f67114861dd15305e689c17
Ratun <www.ratunrahman@gmail.com>
2020-09-20 22:25:49 +0600

    #9 create Bangladesh items

---
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Bdtree.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Bdtree : Tree
+{
+	public void CreateTree()
+    {
+
+    }
+}
---
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Bdwater.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Bdwater : Water
+{
+	public void CreateWater()
+    {
+
+    }
+}
