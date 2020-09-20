b8c714acc8751f9cec577696d17423a3e81cfe73
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-21 04:11:54 +0600

    #12 terrain color set

--- solutions/75-arnob/Age of Villagers/Nation/Arab_Bedouin.cs
+++ solutions/75-arnob/Age of Villagers/Nation/Arab_Bedouin.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class Arab_Bedouin
+    public class Arab_Bedouin : INation
     {
+        string name;
+
+        public Arab_Bedouin(string name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return name;
+        }
+        public void GetHouse(Graphics graphics, Point _point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color getTerrainColour()
+        {
+            return Color.LightYellow;
+        }
+
+        public void GetTree(Graphics graphics, Point _point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetWaterSource(Graphics graphics, Point _point)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
