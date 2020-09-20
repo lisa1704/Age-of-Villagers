bc9a7e77849d9ff94222e734c4b0afe5f35114fb
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-21 00:04:35 +0600

    #1 #7 size class introduced

---
+++ solutions/43-sristy/AgeOfVillagers/Size.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_villagers
+{
+    public abstract class Size
+    {
+
+        public abstract int GetWeidth();
+        public abstract int GetHeight();
+
+    }
+
+    public class Housesize : Size
+    {
+        public override int GetHeight()
+        {
+            return 16;
+        }
+
+        public override int GetWeidth()
+        {
+            return 16;
+        }
+    }
+
+    public class Treesize : Size
+    {
+        public override int GetHeight()
+        {
+            return 24;
+        }
+
+        public override int GetWeidth()
+        {
+            return 16;
+        }
+    }
+
+}
