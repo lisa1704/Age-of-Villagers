5d9de3c929e890db755c7674570a09b012cc5d90
afiarodoshe <afiamuntakim@iut-dhaka.edu>
2020-09-20 23:30:05 +0600

    #& creating NationFactory class and making some changes

---
+++ solutions/29-rodoshe/Age_of_villagers/FactoryClasses/NationFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_Villagers.FactoryClasses
+{
+
+    class NationFactory
+    {
+        string name;
+        public NationFactory(string NationName)
+        {
+            name = NationName;
+        }
+    }
+}
