a7e4a24bc028a03cbe3285e23b281c196557deca
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-21 01:46:35 +0600

    #11 terrain color set

--- solutions/75-arnob/Age of Villagers/Nation/Egyptian_King.cs
+++ solutions/75-arnob/Age of Villagers/Nation/Egyptian_King.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Age_of_Villagers
+namespace Age_of_Villagers.Nation
 {
-    class Egyptian_King
+    public class Egyptian_King : INation
     {
+        public void GetHouse(Graphics graphics, Point _point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color getTerrainColour()
+        {
+            return Color.Yellow;
+        }
+
+        public void GetTree(Graphics graphics, Point _point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetWaterSource(Graphics graphics, Point _point)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
