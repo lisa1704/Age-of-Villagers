33289f31cdb8f49f377fd1e3752d4d00a159d1ab
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 20:57:52 +0600

    #1 fix the size of arab tree

--- solutions/43-sristy/AgeOfVillagers/Arab_house.cs
+++ solutions/43-sristy/AgeOfVillagers/Arab_house.cs
@@ -26,13 +26,13 @@ namespace Age_of_villagers
         {
             Point lptop = new Point(mptop.X - 1, mptop.Y);
             Point rptop = new Point(mptop.X + 1, mptop.Y);
-            Point lpbottom = new Point(mptop.X - 1, mptop.Y + 16);
-            Point rpbottom = new Point(mptop.X + 1, mptop.Y + 16);
-            Point p1 = new Point(mptop.X, mptop.Y - 11);
-            Point p2 = new Point(mptop.X - 8, mptop.Y - 8);
-            Point p3 = new Point(mptop.X + 8, mptop.Y - 8);
-            Point p4 = new Point(mptop.X - 11, mptop.Y - 3);
-            Point p5 = new Point(mptop.X + 11, mptop.Y - 3);
+            Point lpbottom = new Point(mptop.X - 1, mptop.Y + 14);
+            Point rpbottom = new Point(mptop.X + 1, mptop.Y + 14);
+            Point p1 = new Point(mptop.X, mptop.Y - 10);
+            Point p2 = new Point(mptop.X - 5, mptop.Y - 8);
+            Point p3 = new Point(mptop.X + 5, mptop.Y - 8);
+            Point p4 = new Point(mptop.X - 8, mptop.Y - 3);
+            Point p5 = new Point(mptop.X + 8, mptop.Y - 3);
             AddComponent(new obj_rectangle(lptop, rptop, lpbottom, rpbottom));
             AddComponent(new obj_line(mptop, p1));
             AddComponent(new obj_line(mptop, p2));
