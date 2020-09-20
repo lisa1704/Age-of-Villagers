a790d4497bea90e8396a658891310da48bcda9f6
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 14:53:26 +0600

    #10 ArabBedduin Color added(completed)

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Nation/ArabBeduin.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Nation/ArabBeduin.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Nation
 {
-    class ArabBeduin
+    class ArabBeduin:INation 
     {
-
+        private Color colorofbackground = Color.Green;
+        public Color BgColor()
+        {
+            return colorofbackground;
+        }
     }
 }
