e67cc364a2680f7bb0b753a28d97bb32f780e4e0
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-21 00:40:00 +0600

    #8 Test created for Null Items.

---
+++ solutions/90-safayat/AgeofVillagers90Test/NullNationItemTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using AgeOfVillagers90;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgeofVillagers90Test
+{
+    [TestClass]
+    public class NullNationItemTest
+    {
+        Graphics g;
+        Point point = new Point(10, 10);
+        [TestMethod]
+        public void TestNNHouse()
+        {
+            INation nation = new NullNation();
+            object obj = nation.DrawHouse(g, point);
+            NullVillageItem TestNullHouse = new NullVillageItem();
+            TestNullHouse.Equals(obj);
+        }
+
+
+    }
+}
