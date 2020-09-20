45970e3a249e3ccea0a605e160d0e4975acdc61c
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-21 00:38:19 +0600

    #11 #7 EgyptainWaterSource created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillegeItems/EgyptianWaterSource.cs
@@ -0,0 +1,20 @@
+﻿using AgeOfVillagers.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.VillegeItems
+{
+    class EgyptianWaterSource : VillageItem
+    {
+        private readonly Point topLeft, bottomRight;
+
+        public EgyptianWaterSource(Point point)
+        {
+            topLeft = new Point(point.X - 25, point.Y - 25);
+            bottomRight = new Point(point.X + 25, point.Y + 25);
+            addItem(new Circle(topLeft, bottomRight));
+        }
+    }
+}
