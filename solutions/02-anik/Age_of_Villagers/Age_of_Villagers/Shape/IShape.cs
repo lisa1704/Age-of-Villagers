6c7ceb28a9f7f7dfd33b43915a611430b81a3a84
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 21:42:29 +0600

    #7 Ishape edited

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/IShape.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/IShape.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Shape
 {
     interface IShape
     {
-
+        void Draw(Graphics g, int x, int y, int height, int width);
     }
 }
