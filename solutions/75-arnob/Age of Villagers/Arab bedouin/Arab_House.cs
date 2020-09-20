817ef6e6393ab216f9ac11dc26368ee4f2b4456f
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 06:45:59 +0600

    #9 Bd farmer house implemented

---
+++ solutions/75-arnob/Age of Villagers/Arab bedouin/Arab_House.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_Villagers.Arab_bedouin
+{
+    class Arab_House
+    {
+    }
+}
