91ca993a8af6c6be365171b3dcf1bd9813f0a52f
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-21 00:29:47 +0600

    #1 #10 #12 null item class created for village null items

---
+++ solutions/75-arnob/Age of Villagers/Null_Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_Villagers
+{
+    class Null_Item : CompositeShaping
+    {
+        Point point = new Point();
+
+        public Null_Item(Point point)
+        {
+            this.point = point;
+        }
+
+        public Point DrawNothing()
+        {
+            return point;
+        }
+    }
+}
