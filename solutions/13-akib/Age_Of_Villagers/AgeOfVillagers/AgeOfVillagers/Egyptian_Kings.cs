e239135e68066badbdab1036624c1fe7612ef094
akib <akibahmed@iut-dhaka.edu>
2020-09-20 14:29:57 +0600

    #11 Egyptian_Kings Class Implemented

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Egyptian_Kings.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Egyptian_Kings.cs
@@ -4,5 +4,26 @@ namespace AgeOfVillagers
 
 public class Egyptian_Kings : Nation
 {
-	
+    private int x_water = 12, y_water = 12;
+
+    public I_Shape Draw_Tree(int x, int y)
+    {
+        tree = new Egyptian_Tree(x, y);
+        return tree;
+    }
+    public I_Shape Draw_House(int x, int y)
+    {
+        house = new Egyptian_House(x, y);
+        return house;
+    }
+    public I_Shape Draw_WaterRes(int x_water, int y_water)
+    {
+        water_res = new Egyptian_WaterRes(x_water, y_water);
+        return water_res;
+    }
+
+    public string BG_Color()
+    {
+        return "Plain Yellow color";
+    }
 }
