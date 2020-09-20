2d2f19b58bc4bf3b9f5d8964c3ad51ad041105e3
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 20:48:11 +0600

    #10 house item created

--- solutions/75-arnob/Age of Villagers/Egyptian King Items/Egyptian_House.cs
+++ solutions/75-arnob/Age of Villagers/Egyptian King Items/Egyptian_House.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers.Egyptian_King
 {
-    class Egyptian_House
+    class Egyptian_House : CompositeShaping
     {
+        public Egyptian_House(Point top, Point mediumLeft, Point bottom, Point mediumRight)
+        {
+
+            AddComponent(new Line(top, mediumLeft));
+            AddComponent(new Line(mediumLeft, bottom));
+            AddComponent(new Line(bottom, mediumRight));
+            AddComponent(new Line(mediumRight, top));
+            AddComponent(new Line(top, bottom));
+
+
+        }
     }
 }
