21a55db521c8d4ac5f81f91103ef62f745db5dbd
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-21 01:19:43 +0600

    #1 solved bugs in bd farmer tree item

--- solutions/75-arnob/Age of Villagers/Bangladeshi Farmer Items/BDfarmer_tree.cs
+++ solutions/75-arnob/Age of Villagers/Bangladeshi Farmer Items/BDfarmer_tree.cs
@@ -9,13 +9,23 @@ namespace Age_of_Villagers.Bangladeshi_Farmer
 {
     class BDfarmer_Tree
     {
-        public BDfarmer_Tree(Point point, Point point1, Point point2)
-        {
-    }
+
 
         public BDfarmer_Tree(Point mainPoint, Point bottomRight, Point topPoint, Point topLeft)
         {
             AddComponent(new Rectangle(topLeft, bottomRight));
             AddComponent(new EllipseShape(mainPoint, topPoint));
         }
+
+        private void AddComponent(EllipseShape ellipseShape)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddComponent(Rectangle rectangle)
+        {
+            throw new NotImplementedException();
+        }
     }
+}
+    
