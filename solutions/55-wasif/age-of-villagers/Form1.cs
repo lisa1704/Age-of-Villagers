9af6eb05769c336ca383c3f551a0942812a7dce5
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-21 03:56:01 +0600

    #7 Form design updated with constructor

--- solutions/55-wasif/age-of-villagers/Form1.cs
+++ solutions/55-wasif/age-of-villagers/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace age_of_villagers
 {
     public partial class Form1 : Form
@@ -15,63 +14,47 @@ namespace age_of_villagers
         public string village_name;
         public string village_type;
         public string item;
-
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
-
         private void button6_Click(object sender, EventArgs e)
         {
-
         }
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
         }
-
         private void textBox2_KeyDown(object sender, KeyEventArgs e)
         {
             village_name = textBox2.Text;
         }
-
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             village_type = comboBox1.Text;
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             item = "house";
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             item = "tree";
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             item = "water";
         }
-
         private void panel1_MouseClick(object sender, MouseEventArgs e)
         {
             Graphics gp = panel1.CreateGraphics();
             Pen p = new Pen(Color.Violet);
             Point p1 = new Point(100, 100);
             Point p2 = new Point();
-
             gp.DrawLine(p, p1, p2);
-
-            village_factory new_village = new village_factory();
-
+            village_factory new_village = new village_factory(gp);
             if (item == "house")
                 new_village.get_nation(village_type).draw_house();
             else if (item == "tree")
@@ -79,10 +62,9 @@ namespace age_of_villagers
             else if (item == "water")
                 new_village.get_nation(village_type).draw_watersource();
         }
-
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-
         }
     }
 }
+
