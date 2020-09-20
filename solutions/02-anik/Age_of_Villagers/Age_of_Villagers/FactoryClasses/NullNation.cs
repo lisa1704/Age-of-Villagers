c87a4c453c739b17db7b4db23823397a3ae00df8
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-21 02:55:06 +0600

    #10 #11 #12 Null Nation added

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/FactoryClasses/NullNation.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/FactoryClasses/NullNation.cs
@@ -1,8 +1,15 @@
 ﻿using Age_of_Villagers.Nation;
+using System.Drawing;
 
 namespace Age_of_Villagers.FactoryClasses
 {
     internal class NullNation : INation
     {
+        public Color BackGroundColor()
+        {
+            return Color.White;
+        }
+
+      
     }
 }
