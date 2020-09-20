1eb64d086b83bd0e046cfb6da4dcc973066aecef
tahmid1234 <tahmidislam1998@gmail.com>
2020-09-20 10:23:23 +0600

    #10 added ArabianTreeShape

---
+++ solutions/38-tahmid/AgeOfVillagers/AgeOfVillagers/Shape implementing Classes/ArabianTreeShape.cs
@@ -0,0 +1,15 @@
+﻿using AgeOfVillagers.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers.Shape_implementing_Classes
+{
+    class ArabianTreeShape : Shapes
+    {
+        public void makeShape()
+        {
+            
+        }
+    }
+}
