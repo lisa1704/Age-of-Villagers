b7d79ccecf7510e9b85f9bd3bce4cbfa60007027
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 06:47:54 +0600

    #10 Arab house created

--- solutions/75-arnob/Age of Villagers/Arab bedouin/Arab_House.cs
+++ solutions/75-arnob/Age of Villagers/Arab bedouin/Arab_House.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers.Arab_bedouin
 {
-    class Arab_House
+    class Arab_House : CompositeShaping
     {
+        public Arab_House(Point baseLeft, Point baseMedium, Point topOne, Point baseRight, Point topTwo)
+        {
+            //Triangle
+            //AddComponent(new Line(baseLeft, baseMedium)); 
+            AddComponent(new Line(baseMedium, topOne));
+            AddComponent(new Line(topOne, baseLeft));
+            AddComponent(new Line(baseRight, baseLeft)); 
+
+
+            //Rectangle
+            AddComponent(new Line(baseMedium, baseRight));
+            AddComponent(new Line(topOne, topTwo));
+            AddComponent(new Line(topTwo, baseRight));
+
+
+
+        }
     }
 }
