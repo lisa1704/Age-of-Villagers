a0557a225b069736aeee1c2e1db836954e47be6b
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-20 23:23:16 +0600

    # 7 circle shape created for object

---
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class Circle : AbstractShape
+    {
+        private Point topleft;
+        private Point bottomright;
+
+        public Circle(Point topleft, Point bottomright)
+        {
+            this.topleft = topleft;
+            this.bottomright = bottomright;
+            addShape(new Arc(topleft.X, topleft.Y, bottomright.X - topleft.X, bottomright.Y - topleft.Y, 0, 360));
+        }
+    }
+}
