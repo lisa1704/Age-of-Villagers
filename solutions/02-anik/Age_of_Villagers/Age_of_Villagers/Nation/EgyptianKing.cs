f96ec7fd8711ba2cf09533bea81e1ba4eff8bc3f
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 15:25:22 +0600

    #11 EgyptianKing Created

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Nation/EgyptianKing.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Nation/EgyptianKing.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Nation
 {
     class EgyptianKing:INation
     {
-
-    }
+        private Color colorofbackground = Color.Yellow;
+        public Color BgColor()
+        {
+            return colorofbackground;
+        }
 }
