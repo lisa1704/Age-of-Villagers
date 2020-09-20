038655e6a63fb66a2a18cd64c6e1732dbe1978bf
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 19:05:08 +0600

    nullnation house object testing

---
+++ solutions/43-sristy/AgeOfVillagers.Test/Nullnationobjecttesting.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Age_of_villagers;
+using System.Drawing;
+
+namespace AgeOfVillagers.Test
+{
+    public class Nullnationobjecttesting
+    {
+        Nationfactory nationfactory = new Nationfactory();
+        Point lptop = new Point(200, 100);
+        [Fact]
+        public void InuithouseTest()
+        {
+            IObject actual = nationfactory.GetNation("").Draw_house(lptop);
+            IObject expected = new null_obj();
+            actual.Equals(expected);
+        }
+    }
+}
