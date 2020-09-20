d6d54dd0ac47508002acedbbdd5b8eae1d1633c3
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-20 20:31:42 +0600

    #7 #8 #11 EgyptianKings draw tree implemented

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/EgyptianKings.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/EgyptianKings.cs
@@ -22,7 +22,26 @@ namespace AgeOfVillagers
 
         public void drawtree(Point p, Graphics g, Pen pen)
         {
-            throw new NotImplementedException();
+            Point p1 = new Point(p.X, p.Y + 24);
+
+            Point p4 = new Point(p.X - 3, p.Y + 4);
+            Point p5 = new Point(p.X + 3, p.Y + 4);
+            Point p2 = new Point(p4.X - 4, p.Y + 2);
+            Point p3 = new Point(p5.X + 4, p.Y + 2);
+
+            Point p6 = new Point(p3.X + 4, p.Y);
+            Point p7 = new Point(p2.X - 4, p.Y);
+
+            Point p8 = new Point(p1.X - 5, p1.Y - 12);
+            Point p9 = new Point(p1.X + 5, p1.Y - 12);
+            g.DrawLine(pen, p8, p1);
+            g.DrawLine(pen, p9, p1);
+            g.DrawLine(pen, p8, p7);
+            g.DrawLine(pen, p8, p2);
+            g.DrawLine(pen, p8, p4);
+            g.DrawLine(pen, p3, p9);
+            g.DrawLine(pen, p5, p9);
+            g.DrawLine(pen, p6, p9);
         }
 
         public void drawwatersrc(Point p, Graphics g, Pen pen)
