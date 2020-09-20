39a6ea96ff60b69f399a63cd259c23740b4efd57
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 18:59:55 +0600

    #7 #10 arab houses and trees created.

---
+++ solutions/64-ruhee/Age_of_Villagers/ArabBedouin/ArabHouse.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis.Editing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Age_of_Villagers.Age_of_Villagers;
+
+namespace Age_of_Villagers.ArabBedouin
+{
+    public class ArabHouse : CShape
+    {
+        public ArabHouse(Point pt)
+        {
+            Point pt1 = new Point(pt.X - 30, pt.Y + 50);
+            Point pt2 = new Point(pt.X + 30, pt.Y + 50);
+            Point pt3 = new Point(pt.X + 38, pt.Y - 18);
+            Point pt4 = new Point(pt2.X + 35, pt2.Y - 20);
+
+
+            AddComponent(new Triangle(pt, pt1, pt2));
+            AddComponent(new Line(pt, pt3));
+            AddComponent(new Line(pt2, pt4));
+            AddComponent(new Line(pt3, pt4));
+
+        }
+    }
+}
