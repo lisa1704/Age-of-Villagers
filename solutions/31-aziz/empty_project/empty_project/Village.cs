628e47d6a10d554c0533a97425e8950767e5ac65
Asif Aziz <asifaziz2061@gmail.com>
2020-09-20 15:58:26 +0600

    #4 methods implemented and dependency added

--- solutions/31-aziz/empty_project/empty_project/Village.cs
+++ solutions/31-aziz/empty_project/empty_project/Village.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace empty_project
 {
     class Village
     {
         public string village = "";
+        public List<Axis> houses = new List<Axis>();
+        public List<Axis> trees = new List<Axis>();
+        public List<Axis> watersources = new List<Axis>();
         public string getName()
         {
             return village;
@@ -17,15 +22,39 @@ namespace empty_project
         }
         public void insertHouse(Axis house, INation nation)
         {
-
+            houses.Add(house);
+            nation.drawHouse(house);
         }
         public void insertTree(Axis tree, INation nation)
         {
-
+            trees.Add(tree);
+            nation.drawTree(tree);
         }
         public void insertWaterSource(Axis waterSource, INation nation)
         {
-
+            watersources.Add(waterSource);
+            nation.drawWaterSource(waterSource);
+        }
+        public void draw(INation nation)
+        {
+            foreach(Axis house in houses)
+            {
+                nation.drawHouse(house);
+            }
+            foreach (Axis tree in trees)
+            {
+                nation.drawTree(tree);
+            }
+            foreach (Axis waterSource in watersources)
+            {
+                nation.drawWaterSource(waterSource);
+            }
+        }
+        public void RefreshVillage()
+        {
+            houses.Clear();
+            trees.Clear();
+            watersources.Clear();
         }
     }
 }
