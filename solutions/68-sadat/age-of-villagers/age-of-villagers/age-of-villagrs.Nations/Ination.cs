1a54dc3311ea3641e0518dbd15a2a70685bb0352
Sadat Bin Sharfuddin <sadat.bs@gmail.com>
2020-09-20 18:44:51 +0600

    #1 changed location of files to appropriate places.s

---
+++ solutions/68-sadat/age-of-villagers/age-of-villagers/age-of-villagrs.Nations/Ination.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace age_of_villagers
+{
+    interface Ination
+    {
+        void createHouse();
+        void createTree();
+        void createWaterSource();
+        void colorTerrain();
+
+    }
+}
