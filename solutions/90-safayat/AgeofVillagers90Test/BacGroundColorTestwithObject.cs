2848535ee5bf223093b71e15015affd6d5f496c0
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-20 19:12:49 +0600

    #8 #9 unit test added

---
+++ solutions/90-safayat/AgeofVillagers90Test/BacGroundColorTestwithObject.cs
@@ -0,0 +1,28 @@
+﻿using AgeOfVillagers90;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeofVillagers90Test
+{
+    [TestClass]
+    public class BacGroundColorTestwithObject
+    {
+        [TestMethod]
+        public void BDfarmersColorTestWithObject()
+        {
+            NationFactory BDFcolorObject = new NationFactory();
+            INation NationType = BDFcolorObject.GetNation("Bangladeshi Farmers");
+            BangladeshiFarmers BDF = new BangladeshiFarmers();
+            Color color = BDF.BackGroundColor();
+            Color colorObject = NationType.BackGroundColor();
+
+            Assert.AreEqual(color, colorObject);
+        }
+
+    }
+}
