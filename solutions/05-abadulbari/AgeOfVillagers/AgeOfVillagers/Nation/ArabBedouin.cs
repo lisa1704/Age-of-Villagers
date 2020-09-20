b156a2f75d242463e712906f7fcaad8aa59abd64
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-21 02:27:24 +0600

    #10 Arab Beduion nation created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/Nation/ArabBedouin.cs
@@ -0,0 +1,36 @@
+﻿using AgeOfVillagers.VillegeItems;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.Nation
+{
+    class ArabBedouin : INation
+    {
+        public VillageItem getHouse(Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getNationName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color getTerrainColor()
+        {
+            return Color.LightYellow;
+        }
+
+        public VillageItem getTree(Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VillageItem getWaterSource(Point p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
