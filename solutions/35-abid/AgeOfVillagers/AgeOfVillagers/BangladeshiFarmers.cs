a5defa1f6bbbb7f493b802d10b01ee08f41be39a
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-20 19:02:25 +0600

    #7 #8 #9 BangladeshiFarmers draw water source implemented

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
@@ -35,7 +35,23 @@ namespace AgeOfVillagers
 
         public void drawwatersrc(Point p, Graphics g, Pen pen)
         {
-            throw new NotImplementedException();
+            Point p1 = new Point(p.X - 12, p.Y + 8);
+            Point p2 = new Point(p.X + 12, p1.Y);
+            Point p3 = new Point(p.X, p.Y + 11);
+            Point p4 = new Point(p.X, p.Y + 5);
+            Point p5 = new Point(p1.X + 7, p.Y);
+            Point p6 = new Point(p5.X + 10, p.Y);
+            Point p7 = new Point(p5.X, p5.Y + 16);
+            Point p8 = new Point(p6.X, p6.Y + 16);
+            g.DrawLine(pen, p1, p5);
+            g.DrawLine(pen, p1, p7);
+            g.DrawLine(pen, p5, p);
+            g.DrawLine(pen, p, p4);
+            g.DrawLine(pen, p6, p2);
+            g.DrawLine(pen, p8, p3);
+            g.DrawLine(pen, p8, p2);
+            g.DrawLine(pen, p6, p4);
+            g.DrawLine(pen, p3, p7);
         }
     }
 }
