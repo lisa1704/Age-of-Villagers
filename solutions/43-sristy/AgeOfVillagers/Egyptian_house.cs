39a04b309ece0d5688e97c54feb71daff5ee1e56
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 21:22:44 +0600

    #1 egyptian tree size fix up

--- solutions/43-sristy/AgeOfVillagers/Egyptian_house.cs
+++ solutions/43-sristy/AgeOfVillagers/Egyptian_house.cs
@@ -22,14 +22,14 @@ namespace Age_of_villagers
     {
         public Egyptian_tree(Point mptop)
         {
-            Point p1 = new Point(mptop.X + 5, mptop.Y - 8);
-            Point p2 = new Point(mptop.X - 5, mptop.Y - 11);
-            Point p3 = new Point(mptop.X + 13, mptop.Y - 16);
-            Point p4 = new Point(mptop.X + 5, mptop.Y - 16);
-            Point p5 = new Point(mptop.X + 13, mptop.Y - 10);
-            Point p6 = new Point(mptop.X - 1, mptop.Y - 19);
-            Point p7 = new Point(mptop.X - 8, mptop.Y - 19);
-            Point p8 = new Point(mptop.X - 13, mptop.Y - 16);
+            Point p1 = new Point(mptop.X + 3, mptop.Y - 10);
+            Point p2 = new Point(mptop.X - 3, mptop.Y - 15);
+            Point p3 = new Point(mptop.X + 8, mptop.Y - 20);
+            Point p4 = new Point(mptop.X + 3, mptop.Y - 20);
+            Point p5 = new Point(mptop.X + 8, mptop.Y - 13);
+            Point p6 = new Point(mptop.X - 1, mptop.Y - 24);
+            Point p7 = new Point(mptop.X - 5, mptop.Y - 24);
+            Point p8 = new Point(mptop.X - 8, mptop.Y - 20);
             AddComponent(new obj_line(mptop, p1));
             AddComponent(new obj_line(mptop, p2));
             AddComponent(new obj_line(p1, p3));
