178d5eb16cce0f9bb0935949dce1c1187015a58c
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-21 01:31:24 +0600

    #9 Class BangladeshiHouse updated.

--- solutions/53-silvi/AgeOfVillagers/BangladeshiHouse.cs
+++ solutions/53-silvi/AgeOfVillagers/BangladeshiHouse.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using AgeOfVillagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace AgeOfVillagers
+namespace Age_Of_Villagers
 {
-    class BangladeshiHouse
+    class BangladeshiHouse : IVillageItem
     {
+        public void Draw(Graphics g, Pen p)
+        {
+            int x = e.X;
+            int y = e.Y;
+            g.DrawLine(p, x, y, x + 50, y);
+            g.DrawLine(p, x + 50, y, x + 50, y + 25);
+            g.DrawLine(p, x + 50, y + 25, x, y + 25);
+            g.DrawLine(p, x, y + 25, x, y);
+            g.DrawLine(p, x, y, x + 25, y - 25);
+            g.DrawLine(p, x + 50, y, x + 25, y - 25);
+        }
     }
 }
