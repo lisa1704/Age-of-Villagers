9114c9b4542e27629bbb57e49c22eb984cf34544
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 23:36:02 +0600

    #1 #7 #8 #9 Bangladeshi water source crreated.

--- solutions/64-ruhee/Age_of_Villagers/BangladeshiFarmers/BangladeshiWaterS.cs
+++ solutions/64-ruhee/Age_of_Villagers/BangladeshiFarmers/BangladeshiWaterS.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Age_of_Villagers.Age_of_Villagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.BangladeshiFarmers
 {
-    class BangladeshiWaterS
+    class BangladeshiWaterS:CShape
     {
+        public BangladeshiWaterS(Point pt)
+        {
+            Point pt1 = new Point(pt.X - 30, pt.Y + 55);
+            Point pt2 = new Point(pt.X, pt.Y + 80);
+            Point pt3 = new Point(pt.X + 30, pt.Y + 55);
+            Point pt4 = new Point(pt.X + 55, pt.Y + 80);
+            Point pt5 = new Point(pt.X + 80, pt.Y + 30);
+            Point pt6 = new Point(pt.X + 55, pt.Y);
+            Point pt7 = new Point(pt.X + 30, pt.Y + 30);
+            Point pt8 = new Point(pt.X + 30, pt.Y);
+
+            AddComponent(new Line(pt, pt1));
+            AddComponent(new Line(pt1, pt2));
+            AddComponent(new Line(pt2, pt3));
+            AddComponent(new Line(pt3, pt4));
+            AddComponent(new Line(pt4, pt5));
+            AddComponent(new Line(pt5, pt6));
+            AddComponent(new Line(pt6, pt7));
+            AddComponent(new Line(pt7, pt8));
+            AddComponent(new Line(pt8, pt));
+        }
     }
 }
