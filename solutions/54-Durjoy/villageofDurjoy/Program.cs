b3a9b957e664f83d439e984bbeb12385796ce56f
Durjoy54 <ariyandurjoy.88@gmail.com>
2020-09-20 14:23:05 +0600

    #14 project sctructure created

---
+++ solutions/54-Durjoy/villageofDurjoy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace villageofDurjoy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
