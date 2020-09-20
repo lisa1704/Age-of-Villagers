6228e8565b4962981e0945107c86c97f3e2fac1e
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-21 04:24:05 +0600

    #6 Finalized open portion of AOV

--- solutions/19-zarif/AgeOfVillagers/OpenVillageNation.cs
+++ solutions/19-zarif/AgeOfVillagers/OpenVillageNation.cs
@@ -39,7 +39,8 @@ namespace AgeOfVillagers
             main.NationName.Text = Opencombobox.Text;
             main.VillageName.Text = villageState._name;
 
-// panel.BackColor = manager.getNation(new Point()).GetTerritoryColor();
+            panel.BackColor = manager.getNation(new Point()).GetTerritoryColor();
+            panel.Refresh();
 
             opn.ResetState(villageState, Opencombobox.Text);
 
