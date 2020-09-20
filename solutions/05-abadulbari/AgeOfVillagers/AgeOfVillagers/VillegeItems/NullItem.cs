fd744b203b71da88a8e491ed9f885a604285a356
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-21 03:29:49 +0600

    #7 NullItem created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillegeItems/NullItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.VillegeItems
+{
+    class NullItem : VillageItem
+    {
+        private readonly Point point;
+
+        public NullItem(Point point)
+        {
+            this.point = point;
+        }
+        public new void draw(Graphics g, Pen p)
+        {
+
+        }
+    }
+}
