d60acc2eb6bb1b3b9d32b99c08ec8a1b20408be6
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 23:25:14 +0600

    #1  #7 #8 #9 Bangladeshi trees created.

--- solutions/64-ruhee/Age_of_Villagers/BangladeshiFarmers/BangladeshiTrees.cs
+++ solutions/64-ruhee/Age_of_Villagers/BangladeshiFarmers/BangladeshiTrees.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Age_of_Villagers.Age_of_Villagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.BangladeshiFarmers
 {
-    class BangladeshiTrees
+    class BangladeshiTrees:CShape
     {
+        public BangladeshiTrees(Point pt)
+        {
+            Point TopPoint = new Point(pt.X + 60, pt.Y + 70);
+            Point BottomRight = new Point(pt.X + 28, pt.Y + 100);
+            Point TopLeft = new Point(pt.X + 33, pt.Y + 45);
+
+            AddComponent(new Age_of_Villagers.Rectangle(TopLeft, BottomRight));
+            AddComponent(new Ellipse(pt, TopPoint));
+        }
     }
 }
