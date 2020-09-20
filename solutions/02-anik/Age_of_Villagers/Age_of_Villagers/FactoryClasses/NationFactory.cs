d72bbfff7aac31942afb5c617bb824bf556b7fed
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-21 02:27:36 +0600

    #7 #1 Nation factory implemented and null nation created

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/FactoryClasses/NationFactory.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/FactoryClasses/NationFactory.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Age_of_Villagers.Nation;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using static Age_of_Villagers.Nation.INation;
 
 namespace Age_of_Villagers.FactoryClasses
 {
@@ -8,9 +10,31 @@ namespace Age_of_Villagers.FactoryClasses
     class NationFactory
     {
         string name;
-        public NationFactory(string NationName)
+        public Nation.INation GetNation(string NationType)
         {
-            name = NationName;
+            if (NationType == "Bangladeshi Farmers")
+            {
+                return new BangladeshiFarmers();
+            }
+            else if (NationType == "Arab Bedouin")
+            {
+                return new ArabBeduin();
+            }
+            else if (NationType == "Egytian Kings")
+            {
+                return new EgyptianKing();
+            }
+            else if (NationType == "Inuit HUnters")
+            {
+                return new InuitHunters();
+            }
+            else
+            {
+                return new NullNation();
+            }
+            throw new Exception();
         }
+
     }
 }
+
---
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/FactoryClasses/NullNation.cs
@@ -0,0 +1,8 @@
+﻿using Age_of_Villagers.Nation;
+
+namespace Age_of_Villagers.FactoryClasses
+{
+    internal class NullNation : INation
+    {
+    }
+}
