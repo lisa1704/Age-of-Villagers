65ae91132590d24576aecdc2563385c9d068120f
Asif Aziz <asifaziz2061@gmail.com>
2020-09-20 23:15:44 +0600

    #1 Refactored Inuit Hunters House Shape

--- solutions/31-aziz/empty_project/empty_project/InuitHuntersHouse.cs
+++ solutions/31-aziz/empty_project/empty_project/InuitHuntersHouse.cs
@@ -16,9 +16,9 @@ namespace empty_project
             float startAngle = 0.0F;
             float sweepAngle = -180.0F;
 
-            graphics.DrawArc(pen, axis.X, axis.Y, 20, 20, startAngle, sweepAngle);
-            graphics.DrawArc(pen, axis.X + 5, axis.Y + 5,10,10, startAngle, sweepAngle);
-            graphics.DrawLine(pen, new PointF(axis.X, axis.Y + 10), new PointF(axis.X + 20, axis.Y + 10));
+            graphics.DrawArc(pen, axis.X, axis.Y, 40, 40, startAngle, sweepAngle);
+            graphics.DrawArc(pen, axis.X + 10, axis.Y + 10, 20, 20, startAngle, sweepAngle);
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y + 20), new PointF(axis.X + 40, axis.Y + 20));
         }
     }
 }
