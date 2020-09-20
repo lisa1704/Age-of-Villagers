180730679f145e4460c92565620130c698d95347
Jawad Zamal <jawadzamal@iut-dhaka.edu>
2020-09-20 21:27:16 +0600

    #7 arc shape and interface created

---
+++ solutions/61-jawad/Age_of_villagers/Age_of_villagers/Age_of_villagers/Shapes/Arc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age_of_villagers.Shapes
+{
+    class Arc : IShape
+    {
+        public void Draw(Graphics g, int x, int y, int height, int width)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rectangle shape()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
