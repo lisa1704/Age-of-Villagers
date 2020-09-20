dd81336982974ce2b4b4072705595ee0eafc1cd8
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-20 17:27:28 +0600

    # 7 #8 #9 BangladeshiFarmers draw house implemented

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
@@ -9,7 +9,16 @@ namespace AgeOfVillagers
     {
         public void drawhouse(Point p, Graphics g, Pen pen)
         {
-            throw new NotImplementedException();
+            Point p1 = new Point(p.X + 8, p.Y + 8);
+            Point p2 = new Point(p1.X - 16, p1.Y);
+            Point p3 = new Point(p1.X, p1.Y + 8);
+            Point p4 = new Point(p3.X - 16, p3.Y);
+            g.DrawLine(pen, p, p1);
+            g.DrawLine(pen, p, p2);
+            g.DrawLine(pen, p2, p1);
+            g.DrawLine(pen, p3, p1);
+            g.DrawLine(pen, p2, p4);
+            g.DrawLine(pen, p3, p4);
         }
 
         public void drawtree(Point p, Graphics g, Pen pen)
