8650b111acf7a3fcbce25cdf83bf685d772da22b
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 18:47:42 +0600

    #8 #9 #10 #11 #12  Created base classes for the nations.

--- solutions/64-ruhee/Age_of_Villagers/ArabBedouin/ArabBedouin.cs
+++ solutions/64-ruhee/Age_of_Villagers/ArabBedouin/ArabBedouin.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Age_of_Villagers.Age_of_Villagers
+namespace Age_of_Villagers.ArabBedouin
 {
-    class UnitHouse
+    class ArabBedouin
     {
-
     }
 }
