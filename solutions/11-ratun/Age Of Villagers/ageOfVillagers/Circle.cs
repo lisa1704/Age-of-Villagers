5396f384c07c1ea0f44bbbb66e98a5ee6fccff04
Ratun <www.ratunrahman@gmail.com>
2020-09-20 22:38:35 +0600

    #7 implementing Circle

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Circle.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Circle.cs
@@ -2,5 +2,15 @@
 
 public class Circle : Shape
 {
-	
+    private int x, y, radius;
+	public Circle(int x, int y, int radius)
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    public string Draw()
+    {
+        return "Drawing a circle";
+    }
 }
--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Shape.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Shape.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-public class Shape
+public abstract class Shape
 {
-	
+	public abstract void Draw();
 }
