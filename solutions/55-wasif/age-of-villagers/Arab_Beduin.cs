7ad69347c5854581a2119fee38f6eeda445a798f
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-20 22:14:46 +0600

    #1 #10 Created water resource

--- solutions/55-wasif/age-of-villagers/Arab_Beduin.cs
+++ solutions/55-wasif/age-of-villagers/Arab_Beduin.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace age_of_villagers
 {
@@ -40,20 +41,20 @@ namespace age_of_villagers
             Point p7 = new Point(p0.X + 25, p0.Y - 25);
             Point p8 = new Point(p0.X - 35, p0.Y - 10);
             Point p9 = new Point(p0.X + 35, p0.Y - 10);
-            g.DrawLine(pen, p1, p2);
-            g.DrawLine(pen, p1, p3);
-            g.DrawLine(pen, p3, p4);
-            g.DrawLine(pen, p2, p4);
-            g.DrawLine(pen, p0, p5);
-            g.DrawLine(pen, p0, p6);
-            g.DrawLine(pen, p0, p7);
-            g.DrawLine(pen, p0, p8);
-            g.DrawLine(pen, p0, p9);
+            gp.DrawLine(pen, p1, p2);
+            gp.DrawLine(pen, p1, p3);
+            gp.DrawLine(pen, p3, p4);
+            gp.DrawLine(pen, p2, p4);
+            gp.DrawLine(pen, p0, p5);
+            gp.DrawLine(pen, p0, p6);
+            gp.DrawLine(pen, p0, p7);
+            gp.DrawLine(pen, p0, p8);
+            gp.DrawLine(pen, p0, p9);
         }
 
         public void draw_watersource()
         {
-            throw new NotImplementedException();
+            MessageBox.Show("No water resource found");
         }
     }
 }
