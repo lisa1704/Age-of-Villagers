f891718bd49850175283a4e6bfff49dd4ebae883
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-21 00:41:07 +0600

    #1 #7 #8 #9 Bangladeshi Nation created

--- solutions/64-ruhee/Age_of_Villagers/BangladeshiFarmers/BangladeshiFarmers.cs
+++ solutions/64-ruhee/Age_of_Villagers/BangladeshiFarmers/BangladeshiFarmers.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Age_of_Villagers.Age_of_Villagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.BangladeshiFarmers
 {
-    public class BangladeshiFarmers
+    public class BangladeshiFarmers : INation
     {
+        Pen p = new Pen(Color.Green);
+        public void DrawTree(Graphics g, Point point)
+        {
+            BangladeshiTrees bdFarmersTree = new BangladeshiTrees(point);
+            bdFarmersTree.Draw(g);
+        }
+        public void DrawHouse(Graphics g, Point point)
+        {
+            BangladeshiHouse bdFarmersHouse = new BangladeshiHouse(point);
+            bdFarmersHouse.Draw(g);
+        }
+
+        public string BangladeshiFarmerTest()
+        {
+            string NationName = "Bangladesh";
+            return NationName;
+        }
+
+        public void DrawWaterSource(Graphics g, Point point)
+        {
+            BangladeshiWaterS bdFarmersWaterSource = new BangladeshiWaterS(point);
+            bdFarmersWaterSource.Draw(g);
+        }
+
+        public Color BackgroundColor()
+        {
+            return Color.Green;
+        }
     }
 }
