28f4506160789b991845c7c1a49fc4754e654330
mahirashrafemad <mahirashrafemad@users.noreply.github.com>
2020-09-20 20:53:31 +0600

    #9 #10 #11 #12 interface INations created

---
+++ solutions/25-emad/Age_of_villagers/INations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Age_of_villagers.Nations
+{
+    interface INations
+    {
+        string getName();
+         
+        void drawtree();
+
+        void drawhouse();
+
+        void drawwatersource();
+
+    }
+}
