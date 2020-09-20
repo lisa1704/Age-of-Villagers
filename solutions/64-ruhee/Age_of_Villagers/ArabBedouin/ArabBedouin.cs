c6e1dcc459f06bd8ddea9aa4360347292820634d
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 20:09:43 +0600

    #1 #8 #10 Arab nation created.

--- solutions/64-ruhee/Age_of_Villagers/ArabBedouin/ArabBedouin.cs
+++ solutions/64-ruhee/Age_of_Villagers/ArabBedouin/ArabBedouin.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
+using Age_of_Villagers.Age_of_Villagers;
 
 namespace Age_of_Villagers.ArabBedouin
 {
-    class ArabBedouin
+    public class ArabBedouin : INation
     {
+        public Color BackgroundColor()
+        {
+
+            return Color.GreenYellow;
+        }
+
+        public void DrawHouse(Graphics g, Point point)
+        {
+            ArabHouse arabBedouinHouse = new ArabHouse(point);
+            arabBedouinHouse.Draw(g);
+        }
+
+        public void DrawTree(Graphics g, Point point)
+        {
+            ArabTrees arabBedouinTree = new ArabTrees(point);
+            arabBedouinTree.Draw(g);
+        }
+
+        public void DrawWaterSource(Graphics g, Point point)
+        {
+            NullElements arabBedouinWaterSource = new NullElements();
+            arabBedouinWaterSource.drawing();
+        }
+
+
+        Color INation.BackgroundColor()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
