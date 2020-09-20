78db486d73d4eb0f0e77d2b42c052759f12c73ff
jarin_12 <jarintanzim@iut-dhaka.edu>
2020-09-20 17:08:44 +0600

    #4

--- solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/Form1.Designer.cs
+++ solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/Form1.Designer.cs
@@ -31,8 +31,9 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
@@ -55,6 +56,7 @@
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.button2);
             this.panel3.Controls.Add(this.button1);
             this.panel3.Controls.Add(this.label1);
             this.panel3.Location = new System.Drawing.Point(607, 0);
@@ -62,6 +64,16 @@
             this.panel3.Size = new System.Drawing.Size(193, 451);
             this.panel3.TabIndex = 1;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "tree";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -73,15 +85,15 @@
             this.label1.Text = "age of villagers ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button1
+            // button2
             // 
-            this.button1.Location = new System.Drawing.Point(23, 121);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 25);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "tree";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button2.Location = new System.Drawing.Point(111, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "house";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -107,5 +119,6 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/Form1.cs
+++ solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/Form1.cs
@@ -41,5 +41,10 @@ namespace ageofvillagers
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
