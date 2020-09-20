e4d104f6c1b8f96a6b67eb7f7942f27a15a94ac3
Ratun <www.ratunrahman@gmail.com>
2020-09-20 22:40:41 +0600

    #7 Implementing line

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Lines.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Lines.cs
@@ -2,5 +2,16 @@
 
 public class Lines : Shape
 {
-	
+    private int x1, y1, x2, y2;
+    public Lines(int x1, int y1, int x2, int y2)
+    {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+    public string Draw()
+    {
+        return "Drawing a line";
+    }
 }
