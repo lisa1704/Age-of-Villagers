402a2f1c8299c835aef278943b45092e63761052
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-20 22:38:54 +0600

    #7 cirlce implement

--- solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Circle.cs
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Circle.cs
@@ -2,5 +2,16 @@
 
 public class Circle: Shape
 {
+    private int x, y, radius;
+    public Circle(int x, int y, int radius)
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
 
+    }
+    public string Draw()
+    {
+        return "Drawing a circle";
+    }
 }
