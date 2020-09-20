4eff46eaa4f5811c73894ea175735db196547985
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-20 20:56:00 +0600

    #11 water source item created

--- solutions/75-arnob/Age of Villagers/Egyptian King Items/Egyptian_WaterSource.cs
+++ solutions/75-arnob/Age of Villagers/Egyptian King Items/Egyptian_WaterSource.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Age_of_Villagers.Egyptian_King
 {
-    class Egyptian_WaterResource
+    class Egyptian_WaterSource : CompositeShaping
     {
+        public Egyptian_WaterSource(Point pt1, Point pt2)
+        {
+            AddComponent(new EllipseShape(pt1, pt2));
+        }
     }
 }
