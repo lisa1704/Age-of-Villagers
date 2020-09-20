e41ce6ff3a1349de8fbba24a6bb60e0f134d5d6e
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 15:12:31 +0600

    #9 Bangladeshi farmers completed

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Nation/BangladeshiFarmers.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Nation/BangladeshiFarmers.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Nation
 {
-    class BangladeshiFarmers
+    class BangladeshiFarmers : INation
     {
-
+        private Color colorofbackground = Color.Green;
+        public Color BgColor()
+        {
+            return colorofbackground;
+        }
     }
 }
