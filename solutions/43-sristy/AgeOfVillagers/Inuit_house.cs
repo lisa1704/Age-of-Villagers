ab7418af8ca2672b179e0b91fa8be252f7d2a26e
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-20 21:26:59 +0600

    #1 inuit house size fix up

--- solutions/43-sristy/AgeOfVillagers/Inuit_house.cs
+++ solutions/43-sristy/AgeOfVillagers/Inuit_house.cs
@@ -11,12 +11,12 @@ namespace Age_of_villagers
     {
         public Inuit_house(Point mptop)
         {
-            Point outtop = new Point(mptop.X - 12, mptop.Y - 12);
-            Point lpbottom = new Point(mptop.X - 12, mptop.Y);
-            Point rpbottom = new Point(mptop.X + 12, mptop.Y);
-            Point mpmid = new Point(mptop.X - 4, mptop.Y - 4);
-            AddComponent(new obj_arc(mpmid, 8, 8, 180, 180));
-            AddComponent(new obj_arc(outtop, 24, 24, 180, 180));
+            Point outtop = new Point(mptop.X - 8, mptop.Y - 16);
+            Point lpbottom = new Point(mptop.X - 8, mptop.Y);
+            Point rpbottom = new Point(mptop.X + 8, mptop.Y);
+            Point mpmid = new Point(mptop.X - 3, mptop.Y - 6);
+            AddComponent(new obj_arc(mpmid, 6, 12, 180, 180));
+            AddComponent(new obj_arc(outtop, 16, 32, 180, 180));
             AddComponent(new obj_line(lpbottom, rpbottom));
         }
     }
