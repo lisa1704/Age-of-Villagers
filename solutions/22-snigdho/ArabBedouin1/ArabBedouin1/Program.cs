3400aec2faff451a7834adb023eaabdcd6b65ace
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-21 00:44:01 +0600

    #1 Made classes public

---
+++ solutions/22-snigdho/ArabBedouin1/ArabBedouin1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.Arab_Bedouin
+{
+    class ArabBedouin : INation
+    public class ArabBedouin : INation
+    {
+        public Color BackgroundColor()
+        {
+            return Color.GreenYellow;
+        }
+        public void DrawHouse(Graphics g, Point point)
+        {
+            ArabBedouinHouse arabBedouinHouse = new ArabBedouinHouse(point);
+            arabBedouinHouse.Draw(g);
+        }
+        public void DrawTree(Graphics g, Point point)
+        {
+            ArabBedouinTrees arabBedouinTree = new ArabBedouinTrees(point);
+            arabBedouinTree.Draw(g);
+        }
+        public void DrawWaterSource(Graphics g, Point point)
+        {
+            NullItems arabBedouinWaterSource = new NullItems();
+            arabBedouinWaterSource.drawing();
+        }
+    }
+}
