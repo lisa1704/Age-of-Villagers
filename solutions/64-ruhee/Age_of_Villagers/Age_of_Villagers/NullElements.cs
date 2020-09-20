ef38e7b512846cf9c8179b2f26f0c478ad096972
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 18:15:48 +0600

    #1 #7 created default Null Element class.

---
+++ solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/NullElements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Age_of_Villagers.Age_of_Villagers
+{
+    public class NullElements: CShape
+    {
+        public void drawing()
+        {
+            MessageBox.Show("Null Element");
+        }
+    }
+}
