e3462dd9041087a1a1865e625f3c7181ce6dfd41
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-20 21:47:55 +0600

    #7 draw method added in Rectangle Triangle Arc

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Rectangle.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Shape/Rectangle.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Shape
 {
     class Rectangle : IShape
     {
-
+        public void Draw(Graphics g, int x, int y, int height, int width)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
