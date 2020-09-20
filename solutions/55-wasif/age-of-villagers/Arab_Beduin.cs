1ab0d74cc915b2fb20e8ae628583668c15a86a5f
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-20 22:11:23 +0600

    #10 Tried drawing tree

--- solutions/55-wasif/age-of-villagers/Arab_Beduin.cs
+++ solutions/55-wasif/age-of-villagers/Arab_Beduin.cs
@@ -30,7 +30,25 @@ namespace age_of_villagers
 
         public void draw_tree()
         {
-            throw new NotImplementedException();
+            Point p0 = new Point(50, 50);
+            Point p1 = new Point(p0.X - 2, p0.Y);
+            Point p2 = new Point(p0.X + 2, p0.Y);
+            Point p3 = new Point(p0.X - 2, p0.Y + 50);
+            Point p4 = new Point(p0.X + 2, p0.Y + 50);
+            Point p5 = new Point(p0.X, p0.Y - 35);
+            Point p6 = new Point(p0.X - 25, p0.Y - 25);
+            Point p7 = new Point(p0.X + 25, p0.Y - 25);
+            Point p8 = new Point(p0.X - 35, p0.Y - 10);
+            Point p9 = new Point(p0.X + 35, p0.Y - 10);
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p1, p3);
+            g.DrawLine(pen, p3, p4);
+            g.DrawLine(pen, p2, p4);
+            g.DrawLine(pen, p0, p5);
+            g.DrawLine(pen, p0, p6);
+            g.DrawLine(pen, p0, p7);
+            g.DrawLine(pen, p0, p8);
+            g.DrawLine(pen, p0, p9);
         }
 
         public void draw_watersource()
