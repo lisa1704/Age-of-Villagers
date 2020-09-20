53060c72667b86966883e40652e457da97130089
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-20 11:13:41 +0600

    #1 NullItems class implemented

--- solutions/42-rifah/AgeOfVillagers/NullItems.cs
+++ solutions/42-rifah/AgeOfVillagers/NullItems.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
     public class NullItems : Composite
     {
+        void drawing()
+        {
+            MessageBox.Show("NULL ITEMS");
+        }
     }
 }
