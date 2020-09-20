f76ad6efc66906e01c3febd87f237f0c7599e8e7
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-20 15:30:45 +0600

    #5 ISnapState interface and VillageState class created.

---
+++ solutions/33-nafisa/AgeOfVillagers/ISnapState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    interface ISnapState
+    {
+        public void getState();
+    }
+}
---
+++ solutions/33-nafisa/AgeOfVillagers/VillageState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public class VillageState : ISnapState
+    {
+        public void getState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
