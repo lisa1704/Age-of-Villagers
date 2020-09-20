72f9e8ffb895f057bc17a90979f9dc288cc5484e
akib <akibahmed@iut-dhaka.edu>
2020-09-20 16:40:54 +0600

    #7 Lines Class Implemented

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Lines.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Lines.cs
@@ -4,5 +4,16 @@ namespace AgeOfVillagers
 
 public class Lines : Shape
 {
-	
+    private int x1, y1, x2, y2;
+    public Lines(int x1, int y1, int x2, int y2)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public string Draw_Shape()
+    {
+        return "Drawing a Line...";
+    }
 }
