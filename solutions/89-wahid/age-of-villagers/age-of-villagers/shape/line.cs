3eaec31d2d1fd94c359351215d612f258fc14ac1
Md Wahid Shikder <www.wahid16@gmail.com>
2020-09-20 22:00:31 +0600

    #7 line created

---
+++ solutions/89-wahid/age-of-villagers/age-of-villagers/shape/line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace age_of_villagers.shape
+{
+    class line : Ishape
+    {
+        public void draw(Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
