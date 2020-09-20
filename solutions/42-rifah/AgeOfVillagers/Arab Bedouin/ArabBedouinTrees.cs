fbc9b8fc7b030fa3dcdb2e4f0afe63b918a35e45
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-20 09:46:31 +0600

    #1 #7 #10 Chnages made

--- solutions/42-rifah/AgeOfVillagers/Arab Bedouin/ArabBedouinTrees.cs
+++ solutions/42-rifah/AgeOfVillagers/Arab Bedouin/ArabBedouinTrees.cs
@@ -12,8 +12,8 @@ namespace AgeOfVillagers.Arab_Bedouin
         {
             Point point1 = new Point(point.X + 30, point.Y - 10);
             Point point2 = new Point(point.X - 30, point.Y - 10);
-            Point point3 = new Point(point.X + 25, point.Y - 20);
-            Point point4 = new Point(point.X - 25, point.Y - 20);
+            Point point3 = new Point(point.X + 25, point.Y - 25);
+            Point point4 = new Point(point.X - 25, point.Y - 25);
             Point point5 = new Point(point.X, point.Y - 30);
             Point TopLeft = new Point(point.X - 3, point.Y);
             Point RightBottom = new Point(point.X + 3, point.Y + 50);
