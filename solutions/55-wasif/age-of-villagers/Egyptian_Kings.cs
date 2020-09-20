bd652b271b4667280de3e261dc68ec435954c50f
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-20 22:40:10 +0600

    #11 tried drawing tree

--- solutions/55-wasif/age-of-villagers/Egyptian_Kings.cs
+++ solutions/55-wasif/age-of-villagers/Egyptian_Kings.cs
@@ -28,12 +28,29 @@ namespace age_of_villagers
 
         public void draw_tree()
         {
-            throw new NotImplementedException();
+            Point p0 = new Point(50, 50);
+            Point p1 = new Point(p0.X + 25, p0.Y - 25);
+            Point p2 = new Point(p0.X - 25, p0.Y - 25);
+            Point p3 = new Point(p1.X + 25, p1.Y - 25);
+            Point p4 = new Point(p1.X, p1.Y - 25);
+            Point p5 = new Point(p1.X + 25, p0.Y - 25);
+            Point p6 = new Point(p2.X + 25, p2.Y - 25);
+            Point p7 = new Point(p2.X - 5, p2.Y - 25);
+            Point p8 = new Point(p2.X - 25, p2.Y - 25);
+            gp.DrawLine(pen, p0, p1);
+            gp.DrawLine(pen, p0, p2);
+            gp.DrawLine(pen, p1, p3);
+            gp.DrawLine(pen, p1, p4);
+            gp.DrawLine(pen, p1, p5);
+            gp.DrawLine(pen, p2, p6);
+            gp.DrawLine(pen, p2, p7);
+            gp.DrawLine(pen, p2, p8);
         }
 
         public void draw_watersource()
         {
-            throw new NotImplementedException();
+
+            
         }
     }
 }
