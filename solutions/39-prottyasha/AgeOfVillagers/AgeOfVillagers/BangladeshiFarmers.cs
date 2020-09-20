a2f220f88d6fca781fc172638dc600409968ecac
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-20 09:32:10 +0600

    #9 #8 bd house and tree draw implemented. Maintained constraints.

--- solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmers.cs
@@ -10,12 +10,29 @@ namespace AgeOfVillagers
         Pen pen = new Pen(Color.Green);
         public void drawhouse(Point p, Graphics g)
         {
-            throw new NotImplementedException();
+            Point p1 = new Point(p.X - 8, p.Y - 8);
+            Point p2 = new Point(p1.X + 16, p1.Y);
+            Point p3 = new Point(p1.X, p1.Y - 8);
+            Point p4 = new Point(p3.X + 16, p3.Y);
+            g.DrawLine(pen, p, p1);
+            g.DrawLine(pen, p, p2);
+            g.DrawLine(pen, p2, p1);
+            g.DrawLine(pen, p3, p1);
+            g.DrawLine(pen, p2, p4);
+            g.DrawLine(pen, p3, p4);
         }
 
         public void drawtree(Point p, Graphics g)
         {
-            throw new NotImplementedException();
+            g.DrawArc(pen, p.X, p.Y, 16, 16, 0, 360);
+            Point p1 = new Point(p.X - 2, p.Y);
+            Point p2 = new Point(p.X + 2, p.Y);
+            Point p3 = new Point(p1.X, p1.Y - 8);
+            Point p4 = new Point(p2.X, p3.Y);
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p1, p3);
+            g.DrawLine(pen, p4, p2);
+            g.DrawLine(pen, p3, p4);
         }
 
         public void drawwatersource(Point p, Graphics g)
