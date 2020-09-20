5b333be33fe16a31e71861d2d83e6133728d7d1e
mshSAFAYAT <mshSAFAYAT@DESKTOP-8PKMA5A>
2020-09-20 19:45:41 +0600

    #1 refactored.

--- solutions/90-safayat/AgeOfVillagers90/Rectangle.cs
+++ solutions/90-safayat/AgeOfVillagers90/Rectangle.cs
@@ -9,8 +9,6 @@ namespace AgeOfVillagers90
 {
     class Rectangle : CompositeShape
     {
-        private readonly Point TopLeft;
-        private readonly Point BottomRight;
         public Rectangle(Point topLeft, Point BottomRight)
         {
             var TopRight = new Point(BottomRight.X, topLeft.Y);
@@ -21,17 +19,5 @@ namespace AgeOfVillagers90
             AddComponent(new Line(BottomLeft, BottomRight));
             AddComponent(new Line(BottomRight, TopRight));
         }
-
-       
-        /*public void Draw(Graphics g)
-        {
-            var pen = new Pen(Color.Black, 1);
-            g.DrawLine(pen, TopLeft.X, TopLeft.Y, BottomRight.X, TopLeft.Y);
-            g.DrawLine(pen, TopLeft.X, TopLeft.Y, TopLeft.X, BottomRight.Y);
-            g.DrawLine(pen, TopLeft.X, BottomRight.Y, BottomRight.X, BottomRight.Y);
-            g.DrawLine(pen, BottomRight.X, TopLeft.Y, BottomRight.X, BottomRight.Y);
-        }*/
-
-
     }
 }
