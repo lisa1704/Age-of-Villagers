794a5651dfde029673dce312ec33164e14d83338
akib <akibahmed@iut-dhaka.edu>
2020-09-20 14:36:45 +0600

    #12 Inuit_Hunters Class Implemented

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Inuit_Hunters.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Inuit_Hunters.cs
@@ -4,5 +4,22 @@ namespace AgeOfVillagers
 
 public class Inuit_Hunters : Nation
 {
-	
+    public I_Shape Draw_Tree()
+    {
+        return NotSupportedException;
+    }
+    public I_Shape Draw_House(int x, int y)
+    {
+        house = new InuitHunters_House(x, y);
+        return house;
+    }
+    public I_Shape Draw_WaterRes()
+    {
+        return NotSupportedException;
+    }
+
+    public string BG_Color()
+    {
+        return "Pure White";
+    }
 }
