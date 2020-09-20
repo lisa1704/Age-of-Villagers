54c17f4b94190f7852caadeb09f03cf09b1a96fb
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-21 02:49:36 +0600

    #10 #7 ArabianHouse created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/VillegeItems/ArabianHouse.cs
@@ -0,0 +1,26 @@
+﻿using AgeOfVillagers.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.VillegeItems
+{
+    class ArabianHouse : VillageItem
+    {
+        private readonly Point triangleTop, quadTop, bottomLeft, bottomMid, bottomRight;
+
+        public ArabianHouse(Point point)
+        {
+            triangleTop = point;
+            quadTop = new Point(triangleTop.X + 20, triangleTop.Y - 5);
+            bottomLeft = new Point(triangleTop.X - 15, triangleTop.Y + 40);
+            bottomMid = new Point(triangleTop.X + 15, triangleTop.Y + 40);
+            bottomRight = new Point(triangleTop.X + 35, triangleTop.Y + 35);
+            addItem(new Triangle(triangleTop, bottomLeft, bottomMid));
+            addItem(new Line(triangleTop, quadTop));
+            addItem(new Line(bottomRight, quadTop));
+            addItem(new Line(bottomMid, bottomRight));
+        }
+    }
+}
