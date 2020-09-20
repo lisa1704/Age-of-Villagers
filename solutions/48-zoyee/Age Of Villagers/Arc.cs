96bf3e83a7bdbc41286e9113bcacf821d9335675
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-09-20 23:29:56 +0600

    #1 #12 Arc class edited.

--- solutions/48-zoyee/Age Of Villagers/Arc.cs
+++ solutions/48-zoyee/Age Of Villagers/Arc.cs
@@ -9,16 +9,17 @@ namespace Age_Of_Villagers
 {
     class Arc : IShape
     {
-        public float x, y, width, height, i_angle, s_angle;
-        private readonly Point point1;
-        private readonly Point point2;
+        public int x, y;
+        public float width;
+        private float height;
+        public float i_angle, s_angle;
 
-        public Arc(float x, float y, float width, float heigth, float i_angle, float s_angle)
+        public Arc(int x, int y, float width, float height, float i_angle, float s_angle)
         {
             this.x = x;
             this.y = y;
             this.width = width;
-            this.height = heigth;
+            this.height = height;
             this.i_angle = i_angle;
             this.s_angle = s_angle;
         }
@@ -26,6 +27,17 @@ namespace Age_Of_Villagers
         {
             var pen = new Pen(Color.Black, 1);
             g.DrawArc(pen, x, y, width, height, i_angle, s_angle);
+        }
+    }
+    public class Elipse_Shape : CompositeShape
+    {
+        private readonly Point TopLeft;
+        private readonly Point BottomRight;
+        public Elipse_Shape(Point TopLeft, Point BottomRight)
+        {
+            this.TopLeft = TopLeft;
+            this.BottomRight = BottomRight;
+            AddComponent(new Arc(TopLeft.X, TopLeft.Y, BottomRight.X - TopLeft.X, BottomRight.Y - TopLeft.Y, 0, 360));
         }
     }
 }
