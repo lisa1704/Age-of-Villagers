f1afc8b85929af6a0ccb816af8bb32d83fa86321
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-21 00:20:28 +0600

    #7 #11 Draw house implemented for Egyptian Kings

--- solutions/83-abedeen/src/Project/Utilities/Egyptian_Kings.java
+++ solutions/83-abedeen/src/Project/Utilities/Egyptian_Kings.java
@@ -1,5 +1,6 @@
 package Project.Utilities;
 
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
 import java.util.ArrayList;
@@ -14,7 +15,22 @@ public class Egyptian_Kings implements INations{
 
     @Override
     public ArrayList<Shape> drawHouse(double x, double y) {
-        return null;
+        ArrayList<Shape> pyramid = new ArrayList<>();
+        Line l1 = new Line(x,y,x+houseWidth*3/5,y+houseHeight/4);
+        l1.setStrokeWidth(2);
+        pyramid.add(l1);
+        Line l2 = new Line(x+houseWidth*3/5,y+houseHeight/4,x+houseWidth,y-houseHeight/4);
+        l2.setStrokeWidth(2);
+        pyramid.add(l2);
+        Line l3 = new Line(x+houseWidth,y-houseHeight/4,x+houseWidth*2/5,y-houseHeight*3/4);
+        l3.setStrokeWidth(2);
+        pyramid.add(l3);
+        Line l4 = new Line(x+houseWidth*2/5,y-houseHeight*3/4,x,y);
+        l4.setStrokeWidth(2);
+        pyramid.add(l4);
+        Line l5 = new Line(x+houseWidth*3/5,y+houseHeight/4,x+houseWidth*2/5,y-houseHeight*3/4);
+        pyramid.add(l5);
+        return pyramid;
     }
 
     @Override
