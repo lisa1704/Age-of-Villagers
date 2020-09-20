5d02141d706c07ebaa38b4894cae308770a467a5
MD ASHIQUR RAHMAN JOY <ashiqurrahman85@iut-dhaka.edu>
2020-09-20 17:27:26 +0600

    #10 #6 House shape implemented for Arab Bedouin

--- solutions/85-joy/src/project/Utilities/ArabBedouin.java
+++ solutions/85-joy/src/project/Utilities/ArabBedouin.java
@@ -1,14 +1,38 @@
 package project.Utilities;
 
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
 import java.util.ArrayList;
 
 public class ArabBedouin implements  INation{
     private  String TerrainColor = "#EDE574";
+
+    public ArabBedouin(){
+        super();
+    }
     @Override
     public ArrayList<Shape> DrawHouse(double x1, double y1) {
-        return null;
+
+        ArrayList<Shape> House = new ArrayList<Shape>();
+
+        //Triangle
+        Line line1 = new Line(x1,y1,x1+(HouseWidth-4),y1);
+        House.add(line1);
+        Line line2 = new Line(x1+(HouseWidth-4),y1,x1+((HouseWidth-4)/2),y1-12);
+        line2.setStrokeWidth(2.0);
+        House.add(line2);
+        Line line3 = new Line(x1+((HouseWidth-4)/2),y1-12,x1,y1);
+        House.add(line3);
+
+        //Rectangle
+        Line line4 = new Line(x1+(HouseWidth-4),y1,x1+HouseWidth,y1-4);
+        House.add(line4);
+        Line line5 = new Line(x1+HouseWidth,y1-4,x1+((HouseWidth-4)-2),y1-HouseHeight);
+        House.add(line5);
+        Line line6 = new Line(x1+((HouseWidth-4)-2),y1-HouseHeight,x1+((HouseWidth-4)/2),y1-12);
+        House.add(line6);
+        return House;
     }
 
     @Override
