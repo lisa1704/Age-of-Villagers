a3473efab17926ccbdf0cee0fba5c5e9692d4d81
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-09-20 11:27:49 +0600

    #4 canvasSingletone has been updated

--- solutions/44-rafsan/CanvasSingleton.java
+++ solutions/44-rafsan/CanvasSingleton.java
@@ -1,26 +1,26 @@
 package sample;
 import javafx.scene.canvas.Canvas;
 
-public class CanvasSingleton {
-    private static CanvasSingleton instance = null;
-    private Canvas can;
+    public class CanvasSingleton {
+        private static CanvasSingleton instance = null;
+        private Canvas canvas;
 
-    private CanvasSingleton() {
-        can = new Canvas(600, 400);
-    }
+        private CanvasSingleton() {
+            canvas = new Canvas(600, 400);
+        }
 
-    public static CanvasSingleton getInstance() {
-        if (instance == null)
+        public static CanvasSingleton getInstance() {
+            if (instance == null)
+                instance = new CanvasSingleton();
+            return instance;
+        }
+
+        public Canvas getCanvas(){
+            return canvas;
+        }
+
+        public Canvas renewCanvas(){
             instance = new CanvasSingleton();
-        return instance;
-    }
-
-    public Canvas getCanvas(){
-        return can;
-    }
-
-    public Canvas renewCanvas(){
-        instance = new CanvasSingleton();
-        return instance.getCanvas();
-    }
+            return instance.getCanvas();
+        }
 }
