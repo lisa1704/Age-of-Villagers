35f69bc37f30738c0e6013da25131378d4439fe4
tiashaa <hriditatiasha@gmail.com>
2020-09-21 00:03:18 +0600

    #1 refactored

--- solutions/41-tiasha/Age of Villagers/Line.cs
+++ solutions/41-tiasha/Age of Villagers/Line.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class Line
+    public class Line : IComponent
     {
+        public int x1, y1, x2, y2;
+        public Point p1, p2;
+
+        public Line(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            g.DrawLine(pen, x1, y1, x2, y2);
+        }
     }
 }
