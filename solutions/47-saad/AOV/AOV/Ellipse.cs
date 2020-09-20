2c84b3a775435c13bc071beeca21e5d7a67caa6d
Saad <saadbinjohir@gmail.com>
2020-09-20 15:26:16 +0600

    #2 #7 Ellipse

--- solutions/47-saad/AOV/AOV/Ellipse.cs
+++ solutions/47-saad/AOV/AOV/Ellipse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,18 @@ namespace AOV
 {
     class Ellipse : CompoundShape
     {
-        public Ellipse()
+        private Point x1;
+        private Point x2;
+        float height;
+        float width;
+
+        public Ellipse(Point x1,Point x2)
         {
+            this.x1 = x1;
+            this.x2 = x2;
+            height = x2.X - x1.X;
+            width = x2.Y - x1.Y;
+            addShape(new DrawArc(x1.X, x1.Y, height, width, 0, 360));
         }
     }
 }
