ce5c6876c5cf16439ba00d197eaa8d32904c775b
Md Wahid Shikder <www.wahid16@gmail.com>
2020-09-21 02:03:05 +0600

    #11 egyptian  kings nation created

---
+++ solutions/89-wahid/age-of-villagers/age-of-villagers/nation/egyptiankings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace age_of_villagers.nation
+{
+    class egyptiankings : Ination
+    {
+        public void drawhouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawtree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawwatersource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
