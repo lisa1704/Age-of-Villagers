2a92550adb7905069c071fa76d3b79122a91175c
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-20 15:22:49 +0600

    #4 Inserted different nations

--- solutions/22-snigdho/Age Of Villagers/Age Of Villagers/Form1.Designer.cs
+++ solutions/22-snigdho/Age Of Villagers/Age Of Villagers/Form1.Designer.cs
@@ -66,6 +66,11 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bangladeshi Farmers",
+            "Arab Bedouins",
+            "Egyptian Kings",
+            "Inuit Hunters"});
             this.comboBox1.Location = new System.Drawing.Point(377, 52);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
