ac284e88bc636a4e7b69355ee423c0a632f0e5c1
Md Wahid Shikder <www.wahid16@gmail.com>
2020-09-21 02:04:47 +0600

    #12 Inuit Hunters Nation created

---
+++ solutions/89-wahid/age-of-villagers/age-of-villagers/nation/inuithunters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace age_of_villagers.nation
+{
+    class inuithunters : Ination
+    {
+        public void drawhouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawtree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawwatersource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
