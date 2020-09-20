3952bb4421ae250c49f905e4493b85cabc7b8b1c
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-21 00:34:59 +0600

    #7 #10 #12 Null Object Pattern implemented

---
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/NullNation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class NullNation : INation
+    {
+        public Color Color_Terrain()
+        {
+            return Color.White;
+        }
+
+        public void Draw_House(Graphics g, Point p)
+        {
+            NullObject Nullhouse = new NullObject();
+            Nullhouse.Draw_Shape();
+        }
+
+        public void Draw_Tree(Graphics g, Point p)
+        {
+            NullObject NullTree = new NullObject();
+            NullTree.Draw_Shape();
+        }
+
+        public void Draw_WaterSource(Graphics g, Point p)
+        {
+            NullObject NullWater = new NullObject();
+            NullWater.Draw_Shape();
+        }
+    }
+}
