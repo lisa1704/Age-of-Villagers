1fb1e34cee530855efae475fd3797b08178648c7
Ratun <www.ratunrahman@gmail.com>
2020-09-20 20:21:35 +0600

    #10 implement ArabBedouin class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabBedouin.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabBedouin.cs
@@ -1,6 +1,25 @@
 ﻿using System;
 
-public class ArabBedouin
+public class ArabBedouin : Nation
 {
-	
+    public IShape draw_tree(int x, int y)
+    {
+        tree = new Arabtree(x, y);
+        return tree;
+        //return "Drawing a tree. ";
+    }
+    public IShape draw_house(int x, int y)
+    {
+        house = new Arabhouse(x, y);
+        return house;
+        //return "Drawing a tree. ";
+    }
+    public IShape draw_water(int waterX, int waterY)
+    {
+        return NotSupportedException;
+    }
+    public string backgroundColor()
+    {
+        return "yellowish";
+    }
 }
