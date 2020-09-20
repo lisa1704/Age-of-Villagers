4c37253490bc24321dd03ca2020018f10b59049c
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-20 20:54:09 +0600

    #1 #8  null nation updated.

--- solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/NullNation.cs
+++ solutions/64-ruhee/Age_of_Villagers/Age_of_Villagers/NullNation.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.Age_of_Villagers
 {
-    class NullNation
+    public class NullNation : INation
     {
+        public Color BackgroundColor()
+        {
+            return Color.White;
+        }
 
+        public void DrawHouse(Graphics g, Point point)
+        {
+            NullElements NullHouse = new NullElements();
+            NullHouse.drawing();
+        }
+
+        public void DrawTree(Graphics g, Point point)
+        {
+            NullElements NullTree = new NullElements();
+            NullTree.drawing();
+        }
+
+        public void DrawWaterSource(Graphics g, Point point)
+        {
+            NullElements NullWaterSource = new NullElements();
+            NullWaterSource.drawing();
+        }
     }
 }
