e6cab6cdb6bba26ce334d7f0bf68c947f8063fc4
MD ASHIQUR RAHMAN JOY <ashiqurrahman85@iut-dhaka.edu>
2020-09-21 00:50:16 +0600

    #11 Tree Shape implemented for Egyptian Kings

--- solutions/85-joy/src/project/Utilities/EgyptianKings.java
+++ solutions/85-joy/src/project/Utilities/EgyptianKings.java
@@ -41,7 +41,26 @@ public class EgyptianKings implements INation{
 
     @Override
     public ArrayList<Shape> DrawTree(double x1, double y1) {
-        return null;
+        ArrayList<Shape> Tree = new ArrayList<Shape>();
+
+        Line line1 = new Line(x1,y1,x1+4,y1-18);
+        Tree.add(line1);
+        Line line2 = new Line(x1+4,y1-18,x1+8,y1-19);
+        Tree.add(line2);
+        Line line3 = new Line(x1+4,y1-18,x1+6,y1-23);
+        Tree.add(line3);
+        Line line4 = new Line(x1+4,y1-16,x1+3,y1-24);
+        Tree.add(line4);
+
+        Line line5 = new Line(x1,y1,x1-4,y1-18);
+        Tree.add(line5);
+        Line line6 = new Line(x1-4,y1-18,x1-8,y1-19);
+        Tree.add(line6);
+        Line line7 = new Line(x1-4,y1-18,x1-6,y1-23);
+        Tree.add(line7);
+        Line line8 = new Line(x1-4,y1-16,x1-3,y1-24);
+        Tree.add(line8);
+        return Tree;
     }
 
     @Override
