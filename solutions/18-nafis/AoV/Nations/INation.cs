57bf844f9f2f66b6017e689cb765e91dfc92aac1
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2020-09-20 23:59:06 +0600

    #8 Nation Interface Created

---
+++ solutions/18-nafis/AoV/Nations/INation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoV.Nations
+{
+    interface INation
+    {
+        void ColorTerrain(Graphics g);
+        void DrawHouse(Graphics g, Point p);
+        void DrawTree(Graphics g, Point p);
+        void DrawWaterSource(Graphics g, Point p);
+
+    }
+}
