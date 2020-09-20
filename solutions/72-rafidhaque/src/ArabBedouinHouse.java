f7375688e10f29cd3f516670407bb1ca501d7fe8
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-09-20 06:32:15 +0600

    #10 ArabBedouinHouse done

--- solutions/72-rafidhaque/src/ArabBedouinHouse.java
+++ solutions/72-rafidhaque/src/ArabBedouinHouse.java
@@ -1,4 +1,5 @@
 import javafx.scene.Group;
+import javafx.scene.shape.Line;
 
 public class ArabBedouinHouse extends DrawHouse {
     public ArabBedouinHouse(Group g) {
@@ -7,6 +8,14 @@ public class ArabBedouinHouse extends DrawHouse {
 
     @Override
     public void draw() {
+        Line line1 = new Line(x, y + 16, x + 8, y + 4);
+        Line line2 = new Line(x + 12, y + 16, x + 8, y + 4);
+        Line line3 = new Line(x + 12, y + 16, x, y + 16);
+        Line line4 = new Line(x + 8, y + 4, x + 16, y);
+        Line line5 = new Line(x + 12, y + 16, x + 16, y + 12);
+        Line line6 = new Line(x + 16, y, x + 16, y + 12);
 
+        super.g.getChildren().addAll(line1, line2, line3, line4, line5, line6);
+        drawOuterLayer();
     }
 }
