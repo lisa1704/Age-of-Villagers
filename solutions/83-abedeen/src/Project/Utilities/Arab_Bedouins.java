f6423ae79d96974193077de945acb29dd2736978
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-20 19:23:14 +0600

    #1 #7 #8 #10 Arab Bedouin House implemented with removal of empty draw error

--- solutions/83-abedeen/src/Project/Utilities/Arab_Bedouins.java
+++ solutions/83-abedeen/src/Project/Utilities/Arab_Bedouins.java
@@ -1,5 +1,6 @@
 package Project.Utilities;
 
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
 import java.util.ArrayList;
@@ -12,7 +13,17 @@ public class Arab_Bedouins implements INations{
 
     @Override
     public ArrayList<Shape> drawHouse(double x, double y) {
-        return null;
+        ArrayList<Shape> house = new ArrayList<Shape>();
+        //Triangle
+        house.add(new Line(x,y,x+(3*houseWidth/4),y));
+        house.add(new Line(x+(3*houseWidth/4),y,x+(3*houseWidth/4)/2,y-(3*houseHeight/4)));
+        house.add(new Line(x+(3*houseWidth/4)/2,y-(3*houseHeight/4),x,y));
+
+        //Rectangle
+        house.add(new Line(x+(3*houseWidth/4),y,x+houseWidth,y-(houseHeight/4)));
+        house.add(new Line(x+(3*houseWidth/4)/2,y-(3*houseHeight/4),x+((3*houseWidth/4)/2)+(houseWidth/4),y-houseHeight));
+        house.add(new Line(x+houseWidth,y-(houseHeight/4),x+((3*houseWidth/4)/2)+(houseWidth/4),y-houseHeight));
+        return house;
     }
 
     @Override
