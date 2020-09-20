b2c6fd01bffed96f703a92cc59726516bb45274b
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-21 04:13:40 +0600

    #6 Created new class for opening and redrawing

---
+++ solutions/19-zarif/AgeOfVillagers/OpenVillageEdit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    public class OpenVillageEdit
+    {
+        private readonly Graphics g;
+        private readonly Pen pen;
+        private readonly Panel panel;
+
+        public OpenVillageEdit(Graphics g , Pen pen,Panel panel)
+        {
+            this.g = g;
+            this.pen = pen;
+            this.panel = panel;
+        }
+        public void ResetState(VillageState village, string check)
+        {
+            NationManager manager = new NationManager(check);
+           
+
+            foreach (Point pt in village._housePoints)
+            {
+                manager.getNation(pt).GetHouse().draw(g, pen);
+            }
+            foreach (Point pt in village._treePoints)
+            {
+                manager.getNation(pt).GetTrees().draw(g, pen);
+            }
+            foreach (Point pt in village._WaterScPoints)
+            {
+                manager.getNation(pt).GetWaterSources().draw(g, pen);
+            }
+
+            
+        }
+
+    }
+}
