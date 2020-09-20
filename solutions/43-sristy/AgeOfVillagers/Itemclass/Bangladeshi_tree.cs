22541d11ed53931e45a4433a009889b736ce512a
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-21 00:17:34 +0600

    #1 bangladeshi tree size added

--- solutions/43-sristy/AgeOfVillagers/Itemclass/Bangladeshi_tree.cs
+++ solutions/43-sristy/AgeOfVillagers/Itemclass/Bangladeshi_tree.cs
@@ -4,14 +4,14 @@ namespace Age_of_villagers
 {
     public class Bangladeshi_tree: Compositeobj
     {
-        public Bangladeshi_tree(Point mptop)
+        public Bangladeshi_tree(Point mptop,int height,int weidth)
         {
             Point lptop = new Point(mptop.X - 1, mptop.Y);
             Point rptop = new Point(mptop.X + 1, mptop.Y);
-            Point lpbottom = new Point(mptop.X - 1, mptop.Y + 16);
-            Point rpbottom = new Point(mptop.X + 1, mptop.Y + 16);
-            Point mpmid = new Point(mptop.X - 8, mptop.Y - 8);
-            AddComponent(new obj_arc(mpmid, 16, 16, 0, 360));
+            Point lpbottom = new Point(mptop.X - 1, mptop.Y + height-(weidth/2));
+            Point rpbottom = new Point(mptop.X + 1, mptop.Y + height- (weidth / 2));
+            Point mpmid = new Point(mptop.X - (weidth / 2), mptop.Y - (weidth / 2));
+            AddComponent(new obj_arc(mpmid, weidth, weidth, 0, 360));
             AddComponent(new obj_rectangle(lptop, rptop, lpbottom, rpbottom));
         }
     }
--- solutions/43-sristy/AgeOfVillagers/Nationclass/Bangladeshi.cs
+++ solutions/43-sristy/AgeOfVillagers/Nationclass/Bangladeshi.cs
@@ -7,6 +7,7 @@ namespace Age_of_villagers
     {
         Size housesize=new Housesize();
         Size treesize = new Treesize();
+
         public IObject Draw_house(Point lptop)
         {
             int height = housesize.GetHeight();
@@ -14,16 +15,21 @@ namespace Age_of_villagers
             Compositeobj house = new Bangladeshi_house(lptop,height,weight);
             return house;
         }
+
         public IObject Draw_tree(Point mptop)
         {
-            Compositeobj tree = new Bangladeshi_tree(mptop);
+            int height = treesize.GetHeight();
+            int weight = treesize.GetWeidth();
+            Compositeobj tree = new Bangladeshi_tree(mptop, height, weight);
             return tree;
         }
+
         public IObject Draw_watersource(Point p1)
         {
             Compositeobj watersource = new Bangladeshi_watersource(p1);
             return watersource;
         }
+
 
         public Color get_backcolor()
         {
