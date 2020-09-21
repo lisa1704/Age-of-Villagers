b941b5de3b202c9e2483eb865b041d3a6b4b54ea
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-21 16:14:17 +0600

    #7 arc shape updated

--- solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/Arc.cs
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/Arc.cs
@@ -10,11 +10,16 @@ namespace AgeofVillagers
         private Point point_1;
         private Point point_2;
 
-        public int x, y, height, width, start, sweep;
+        public float x, y, start, height, width, sweep;
 
-        public Arc()
+        public Arc(float x, float y, float width, float height, float start, float sweep)
         {
-
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.start = start;
+            this.sweep = sweep;
         }
 
         public void Paint(Graphics g, Pen p)
