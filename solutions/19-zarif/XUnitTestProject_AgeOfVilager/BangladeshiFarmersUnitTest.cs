7c9f09540367bfd56e5da7c16482806616dbce18
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-09-21 12:45:09 +0600

    #10 Test case created for BDFarmers back color and housepoint

---
+++ solutions/19-zarif/XUnitTestProject_AgeOfVilager/BangladeshiFarmersUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AgeOfVillagers;
+
+using System.Drawing;
+using Xunit;
+
+namespace XUnitTestProject_AgeOfVilager
+{
+   public class BangladeshiFarmersUnitTest
+    {
+        [Fact]
+        public void BDbackcolorTest()
+        {
+            string checkstring = "Bangladeshi Farmers";
+            NationManager man = new NationManager(checkstring);
+
+            var result = man.getNation(new Point()).GetTerritoryColor();
+            Assert.Equal(Color.Green, result);
+
+        }
+
+        [Fact]
+        public void BDHousepointTest()
+        {
+            string checkstring = "Bangladeshi Farmers";
+            NationManager man = new NationManager(checkstring);
+
+            Point point = new Point(10,10);
+            Point result = man.getNation(point).GetHousePoint();
+            Assert.Equal(point, result);
+
+        }
+        [Fact]
+        public void BDTreepointTest()
+        {
+            string checkstring = "Bangladeshi Farmers";
+            NationManager man = new NationManager(checkstring);
+
+            Point point = new Point(10, 10);
+            Point result = man.getNation(point).GetTreePoint();
+            Assert.Equal(point, result);
+
+        }
+    }
+}
