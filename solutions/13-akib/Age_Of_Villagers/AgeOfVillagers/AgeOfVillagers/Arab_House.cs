37d5f57045fd4df255626a3e08ab430667b57a2e
akib <akibahmed@iut-dhaka.edu>
2020-09-21 16:41:18 +0600

    #10 Arab_House Class implemented by creating house

---
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Arab_House.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AgeOfVillagers
+
+public class Arab_House
+{
+    public void Create_House(int x, int y)
+    {
+        Triangle ah_triangle = new Triangle(x, y, x - 8, y, x - 4, y + 12);
+        Rectangle ah_rectangle = new Rectangle(x, y, x + 8, y + 4, x, y + 16, x - 4, y + 12);
+
+        ah_triangle.Draw_Shape();
+        ah_rectangle.Draw_Shape();
+    }
+}
