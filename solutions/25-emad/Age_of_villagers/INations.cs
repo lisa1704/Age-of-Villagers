a3a264786a26ce367b7fbe4dea40c0db0acfab8d
mahirashrafemad <mahirashrafemad@users.noreply.github.com>
2020-09-21 17:51:39 +0600

    #7 #8 #9 #10 #11 INation methods changed

--- solutions/25-emad/Age_of_villagers/INations.cs
+++ solutions/25-emad/Age_of_villagers/INations.cs
@@ -2,18 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace Age_of_villagers
 {
     public interface INations
     {
-        string getName();
-         
-        void drawtree();
-
-        void drawhouse();
-
-        void drawwatersource();
+        String DrawTree(Graphics graphics, Point point);
+        String DrawHouse(Graphics graphics, Point point);
+        String DrawWaterSource(Graphics graphics, Point point);
+        Color DrawTerrain();
 
     }
 }
---
+++ solutions/25-emad/Age_of_villagers/IShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_of_villagers
+{
+    public interface IShape
+    {
+        public void draw(Point p, Graphics g);
+        public void draw(Point p, Graphics graphics);
+    }
+}
