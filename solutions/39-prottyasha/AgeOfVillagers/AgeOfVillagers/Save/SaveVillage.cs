e7e1612fe2cd9646b4897bd7ddf847794ab10815
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-22 05:19:23 +0600

    #5 new save village class implemented fully.

--- solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Save/SaveVillage.cs
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Save/SaveVillage.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Windows.Forms;
 
 namespace AgeOfVillagers.Save
 {
@@ -9,7 +12,35 @@ namespace AgeOfVillagers.Save
         Village vill;
         public SaveVillage(Village vill)
         {
-            vill = this.vill;
+            this.vill = vill;
         }
+
+        public void SaveFile()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "*.aov";
+            sfd.Filter = "AoV file(*.aov)| *.aov";
+            sfd.DefaultExt = "aov";
+            sfd.Title = "Save an aov File";
+            sfd.InitialDirectory = @"H:\aov_saved\";
+            sfd.RestoreDirectory = true;
+            sfd.ShowDialog();
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Stream fileStream = sfd.OpenFile();
+                using (StreamWriter sw = new StreamWriter(fileStream))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(sw, vill);
+                    sw.Close();
+                }
+                fileStream.Close();
+            }
+        }
+
+        
+        
+        
     }
 }
