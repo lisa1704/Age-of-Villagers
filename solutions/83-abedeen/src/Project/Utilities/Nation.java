83d5b0bce74b6638547f28d1d1adc6816b155326
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-21 19:56:56 +0600

    #1 #7 implemented Draw House for Context Nation

--- solutions/83-abedeen/src/Project/Utilities/Nation.java
+++ solutions/83-abedeen/src/Project/Utilities/Nation.java
@@ -2,6 +2,9 @@ package Project.Utilities;
 
 import Project.Utilities.NationItems.Bangladeshi_House;
 import Project.Utilities.NationItems.IHouse;
+import javafx.scene.shape.Shape;
+
+import java.util.ArrayList;
 
 public class Nation {
     private IHouse house;
@@ -10,5 +13,9 @@ public class Nation {
         if(nation_type == "Bangladeshi Farmers"){
             house = new Bangladeshi_House();
         }
+    }
+
+    public ArrayList<Shape> drawHouse(double x, double y){
+        return house.drawHouse(x,y);
     }
 }
