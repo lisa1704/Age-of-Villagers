320c73ba76c5c05e596667654f558b1c2574e66f
Ratun <www.ratunrahman@gmail.com>
2020-09-21 11:32:04 +0600

    #10 implement Arab House

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabHouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabHouse.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 
-public class ArabHouse
+public class ArabHouse : House
 {
-	
+    public void CreateHouse(int x, int y)
+    {
+        Triangle t = new Triangle(x-2,y+6,x-8,y-8,x+2,y-8);
+        t.Draw();
+        Rectangle r = new Rectangle(x - 2, y + 6, x + 2, y - 8, x + 8, y - 6, x + 1, y + 8);
+        r.Draw();
+    }
 }
