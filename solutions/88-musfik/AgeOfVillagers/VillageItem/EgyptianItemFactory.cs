21ff7104884bf60d3dcfae808413366f0732e84e
musfiksifat <musfiksifat766@gmail.com>
2020-09-22 05:38:46 +0600

    #11 EgyptianItemFactory class created & implemented

---
+++ solutions/88-musfik/AgeOfVillagers/VillageItem/EgyptianItemFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.VillageItem
+{
+    public class EgyptianItemFactory
+    {
+        private String type;
+        public EgyptianItemFactory(String ItemType)
+        {
+            type = ItemType;
+        }
+
+        internal IVillageItem ItemProducer(Point point)
+        {
+            if (type == "House")
+                return new EgyptianHouse(point);
+
+            else if (type == "Tree")
+                return new EgyptianTree(point);
+
+            else if (type == "Water")
+                return new EgyptianWaterResource(point);
+
+            return new NoTree(point);
+        }
+    }
+}
