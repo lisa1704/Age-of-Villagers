2d601f9abaf14892093e23507462edbbe9ad1114
Ratun <www.ratunrahman@gmail.com>
2020-09-21 21:20:20 +0600

    #1 fixed Inuit house class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHouse.cs
@@ -1,12 +1,12 @@
-﻿using System;
-
-public class InuitHouse : House
+﻿namespace ageOfVillagers
 {
-	public void CreateHouse(int x, int y)
+    internal class InuitHouse : IShape
     {
-        HalfCircle h1 = new HalfCircle(x,y,8);
-        h1.Draw();
-        HalfCircle h2 = new HalfCircle(x, y, 2);
-        h2.Draw();
+        public string draw(int x, int y)
+        {
+            HalfCircle h1 = new HalfCircle(x, y, 8);
+            HalfCircle h2 = new HalfCircle(x, y, 2);
+            return h1.Draw() + h2.Draw();
+        }
     }
 }
