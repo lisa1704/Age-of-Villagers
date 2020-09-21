520de2346308e78d2a8fd2a9e9bbc8f849064d3a
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-09-21 19:35:26 +0600

    #15 #8 #10 tests written for Arab Bedouin and passed

---
+++ solutions/72-rafidhaque/tests/ArabBedouinTest.java
@@ -0,0 +1,40 @@
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArabBedouinTest {
+
+    private ArabBedouin arabBedouin;
+    private JFXPanel panel = new JFXPanel();
+    private String nationName;
+    private DrawHouse newHouse;
+
+    @Before
+    public void setUp() throws Exception {
+        arabBedouin = new ArabBedouin(new Group());
+        nationName = "Arab Bedouin";
+    }
+
+    @Test
+    public void getNationName() {
+        assertEquals(nationName, arabBedouin.getNationName());
+    }
+
+    @Test
+    public void getHouse() {
+        assertEquals("ArabBedouinHouse", arabBedouin.getHouse().getClass().getSimpleName());
+    }
+
+    @Test
+    public void getTree() {
+        assertEquals("ArabBedouinTree", arabBedouin.getHouse().getClass().getSimpleName());
+    }
+
+    @Test
+    public void getWater() {
+        assertEquals("ArabBedouinWater", arabBedouin.getHouse().getClass().getSimpleName());
+    }
+}
