041443d34309d27431505dccebddad0e233a4b6e
tiashaa <hriditatiasha@gmail.com>
2020-09-21 19:08:51 +0600

    #1 #7 refactoring and placing items

--- solutions/41-tiasha/Age of Villagers/Tree/BangladeshiTree.cs
+++ solutions/41-tiasha/Age of Villagers/Tree/BangladeshiTree.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using AgeOfVillagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers.Tree
 {
-    class BangladeshiTree
+    public class BangladeshiTree : CompositeShape
     {
+        public BangladeshiTree(Point p, ElementSize sz)
+        {
+            Point topleft = p;
+            Point topright = new Point(p.X + 2, p.Y);
+            Point bottomleft = new Point(p.X, p.Y + sz.height / 2);
+            Point bottomRight = new Point(p.X + 2, p.Y + sz.height / 2);
+
+            AddComponent(new Rectangle(topleft, topright, bottomleft, bottomRight));
+            AddComponent(new Arc(p.X - sz.width / 3, p.Y - sz.height / 2, sz.height / 2, sz.width, 0, 360));
+        }
+
     }
 }
