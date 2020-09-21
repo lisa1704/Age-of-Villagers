7d8968fb16402cb4bb81efee68151430352d6073
Nabil Hossain Nico <hossaintalukder108@gmail.com>
2020-09-21 20:45:43 +0600

    #15 Test framework recreated

---
+++ solutions/76-nico/AgeOfVillagers/AgeOfVillagers_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AgeOfVillagers_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AgeOfVillagers_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3ac2989-e268-40e1-8fe3-a5c143d40885")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ solutions/76-nico/AgeOfVillagers/AgeOfVillagers_Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgeOfVillagers_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int n = 5;
+            Assert.AreEqual(5, n);
+
+        }
+    }
+}
