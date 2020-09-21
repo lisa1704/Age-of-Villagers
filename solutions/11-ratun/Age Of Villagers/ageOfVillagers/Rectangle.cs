6751e2ac49a899cb9b520ecc3b9152831a7a04c0
Ratun <www.ratunrahman@gmail.com>
2020-09-21 23:58:13 +0600

    #2 #7 rectangle with line

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Rectangle.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Rectangle.cs
@@ -2,21 +2,18 @@
 {
     internal class Rectangle
     {
-        private int x1, y1, x2, y2, x3, y3, x4, y4;
+        private string status = "";
         public Rectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
         {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-            this.x3 = x3;
-            this.y3 = y3;
-            this.x4 = x4;
-            this.y4 = y4;
+            Line l1 = new Line(x1, y1, x2, y2);
+            Line l2 = new Line(x2, y2, x3, y3);
+            Line l3 = new Line(x3, y3, x4, y4);
+            Line l4 = new Line(x4, y4, x1, y1);
+            status = l1.Draw() + l2.Draw() + l3.Draw()+ l4.Draw();
         }
         public string Draw()
         {
-            return "Drawing a Rectangle. ";
+            return status;
         }
     }
 }
--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Triangle.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Triangle.cs
@@ -2,7 +2,6 @@
 {
     internal class Triangle
     {
-        private int x1, y1, x2, y2, x3, y3;
         private string status = "";
         public Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
         {
