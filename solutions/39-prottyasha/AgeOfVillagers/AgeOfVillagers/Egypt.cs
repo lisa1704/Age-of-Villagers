9be7076b53940cc120afd8f616f7262540252e5a
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-22 00:57:00 +0600

    #1 egypt component factory class

--- solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Egypt.cs
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Egypt.cs
@@ -8,7 +8,18 @@ namespace AgeOfVillagers
     {
         public override IComponent getComponent(string component_type)
         {
-            throw new NotImplementedException();
+            if (component_type == "tree")
+            {
+                return new EgyptTree();
+            }
+            if (component_type == "house")
+            {
+                return new EgyptHouse();
+            }
+            if (component_type == "water_source")
+            {
+                return new EgyptRiver();
+            }
         }
     }
 }
