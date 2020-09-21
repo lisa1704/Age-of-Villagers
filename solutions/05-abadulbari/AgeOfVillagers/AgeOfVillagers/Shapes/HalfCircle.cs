9d2ad32f43e05eeb0e6a7b6bf0eb5a77ee5ec1a0
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-21 19:49:04 +0600

    #7 HalfCircle shape created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/Shapes/HalfCircle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.Shapes
+{
+    class HalfCircle : IShape
+    {
+        private float x, y, width, height, startAngle, sweepAngle;
+        public HalfCircle(Point topLeft, Point bottomRight)
+        {
+            x = topLeft.X;
+            y = topLeft.Y;
+            width = bottomRight.X - topLeft.X;
+            height = bottomRight.Y - topLeft.Y;
+            startAngle = 180;
+            sweepAngle = 180;
+        }
+        public void draw(Graphics g, Pen p)
+        {
+            g.DrawArc(p, x, y, width, height, startAngle, sweepAngle);
+        }
+    }
+}
