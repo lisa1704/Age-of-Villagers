59b4a301f77442f7f0fca433f8f93d9a2ad2b6d4
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-22 02:09:16 +0600

    #9 #8 passed mouse pointer

--- solutions/55-wasif/age-of-villagers/village_factory.cs
+++ solutions/55-wasif/age-of-villagers/village_factory.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+
 namespace age_of_villagers
 {
     public class village_factory
     {
         public INations village;
         public Graphics gp;
-        public village_factory(Graphics gp)
+        public Point p;
+        public village_factory(Graphics gp, Point p)
         {
             this.gp = gp;
+            this.p = p;
         }
         public INations get_nation(string village_type)
         {
             if (village_type == "Bangladeshi Farmers")
-                return new Bangladeshi_Farmers(gp);
+                return new Bangladeshi_Farmers(gp, p);
             else if (village_type == "Arab Bedouin")
                 return new Arab_Beduin(gp);
             else if (village_type == "Egyptian Kings")
