7ab6c205ce5d99274277d11c9e37a91f153b63d3
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-22 03:20:32 +0600

    #7 Village created

---
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/Village.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Age_of_Villagers.Nation;
+
+namespace Age_of_villagers
+{
+    class Village
+    {
+        public INation nation;
+        public string villageName;
+       
+    }
+}
