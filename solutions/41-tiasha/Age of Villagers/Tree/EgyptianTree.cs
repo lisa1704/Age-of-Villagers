89057884cdf92cbce1929789e929e86f46d10193
tiashaa <hriditatiasha@gmail.com>
2020-09-21 19:17:43 +0600

    #1 #7 refactoring and placing items

--- solutions/41-tiasha/Age of Villagers/Tree/EgyptianTree.cs
+++ solutions/41-tiasha/Age of Villagers/Tree/EgyptianTree.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class EgyptianTree
+    public class EgyptianTree : CompositeShape
     {
+
+        public EgyptianTree(Point p, ElementSize sz)
+        {
+            Point p1 = new Point(p.X + sz.width / 3, p.Y - (sz.height - 4));
+            Point p2 = new Point(p.X - sz.width / 3, p.Y - (sz.height - 4));
+            Point p3 = new Point(p.X - sz.width / 2, p.Y - sz.height);
+            Point p4 = new Point(p.X + sz.width / 2, p.Y - sz.height);
+
+            AddComponent(new Line(p.X, p.Y, p1.X, p1.Y));
+            AddComponent(new Line(p.X, p.Y, p2.X, p2.Y));
+            AddComponent(new Line(p1.X, p1.Y, p4.X, p4.Y));
+            AddComponent(new Line(p2.X, p2.Y, p3.X, p3.Y));
+        }
     }
 }
