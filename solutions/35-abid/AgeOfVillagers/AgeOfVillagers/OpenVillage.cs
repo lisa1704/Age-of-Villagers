cfa909c8168ca6a21b20fb83deb89bd3898f2a3d
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-21 21:44:43 +0600

    #6 Implemented json deserialization and added a method to return saved state of the village.

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/OpenVillage.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/OpenVillage.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
     class OpenVillage : IOpenSave
     {
+        VillageState savedVillageState;
         public void execute()
         {
-            throw new NotImplementedException();
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                Stream AoVstream = openFile.OpenFile();
+                using (StreamReader stream = new StreamReader(AoVstream))
+                {
+                    string data = stream.ReadToEnd();
+                    savedVillageState = JsonConvert.DeserializeObject<VillageState>(data);
+                }
+            }
+        }
+        public VillageState getsavedvillage()
+        {
+            return savedVillageState;
         }
     }
 }
