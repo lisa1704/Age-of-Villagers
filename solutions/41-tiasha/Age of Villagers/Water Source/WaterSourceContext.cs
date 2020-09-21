75ba31f838ba067664187b92d1995de36bc5c39d
tiashaa <hriditatiasha@gmail.com>
2020-09-21 19:53:29 +0600

    #1 refactored

--- solutions/41-tiasha/Age of Villagers/Water Source/WaterSourceContext.cs
+++ solutions/41-tiasha/Age of Villagers/Water Source/WaterSourceContext.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class WaterSourceContext
+    public class WaterSourceContext
     {
+        public IWaterResource _watersourcetype;
+        public Graphics _g;
+        public Point _p;
+        public ElementSize _sz;
+
+        public WaterSourceContext(IWaterResource house, Graphics g, Point p, ElementSize sz)
+        {
+            this._watersourcetype = house;
+            this._g = g;
+            this._p = p;
+            this._sz = sz;
+        }
+
+        public void WaterSourceDrawing()
+        {
+            _watersourcetype.Draw(_sz, _g, _p);
+        }
     }
 }
