da47e239168f0b183e68304c26e77b57771bbd8e
akib <akibahmed@iut-dhaka.edu>
2020-09-21 16:35:27 +0600

    #10 Arab_Tree Class implemented by creating tree

---
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Arab_Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AgeOfVillagers
+
+public class Arab_Tree
+{
+	public void Create_Tree(int x, int y)
+    {
+        Rectangle at_rectangle = new Rectangle(x + 1, y, x + 1, y - 12, x - 1, y - 12, x - 1, y);
+        Lines at_line1 = new Lines(x, y, x, y+12);
+        Lines at_line2 = new Lines(x, y, x+8, y+3);
+        Lines at_line3 = new Lines(x, y, x-8, y+3);
+        Lines at_line4 = new Lines(x, y, x+5, y+8);
+        Lines at_line5 = new Lines(x, y, x-5, y+8);
+
+        at_rectangle.Draw_Shape();
+        at_line1.Draw_Shape();
+        at_line2.Draw_Shape();
+        at_line3.Draw_Shape();
+        at_line4.Draw_Shape();
+        at_line5.Draw_Shape();
+    }
+}
