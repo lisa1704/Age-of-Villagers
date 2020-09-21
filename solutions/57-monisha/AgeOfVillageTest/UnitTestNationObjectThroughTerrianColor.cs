8b3a3eb5643b1888552ede39fdae3ee2b84ed450
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2020-09-21 19:46:10 +0600

    #1 Renamed test class

--- solutions/57-monisha/AgeOfVillageTest/UnitTestNationObjectThroughTerrianColor.cs
+++ solutions/57-monisha/AgeOfVillageTest/UnitTestNationObjectThroughTerrianColor.cs
@@ -6,7 +6,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AgeOfVillageTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestNationObjectThroughTerrianColor
     {
         [TestMethod]
         public void TestBangladeshiNationObjectThroughColor()
