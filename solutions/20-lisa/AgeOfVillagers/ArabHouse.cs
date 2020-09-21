ec3d563b37830224362248be641238288a4b13ca
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-21 21:35:58 +0600

    #10 ArabHouse implemented

--- solutions/20-lisa/AgeOfVillagers/ArabHouse.cs
+++ solutions/20-lisa/AgeOfVillagers/ArabHouse.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    public class ArabHouse:CompositeShape
+    public class ArabHouse : CompositeShape
     {
+        public ArabHouse(Point point)
+        {
+            Point p1 = new Point(point.X - 20, point.Y + 40);
+            Point p2 = new Point(point.X + 20, point.Y + 40);
+            Point p3 = new Point(point.X + 25, point.Y + 15);
+            Point p4 = new Point(p2.X + 25, p2.Y + 10);
+
+            AddComponent(new Triangle(point, p1, p2));
+            AddComponent(new Line(point, p3));
+            AddComponent(new Line(p2, p4));
+            AddComponent(new Line(p3, p4));
+        }
     }
 }
