02aae53c706359ff1b597b1d8f873fc8179faab1
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-21 23:55:24 +0600

    #1 #7 #12 House of Inuit Hunters is made Concrete Strategy

---
+++ solutions/83-abedeen/src/Project/Utilities/NationItems/Inuit_Hunters_House.java
@@ -0,0 +1,44 @@
+package Project.Utilities.NationItems;
+
+import Project.Utilities.Inuit_Hunters;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Shape;
+
+import java.util.ArrayList;
+
+public class Inuit_Hunters_House implements IHouse{
+    public Inuit_Hunters_House(){ super(); }
+    @Override
+    public ArrayList<Shape> drawHouse(double x, double y) {
+        ArrayList<Shape> igloo = new ArrayList<>();
+        Arc Dome = new Arc();
+        Dome.setCenterX(x);
+        Dome.setCenterY(y);
+        Dome.setRadiusX(houseWidth/2);
+        Dome.setRadiusY(houseHeight/2);
+        Dome.setType(ArcType.ROUND);
+        Dome.setStroke(Color.BLACK);
+        Dome.setStrokeWidth(1);
+        Dome.setFill(Color.TRANSPARENT);
+        Dome.setStartAngle(0.0);
+        Dome.setLength(180.0);
+        igloo.add(Dome);
+
+        double inner_radius = houseWidth/6;
+        Arc Entry = new Arc();
+        Entry.setCenterX(x);
+        Entry.setCenterY(y);
+        Entry.setRadiusX(inner_radius);
+        Entry.setRadiusY(inner_radius);
+        Entry.setType(ArcType.OPEN);
+        Entry.setStroke(Color.BLACK);
+        Entry.setStrokeWidth(1);
+        Entry.setFill(Color.TRANSPARENT);
+        Entry.setStartAngle(0.0);
+        Entry.setLength(180.0);
+        igloo.add(Entry);
+        return igloo;
+    }
+}
