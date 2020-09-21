b607319a21168a8678f39b62cb49afde7442243e
Tanvirus06 <tanvirrahman3344@gmail.com>
2020-09-21 14:42:41 +0600

    Create Line.cs

---
+++ solutions/06-tanvir/AgeOfVillagers/Line.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.Shapes
+{
+    class Line : IShape
+    {
+        Point p1;
+        Point p2;
+        Pen pen;
+        Graphics grap;
+        public Line(Graphics grap, Pen pen, Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.pen = pen;
+            this.g = grap;
+        }
+        public void CreateShape()
+        {
+            grap.DrawLine(pen, p1, p2);
+        }
+    }
+}
