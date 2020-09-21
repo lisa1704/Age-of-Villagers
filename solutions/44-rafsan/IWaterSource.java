7584d26bc41c0811a70a1eb5ac7aaa878126f5d7
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-09-21 11:16:31 +0600

    # WaterResource Interface created

---
+++ solutions/44-rafsan/IWaterSource.java
@@ -0,0 +1,11 @@
+package sample;
+
+public interface IWaterSource {
+    void draw(Point point);
+
+    String canvasController(int point1,int pont2);
+
+    String releaseCanvas(int point1,int pont2);
+
+    String getCanvas(int point1,int pont2);
+}
