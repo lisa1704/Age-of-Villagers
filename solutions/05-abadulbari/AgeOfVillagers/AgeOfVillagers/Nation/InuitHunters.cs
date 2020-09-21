222789e273cfd8b80d58c116b5b49409eb4c2a3f
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-21 19:27:02 +0600

    #12 InuitHunters nation created

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers/Nation/InuitHunters.cs
@@ -0,0 +1,36 @@
+﻿using AgeOfVillagers.VillegeItems;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.Nation
+{
+    class InuitHunters : INation
+    {
+        public VillageItem getHouse(Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getNationName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color getTerrainColor()
+        {
+            return Color.White;
+        }
+
+        public VillageItem getTree(Point pointp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VillageItem getWaterSource(Point pointp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
