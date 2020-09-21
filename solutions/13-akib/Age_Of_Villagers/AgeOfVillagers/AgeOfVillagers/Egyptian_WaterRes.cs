d829198dee94524323ffa0f2d6c1e2d8d53953a6
akib <akibahmed@iut-dhaka.edu>
2020-09-21 17:24:03 +0600

    #11 Egyptian_WaterRes Class Implemented by creating water resources

---
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Egyptian_WaterRes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AgeOfVillagers
+
+public class Egyptian_WaterRes
+{
+	private int x_size = 12;
+	private int y_size = 12;
+
+	public void Create_WaterResource(int x, int y)
+    {
+		Circle ew_circle = new Circle(x, y, 6);
+
+		ew_circle.Draw_Shape();
+    }
+
+}
