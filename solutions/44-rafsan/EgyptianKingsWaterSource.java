74705b4df7c8696bacdca4e95f4c856d980fda2f
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-09-21 20:02:17 +0600

    #11 EgyptianKings watersource updated

--- solutions/44-rafsan/EgyptianKingsWaterSource.java
+++ solutions/44-rafsan/EgyptianKingsWaterSource.java
@@ -1,23 +1,61 @@
 package sample;
 
-public class EgyptianKingsWaterSource implements IWaterSource{
+import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+
+
+public class EgyptianKingsWaterSource implements IWaterSource {
+    private Canvas canvas;
+    private GraphicsContext graphicsContext;
+    private CurrentState currentState;
+    private boolean shouldBeDrawn;
+    private CanvasSingleton canvasSingleton;
+
+    private Point center;
+
+    public EgyptianKingsWaterSource() {
+        canvasSingleton = CanvasSingleton.getInstance();
+        this.canvas = canvasSingleton.getCanvas();
+        currentState = CurrentState.getInstance();
+    }
+
+    private void calculateCornerPoints(Point selectedPoint) {
+        center = new Point(selectedPoint.x, selectedPoint.y);
+    }
+
     @Override
     public void draw(Point point) {
-
+        calculateCornerPoints(point);
+        new Circle(center, 12).draw();
     }
 
     @Override
-    public String canvasController(int point1, int pont2) {
-        return point1+" and "+pont2;
+    public void canvasControlle() {
+        graphicsContext = canvas.getGraphicsContext2D();
+
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED,
+                new EventHandler<MouseEvent>() {
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (shouldBeDrawn) {
+                            Point selectedPoint = new Point((int) event.getX(), (int) event.getY());
+                            draw(selectedPoint);
+                            currentState.addWaterResource(selectedPoint);
+                        }
+                    }
+                });
     }
 
     @Override
-    public String releaseCanvas(int point1, int pont2) {
-        return point1+" and "+pont2;
+    public void releaseCanvas() {
+        shouldBeDrawn = false;
     }
 
     @Override
-    public String getCanvas(int point1, int pont2) {
-        return point1+" and "+pont2;
+    public void getCanvas() {
+        shouldBeDrawn = true;
     }
 }
