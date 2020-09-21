fade237e9084f1295c65abdee73a76e5f419fe10
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-21 16:24:23 +0600

    #9 bd tree and house implemented

--- solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Bdhouse.cs
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Bdhouse.cs
@@ -2,8 +2,11 @@
 
 public class Bdhouse : House
 {
-	public void CreateHouse()
+	public void CreateHouse(int x, int y)
     {
-
+        new Triangle(x - 12, y, x, y + 8, x + 12, y);
+        t.Draw();
+        new Rectangle(x - 12, y, x + 12, y, x + 12, y - 12, x - 12, y - 12);
+        r.Draw();
     }
 }
--- solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Bdtree.cs
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Bdtree.cs
@@ -2,8 +2,11 @@
 
 public class Bdtree : Tree
 {
-	public void CreateTree()
+	public void CreateTree(int x, int y)
     {
-
+        new Circle(x, y+2, 8);
+        c.Draw();
+        new Rectangle(x-1, y, x+1, y, x+1, y-12, x-1, y-12);
+        r.Draw();
     }
 }
