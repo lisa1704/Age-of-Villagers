adf4dd7625c4f78c9f08790d697771b8aef7dbaf
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-21 22:53:50 +0600

    #1 Bangladeshi farmers house class modified

--- solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersHouse.cs
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersHouse.cs
@@ -9,11 +9,14 @@ namespace AgeOfVillagers
 {
     class BangladeshiFarmersHouse : AbstractShape
     {
-        public BangladeshiFarmersHouse(Point top, Point topleft, Point bottomright)
+        public BangladeshiFarmersHouse(Point Initialpoint)
         {
-            Point topright = new Point(bottomright.X, topleft.Y);
-            addShape(new Rectangle(topleft, bottomright));
-            addShape(new Triangle(top, topleft, topright ));
+            Point Rectangle1 = new Point(Initialpoint.X - 8, Initialpoint.Y + 8);
+            Point Rectangle2 = new Point(Initialpoint.X + 8, Initialpoint.Y + 16);
+            Point Triangle = new Point(Initialpoint.X + 8, Initialpoint.Y + 8);
+
+            addShape(new Rectangle(Rectangle1, Rectangle2));
+            addShape(new Triangle(Initialpoint, Rectangle1, Triangle));
         }
     }
 }
