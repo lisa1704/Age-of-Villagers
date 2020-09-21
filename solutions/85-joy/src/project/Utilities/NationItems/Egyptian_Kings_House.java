e57a11d4b41cdc074b08f4e0da5a9a1b0c2f473b
MD ASHIQUR RAHMAN JOY <ashiqurrahman85@iut-dhaka.edu>
2020-09-22 00:13:46 +0600

    #! #11 Arab_Bedouin_House(Concrete Strategy) initiated

---
+++ solutions/85-joy/src/project/Utilities/NationItems/Egyptian_Kings_House.java
@@ -0,0 +1,12 @@
+package project.Utilities.NationItems;
+
+import javafx.scene.shape.Shape;
+
+import java.util.ArrayList;
+
+public class Egyptian_Kings_House implements IHouse{
+    @Override
+    public ArrayList<Shape> DrawHouse(double x1, double y1) {
+        return null;
+    }
+}
