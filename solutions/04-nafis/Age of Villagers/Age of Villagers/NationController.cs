64b3cac6161aca181b2d32f1f783abe9ab5a6e26
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-22 04:09:37 +0600

    #9 #10 #11 #12 Nation Controller is added

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/NationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    class NationController
+    {
+        private string NationName;
+        public NationController(string NationName)
+        {
+            this.NationName = NationName;
+        }
+
+        public void getNation()
+        {
+
+        }
+
+    }
+}
