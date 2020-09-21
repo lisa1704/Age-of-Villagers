f7e4d1f4f9ba1a26578df05f66fadc6d63c28938
afiarodoshe <afiamuntakim@iut-dhaka.edu>
2020-09-22 00:05:25 +0600

    #4 #12 #11 implementing classes

--- solutions/29-rodoshe/Age_of_villagers/FactoryClasses/NationFactory.cs
+++ solutions/29-rodoshe/Age_of_villagers/FactoryClasses/NationFactory.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Age_of_Villagers.Nation;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using static Age_of_Villagers.Nation.INation;
 
 namespace Age_of_Villagers.FactoryClasses
 {
@@ -8,9 +10,30 @@ namespace Age_of_Villagers.FactoryClasses
     class NationFactory
     {
         string name;
-        public NationFactory(string NationName)
+        public Nation.INation getNationName(string NationType)
         {
-            name = NationName;
+            if (NationType == "Bangladeshi Farmers")
+            {
+                return new BangladeshiFarmers();
+            }
+            else if (NationType == "Arab Beduin")
+            {
+                return new ArabBeduin();
+            }
+            else if (NationType == "Egytian King")
+            {
+                return new EgyptianKing();
+            }
+            else if (NationType == "Inuit Hunters")
+            {
+                return new InuitHunters();
+            }
+            else
+            {
+                return new NullNation();
+            }
+            throw new Exception();
         }
+
     }
 }
---
+++ solutions/29-rodoshe/Age_of_villagers/FactoryClasses/NullNation.cs
@@ -0,0 +1,16 @@
+﻿using Age_of_Villagers.Nation;
+using System.Drawing;
+
+namespace Age_of_Villagers.FactoryClasses
+{
+    internal class NullNation : INation
+    {
+        public Color BackGroundColor()
+        {
+            return Color.White;
+        }
+
+
+
+    }
+}
