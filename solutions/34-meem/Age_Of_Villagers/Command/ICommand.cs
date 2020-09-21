681a6461822e329edabeb0741394f8d04b9bb1c7
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-22 01:22:51 +0600

    #5 command pattern applied to save a village

---
+++ solutions/34-meem/Age_Of_Villagers/Command/ICommand.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_Of_Villagers
+{
+    public interface ICommand
+    {
+        void execute();
+    }
+
+    public struct Village
+    {
+        public string villageName;
+        public List<Point> housePoints;
+        public List<Point> treePoints;
+        public List<Point> waterSourcePoints;
+
+
+    }
+
+
+}
+    
