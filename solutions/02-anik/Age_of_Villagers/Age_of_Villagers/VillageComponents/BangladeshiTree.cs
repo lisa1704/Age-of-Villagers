79b4d9f68b7fa8f4a569d28b64f707f6aa7ddb19
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-22 02:29:06 +0600

    #9  Bangladeshi tree created

---
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/VillageComponents/BangladeshiTree.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using Age_of_Villagers.VillageComponents;
+using Age_of_Villagers.Shape;
+
+namespace Age_of_villagers.Components
+{
+    class BangladeshiTree : VillageComponents
+    {
+        private readonly Point topLeft, bottomRight, centre;
+        public BangladeshiTree(Point point)
+        {
+            centre = point;
+            topLeft.X = point.X - 20;
+            topLeft.Y = point.Y - 20;
+            bottomRight.X = point.X + 20;
+            bottomRight.Y = point.Y + 20;
+            addComponent(new Arc(topLeft, bottomRight));
+            addComponent(new Age_of_Villagers.Shape.Rectangle(new Point(centre.X - 2, centre.Y), new Point(centre.X + 2, centre.Y + 40)));
+        }
+    }
+}
