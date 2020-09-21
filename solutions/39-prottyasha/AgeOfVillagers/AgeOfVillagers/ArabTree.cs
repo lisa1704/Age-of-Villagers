afabd06be23e34d7a35f6967cf9809d8e88fd20e
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-22 00:33:15 +0600

    #1 abstract factory pattern implement started.

---
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/ArabTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class ArabTree : IComponent
+    {
+        public void draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
