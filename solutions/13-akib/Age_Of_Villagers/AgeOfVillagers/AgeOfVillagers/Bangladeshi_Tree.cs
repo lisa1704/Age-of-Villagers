f6a110991d5b260416928727a3ac50dd4f07c8ea
akib <akibahmed@iut-dhaka.edu>
2020-09-21 07:24:39 +0600

    #9 Bangladeshi_Tree Class is implemented with creating tree feature

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Bangladeshi_Tree.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Bangladeshi_Tree.cs
@@ -4,8 +4,12 @@ namespace AgeOfVillagers
 
 public class Bangladeshi_Tree : Tree
 {
-	public void Create_Tree()
+	public void Create_Tree(int x, int y)
     {
+        Circle bdt_circle = new Circle(x, y+1, 8);
+        Rectangle bdt_rectangle = new Rectangle(x+1, y, x+1, y-12, x-1, y-12, x-1, y);
 
+        bdt_circle.Draw_Shape();
+        bdt_rectangle.Draw_Shape();
     }
 }
