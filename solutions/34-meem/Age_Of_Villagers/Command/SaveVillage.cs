7ee522129a51ce84de055a3d5e10c47a30f1834f
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2020-09-22 01:25:02 +0600

    #5 class created to execute functionality of saving village

---
+++ solutions/34-meem/Age_Of_Villagers/Command/SaveVillage.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_Of_Villagers.Command
+{
+    public class SaveVillage : ICommand
+    {
+        Village _village;
+        public SaveVillage(Village village)
+        {
+            _village = village;
+
+        }
+
+        public void execute()
+        {
+            
+
+
+        }
+    }
+}
