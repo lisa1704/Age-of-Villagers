5e8bc8bcfecd9af7f4100ed10c66a70334456418
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-21 18:54:04 +0600

    #9 bd tree class implemented

--- solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Bdtree.cs
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Bdtree.cs
@@ -4,9 +4,10 @@ public class Bdtree : Tree
 {
 	public void CreateTree(int x, int y)
     {
-        new Circle(x, y+2, 8);
-        c.Draw();
-        new Rectangle(x-1, y, x+1, y, x+1, y-12, x-1, y-12);
-        r.Draw();
+        Circle bdtCircle = new Circle(x, y + 1, 8);
+        bdtCirlce.Draw_Shape();
+
+        Rectangle bdtRectangle = new Rectangle(x + 1, y, x + 1, y - 12, x - 1, y - 12, x - 1, y);
+        bdtRectangle.Draw_Shape();
     }
 }
