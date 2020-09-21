9df1466992650da19edbd075131eee6474edc27f
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-21 22:48:31 +0600

    #1 #8 #10 Concrete Color method implemented

---
+++ solutions/83-abedeen/src/Project/Utilities/NationItems/Arab_Bedouin_TerrainColor.java
@@ -0,0 +1,9 @@
+package Project.Utilities.NationItems;
+
+public class Arab_Bedouin_TerrainColor implements INationColor{
+    private String TerrainColor = "#fffde3";
+    @Override
+    public String GetNationColor() {
+        return TerrainColor;
+    }
+}
