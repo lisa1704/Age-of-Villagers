b88cef3eea3a355159c84d660723b7fdc1453d48
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-22 02:50:06 +0600

    #9 Egyptian House created and implemented

---
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/VillageComponents/EgyptianHouse.cs
@@ -0,0 +1,24 @@
+﻿using Age_of_Villagers.Shape;
+using Age_of_Villagers.VillageComponents;
+using System.Drawing;
+
+namespace AgeOfVillagers.VillegeItems
+{
+    class EgyptianHouse : VillageComponents
+    {
+        private readonly Point top, bottomLeft, bottomMid, bottomRight;
+
+        public EgyptianHouse(Point point)
+        {
+            top = point;
+            bottomLeft = new Point(top.X - 40, top.Y + 35);
+            bottomMid = new Point(top.X + 5, top.Y + 60);
+            bottomRight = new Point(top.X + 25, top.Y + 30);
+            addComponent(new Line(top, bottomLeft));
+            addComponent(new Line(top, bottomMid));
+            addComponent(new Line(top, bottomRight));
+            addComponent(new Line(bottomLeft, bottomMid));
+            addComponent(new Line(bottomMid, bottomRight));
+        }
+    }
+}
