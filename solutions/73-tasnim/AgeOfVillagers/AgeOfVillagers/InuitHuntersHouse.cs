a31c8c9593587cf5c68406fc0bd135c941aa2a0b
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-22 00:32:09 +0600

    #1 Funal inuit  house completed

--- solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/InuitHuntersHouse.cs
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/InuitHuntersHouse.cs
@@ -11,14 +11,9 @@ namespace AgeOfVillagers
     {
         public InuitHuntersHouse(Point Iniatialpoint)
         {
-            Point linePoint1 = new Point(Iniatialpoint.X, Iniatialpoint.Y - 20);
-            Point linePoint2 = new Point(Iniatialpoint.X + 50, Iniatialpoint.Y - 20);
-            Point sidePoint1 = new Point(linePoint1.X - 25, linePoint1.Y + 100);
-            Point sidePoint2 = new Point(sidePoint1.X + 150, sidePoint1.Y);
-
-            addShape(new Line(sidePoint1, sidePoint2));
-            addShape(new Arc(linePoint1.X, linePoint1.Y, 100F, 200F, 0, sweepAngle: -180.0F));
-            addShape(new Arc(linePoint1.X - 25, linePoint1.Y - 50, 150F, 300F, 0, sweepAngle: -180.0F));
+            addShape(new Line((new Point(Iniatialpoint.X + 16, Iniatialpoint.Y + 8)), (new Point(Iniatialpoint.X, Iniatialpoint.Y + 8))));
+            addShape(new Arc(Iniatialpoint.X, Iniatialpoint.Y, 16F, 16F, 0, -180.0F));
+            addShape(new Arc(Iniatialpoint.X + 4, Iniatialpoint.Y + 4, 8F, 8F, 0, -180.0F));
         }
     }
 }
