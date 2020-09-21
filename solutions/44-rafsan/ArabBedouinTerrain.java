30cee60dc792f277691dd3fe9aaf08c128504595
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-09-21 19:18:03 +0600

    #10 ArabBedouin drwing updated and background updated

--- solutions/44-rafsan/ArabBedouinTerrain.java
+++ solutions/44-rafsan/ArabBedouinTerrain.java
@@ -1,8 +1,14 @@
 package sample;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class ArabBedouinTerrain implements ITerrain {
     @Override
-    public String draw(int point1, int point2) {
-        return point1+"and"+point2;
+    public void draw() {
+        CanvasSingleton canvasSingleton = CanvasSingleton.getInstance();
+        GraphicsContext graphicsContext = canvasSingleton.getCanvas().getGraphicsContext2D();
+        graphicsContext.setFill(Color.web("#e6dcae"));
+        graphicsContext.fillRect(0,0,600,400);
     }
 }
