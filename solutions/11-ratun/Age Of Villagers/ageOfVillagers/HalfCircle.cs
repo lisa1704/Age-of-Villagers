5291eb30b0a5dc9541052782ca08a7c20a66df34
Ratun <www.ratunrahman@gmail.com>
2020-09-21 13:07:01 +0600

    #9 #10 #11 #12 implement Half Circle

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/HalfCircle.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/HalfCircle.cs
@@ -2,5 +2,15 @@
 
 public class HalfCircle : Shape
 {
-	
+    private int x, y, radius;
+    public HalfCircle(int x, int y, int radius)
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    public string Draw()
+    {
+        return "Drawing a half circle";
+    }
 }
