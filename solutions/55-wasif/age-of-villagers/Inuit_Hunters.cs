62562370cf8694b9a56dec0ad50683075b92ced2
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-22 02:37:45 +0600

    #12 Mouse pointer added for Inuit Hunters nation class

--- solutions/55-wasif/age-of-villagers/Inuit_Hunters.cs
+++ solutions/55-wasif/age-of-villagers/Inuit_Hunters.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace age_of_villagers
 {
@@ -12,31 +12,31 @@ namespace age_of_villagers
     {
         Pen pen = new Pen(Color.Red);
         Graphics gp;
-        public Inuit_Hunters(Graphics gp)
+        Point p0;
+        public Inuit_Hunters(Graphics gp, Point p0)
         {
             this.gp = gp;
+            this.p0 = p0;
         }
         public void draw_house()
         {
-            
-            Point p0 = new Point(50, 50);
             Point p1 = new Point(p0.X - 75, p0.Y);
             Point p2 = new Point(p0.X + 75, p0.Y);
             Point p3 = new Point(p0.X - 50, p0.Y - 50);
             gp.DrawArc(pen, p3.X, p3.Y, 150, 150, 180, 180);
             gp.DrawArc(pen, p2.X, p2.Y, 150, 150, 180, 180);
             gp.DrawLine(pen, p1, p2);
-            
         }
 
         public void draw_tree()
         {
-                MessageBox.Show("Inuit Hunters can not plant trees");
+            MessageBox.Show("Inuit Hunters can not plant trees");
         }
 
         public void draw_watersource()
         {
-            MessageBox.Show("Inuit Hunters doesn't have water source");
+            MessageBox.Show("Inuit Hunters can not plant water source");
         }
     }
 }
+
