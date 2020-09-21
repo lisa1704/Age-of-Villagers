be3696dea0349f95d13ab00622df8d55d4da9e19
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-21 19:22:57 +0600

    #9 Bangladeshi water resorce added

--- solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/BangladeshiTree.cs
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/BangladeshiTree.cs
@@ -7,14 +7,14 @@ namespace AgeofVillagers
 {
     public class BangladeshiTree : CompositeShape
     {
-        public BangladeshiTree(Point MPt)
+        public BangladeshiTree(Point mid)
         {
-            Point bottomRight = new Point(MPt.X + 8, MPt.Y + 24);
-            Point topPt = new Point(MPt.X + 16, MPt.Y + 18);
-            Point RTopLeft = new Point(MPt.X + 6, MPt.Y + 12);
+            Point bottomRight = new Point(mid.X + 8, mid.Y + 24);
+            Point topPt = new Point(mid.X + 16, mid.Y + 18);
+            Point RTopLeft = new Point(mid.X + 6, mid.Y + 12);
 
             AddComponents(new RectangleShape(RTopLeft, bottomRight));
-            AddComponents(new EllipseShape(MPt, topPt));
+            AddComponents(new EllipseShape(mid, topPt));
 
         }
     }
