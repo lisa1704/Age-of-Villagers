f516fdace27bf4f2f3cafc5e8a2628a89af6f8c7
tiashaa <hriditatiasha@gmail.com>
2020-09-21 19:42:31 +0600

    #1 #11 refactored and egyptian water source

--- solutions/41-tiasha/Age of Villagers/Water Source/EgyptianWaterSource.cs
+++ solutions/41-tiasha/Age of Villagers/Water Source/EgyptianWaterSource.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using AgeOfVillagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class EgyptianWaterSource
+    public class EgyptianWaterSource : CompositeShape
     {
+        public EgyptianWaterSource(Point p, ElementSize sz)
+        {
+            AddComponent(new Arc(p.X, p.Y, sz.width, sz.height, 0, 360));
+        }
     }
 }
