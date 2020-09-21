5198d6f1abc02aab17fec07c85cb9ce571751c3d
akib <akibahmed@iut-dhaka.edu>
2020-09-21 17:17:18 +0600

    #11 Egyptian_House Class implemented by creating house

---
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Egyptian_House.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AgeOfVillagers
+
+public class Egyptian_House
+{
+	public void Create_House(int x, int y)
+    {
+        Triangle eh_triangle1 = new Triangle(x, y, x - 8, y + 2, x - 2, y + 16);
+        Triangle eh_triangle2 = new Triangle(x, y, x + 8, y + 5, x - 2, y + 16);
+
+        eh_triangle1.Draw_Shape();
+        eh_triangle2.Draw_Shape();
+    }
+}
