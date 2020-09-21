7d69210446c67a2fba67072d0222962adeefc2ee
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-21 11:42:44 +0600

    #1 #7 #8 #11 updated Egyptian Tree.

--- solutions/64-ruhee/Age_of_Villagers/EgyptianKings/EgyptianTrees.cs
+++ solutions/64-ruhee/Age_of_Villagers/EgyptianKings/EgyptianTrees.cs
@@ -8,5 +8,23 @@ namespace Age_of_Villagers.EgyptianKings
 {
     class EgyptianTrees:CShape
     {
+        public EgyptianTrees(Point pt)
+        {
+            Point pt1 = new Point(pt.X + 50, pt.Y + 10);
+            Point pt2 = new Point(pt.X - 50, pt.Y + 10);
+            Point pt3 = new Point(pt.X + 25, pt.Y +  5);
+            Point pt4 = new Point(pt.X - 25, pt.Y +  5);
+            Point pt5 = new Point(pt.X + 55, pt.Y + 10);
+            Point pt6 = new Point(pt.X + 45, pt.Y + 10);
+            Point pt7 = new Point(pt.X - 55, pt.Y + 10);
+            Point pt8 = new Point(pt.X - 45, pt.Y + 10);
+
+            AddComponent(new Line(pt, pt1));
+            AddComponent(new Line(pt, pt2));
+            AddComponent(new Line(pt3, pt5));
+            AddComponent(new Line(pt3, pt6));
+            AddComponent(new Line(pt4, pt7));
+            AddComponent(new Line(pt4, pt8));
+        }
     }
 }
