271964b3d0baabb2b42247eea96e3c0eca8842d7
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-21 07:44:10 +0600

    #7 triangleShape Added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/TriangleShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class TriangleShape : CompositeShape
+    {
+        public TriangleShape(Point pt1, Point pt2, Point pt3)
+        {
+            AddComponents(new LineShape(pt1, pt2));
+            AddComponents(new LineShape(pt2, pt3));
+            AddComponents(new LineShape(pt1, pt3));
+        }
+    }
+}
