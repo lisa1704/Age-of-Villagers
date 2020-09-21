96341cd756f022fef1c3fd997bae632214be70fa
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-09-22 02:57:32 +0600

    sizeof item testing

---
+++ solutions/43-sristy/AgeOfVillagers.Test/sizetesting.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using Age_of_villagers;
+using System.Drawing;
+using Size = Age_of_villagers.Size;
+
+namespace AgeOfVillagers.Test
+{
+    public class sizetesting
+    {
+        Size housesize = new Housesize();
+        Size treesize = new Treesize();
+        Size bdwatersourcesize = new BDWatersourcesize();
+        Size egptwatersourcesize = new EGPTWatersourcesize();
+
+        [Fact]
+        public void housesizeTest()
+        {
+            Assert.Equal(16, housesize.GetHeight());
+            Assert.Equal(16, housesize.GetWeidth());
+        }
+        [Fact]
+        public void treesizeTest()
+        {
+            Assert.Equal(24, treesize.GetHeight());
+            Assert.Equal(16, treesize.GetWeidth());
+        }
+        [Fact]
+        public void bdwatersourcesizeTest()
+        {
+            Assert.Equal(16, bdwatersourcesize.GetHeight());
+            Assert.Equal(24, bdwatersourcesize.GetWeidth());
+        }
+        [Fact]
+        public void egptwatersourcesizeTest()
+        {
+            Assert.Equal(12, egptwatersourcesize.GetHeight());
+            Assert.Equal(12, egptwatersourcesize.GetWeidth());
+        }
+
+    }
+}
