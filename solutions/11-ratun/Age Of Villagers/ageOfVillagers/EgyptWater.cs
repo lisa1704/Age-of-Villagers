1e9d51ea54f10077aa211a1e1f9aecd848a120f9
Ratun <www.ratunrahman@gmail.com>
2020-09-21 12:58:44 +0600

    #11 implement Egypt Water

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptWater.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptWater.cs
@@ -2,5 +2,11 @@
 
 public class EgyptWater : Water
 {
-	
+	private int Xaxis = 16;
+	private int Yaxis = 16;
+	public void CreateWater(int x, int y)
+    {
+		Circle c = new Circle(x, y, 8);
+		c.Draw();
+    }
 }
