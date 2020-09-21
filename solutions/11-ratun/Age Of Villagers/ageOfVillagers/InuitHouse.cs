afaca029faf93cb99fb32c49311c07fc902e95cd
Ratun <www.ratunrahman@gmail.com>
2020-09-21 13:11:29 +0600

    #12 implement Inuit House class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHouse.cs
@@ -4,6 +4,9 @@ public class InuitHouse : House
 {
 	public void CreateHouse(int x, int y)
     {
-
+        HalfCircle h1 = new HalfCircle(x,y,8);
+        h1.Draw();
+        HalfCircle h2 = new HalfCircle(x, y, 2);
+        h2.Draw();
     }
 }
