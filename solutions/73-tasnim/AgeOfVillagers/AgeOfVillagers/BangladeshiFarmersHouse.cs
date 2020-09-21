30db72a86ae833beb7387978d2657fbfbffc82b4
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-22 00:40:33 +0600

    #1 #7 Bangladeshi farmer house size fixed

--- solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersHouse.cs
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersHouse.cs
@@ -11,12 +11,13 @@ namespace AgeOfVillagers
     {
         public BangladeshiFarmersHouse(Point Initialpoint)
         {
-            Point Rectangle1 = new Point(Initialpoint.X - 40, Initialpoint.Y + 40);
-            Point Rectangle2 = new Point(Initialpoint.X + 40, Initialpoint.Y + 80);
-            Point Triangle = new Point(Initialpoint.X + 40, Initialpoint.Y + 40);
+            Point Triangle = new Point(Initialpoint.X + 8, Initialpoint.Y + 8);
+            Point RectPoint1 = new Point(Initialpoint.X - 8, Initialpoint.Y + 8);
+            Point RectPoint2 = new Point(Initialpoint.X + 8, Initialpoint.Y + 16);
 
-            addShape(new Rectangle(Rectangle1, Rectangle2));
-            addShape(new Triangle(Initialpoint, Rectangle1, Triangle));
+            addShape(new Triangle(Initialpoint, RectPoint1, Triangle));
+            addShape(new Rectangle(RectPoint1, RectPoint2));
+ 
         }
     }
 }
