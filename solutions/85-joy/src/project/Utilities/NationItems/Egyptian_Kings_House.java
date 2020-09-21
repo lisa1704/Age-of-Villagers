656b9f88cfd35a1fb61d172e616342254e903c38
MD ASHIQUR RAHMAN JOY <ashiqurrahman85@iut-dhaka.edu>
2020-09-22 00:16:12 +0600

    #1 #11 Arab_bedouin_House implemented

--- solutions/85-joy/src/project/Utilities/NationItems/Egyptian_Kings_House.java
+++ solutions/85-joy/src/project/Utilities/NationItems/Egyptian_Kings_House.java
@@ -1,5 +1,6 @@
 package project.Utilities.NationItems;
 
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
 import java.util.ArrayList;
@@ -7,6 +8,27 @@ import java.util.ArrayList;
 public class Egyptian_Kings_House implements IHouse{
     @Override
     public ArrayList<Shape> DrawHouse(double x1, double y1) {
-        return null;
+        ArrayList<Shape> House = new ArrayList<Shape>();
+
+        Line line1 = new Line(x1,y1,x1+12,y1+3);
+        line1.setStrokeWidth(2);
+        House.add(line1);
+
+        Line line2 = new Line(x1+12,y1+3,x1+HouseWidth,y1-5);
+        line2.setStrokeWidth(2);
+        House.add(line2);
+
+        Line line3 = new Line(x1+HouseWidth,y1-5,x1+8,y1-13);
+        line3.setStrokeWidth(2);
+        House.add(line3);
+
+        Line line4 = new Line(x1+8,y1-13,x1,y1);
+        line4.setStrokeWidth(2);
+        House.add(line4);
+
+        Line line5 = new Line(x1+8,y1-13,x1+12,y1+3);
+        House.add(line5);
+
+        return House;
     }
 }
