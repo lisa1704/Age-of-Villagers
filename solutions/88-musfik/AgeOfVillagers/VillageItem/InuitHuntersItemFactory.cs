bdc4f771a82f3a876981e262de96a45844c3dc47
musfiksifat <musfiksifat766@gmail.com>
2020-09-22 05:39:40 +0600

    #12 InuitHunterItemFactory class created & implemented

---
+++ solutions/88-musfik/AgeOfVillagers/VillageItem/InuitHuntersItemFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.VillageItem
+{
+    public class InuitHuntersItemFactory
+    {
+        private String type;
+        public InuitHuntersItemFactory(String nationItem)
+        {
+            type = nationItem;
+        }
+
+        internal IVillageItem ItemProducer(Point point)
+        {
+            if (type == "House")
+                return new InuitHuntersHouse(point);
+
+            else if (type == "Tree")
+                return new NoTree(point);
+
+            else if (type == "Water")
+                return new NoWaterResource(point);
+
+            return new NoTree(point);
+        }
+    }
+}
