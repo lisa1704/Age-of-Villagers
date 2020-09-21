526470037e38ce5588f38658369dc5c82eba14ad
akib <akibahmed@iut-dhaka.edu>
2020-09-21 22:32:58 +0600

    #7 #12 New Shape Half_Circle created and InuitHnters_House is implemented

---
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Half_Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Half_Circle
+{
+    private int x, y, radius;
+    public Half_Circle(int x, int y, int radius)
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    public string Draw_Shape()
+    {
+        return "Drawing a Half Circle...";
+    }
+}
---
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/InuitHunters_House.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class InuitHunters_House : I_Shape
+{
+	private string draw = "";
+
+	public string Draw(int x, int y)
+    {
+        Half_Circle ihh_circle = new Half_Circle(x, y, 8);
+
+        draw = draw + ihh_circle.Draw_Shape();
+
+        return draw;
+    }
+}
