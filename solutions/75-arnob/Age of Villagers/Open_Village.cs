33e3cdbcbb61999b0835c5184eeea4a85864fa9a
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-09-22 00:47:56 +0600

    #1 #6 open village class updated

--- solutions/75-arnob/Age of Villagers/Open_Village.cs
+++ solutions/75-arnob/Age of Villagers/Open_Village.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Age_of_Villagers
 {
     class Open_Village
     {
+        Village village;
+
+        public void action()
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = open.OpenFile();
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    string json = streamReader.ReadToEnd();
+                    village = JsonConvert.DeserializeObject<Village>(json);
+                }
+
+            }
+
+        }
+        public Village openVillage()
+        {
+            return this.village;
+        }
     }
 }
