acfd78d9669327cd8baf365926ece6d6dc952ad9
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-21 20:37:12 +0600

    #1 #5 #6 Added a method to the interface and a struct to use in json serialization later.

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/IOpenSave.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/IOpenSave.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
     interface IOpenSave
     {
+        void execute();
+    }
 
+    public struct VillageState
+    {
+        public string Vname;
+        public List<Point> HPoint;
+        public List<Point> TPoint;
+        public List<Point> WPoint;
     }
 }
