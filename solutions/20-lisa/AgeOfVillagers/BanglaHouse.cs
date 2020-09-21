edd885fcec6c354e98702663371f5c3b28c354de
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-21 20:59:45 +0600

    #7 #9 BanglaHouse class implemented

--- solutions/20-lisa/AgeOfVillagers/BanglaHouse.cs
+++ solutions/20-lisa/AgeOfVillagers/BanglaHouse.cs
@@ -9,7 +9,12 @@ namespace AgeOfVillagers
     {
         public BanglaHouse(Point p)
         {
-            
+            Point top = new Point(p.X, p.Y);
+            Point topLeft = new Point(p.X - 40, p.Y + 20);
+            Point bottomRight = new Point(p.X + 40, p.Y + 70);
+            Point topRight = new Point(bottomRight.X, topLeft.Y);
+            AddComponent(new Rectangle(topLeft, bottomRight));
+            AddComponent(new Triangle(top, topLeft, topRight));
         }
     }
 }
