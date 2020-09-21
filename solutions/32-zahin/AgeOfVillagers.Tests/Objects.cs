f5342ac5ad132ec8e52add595cfb6f61f7b7a807
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-22 00:58:38 +0600

    #1 removed unnecessary stuff

--- solutions/32-zahin/AgeOfVillagers.Tests/Objects.cs
+++ solutions/32-zahin/AgeOfVillagers.Tests/Objects.cs
@@ -1,5 +1,4 @@
 ﻿using Xunit;
-using System.Drawing;
 
 namespace AgeOfVillagers.Tests
 {
--- solutions/32-zahin/AgeOfVillagers/Items/ArabTree.cs
+++ solutions/32-zahin/AgeOfVillagers/Items/ArabTree.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AgeOfVillagers.Items;
+﻿using System.Drawing;
 using AgeOfVillagers.Shapes;
-using System.Drawing;
 
 namespace AgeOfVillagers.Items
 {
