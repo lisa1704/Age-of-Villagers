3db574aefc906d9fced96566782c7992328ccd57
afiarodoshe <afiamuntakim@iut-dhaka.edu>
2020-09-21 23:54:00 +0600

    #12 Inuit hunter class implemented

--- solutions/29-rodoshe/Age_of_villagers/Nation/InuitHunters.cs
+++ solutions/29-rodoshe/Age_of_villagers/Nation/InuitHunters.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers.Nation
+{
+    class InuitHunters : INation
+    {
+        private Color Colorbackground = Color.White;
+        public Color BgColor()
+        {
+            return Colorbackground;
+        }
+        public void DrawHouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+
+}
