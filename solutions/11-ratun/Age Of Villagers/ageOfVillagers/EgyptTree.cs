21744b17222782d4ba17d855ef67e072dbb4e3fb
Ratun <www.ratunrahman@gmail.com>
2020-09-21 21:13:59 +0600

    #1 fixed egypt tree class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptTree.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptTree.cs
@@ -1,20 +1,16 @@
-﻿using System;
-
-public class EgyptTree : Tree
+﻿namespace ageOfVillagers
 {
-	public void CreateTree(int x, int y)
+    internal class EgyptTree : IShape
     {
-        Line l1 = new Line(x, y - 12, x - 6, y + 10);
-        l1.Draw();
-        Line l2 = new Line(x, y - 12, x + 6, y + 10);
-        l2.Draw();
-        Line l3 = new Line(x - 5, y + 8, x - 8, y + 6);
-        l3.Draw();
-        Line l4 = new Line(x - 5, y + 8, x - 2, y + 12);
-        l4.Draw();
-        Line l5 = new Line(x + 5, y + 8, x + 8, y + 6);
-        l5.Draw();
-        Line l6 = new Line(x + 5, y + 8, x + 2, y + 12);
-        l6.Draw();
+        public string draw(int x, int y)
+        {
+            Line l1 = new Line(x, y - 12, x - 6, y + 10);
+            Line l2 = new Line(x, y - 12, x + 6, y + 10);
+            Line l3 = new Line(x - 5, y + 8, x - 8, y + 6);
+            Line l4 = new Line(x - 5, y + 8, x - 2, y + 12);
+            Line l5 = new Line(x + 5, y + 8, x + 8, y + 6);
+            Line l6 = new Line(x + 5, y + 8, x + 2, y + 12);
+            return l1.Draw() + l2.Draw() + l3.Draw() + l4.Draw() + l5.Draw() + l6.Draw();
+        }
     }
 }
