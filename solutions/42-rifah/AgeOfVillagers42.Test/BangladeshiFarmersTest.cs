7f01fb551c0b9d9cb63f20c88f9028602b9e70b2
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-22 04:27:09 +0600

    #15 #9 Bangladeshi Farmers background color tested

--- solutions/42-rifah/AgeOfVillagers42.Test/BangladeshiFarmersTest.cs
+++ solutions/42-rifah/AgeOfVillagers42.Test/BangladeshiFarmersTest.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using AgeOfVillagers;
 using AgeOfVillagers.Bangladeshi_Farmers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
 
 namespace AgeOfVillagers42.Test
 {
-    class BangladeshiFarmersTest
+    [TestClass]
+    public class BangladeshiFarmersTest
     {
-        Factory_Nation nationfactory = new Factory_Nation();
+        [TestMethod]
+        public void BackgroundColorTest()
+        {
+            BangladeshiFarmers bdFarmer = new BangladeshiFarmers();
+            Color color = bdFarmer.BackgroundColor();
+            Color expected = Color.Green;
+
+            Assert.AreEqual(color, expected);
+        }
     }
 }
