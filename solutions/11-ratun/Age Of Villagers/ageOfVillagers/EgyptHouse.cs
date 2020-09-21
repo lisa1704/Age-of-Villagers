2bb87e5e310ba7b9be9dfcea315cac49e3bc8ef3
Ratun <www.ratunrahman@gmail.com>
2020-09-21 12:48:04 +0600

    #11 implement Egypt House

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptHouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptHouse.cs
@@ -2,5 +2,11 @@
 
 public class EgyptHouse : House
 {
-	
+	public void CreateHouse(int x, int y)
+    {
+        Triangle t1 = new Triangle(x,y+8,x-8,y-4,x+2,y-8);
+        t1.Draw();
+        Triangle t2 = new Triangle(x,y+8,x+8,y,x+2,y-8);
+        t2.Draw();
+    }
 }
