73b6a5b84055469c0044d6f360d581077ac8b72b
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-22 02:58:10 +0600

    #9 #10 #11 #7 # NullNation implemented

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/FactoryClasses/NullNation.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/FactoryClasses/NullNation.cs
@@ -10,6 +10,44 @@ namespace Age_of_Villagers.FactoryClasses
             return Color.White;
         }
 
-      
+        public Color BgColor()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DrawHouse()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public VillageComponents.VillageComponents getHouse(Point p)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string getNationName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public VillageComponents.VillageComponents getTree(Point p)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public VillageComponents.VillageComponents getWaterSource(Point p)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
