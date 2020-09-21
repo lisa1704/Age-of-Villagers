f80b9f9d826582da86ebf8cb866f087e8995b906
akib <akibahmed@iut-dhaka.edu>
2020-09-21 08:00:34 +0600

    #9 Bangladeshi_WaterRes class implemented by creating water resources

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Bangladeshi_WaterRes.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Bangladeshi_WaterRes.cs
@@ -4,8 +4,24 @@ namespace AgeOfVillagers
 
 public class Bangladeshi_WaterRes : Water_Resource
 {
+	private int x_size = 24;
+	private int y_size = 16;
+
 	public void Create_WaterResource()
 	{
+		Lines bdw_line1 = new Lines(x, y+8, x, y+1);
+		Lines bdw_line2 = new Lines(x, y+1, x+7, y+7);
+		Lines bdw_line3 = new Lines(x+7, y+7, x+12, y+2);
+		Lines bdw_line4 = new Lines(x+12, y+2, x+9, y-7);
+		Lines bdw_line5 = new Lines(x+9, y-7, x+1, y-1);
+		Lines bdw_line6 = new Lines(x+1, y-1, x-3, y-8);
+		Lines bdw_line7 = new Lines(x-3, y-8, x-12, y-2);
+		Lines bdw_line8 = new Lines(x-12, y-2, x-5, y+6);
+		Lines bdw_line9 = new Lines(x-5, y+6, x, y+8);
 
+		for(int i=1; i<10; i++)
+        {
+			bdw_linei.Draw_Shape();
+        }
 	}
 }
--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Water_Resource.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Water_Resource.cs
@@ -4,5 +4,5 @@ namespace AgeOfVillagers
 
 internal abstract class Water_Resource
 {
-	public abstract void Create_WaterResource();
+	public abstract void Create_WaterResource(int x, int y);
 }
