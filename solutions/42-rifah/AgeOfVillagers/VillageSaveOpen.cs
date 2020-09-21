3f9ab8f66785d7e33895f9167fe4db46f35772b5
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-22 02:52:45 +0600

    #5 implemented saveMyVillage method

--- solutions/42-rifah/AgeOfVillagers/VillageSaveOpen.cs
+++ solutions/42-rifah/AgeOfVillagers/VillageSaveOpen.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
+using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
     public class VillageSaveOpen
     {
+        private Items myItems;
         public void saveMyVillage()
         {
+            SaveFileDialog savefiledialog = new SaveFileDialog();
+            savefiledialog.InitialDirectory = @"G:\3-1\Design Pattern (Assignments)";
+            savefiledialog.RestoreDirectory = true;
+            savefiledialog.FileName = "*.aov";
+            savefiledialog.DefaultExt = "aov";
+            savefiledialog.Filter = "AoV file(*.aov) | *.aov";
 
+            if (savefiledialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = savefiledialog.OpenFile();
+                using (StreamWriter streamWriter = new StreamWriter(stream))
+                {
+                    JsonSerializer jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(streamWriter, myItems);
+                    streamWriter.Close();
+                }
+                stream.Close();
+            }
         }
 
         public void openMyVillage()
@@ -20,7 +41,7 @@ namespace AgeOfVillagers
 
     public struct Items
     {
-        public string NameofVillage;
+        public string VillageName;
         public List<Point> House;
         public List<Point> Tree;
         public List<Point> WaterSource;
