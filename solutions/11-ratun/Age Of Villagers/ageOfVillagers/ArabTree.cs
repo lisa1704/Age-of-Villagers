a4128f833a8a378e5c4fab55a6342b2bf5bdf3a3
Ratun <www.ratunrahman@gmail.com>
2020-09-21 20:54:07 +0600

    #1 fix Arab tree class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabTree.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabTree.cs
@@ -1,20 +1,16 @@
-﻿using System;
-
-public class ArabTree : Tree
+﻿namespace ageOfVillagers
 {
-	public void CreateTree(int x, int y)
+    internal class ArabTree : IShape
     {
-        Rectangle r = new Rectangle(x - 1, y, x - 1, y - 12, x + 1, y - 12, x + 1, y);
-        r.Draw();
-        Line l1 = new Line(x, y, x - 8, y + 4);
-        l1.Draw();
-        Line l2 = new Line(x, y, x - 4, y + 8);
-        l2.Draw();
-        Line l3 = new Line(x, y, x, y + 12);
-        l3.Draw();
-        Line l4 = new Line(x, y, x + 4, y + 8);
-        l4.Draw();
-        Line l5 = new Line(x, y, x + 8, y + 4);
-        l5.Draw();
+        public string draw(int x, int y)
+        {
+            Rectangle r = new Rectangle(x - 1, y, x - 1, y - 12, x + 1, y - 12, x + 1, y);
+            Line l1 = new Line(x, y, x - 8, y + 4);
+            Line l2 = new Line(x, y, x - 4, y + 8);
+            Line l3 = new Line(x, y, x, y + 12);
+            Line l4 = new Line(x, y, x + 4, y + 8);
+            Line l5 = new Line(x, y, x + 8, y + 4);
+            return r.Draw() + l1.Draw() + l2.Draw() + l3.Draw() + l4.Draw() + l5.Draw();
+        }
     }
 }
