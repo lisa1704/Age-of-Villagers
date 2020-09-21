49bbc01dd7c01f7a6012951cbe0bdd1b29c4393b
tiashaa <hriditatiasha@gmail.com>
2020-09-21 18:59:11 +0600

    #1 #7 refactoring placed item

--- solutions/41-tiasha/Age of Villagers/Tree/ArabianTree.cs
+++ solutions/41-tiasha/Age of Villagers/Tree/ArabianTree.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class ArabianTree
+    public class ArabianTree : CompositeShape
     {
+        public ArabianTree(Point p, ElementSize sz)
+        {
+            Point mid = p;
+            Point l1 = new Point(p.X - sz.width / 2, p.Y - sz.height / 4);
+            Point l2 = new Point(p.X - sz.width / 2, p.Y - sz.height / 2);
+            Point l3 = new Point(p.X, p.Y - sz.height / 2);
+            Point l4 = new Point(p.X + sz.width / 2, p.Y - sz.height / 4);
+            Point l5 = new Point(p.X + sz.width / 2, p.Y - sz.height / 2);
+            Point topleft = new Point(p.X - 2, p.Y);
+            Point topright = new Point(p.X + 2, p.Y);
+            Point bottomleft = new Point(p.X - 2, p.Y + sz.height / 2);
+            Point bottomRight = new Point(p.X + 2, p.Y + sz.height / 2);
+
+            AddComponent(new Rectangle(topleft, topright, bottomleft, bottomRight));
+            AddComponent(new Line(mid.X, mid.Y, l1.X, l1.Y));
+            AddComponent(new Line(mid.X, mid.Y, l2.X, l2.Y));
+            AddComponent(new Line(mid.X, mid.Y, l3.X, l3.Y));
+            AddComponent(new Line(mid.X, mid.Y, l4.X, l4.Y));
+            AddComponent(new Line(mid.X, mid.Y, l5.X, l5.Y));
+
+        }
+
     }
 }
