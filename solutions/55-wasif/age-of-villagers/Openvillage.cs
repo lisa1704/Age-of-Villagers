54b16bcaa713c0b4ba8a37398bc068a42662f2c1
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-22 03:00:52 +0600

    #6 Open Village class edited

--- solutions/55-wasif/age-of-villagers/Openvillage.cs
+++ solutions/55-wasif/age-of-villagers/Openvillage.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace age_of_villagers
 {
     class Openvillage
     {
+        OpenFileDialog openFileDialog1 = new OpenFileDialog
+        {
+            CheckFileExists = true,
+            CheckPathExists = true,
+
+            DefaultExt = "txt",
+            Filter = "aov files (*.aov)|*.txt",
+            FilterIndex = 2,
+            RestoreDirectory = true,
+
+            ReadOnlyChecked = true,
+            ShowReadOnly = true
+        };
+
     }
 }
