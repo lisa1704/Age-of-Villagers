badaf47ac7e5f3b31287d188073f1e3f90cf1df0
Sakib58 <rabiulahmed.sakib@gmail.com>
2020-09-22 03:04:45 +0600

    #4 UI design created

--- solutions/58-sakib-2/src/sample/Main.java
+++ solutions/58-sakib-2/src/sample/Main.java
@@ -6,13 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent xml=FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Age of villagers");
+        primaryStage.setScene(new Scene(xml,800,400));
         primaryStage.show();
     }
 
