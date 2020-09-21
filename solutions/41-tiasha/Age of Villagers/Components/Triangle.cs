d86e8e39c846197224314714e45a8fa185959916
tiashaa <hriditatiasha@gmail.com>
2020-09-21 16:53:31 +0600

    #1 triangle refactored

--- solutions/41-tiasha/Age of Villagers/Components/Triangle.cs
+++ solutions/41-tiasha/Age of Villagers/Components/Triangle.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class Triangle
+    public class Triangle : CompositeShape
     {
+        public Point top, left, right;
+
+        public Triangle(Point top, Point left, Point right)
+        {
+            this.top = top;
+            this.left = left;
+            this.right = right;
+
+            AddComponent(new Line(top.X, top.Y, left.X, left.Y));
+            AddComponent(new Line(left.X, left.Y, right.X, right.Y));
+            AddComponent(new Line(right.X, right.Y, top.X, top.Y));
+        }
     }
 }
