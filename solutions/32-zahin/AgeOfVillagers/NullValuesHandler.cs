a3ae7f11b858c4d224c0900b8108e76518103dd4
zahin32 <56842235+zahin32@users.noreply.github.com>
2020-09-22 01:00:55 +0600

    #1 refactoring

--- solutions/32-zahin/AgeOfVillagers/NullValuesHandler.cs
+++ solutions/32-zahin/AgeOfVillagers/NullValuesHandler.cs
@@ -10,28 +10,24 @@ namespace AgeOfVillagers
 {
     class NullValuesHandler : INation
     {
-        public string DrawHouse(Graphics g, Point point)
+        public void DrawHouse(Graphics g, Point point)
         {
             MessageBox.Show("Can not Draw House for the chosen Nation!!");
-            return "Can not Draw House for the chosen Nation!!";
         }
 
+        public void DrawTree(Graphics g, Point point)
+        {
+            MessageBox.Show("Can not Draw Tree for the Chosen Nation!!");
+        }
+
+        public void DrawWaterSource(Graphics g, Point point)
+        {
+            MessageBox.Show("Can not Draw Water for the Chosen Nation!!");
+        }
         public Color DrawTerrain()
         {
             MessageBox.Show("Can not Draw Terrain for the Chosen Nation!!");
             return Color.Empty;
-        }
-
-        public string DrawTree(Graphics g, Point point)
-        {
-            MessageBox.Show("Can not Draw Tree for the Chosen Nation!!");
-            return "Can not Draw Tree for the Chosen Nation!!";
-        }
-
-        public string DrawWaterSource(Graphics g, Point point)
-        {
-            MessageBox.Show("Can not Draw Water for the Chosen Nation!!");
-            return "Can not Draw Water for the Chosen Nation!!";
         }
 
     }
--- solutions/32-zahin/AgeOfVillagers/Shapes/IShape.cs
+++ solutions/32-zahin/AgeOfVillagers/Shapes/IShape.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
-
-namespace AgeOfVillagers.Shapes
+﻿namespace AgeOfVillagers.Shapes
 {
     interface IShape
     {
