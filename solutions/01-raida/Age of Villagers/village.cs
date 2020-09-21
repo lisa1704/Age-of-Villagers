79adcd78707fc75ece994d1e09cd59ed0db2b09b
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-09-21 15:15:17 +0600

    #9 #10 #11 #12 stuct village equal method added explicitly

--- solutions/01-raida/Age of Villagers/village.cs
+++ solutions/01-raida/Age of Villagers/village.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
-
+using System.Linq;
 
 namespace Age_of_Villagers
 {
@@ -10,5 +10,13 @@ namespace Age_of_Villagers
         public List<Point> tree_points;
         public List<Point> house_points;
         public List<Point> river_points;
+
+        public bool Equals(village villa)
+        {
+            return name == villa.name &&
+                tree_points.SequenceEqual(villa.tree_points) && 
+                house_points.SequenceEqual(villa.house_points) && 
+                river_points.SequenceEqual(villa.house_points);
+        }
     };
 }
