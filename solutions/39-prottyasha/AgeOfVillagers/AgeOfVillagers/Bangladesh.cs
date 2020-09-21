8f709db71a3ce72486812ad9b445ed259116eeb2
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-22 00:54:56 +0600

    #1 bd component factory class

--- solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Bangladesh.cs
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Bangladesh.cs
@@ -8,7 +8,19 @@ namespace AgeOfVillagers
     {
         public override IComponent getComponent(string component_type)
         {
-            throw new NotImplementedException();
+            if (component_type == "tree")
+            {
+                return new BdTree();
+            }
+            if (component_type == "house")
+            {
+                return new BdHouse();
+            }
+            if (component_type == "water_source")
+            {
+                return new BdRiver();
+            }
+            
         }
     }
 }
