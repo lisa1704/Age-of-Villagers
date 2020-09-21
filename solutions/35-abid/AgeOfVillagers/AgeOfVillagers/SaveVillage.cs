eee8a9d26374cfb79702cf28302cd75c88f24f7a
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-21 21:24:06 +0600

    #5 Implemented Json serialization in SaveVillage class to save the village state.

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagers/SaveVillage.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagers/SaveVillage.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using Newtonsoft.Json;
+using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
     class SaveVillage : IOpenSave
     {
+
+        private VillageState villageState;
+        public SaveVillage(VillageState villageState)
+        {
+            this.villageState = villageState;
+        }
+
+        public object JsonSerializer { get; private set; }
+
         public void execute()
         {
-            throw new NotImplementedException();
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = @"D:\AoV";
+            sfd.RestoreDirectory = true;
+            sfd.FileName = "*.aov";
+            sfd.DefaultExt = "aov";
+            sfd.Filter = "AoV file(*.aov) | *.aov";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Stream AoVstream = sfd.OpenFile();
+                using (StreamWriter streamWriter = new StreamWriter(AoVstream))
+                {
+                    JsonSerializer jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(streamWriter, villageState);
+                    streamWriter.Close();
+                }
+                AoVstream.Close();
+            }
         }
     }
+
 }
