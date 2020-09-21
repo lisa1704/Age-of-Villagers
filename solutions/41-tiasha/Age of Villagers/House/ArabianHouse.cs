1784fd6fc600c92db58c9f128f6c6cdf617fb1cd
tiashaa <hriditatiasha@gmail.com>
2020-09-21 18:37:51 +0600

    #1 refactoring Arabian house

--- solutions/41-tiasha/Age of Villagers/House/ArabianHouse.cs
+++ solutions/41-tiasha/Age of Villagers/House/ArabianHouse.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class ArabianHouse
+    public class ArabianHouse : CompositeShape
     {
+        public ArabianHouse(Point p, ElementSize sz)
+        {
+            var topLeft = p;
+
+            var bottomLeft = new Point(p.X - sz.width * 5 / 16, p.Y + sz.height * 5 / 8);
+            var bottomMid = new Point(p.X + sz.width * 5 / 16, p.Y + sz.height * 5 / 8);
+            var bottomRight = new Point(bottomMid.X + sz.width * 6 / 16, bottomMid.Y - sz.height * 6 / 16);
+            var topRight = new Point(p.X + sz.width * 6 / 16, p.Y - sz.height * 6 / 16);
+
+
+
+            AddComponent(new Triangle(topLeft, bottomLeft, bottomMid));
+            AddComponent(new Rectangle(topLeft, topRight, bottomMid, bottomRight));
+        }
     }
 }
