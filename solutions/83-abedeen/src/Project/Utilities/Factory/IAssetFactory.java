bc10a3bcd1272dbbd3d1e3da67bd49ef3cedde64
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-21 23:21:44 +0600

    #1 #7 #11 Concrete Tree for Egyptian Kings Implemented

---
+++ solutions/83-abedeen/src/Project/Utilities/Factory/IAssetFactory.java
@@ -0,0 +1,4 @@
+package Project.Utilities.Factory;
+
+public interface IAssetFactory {
+}
---
+++ solutions/83-abedeen/src/Project/Utilities/NationItems/Egyptian_Kings_Tree.java
@@ -0,0 +1,21 @@
+package Project.Utilities.NationItems;
+
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
+
+import java.util.ArrayList;
+
+public class Egyptian_Kings_Tree implements ITree{
+    @Override
+    public ArrayList<Shape> drawTree(double x, double y) {
+        double half_width = TreeWidth/2;
+        ArrayList<Shape> tree = new ArrayList<>();
+        tree.add(new Line(x,y,x-half_width+2,y-TreeHeight));
+        tree.add(new Line(x,y,x+half_width-2,y-TreeHeight));
+        tree.add(new Line(x-half_width/2,y-TreeHeight/2,x-2,y-TreeHeight));
+        tree.add(new Line(x-half_width/2,y-TreeHeight/2,x-half_width,y-TreeHeight*2/3));
+        tree.add(new Line(x+half_width/3,y-TreeHeight/3,x+2,y-TreeHeight));
+        tree.add(new Line(x+half_width/3,y-TreeHeight/3,x+half_width,y-TreeHeight*2/3));
+        return tree;
+    }
+}
