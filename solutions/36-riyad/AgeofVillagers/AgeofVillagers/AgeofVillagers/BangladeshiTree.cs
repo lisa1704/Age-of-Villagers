97dee86e53fa19a9d1dde7d86972f2363a13bddf
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-21 16:43:31 +0600

    #9 bangladeshi tree completed

--- solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/BangladeshiTree.cs
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/BangladeshiTree.cs
@@ -7,6 +7,15 @@ namespace AgeofVillagers
 {
     public class BangladeshiTree : CompositeShape
     {
-       
+        public BangladeshiTree(Point mid)
+        {
+            Point bottomRight = new Point(mid.X + 8, mid.Y + 24);
+            Point top = new Point(mid.X + 16, mid.Y + 18);
+            Point topLeft = new Point(mid.X + 6, mid.Y + 12);
+
+
+            AddComponents(new RectangleShape(topLeft, bottomRight));
+            AddComponents(new EllipseShape(mid, top));
+        }
     }
 }
