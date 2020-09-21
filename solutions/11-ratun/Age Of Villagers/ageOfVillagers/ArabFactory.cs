fe28f5f0f2d8d1fa62394c8a88599162cdaa362a
Ratun <www.ratunrahman@gmail.com>
2020-09-21 20:51:31 +0600

    #1 implement Arab factory class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabFactory.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabFactory.cs
@@ -2,5 +2,14 @@
 {
     internal class ArabFactory : Shape
     {
+        public override IShape GetItem(string itemType)
+        {
+            if (itemType == "tree")
+                return new ArabTree();
+            else if (itemType == "house")
+                return new ArabHouse();
+            else
+                return null;
+        }
     }
 }
