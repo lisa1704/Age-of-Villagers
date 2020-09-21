608749a6b71fbb98903ca83fa20a1c50e86849f8
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-21 06:52:32 +0600

    #7 rectangle shape updated

--- solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/RectangleShape.cs
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/RectangleShape.cs
@@ -18,7 +18,13 @@ namespace AgeofVillagers
 
         public void Paint(Graphics g, Pen p)
         {
-            throw new NotImplementedException();
+            var pen = new Pen(Color.Black, 1);
+
+
+            g.DrawLine(pen, leftTop.X, leftTop.Y, RightBottom.X, leftTop.Y);
+            g.DrawLine(pen, leftTop.X, leftTop.Y, leftTop.X, RightBottom.Y);
+            g.DrawLine(pen, leftTop.X, RightBottom.Y, RightBottom.X, RightBottom.Y);
+            g.DrawLine(pen, RightBottom.X, leftTop.Y, RightBottom.X, RightBottom.Y);
         }
     }
 }
