592fd004e4cbf34658f29fd653f1d4bbee440b99
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-09-21 23:45:00 +0600

    #1 Arab house modified

--- solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/ArabBedouinHouse.cs
+++ solutions/73-tasnim/AgeOfVillagers/AgeOfVillagers/ArabBedouinHouse.cs
@@ -11,16 +11,16 @@ namespace AgeOfVillagers
     {
         public ArabBedouinHouse(Point initialPoint)
         {
-            Point topPoint1 = new Point(initialPoint.X - 24, initialPoint.Y + 50);
-            Point topPoint2 = new Point(initialPoint.X + 16, initialPoint.Y + 50);
-            Point topPoint3 = new Point(initialPoint.X + 25, initialPoint.Y - 30);
-            Point topPoint4 = new Point(topPoint2.X + 25, topPoint2.Y - 30);
+            Point Point1 = new Point(initialPoint.X - 45, initialPoint.Y + 60);
+            Point Point2 = new Point(initialPoint.X + 45, initialPoint.Y + 60);
+            Point Point3 = new Point(initialPoint.X + 30, initialPoint.Y - 20);
+            Point Point4 = new Point(Point2.X + 30, Point2.Y - 20);
 
 
-            addShape(new Triangle(topPoint2, topPoint1, initialPoint));
-            addShape(new Line(topPoint3, initialPoint));
-            addShape(new Line(topPoint2, topPoint4));
-            addShape(new Line(topPoint3, topPoint4));
+            addShape(new Triangle(Point2, Point1, initialPoint));
+            addShape(new Line(Point3, initialPoint));
+            addShape(new Line(Point2, Point4));
+            addShape(new Line(Point3, Point4));
 
         }
     }
