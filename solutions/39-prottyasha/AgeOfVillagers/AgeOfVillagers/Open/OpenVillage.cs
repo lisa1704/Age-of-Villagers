45fb42849216d5e1254274435bf179812a48d31c
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-22 05:30:30 +0600

    #6 new open class created.

---
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Open/OpenVillage.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers.Open
+{
+    class OpenVillage
+    {
+        Village vill = new Village();
+        public void open()
+        {
+            
+        }
+    }
+}
