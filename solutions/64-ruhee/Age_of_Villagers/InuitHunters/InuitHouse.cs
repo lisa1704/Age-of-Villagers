495093b46d3ec9076db60686ad3b93ee099515ce
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-21 16:43:17 +0600

    #1 #7 #8 #12 updated Inuithouse to draw.

--- solutions/64-ruhee/Age_of_Villagers/InuitHunters/InuitHouse.cs
+++ solutions/64-ruhee/Age_of_Villagers/InuitHunters/InuitHouse.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Age_of_Villagers.Age_of_Villagers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.InuitHunters
 {
-    class InuitHouse
+    class InuitHouse:CShape
     {
+        public InuitHouse(Point pt)
+        {
+            Point x1 = new Point(pt.X - 50, pt.Y + 50);
+            Point x2 = new Point(pt.X + 50, pt.Y + 50);
 
+
+            AddComponent(new Line(x1, x2));
+            AddComponent(new Arc(x1.X,x2.Y,80,100,0,-180));
+            AddComponent(new Arc(x1.X+20,x2.Y-20,60,80,0,-180));
+
+        }
     }
 }
