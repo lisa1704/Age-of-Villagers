88da006b90374fdb81151cb2fdfeaea161192691
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-21 16:54:09 +0600

    #10 created arab house and tree class

---
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Arabhouse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Arabhouse
+{
+    public void CreateHouse(int x, int y)
+    {
+        //Triangle(x - 12, y, x, y + 8, x + 12, y);
+        //t.Draw();
+        //new Rectangle(x - 12, y, x + 12, y, x + 12, y - 12, x - 12, y - 12);
+        //r.Draw();
+    }
+}
---
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Arabtree.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Arabtree
+{
+    public void CreateTree(int x, int y)
+    {
+        //new Rectangle(x - 12, y, x + 12, y, x + 12, y - 12, x - 12, y - 12);
+        //r.Draw();
+        //new Lines()
+    }
+}
