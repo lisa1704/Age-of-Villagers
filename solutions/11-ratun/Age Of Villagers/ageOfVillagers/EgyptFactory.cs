b8d2815dc5068b0d9dcd6bab825a0030a39926fb
Ratun <www.ratunrahman@gmail.com>
2020-09-21 21:11:54 +0600

    #1 implement egypt factory class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptFactory.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptFactory.cs
@@ -2,5 +2,16 @@
 {
     internal class EgyptFactory : Shape
     {
+        public override IShape GetItem(string itemType)
+        {
+            if (itemType == "tree")
+                return new EgyptTree();
+            else if (itemType == "house")
+                return new EgyptHouse();
+            else if (itemType == "water")
+                return new EgyptWater();
+            else
+                return null;
+        }
     }
 }
