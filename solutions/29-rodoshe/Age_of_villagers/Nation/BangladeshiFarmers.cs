2fd666ec765fafb9cbfee2a240e63026ad4f01bc
afiarodoshe <afiamuntakim@iut-dhaka.edu>
2020-09-21 23:50:55 +0600

    #9 implementing Bangladeshi farmar class

--- solutions/29-rodoshe/Age_of_villagers/Nation/BangladeshiFarmers.cs
+++ solutions/29-rodoshe/Age_of_villagers/Nation/BangladeshiFarmers.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers.Nation
+{
+    class BangladeshiFarmers : INation
+    {
+        private Color colorofbackground = Color.Green;
+        public Color BgColor()
+        {
+            return colorofbackground;
+        }
+        public void DrawHouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
