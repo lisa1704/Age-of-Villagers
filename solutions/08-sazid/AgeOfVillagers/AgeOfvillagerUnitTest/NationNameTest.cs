2810dc83d24cbd691ac8ed3e930bcb9438524a42
ESD <ESD@DESKTOP-SNRCVRJ>
2020-09-21 17:03:52 +0600

    #9 #10 #11 #12 Tests added for nationName

---
+++ solutions/08-sazid/AgeOfVillagers/AgeOfvillagerUnitTest/NationNameTest.cs
@@ -0,0 +1,51 @@
+﻿using AgeOfVillagers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AgeOfvillagerUnitTest
+{
+    public class NationNameTest
+    {
+        [Fact]
+        public void BdVillageTest()
+        {
+            string expected = "Bangladeshi Farmers";
+            NationFactory nationFactory = new NationFactory();
+            INation nation = nationFactory.GetNation("Bangladeshi Farmers");
+            string actual = nation.getNationName();
+            actual.Equals(expected);
+        }
+
+        [Fact]
+        public void ArabVillageTest()
+        {
+            string expected = "Arab Beduin";
+            NationFactory nationFactory = new NationFactory();
+            INation nation = nationFactory.GetNation("Arab Beduin");
+            string actual = nation.getNationName();
+            actual.Equals(expected);
+        }
+
+        [Fact]
+        public void EgyptianVillageTest()
+        {
+            string expected = "Egyptian King";
+            NationFactory nationFactory = new NationFactory();
+            INation nation = nationFactory.GetNation("Egyptian king");
+            string actual = nation.getNationName();
+            actual.Equals(expected);
+        }
+
+        [Fact]
+        public void InuitVillageTest()
+        {
+            string expected = "Inuit Hunter";
+            NationFactory nationFactory = new NationFactory();
+            INation nation = nationFactory.GetNation("Intuit Hunter");
+            string actual = nation.getNationName();
+            actual.Equals(expected);
+        }
+    }
+}
