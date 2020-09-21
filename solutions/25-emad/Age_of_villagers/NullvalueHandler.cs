a127062b20e49c5d3575f138b783974872dd4b7d
mahirashrafemad <mahirashrafemad@users.noreply.github.com>
2020-09-21 18:32:28 +0600

    #7 #8 #9 #10 #11 NullvalueHandler class created

---
+++ solutions/25-emad/Age_of_villagers/NullvalueHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Age_of_villagers
+{
+    class NullvalueHandler : INations
+    {
+        public string DrawHouse(Graphics graphics, Point point)
+        {
+            MessageBox.Show("No house available for this Nation");
+            return "No house available for this Nation";
+        }
+
+        public string DrawWaterSource(Graphics graphics, Point point)
+        {
+            MessageBox.Show("No house available for this Nation");
+            return "No house available for this Nation";
+        }
+
+        public string DrawTree(Graphics graphics, Point point)
+        {
+            MessageBox.Show("No house available for this Nation");
+            return "No house available for this Nation";
+        }
+
+
+        public Color DrawTerrain()
+        {
+            MessageBox.Show("No house available for this Nation");
+            return Color.Empty;
+        }
+
+        
+    }
+}
