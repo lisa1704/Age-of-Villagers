5e27f12b4ba98095dc840d462c0f988de0b249bc
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-09-21 18:07:51 +0600

    #1 #11 refactored egyptian kings' house

--- solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/EgyptianKingHouse.cs
+++ solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/EgyptianKingHouse.cs
@@ -6,18 +6,22 @@ using System.Windows.Forms;
 
 namespace AgeOfVillagers
 {
-    public class EgyptianKingHouse : IShape
+    public class EgyptianKingHouse : CompositeShapes
     {
-        
+        private Point point;
         public EgyptianKingHouse(Point point)
         {
-            
+            Point p1 = point;
+            Point bottomleft = new Point(p1.X - 8, p1.Y + 16);
+            Point bottomright = new Point(p1.X + 8, p1.Y + 16);
+            Point corner = new Point(p1.X + 16, p1.Y + 8 );
+
+            addshapes(new Triangle(p1, bottomright, bottomleft));
+            addshapes(new Triangle(p1, bottomright, corner));
+
         }
         
 
-        public void draw(Pen pen, Graphics graphics)
-        {
-           
-        }
+        
     }
 }
