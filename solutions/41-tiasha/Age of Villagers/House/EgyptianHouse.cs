5b49a1fee3abb670281e3fd3391e0e7ca64f5640
tiashaa <hriditatiasha@gmail.com>
2020-09-21 18:52:36 +0600

    #1 refactored

--- solutions/41-tiasha/Age of Villagers/House/EgyptianHouse.cs
+++ solutions/41-tiasha/Age of Villagers/House/EgyptianHouse.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class EgyptianHouse
+    public class EgyptianHouse : CompositeShape
     {
+       
+
+        public EgyptianHouse(Point p, ElementSize sz)
+        {
+            Point top = p;
+            Point bottomMid = new Point(p.X + sz.width / 4, p.Y + sz.height);
+            Point bottomleft = new Point(bottomMid.X - sz.width * 10 / 16, p.Y + sz.height - sz.height / 4);
+            Point bottomRight = new Point(bottomMid.X + sz.width * 6 / 16, p.Y + (sz.height / 2));
+
+
+
+            AddComponent(new Triangle(top, bottomMid, bottomleft));
+            AddComponent(new Triangle(top, bottomMid, bottomRight));
+
+
+
+        }
     }
 }
