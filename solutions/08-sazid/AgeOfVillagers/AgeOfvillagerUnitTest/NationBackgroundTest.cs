6056b0422c5fb069eb650467540ed803e8074972
ESD <ESD@DESKTOP-SNRCVRJ>
2020-09-21 17:10:58 +0600

    #9 #10 #11 #12 Added tests for nation background color

---
+++ solutions/08-sazid/AgeOfVillagers/AgeOfvillagerUnitTest/NationBackgroundTest.cs
@@ -0,0 +1,51 @@
+﻿using AgeOfVillagers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Xunit;
+
+namespace AgeOfvillagerUnitTest
+{
+    public class NationBackgroundTest
+    {
+        [Fact]
+        public void BdBackGroundTest()
+        {
+            Color expected = Color.Green;
+            INation nation = new BangladeshiFarmer();
+            Color actual = nation.GetColor();
+            actual.Equals(expected);
+        }
+
+        [Fact]
+        public void arabBackGroundTest()
+        {
+            Color expected = Color.Orange;
+            INation nation = new ArabBeduin();
+            Color actual = nation.GetColor();
+            actual.Equals(expected);
+        }
+
+        [Fact]
+        public void egyptianBackGroundTest()
+        {
+            Color expected = Color.Yellow;
+            INation nation = new BangladeshiFarmer();
+            Color actual = nation.GetColor();
+            actual.Equals(expected);
+        }
+
+        [Fact]
+        public void inuitBackGroundTest()
+        {
+            Color expected = Color.White;
+            INation nation = new BangladeshiFarmer();
+            Color actual = nation.GetColor();
+            actual.Equals(expected);
+        }
+
+
+
+    }
+}
