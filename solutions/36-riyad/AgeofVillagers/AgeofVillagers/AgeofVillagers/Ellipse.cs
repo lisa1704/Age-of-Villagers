d25b6dba07840bbdd5cb497203423498fdf9730b
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-21 16:21:33 +0600

    #7 Ellipse Shape implemented

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/Ellipse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class Ellipse : CompositeShape
+    {
+        private Point leftTop;
+        private Point rightBottom;
+
+        public Ellipse(Point Left, Point Right)
+        {
+            leftTop = Left;
+            rightBottom = Right;
+            AddComponents(new Arc(leftTop.X, leftTop.Y, rightBottom.X - leftTop.X, rightBottom.Y - leftTop.Y, 0, 360));
+        }
+    }
+}
