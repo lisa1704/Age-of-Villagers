5a3e3731548a5c3ed99cbc25d8b690d28596a166
tiashaa <hriditatiasha@gmail.com>
2020-09-21 16:46:33 +0600

    #1 rectangle refactored

--- solutions/41-tiasha/Age of Villagers/Components/Rectangle.cs
+++ solutions/41-tiasha/Age of Villagers/Components/Rectangle.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class Rectangle
+    public class Rectangle : CompositeShape
     {
+        public Point topLeft, topRight, bottomLeft, bottomRight;
+
+        public Rectangle(Point topLeft, Point topRight, Point bottomLeft, Point bottomRight)
+        {
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+
+            AddComponent(new Line(topLeft.X, topLeft.Y, topRight.X, topRight.Y));
+            AddComponent(new Line(topLeft.X, topLeft.Y, bottomLeft.X, bottomLeft.Y));
+            AddComponent(new Line(bottomLeft.X, bottomLeft.Y, bottomRight.X, bottomRight.Y));
+            AddComponent(new Line(bottomRight.X, bottomRight.Y, topRight.X, topRight.Y));
+        }
     }
 }
