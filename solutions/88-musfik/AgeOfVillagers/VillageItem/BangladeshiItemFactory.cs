89f6ea3491a3b6afdb5a3cc57ace0704738de176
musfiksifat <musfiksifat766@gmail.com>
2020-09-22 05:37:54 +0600

    #9 BangladeshiItemFactory class created & implemented

---
+++ solutions/88-musfik/AgeOfVillagers/VillageItem/BangladeshiItemFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.VillageItem
+{
+    public class BangladeshiItemFactory
+    {
+        private String type;
+        public BangladeshiItemFactory(String ItemType)
+        {
+            type = ItemType;
+        }
+
+        internal IVillageItem ItemProducer(Point point)
+        {
+            if (type == "House")
+                return new BangladeshiHouse(point);
+
+            else if (type == "Tree")
+                return new BangladeshiTree(point);
+
+            else if (type == "Water")
+                return new BangladeshiWaterResource(point);
+
+            return new NoTree(point);
+        }
+    }
+}
