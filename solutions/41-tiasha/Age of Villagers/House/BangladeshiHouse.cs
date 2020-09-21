d2aef13d83136620a2b2a6b42dc04c85786f19b5
tiashaa <hriditatiasha@gmail.com>
2020-09-21 18:41:58 +0600

    #1 refactored Bangladeshi house

--- solutions/41-tiasha/Age of Villagers/House/BangladeshiHouse.cs
+++ solutions/41-tiasha/Age of Villagers/House/BangladeshiHouse.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class BangladeshiHouse
+    public class BangladeshiHouse : CompositeShape
     {
+        public BangladeshiHouse(Point p, ElementSize sz)
+        {
+            Point top = new Point(p.X + (sz.width / 2), p.Y - 6);
+            Point topLeft = p;
+            Point bottomLeft = new Point(p.X, p.Y + (sz.height - 6));
+            Point topRight = new Point(p.X + sz.width, p.Y);
+            Point bottomRight = new Point(p.X + sz.width, p.Y + (sz.height - 6));
+
+            AddComponent(new Triangle(top, topLeft, topRight));
+            AddComponent(new Rectangle(topLeft, topRight, bottomLeft, bottomRight));
+        }
     }
 }
