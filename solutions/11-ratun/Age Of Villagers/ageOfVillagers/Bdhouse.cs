84c8e6472d66937d720704f9c2d1738801951c28
Ratun <www.ratunrahman@gmail.com>
2020-09-21 10:23:52 +0600

    #9 implement Bdhouse

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Bdhouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Bdhouse.cs
@@ -2,8 +2,11 @@
 
 public class Bdhouse : House
 {
-	public void CreateHouse()
+	public void CreateHouse(int x, int y)
     {
-
+        Triangle t = new Triangle(x, y + 12, x - 12, y, x + 12, y);
+        t.Draw();
+        Rectangle r = new Rectangle(x - 12, y, x + 12, y, x + 12, y - 12, x - 12, y - 12);
+        r.Draw();
     }
 }
