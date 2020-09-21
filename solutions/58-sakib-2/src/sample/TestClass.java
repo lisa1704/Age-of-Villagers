4fe5e1e98d90ac677427774b62ce49bafb363320
Sakib58 <rabiulahmed.sakib@gmail.com>
2020-09-21 14:38:25 +0600

    #15 test framework prepared

---
+++ solutions/58-sakib-2/src/sample/TestClass.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestClass {
+    @Test
+    void assertTest(){
+        Assertions.assertEquals(2,2);
+    }
+}
