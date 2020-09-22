7c83d605b1ecbe81d7fce00701d492f780fa76ff
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-23 03:56:41 +0600

    #6 functionality for opening a save village is added

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/VillageConditionOpen.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Age_of_Villagers
+{
+    class VillageConditionOpen : IVillage
+    {
+        private VillageCondition villageCondition;
+        public void PerformOperation()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream fileStream = openFileDialog.OpenFile();
+                using (StreamReader stream = new StreamReader(fileStream))
+                {
+                    string json = stream.ReadToEnd();
+                    villageCondition = JsonConvert.DeserializeObject<VillageCondition>(json);
+                }
+            }
+        }
+    }
+}
