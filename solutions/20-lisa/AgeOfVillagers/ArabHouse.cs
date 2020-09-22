ac9b85e6667e12c6a92a8e6b699261aa0dd9448a
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-22 21:41:17 +0600

    #1 #7 #10 Arab House shape correctly implemented

--- solutions/20-lisa/AgeOfVillagers/ArabHouse.cs
+++ solutions/20-lisa/AgeOfVillagers/ArabHouse.cs
@@ -11,8 +11,8 @@ namespace AgeOfVillagers
         {
             Point p1 = new Point(point.X - 20, point.Y + 40);
             Point p2 = new Point(point.X + 20, point.Y + 40);
-            Point p3 = new Point(point.X + 25, point.Y + 15);
-            Point p4 = new Point(p2.X + 25, p2.Y + 10);
+            Point p3 = new Point(point.X + 25, point.Y - 15);
+            Point p4 = new Point(p2.X + 25, p2.Y - 10);
 
             AddComponent(new Triangle(point, p1, p2));
             AddComponent(new Line(point, p3));
