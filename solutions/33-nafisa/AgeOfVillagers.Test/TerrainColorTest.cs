5ce441fd1ccacbdd7dc6710149d6936fdf01595c
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-23 02:46:45 +0600

    #15 Terraincolor test class created

---
+++ solutions/33-nafisa/AgeOfVillagers.Test/TerrainColorTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Xunit;
+using AgeOfVillagers;
+
+
+
+namespace AgeOfVillagers.Test
+{
+    [TestClass]
+    public class TerrainColorTest
+    {
+
+        [TestMethod]
+        public void BangladeshiFarmerTest()
+        {
+            
+        }
+    }
+}
--- solutions/33-nafisa/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/33-nafisa/AgeOfVillagers/BangladeshiFarmers.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+
 namespace AgeOfVillagers
 {
     public class BangladeshiFarmers : INation
