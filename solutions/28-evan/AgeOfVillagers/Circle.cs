3fdd22b917fb912f2ab0a044e227b7a81eb6ddc6
Raihan Evan <raihan.evan@gmail.com>
2020-09-22 22:41:40 +0600

    #7 Circle added

--- solutions/28-evan/AgeOfVillagers/Circle.cs
+++ solutions/28-evan/AgeOfVillagers/Circle.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgeOfVillagers
 {
-    class Circle
+    class Circle : IShape
     {
+        Point x,y;
+        int r, h, w;
+        Graphics g;
+        Pen p;
+        public Circle(Graphics g, Pen p, Point x,Point y,int r,int h,int w)
+        {
+            this.g = g;
+            this.p = p;
+            this.x = x;
+            this.y = y;
+            this.r = r;
+            this.h = h;
+            this.w = w;
+        }
 
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawShape(Graphics g, Pen p)
+        {
+            
+        }
     }
 }
