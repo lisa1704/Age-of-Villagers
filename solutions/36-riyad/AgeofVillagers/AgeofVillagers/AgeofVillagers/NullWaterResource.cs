6af427b62e3e7f559388b6ca62f9ebc28e60a1bb
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-22 11:35:10 +0600

    #10 #12 NullWater added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/NullWaterResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class NullWaterResource : CompositeShape
+    {
+        public NullWaterResource()
+        {
+            
+        }
+
+        internal void Paint(Graphics g, Pen p)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
