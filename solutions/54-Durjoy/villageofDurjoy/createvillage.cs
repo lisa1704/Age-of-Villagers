ec346faefd91aa9082332e2329043cb0c3f9e8cb
Durjoy54 <ariyandurjoy.88@gmail.com>
2020-09-23 02:13:31 +0600

    #7 Draw house function created

--- solutions/54-Durjoy/villageofDurjoy/createvillage.cs
+++ solutions/54-Durjoy/villageofDurjoy/createvillage.cs
@@ -59,6 +59,15 @@ namespace villageofDurjoy
         {
 
         }
+        private void drawHouse(int X, int Y)
+        {
+
+            g.DrawLine(p, X - 25, Y + 35, X + 25, Y - 25);
+            g.DrawLine(p, X + 25, Y - 25, X + 50, Y + 50);
+            g.DrawLine(p, X + 50, Y + 50, X - 25, Y + 35);
+            g.DrawLine(p, X + 25, Y - 25, X + 75, Y + 25);
+            g.DrawLine(p, X + 75, Y + 25, X + 50, Y + 50);
+        }
 
 
 
