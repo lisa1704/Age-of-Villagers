73864cf159f42b1bf87ba8439b6ddacb36d6c83e
66-Nowshad <nowshadulislam@iut-dhaka.edu>
2020-09-22 22:36:41 +0600

    #7  ITree created

---
+++ solutions/66-Nowshad/ITree.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Office.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Age_of_Villagers.Age_of_Villagers
+{
+    interface ITree
+    {
+        void drawTree(Axes Axes, Panel drawingpanel);
+    }
+}
