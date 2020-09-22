7babbcb612bcf060992935964e9c039bd5595a89
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 00:31:45 +0600

    #7 #8 #9 BangladeshiFarmers implementing Ination.

--- solutions/53-silvi/AgeOfVillagers/BangladeshiFarmers.cs
+++ solutions/53-silvi/AgeOfVillagers/BangladeshiFarmers.cs
@@ -8,5 +8,19 @@ namespace AgeOfVillagers
 {
     class BangladeshiFarmers : INation
     {
+        public void DrawHouse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
