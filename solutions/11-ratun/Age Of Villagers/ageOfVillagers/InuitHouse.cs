29d49ca04c1dade5c6a548b738e912c246058c76
Ratun <www.ratunrahman@gmail.com>
2020-09-22 19:48:52 +0600

    #12 fixed Inuit House class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHouse.cs
@@ -8,7 +8,11 @@ namespace ageOfVillagers
         public string draw(int x, int y, Graphics g)
         {
             HalfCircle h1 = new HalfCircle(g,p,x, y, 8);
+            h1.Draw();
             HalfCircle h2 = new HalfCircle(g,p,x, y, 2);
+            h2.Draw();
+            Line l1 = new Line(g, p, x - 8, y - 8, x + 8, y - 8);
+            l1.Draw();
             return "Drawing Inuit House";
         }
     }
