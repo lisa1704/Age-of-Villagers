540a07656bb457d4c3119a13e5bc056c69ceb283
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-09-22 17:48:15 +0600

    #6 open village states are implemented in a class

---
+++ solutions/77-mishra/AgeOfVillagers/AgeOfVillagers/OpenVillage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    class OpenVillage : IVillage
+    {
+        private VillageState villageState;
+        public void execute()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = openFile.OpenFile();
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    string json = streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
