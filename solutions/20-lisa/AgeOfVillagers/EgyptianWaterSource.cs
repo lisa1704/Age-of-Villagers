28fd7cfd384d23d17ef3198689817bd953336859
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-22 20:36:21 +0600

    #7 #11 Egyptian Kings Water Source implemented

--- solutions/20-lisa/AgeOfVillagers/EgyptianWaterSource.cs
+++ solutions/20-lisa/AgeOfVillagers/EgyptianWaterSource.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    class EgyptianWaterSource
+    class EgyptianWaterSource : CompositeShape
     {
+        public EgyptianWaterSource(Point p)
+        {
+            Point p1 = new Point(p.X + 60, p.Y + 60);
+            AddComponent(new Ellipse(p, p1));
+        }
     }
 }
