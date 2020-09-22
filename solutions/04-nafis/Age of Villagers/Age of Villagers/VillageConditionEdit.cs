372a878d182b985be4e49c6617da9cd369e80bfd
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-23 04:30:16 +0600

    #7 functionality for editing village is added

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/VillageConditionEdit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Age_of_Villagers
+{
+    class VillageConditionEdit
+    {
+        private readonly NationController nationController;
+        private readonly Graphics graphics;
+        private readonly Pen pen;
+        private readonly Panel panel;
+       
+        public VillageConditionEdit(Graphics graphics, Pen pen, Panel panel, NationController nationController)
+        {
+            this.graphics = graphics;
+            this.pen = pen;
+            this.panel = panel;
+            this.nationController = nationController;
+        }
+
+    }
+}
