9307ad9f0fac86719708f7ddae04fbdb4c2b7bb5
jarin_12 <jarintanzim@iut-dhaka.edu>
2020-09-23 02:12:58 +0600

    #7 line created

--- solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/Linecreate.cs
+++ solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/Linecreate.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace ageofvillagers
 {
-   public  class Linecreate:Shapecreate
+   public  class Linecreate
     {
-
+        
+            private int x1, y1, x2, y2;
+            public Linecreate(int x1, int y1, int x2, int y2)
+            {
+                this.x1 = x1;
+                this.y1 = y1;
+                this.x2 = x2;
+                this.y2 = y2;
+            }
+            public string Draw_Shape()
+            {
+                return "now draw a line ";
+            }
+        }
     }
 }
