4d1125fc73cebac5ba4b8057a751164ccba5fb63
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-22 19:19:37 +0600

    #1 #8 Implemented NullNation Properties

---
+++ solutions/22-snigdho/Age Of Villagers/Age Of Villagers/NullNation/NullNation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public class NullNation : INation
+    {
+        public Color BackgroundColor()
+        {
+            return Color.White;
+        }
+
+        public void DrawHouse(Graphics g, Point point)
+        {
+            NullItems NullHouse = new NullItems();
+            NullHouse.drawing();
+        }
+
+        public void DrawTree(Graphics g, Point point)
+        {
+            NullItems NullTree = new NullItems();
+            NullTree.drawing();
+        }
+
+        public void DrawWaterSource(Graphics g, Point point)
+        {
+            NullItems NullWaterSource = new NullItems();
+            NullWaterSource.drawing();
+        }
+    }
+}
