fff893467d1487177103e182334a0d9b4a512b3f
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-09-22 21:09:46 +0600

    #8 #9 #10 #11 #12 Tested Nation objects.

--- solutions/35-abid/AgeOfVillagers/AgeOfVillagersTest/NationObjTest.cs
+++ solutions/35-abid/AgeOfVillagers/AgeOfVillagersTest/NationObjTest.cs
@@ -2,17 +2,53 @@ using System;
 using AgeOfVillagers;
 using Xunit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
 
 namespace AgeOfVillagersTest
 {
     [TestClass]
     public class NationObjTest
     {
+        private Panel drawingPanel;
+
         [TestMethod]
-        public void TestBangladeshiFarmersObj()
+        public void TestBangladeshiNationObj()
         {
-           NationFactory
+            NationFactory BDobj = new NationFactory(drawingPanel);
+            INation nation1 = BDobj.nationUpdate(0);
+            INation nation2 = new BangladeshiFarmers(drawingPanel);
+            nation1.Equals(nation2);
     
+        }
+
+        [TestMethod]
+        public void TestArabianNationObj()
+        {
+            NationFactory Arabobj = new NationFactory(drawingPanel);
+            INation nation1 = Arabobj.nationUpdate(1);
+            INation nation2 = new ArabBedouins(drawingPanel);
+            nation1.Equals(nation2);
+
+        }
+
+        [TestMethod]
+        public void TestEgyptianNationObj()
+        {
+            NationFactory Egyobj = new NationFactory(drawingPanel);
+            INation nation1 = Egyobj.nationUpdate(2);
+            INation nation2 = new EgyptianKings(drawingPanel);
+            nation1.Equals(nation2);
+
+        }
+
+        [TestMethod]
+        public void TestInuitNationObj()
+        {
+            NationFactory Inuitobj = new NationFactory(drawingPanel);
+            INation nation1 = Inuitobj.nationUpdate(3);
+            INation nation2 = new InuitHunters(drawingPanel);
+            nation1.Equals(nation2);
+
         }
     }
 }
