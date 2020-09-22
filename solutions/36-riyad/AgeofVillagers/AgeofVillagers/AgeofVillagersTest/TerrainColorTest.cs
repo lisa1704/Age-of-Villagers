1bd3f4707f4f7fcea55bca25b88e750d4b7dd907
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-23 00:13:12 +0600

    #1 #10 UnitTest of bangladeshi Terrain color

--- solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagersTest/TerrainColorTest.cs
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagersTest/TerrainColorTest.cs
@@ -1,4 +1,6 @@
+using AgeofVillagers;
 using System;
+using System.Drawing;
 using Xunit;
 
 namespace AgeofVillagersTest
@@ -6,9 +8,12 @@ namespace AgeofVillagersTest
     public class TerrainColorTest
     {
         [Fact]
-        public void TerrainColor()
+        public void BdTerrainColor()
         {
-            
+            BangladeshiFarmers bdFarmers = new BangladeshiFarmers();
+            Color actual = bdFarmers.TerrainColor();
+            Color test = Color.Green;
+            Assert.Equal(actual, test);
         }
     }
 }
