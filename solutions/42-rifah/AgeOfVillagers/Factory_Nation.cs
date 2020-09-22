8f0337b14ad32c791062d1b5e0d28c86feca4eca
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-23 05:00:11 +0600

    #1 Factory_Nation class separated

---
+++ solutions/42-rifah/AgeOfVillagers/Factory_Nation.cs
@@ -0,0 +1,38 @@
+﻿using AgeOfVillagers.Arab_Bedouin;
+using AgeOfVillagers.Bangladeshi_Farmers;
+using AgeOfVillagers.Egyptian_Kings;
+using AgeOfVillagers.Inuit_Hunters;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public class Factory_Nation
+    {
+        public INation GetNation(string NationName)
+        {
+            if (NationName == "Bangladeshi Farmers")
+            {
+                return new BangladeshiFarmers();
+            }
+            else if (NationName == "Arab Bedouin")
+            {
+                return new ArabBedouin();
+            }
+            else if (NationName == "Egyptian Kings")
+            {
+                return new EgyptianKings();
+            }
+            else if (NationName == "Inuit Hunters")
+            {
+                return new InuitHunters();
+            }
+            else
+            {
+                return new NullNation();
+            }
+        }
+    }
+}
--- solutions/42-rifah/AgeOfVillagers/INation.cs
+++ solutions/42-rifah/AgeOfVillagers/INation.cs
@@ -17,32 +17,5 @@ namespace AgeOfVillagers
         Color BackgroundColor();
 
     }
-    public class Factory_Nation
-    {
-        public INation GetNation(string NationName)
-        {
-            if (NationName == "Bangladeshi Farmers")
-            {
-                return new BangladeshiFarmers();
-            }
-            else if (NationName == "Arab Bedouin")
-            {
-                return new ArabBedouin();
-            }
-            else if (NationName == "Egyptian Kings")
-            {
-                return new EgyptianKings();
-            }
-            else if (NationName == "Inuit Hunters")
-            {
-                return new InuitHunters();
-            }
-            else
-            {
-                return new NullNation();
-            }
-        }
-    }
-
 
 }
