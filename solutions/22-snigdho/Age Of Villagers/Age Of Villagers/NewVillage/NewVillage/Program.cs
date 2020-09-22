c98cc4aa4e722ca0f460ffed0afbf3803c8941e6
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-22 20:02:48 +0600

    #5 #6 Classes created for save and open village

---
+++ solutions/22-snigdho/Age Of Villagers/Age Of Villagers/NewVillage/NewVillage/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+namespace AgeOfVillagers
+{
+    public interface INation
+    {
+        void DrawTree(Graphics g, Point point);
+        void DrawHouse(Graphics g, Point point);
+        void DrawWaterSource(Graphics g, Point point);
+        Color BackgroundColor();
+
+    }
+    public class Factory_Nation
+    {
+        public INation GetNation(string NationName)
+        {
+            if (NationName == "Bangladeshi Farmers")
+            {
+                return new BangladeshiFarmers();
+            }
+            else if (NationName == "Arab Bedouin")
+            {
+                return new ArabBedouin();
+            }
+            else if (NationName == "Egyptian Kings")
+            {
+                return new EgyptianKings();
+            }
+            else if (NationName == "Inuit Hunters")
+            {
+                return new InuitHunters();
+            }
+            else
+            {
+                return new NullNation();
+            }
+        }
+    }
+}
