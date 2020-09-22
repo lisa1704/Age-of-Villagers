ff192a4bc6291df30e6e233758d2e7ee16e6a5ae
JAZZHAND1 <rotorblade52@outlook.com>
2020-09-22 16:18:56 +0600

    #11 Egyptian King House implemented

--- solutions/84-nawsad/Ageofvillagers/Nations/EgyptianKing.cs
+++ solutions/84-nawsad/Ageofvillagers/Nations/EgyptianKing.cs
@@ -19,7 +19,16 @@ namespace Ageofvillagers.Nations
       
         public void drawHouse(int x, int y)
         {
-            throw new NotImplementedException();
+            Line L = new Line(g,p,x,y,50,-80);
+            Line L1 = new Line(g,p,x,y,50,10);
+            Line L2 = new Line(g, p, x + 50, y - 80, x+50, y+10);
+            Line L3 = new Line(g,p,x+50,y-80,x+50+50,y-80+50);
+            Line L4 = new Line(g,p,x+50,y+10, x + 50 + 50, y - 80 + 50);
+            L.drawshape();
+            L1.drawshape();
+            L2.drawshape("Coordinate");
+            L3.drawshape("Coordinate");
+            L4.drawshape("Coordinate");
         }
 
         public void drawTree(int x, int y)
@@ -32,5 +41,7 @@ namespace Ageofvillagers.Nations
             Circle C = new Circle(g, p, x, y);
             C.drawshape();
         }
+
+
     }
 }
