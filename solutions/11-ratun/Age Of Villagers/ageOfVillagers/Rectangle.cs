7e629e4f8c4116d22d5a76d45532ee1b8d00b3de
Ratun <www.ratunrahman@gmail.com>
2020-09-22 14:00:15 +0600

    #1 fixed shapes

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Rectangle.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Rectangle.cs
@@ -4,22 +4,33 @@ namespace ageOfVillagers
 {
     internal class Rectangle
     {
+        Graphics g;
+        Pen p;
+        int x1, y1, x2, y2, x3, y3, x4, y4;
         //private string status = "";
         public Rectangle(Graphics g,Pen p,int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
         {
-            Line l1 = new Line(x1, y1, x2, y2);
-            l1.Draw(g,p);
-            Line l2 = new Line(x2, y2, x3, y3);
-            l2.Draw(g,p);
-            Line l3 = new Line(x3, y3, x4, y4);
-            l3.Draw(g,p);
-            Line l4 = new Line(x4, y4, x1, y1);
-            l4.Draw(g,p);
-            //status = l1.Draw() + l2.Draw() + l3.Draw()+ l4.Draw();
+            this.g = g;
+            this.p = p;
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+            this.x4 = x4;
+            this.y4 = y4;
         }
-        public string Draw()
+        public void Draw()
         {
-            return "Drawing a Rectangle";
+            Line l1 = new Line(x1, y1, x2, y2);
+            l1.Draw(g, p);
+            Line l2 = new Line(x2, y2, x3, y3);
+            l2.Draw(g, p);
+            Line l3 = new Line(x3, y3, x4, y4);
+            l3.Draw(g, p);
+            Line l4 = new Line(x4, y4, x1, y1);
+            l4.Draw(g, p);
         }
     }
 }
--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Triangle.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Triangle.cs
@@ -4,20 +4,28 @@ namespace ageOfVillagers
 {
     internal class Triangle
     {
-        private string status = "";
+        Graphics g;
+        Pen p;
+        int x1, y1, x2, y2, x3, y3;
         public Triangle(Graphics g, Pen p, int x1, int y1, int x2, int y2, int x3, int y3)
+        {
+            this.g = g;
+            this.p = p;
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+        }
+        public void Draw()
         {
             Line l1 = new Line(x1, y1, x2, y2);
             Line l2 = new Line(x2, y2, x3, y3);
             Line l3 = new Line(x3, y3, x1, y1);
-            l1.Draw(g,p);
-            l2.Draw(g,p);
-            l3.Draw(g,p);
-            //status = l1.Draw() + l2.Draw() + l3.Draw();
-        }
-        public string Draw()
-        {
-            return status;
+            l1.Draw(g, p);
+            l2.Draw(g, p);
+            l3.Draw(g, p);
         }
     }
 }
