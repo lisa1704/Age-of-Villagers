f0253b90ec30033320422fc1cd7cafb556c95d94
sharminnaj@iut-dhaka.edu <sharminnaj@iut-dhaka.edu>
2020-09-22 19:15:41 +0600

    #11 egyptian tree added

--- solutions/74-mou/AgeOfVillagers/EgyptianTree.cs
+++ solutions/74-mou/AgeOfVillagers/EgyptianTree.cs
@@ -7,17 +7,29 @@ namespace AgeOfVillagers
 {
     class EgyptianTree : CompositeShapes
     {
-        private Point p1, p2, p3, p4, p5;
+        private Point pl, pr,pl1,pl2, pl3, pr1, pr2, pr3;
         public EgyptianTree(Point p)
         {
-            this.p1 = p;
-            p2 = new Point(p1.X + 9, p1.Y + 25);
-            p3 = new Point(p2.X, p2.Y + 20);
-            p4 = new Point(p2.X + 5, p2.Y + 20);
-            p5 = new Point(p2.X + 5, p2.Y);
+            pl = new Point(p.X - 20, p.Y - 25);
+            pr = new Point(p.X + 20, p.Y - 25);
+            add_shapes(new Line(p, pl));
+            add_shapes(new Line(p, pr));
 
-            add_shapes(new Circle(p, 25));
-            add_shapes(new Rectangle(p2, p3, p4, p5));
+            pl1 = new Point(pl.X + 16, pl.Y - 12);
+            pl2 = new Point(pl.X - 16, pl.Y - 12);
+            pl3 = new Point(pl.X, pl.Y - 16);
+            add_shapes(new Line(pl, pl1));
+            add_shapes(new Line(pl, pl2));
+            add_shapes(new Line(pl, pl3));
+
+            pr1 = new Point(pr.X + 16, pr.Y - 12);
+            pr2 = new Point(pr.X - 16, pr.Y - 12);
+            pr3 = new Point(pr.X, pr.Y - 12);
+            add_shapes(new Line(pr, pr1));
+            add_shapes(new Line(pr, pr2));
+            add_shapes(new Line(pr, pr3));
+
+            
 
         }
     }
