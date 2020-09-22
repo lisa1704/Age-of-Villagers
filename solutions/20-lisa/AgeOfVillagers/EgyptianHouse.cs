bbe22f53df354177170df01f72330f544def2a54
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-22 20:33:09 +0600

    #11 Egyptian Kings House implemented

--- solutions/20-lisa/AgeOfVillagers/EgyptianHouse.cs
+++ solutions/20-lisa/AgeOfVillagers/EgyptianHouse.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    class EgyptianHouse
+    public class EgyptianHouse : CompositeShape
     {
+        public EgyptianHouse(Point point)
+        {
+            Point p1 = new Point(point.X - 20, point.Y + 50);
+            Point p2 = new Point(point.X + 25, point.Y + 55);
+            Point p3 = new Point(point.X + 35, point.Y + 30);
+            AddComponent(new Triangle(p2, p1, point));
+            AddComponent(new Triangle(p3, p2, point));
+        }
     }
 }
