e8c6c1c13bd44ac44e12c54c1b6ee9c01360df2f
tiashaa <hriditatiasha@gmail.com>
2020-09-22 16:47:46 +0600

    #1 #10 refactored for Arab

--- solutions/41-tiasha/Age of Villagers/Nation/Arab.cs
+++ solutions/41-tiasha/Age of Villagers/Nation/Arab.cs
@@ -5,8 +5,39 @@ using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    public class Arab
+    public class Arab : Nation
     {
-       
+        public Arab()
+        {
+            this.hasTree = true;
+            this.hasWaterSource = false;
+        }
+        public override void BackgoundColor(Graphics g)
+        {
+            g.Clear(Color.LightYellow);
+            Debug.WriteLine("Yellowish");
+        }
+
+        public override void DrawHouse(Graphics g, Point location)
+        {
+            ArabianHouse arabianHouse = new ArabianHouse(location, houseSize);
+            arabianHouse.Draw(g);
+
+            Debug.WriteLine("Drawing Arab House");
+        }
+
+        public override void DrawTree(Graphics g, Point location)
+        {
+            ArabianTree arabianTree = new ArabianTree(location, treeSize);
+            arabianTree.Draw(g);
+
+            Debug.WriteLine("Drawing Arab Tree");
+        }
+
+        
+        public override void DrawWaterResource(Graphics g, Point location)
+        {
+
+        }
     }
 }
