f0e927ab2680f08e9c0533d9e388447cb954df2f
Nabil Hossain Nico <hossaintalukder108@gmail.com>
2020-09-22 14:57:18 +0600

    #8 function added for INation

--- solutions/76-nico/AgeOfVillagers/AgeOfVillagers/Ination.cs
+++ solutions/76-nico/AgeOfVillagers/AgeOfVillagers/Ination.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace AgeOfVillagers
 {
-    class INation
+    public interface INation
     {
+        void draw_house();
+        void draw_tree();
+        void draw_watersource();
     }
 }
