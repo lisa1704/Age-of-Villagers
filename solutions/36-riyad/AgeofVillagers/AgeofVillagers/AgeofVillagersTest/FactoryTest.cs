b78b509ee3ef847abed8c8cc9c97779b912d7f86
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-23 00:58:07 +0600

    #1 #7 #9 UnitTest BangladeshiFarmers Nation

--- solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagersTest/FactoryTest.cs
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagersTest/FactoryTest.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 using AgeofVillagers;
+using Xunit;
 
 namespace AgeofVillagersTest
 {
     public class FactoryTest
     {
-        
-       
+        [Fact]
+        public void BdNationFactory()
+        {
+            FactoryPattern nationFactory = new FactoryPattern();
+            var actual = nationFactory.GetNation("BangladeshiFarmers");
+            var expected = typeof(BangladeshiFarmers);
+            Assert.IsType(expected, actual);
+
+        }
     }
 }
