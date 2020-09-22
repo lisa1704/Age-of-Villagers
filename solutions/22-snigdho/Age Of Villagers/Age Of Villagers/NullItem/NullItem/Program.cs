79b6bde7aa4066b22aee991310da9c552f1e6e6d
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-22 22:36:29 +0600

    #1 #8 Null Item implemented

---
+++ solutions/22-snigdho/Age Of Villagers/Age Of Villagers/NullItem/NullItem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using System.Windows.Form1;
+
+namespace AgeOfVillagers
+{
+    public class NullItems : Composite
+    {
+        public void drawing()
+        {
+            MessageBox.Show("NULL ITEMS");
+        }
+    }
+}
