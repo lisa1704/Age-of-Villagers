b3be4d0749d46b528471c721b87da67f0950d5cb
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-22 17:08:59 +0600

    #5 class initialized and made some changes

--- solutions/55-wasif/age-of-villagers/Save_village.cs
+++ solutions/55-wasif/age-of-villagers/Save_village.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace age_of_villagers
 {
     class Save_village
     {
+        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+        saveFileDialog1.DefaultExt = "txt";      
+        saveFileDialog1.Filter = "AOV (*.aov)|*.txt";      
+        saveFileDialog1.FilterIndex = 1;      
+        saveFileDialog1.RestoreDirectory = true;      
+        if (saveFileDialog1.ShowDialog() == DialogResult.OK) {      
+            textBox1.Text = saveFileDialog1.FileName;
 
     }
 }
