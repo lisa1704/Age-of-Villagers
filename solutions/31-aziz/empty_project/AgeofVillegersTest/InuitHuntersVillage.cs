d115a13338e6b42e6702252fc5cf37b3ec806629
Asif Aziz <asifaziz2061@gmail.com>
2020-09-23 00:26:54 +0600

    #12 InuitHuntersVillage class added to test Inuit Hunters village

---
+++ solutions/31-aziz/empty_project/AgeofVillegersTest/InuitHuntersVillage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using empty_project;
+using Xunit;
+using System.Windows.Forms;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace AgeofVillegersTest
+{
+    class InuitHuntersVillage
+    {
+    }
+}
