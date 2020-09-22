d790eaf758f6fef068c6b454e624f8ef11e9360e
Tasnuva12 <47001609+Tasnuva12@users.noreply.github.com>
2020-09-23 00:19:41 +0600

    #8 Factory_Nation class is completed

--- solutions/37-tasnuva/AgeOfVillegers/Factory_Nation.cs
+++ solutions/37-tasnuva/AgeOfVillegers/Factory_Nation.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace AgeOfVillegers 
 {
-	class Factory_Nation
+	public class Factory_Nation
 	{
+		public INation GetNation(string type)
+        {
+			if(type=="Bangladeshi Farmers")
+            {
+				return new Bangladehsi_farmers() ;
+            }
+			else if (type=="Egyptian Kings")
+            {
+				return new Egyptian_kings();
+            }
+			else if (type=="Inuit Hunters")
+            {
+				return new Inuit_hunters();
+            }
+			else if(type== "Arab Bedouins")
+            {
+				return new Arab_bedouins();
+
+			}
+			else
+            {
+				return new NullNation();
+            }
+
+        }
 	}
 }
