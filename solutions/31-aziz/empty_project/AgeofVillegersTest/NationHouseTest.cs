57f5ee833ba116110a710b979182f849871974f0
Asif Aziz <asifaziz2061@gmail.com>
2020-09-22 22:10:47 +0600

    #9 #10 #11 #12 NationHouseTest class added

---
+++ solutions/31-aziz/empty_project/AgeofVillegersTest/NationHouseTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using empty_project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using System.Windows.Forms;
+
+namespace AgeofVillegersTest
+{
+    public class NationHouseTest
+    {
+        [Fact]
+        public void HouseTest()
+        {
+            
+        }
+    }
+}
