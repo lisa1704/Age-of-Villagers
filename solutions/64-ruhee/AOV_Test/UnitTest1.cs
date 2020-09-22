6ffa08685536ac3038ca7f66d2765daf2541ef9b
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-23 02:20:17 +0600

    #1 solved testing error.

---
+++ solutions/64-ruhee/AOV_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace AOV_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+        }
+
+
+    }
+}
