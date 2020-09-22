2847aff8a54f1446c3189e62a89a3981f2ccc604
Ratun <www.ratunrahman@gmail.com>
2020-09-22 19:15:56 +0600

    #1 #12 fixed Inuit House class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/InuitHouse.cs
@@ -4,11 +4,12 @@ namespace ageOfVillagers
 {
     internal class InuitHouse : IShape
     {
+        Pen p = new Pen(Color.Pink);
         public string draw(int x, int y, Graphics g)
         {
-            HalfCircle h1 = new HalfCircle(x, y, 8);
-            HalfCircle h2 = new HalfCircle(x, y, 2);
-            return h1.Draw() + h2.Draw();
+            HalfCircle h1 = new HalfCircle(g,p,x, y, 8);
+            HalfCircle h2 = new HalfCircle(g,p,x, y, 2);
+            return "Drawing Inuit House";
         }
     }
 }
