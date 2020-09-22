30ac08f293a65b0a56bc496a251272eb499c423c
Raihan Evan <raihan.evan@gmail.com>
2020-09-22 22:30:40 +0600

    #7 Rectangle class added with properties

--- solutions/28-evan/AgeOfVillagers/Rectangle.cs
+++ solutions/28-evan/AgeOfVillagers/Rectangle.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgeOfVillagers
 {
-    class Rectangle
+    class Rectangle : IShape
     {
+        Point w, x, y, z;
+        Pen p;
+        Graphics g;
 
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rectangle(Graphics g, Pen p, Point w, Point x, Point y, Point z)
+        {
+            this.w = w;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.g = g;
+            this.p = p;
+        }
+
+        public void DrawShape(Graphics g, Pen p)
+        {
+            g.DrawLine(p, w, x);
+            g.DrawLine(p, w, y);
+            g.DrawLine(p, y, z);
+            g.DrawLine(p, x, y);
+        }
     }
 }
