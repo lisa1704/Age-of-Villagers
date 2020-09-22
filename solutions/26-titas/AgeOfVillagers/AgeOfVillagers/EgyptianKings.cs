1f39fe692879e0c84f44dfb1e2e659cf66d733a7
raihantitas <aburaihan@iut-dhaka.edu>
2020-09-23 02:51:16 +0600

    #11 Egyptian kings tree painted

--- solutions/26-titas/AgeOfVillagers/AgeOfVillagers/EgyptianKings.cs
+++ solutions/26-titas/AgeOfVillagers/AgeOfVillagers/EgyptianKings.cs
@@ -28,7 +28,25 @@ namespace AgeOfVillagers
 
         public void paint_tree(MouseEventArgs e, Graphics g, Pen p)
         {
-            throw new NotImplementedException();
+            int x, y;
+            x = e.X;
+            y = e.Y;
+            Point pt1 = new Point(x, y);
+            Point pt2 = new Point(x - 2, y - 24);
+            Point pt3 = new Point(x - 5, y - 22);
+            Point pt4 = new Point(x - 8, y - 20);
+            Point pt5 = new Point(x + 2, y - 24);
+            Point pt6 = new Point(x + 5, y - 23);
+            Point pt7 = new Point(x + 8, y - 23);
+            Point pt8 = new Point(x + 3, y - 16);
+            Point pt9 = new Point(x - 3, y - 16);
+            g.DrawLine(p, pt1, pt3);
+            g.DrawLine(p, pt1, pt6);
+            g.DrawLine(p, pt8, pt5);
+            g.DrawLine(p, pt8, pt7);
+            g.DrawLine(p, pt9, pt2);
+            g.DrawLine(p, pt9, pt4);
+            
         }
 
         public void paint_waterSource(MouseEventArgs e, Graphics g, Pen p)
@@ -39,4 +57,7 @@ namespace AgeOfVillagers
             g.DrawArc(p, x - 6, y - 6, 12, 12, 0, 360);
         }
     }
+
+       
+    
 }
