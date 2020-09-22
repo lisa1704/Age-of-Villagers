51b9517d677bb2998006b42704331842966b8a3f
Nabil Hossain Nico <hossaintalukder108@gmail.com>
2020-09-23 02:33:50 +0600

    #8 #9Bangladesh Farmers implementing Ination created

---
+++ solutions/76-nico/AgeOfVillagers/AgeOfVillagers/BangladeshFarmers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    public class BangladeshFarmers : INation
+    {
+        public void draw_house()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void draw_tree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void draw_watersource()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
