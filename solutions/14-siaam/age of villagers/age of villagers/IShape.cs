9dd1c6f88798e99c81c928510ade2f8175281d12
Istiaq Siaam <i.b.s.siaam@gmail.com>
2020-09-22 11:58:02 +0600

    #7 triangle shape implemented

--- solutions/14-siaam/age of villagers/age of villagers/IShape.cs
+++ solutions/14-siaam/age of villagers/age of villagers/IShape.cs
@@ -44,9 +44,20 @@ namespace age_of_villagers
 
     class Triangle : IShape
     {
+        Point p1,p2, p3;
+
+        public Triangle(Point p1, Point p2, Point p3)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+        }
+
         public void draw(Graphics g, Pen p)
         {
-            throw new NotImplementedException();
+            g.DrawLine(p, p1, p2);
+            g.DrawLine(p, p2, p3);
+            g.DrawLine(p, p3, p1);
         }
     }
 
