660daa35195075bd3b3bcda0ad05c8df5fa781ab
Ratun <www.ratunrahman@gmail.com>
2020-09-22 16:52:41 +0600

    #10 create pen in Arab House

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabHouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/ArabHouse.cs
@@ -4,11 +4,14 @@ namespace ageOfVillagers
 {
     internal class ArabHouse : IShape
     {
+        Pen p = new Pen(Color.LightYellow);
         public string draw(int x, int y, Graphics g)
         {
-            Triangle t = new Triangle(x - 2, y + 6, x - 8, y - 8, x + 2, y - 8);
-            Rectangle r = new Rectangle(x - 2, y + 6, x + 2, y - 8, x + 8, y - 6, x + 1, y + 8);
-            return t.Draw() + r.Draw();
+            Triangle t = new Triangle(g,p,x - 2, y + 6, x - 8, y - 8, x + 2, y - 8);
+            t.Draw();
+            Rectangle r = new Rectangle(g,p,x - 2, y + 6, x + 2, y - 8, x + 8, y - 6, x + 1, y + 8);
+            r.Draw();
+            return "Drawing Arab House";
         }
     }
 }
