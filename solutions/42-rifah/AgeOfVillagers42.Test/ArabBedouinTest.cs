7de0277fd6536365d8b55a22adece419fb636fcb
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-23 04:53:47 +0600

    #15 #10 #12 Test classes created and BG color tested of Arab Bedouin and Inuit Hunters

---
+++ solutions/42-rifah/AgeOfVillagers42.Test/ArabBedouinTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AgeOfVillagers.Arab_Bedouin;
+using System.Drawing;
+
+namespace AgeOfVillagers42.Test
+{
+    [TestClass]
+    public class ArabBedouinTest
+    {
+        [TestMethod]
+        public void BackgroundColorTest()
+        {
+            ArabBedouin arabBedouinBackgroundColor = new ArabBedouin();
+            Color actual = arabBedouinBackgroundColor.BackgroundColor();
+            Color expected = Color.GreenYellow;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ solutions/42-rifah/AgeOfVillagers42.Test/InuitHuntersTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AgeOfVillagers.Inuit_Hunters;
+using System.Drawing;
+
+namespace AgeOfVillagers42.Test
+{
+    [TestClass]
+    public class InuitHuntersTest
+    {
+        [TestMethod]
+        public void BackgroundColorTest()
+        {
+            InuitHunters inuitHuntersBackgroundColor = new InuitHunters();
+            Color actual = inuitHuntersBackgroundColor.BackgroundColor();
+            Color expected = Color.White;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
