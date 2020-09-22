dd2d7c5c4318c08459930f8003e7991f21b68033
tiashaa <hriditatiasha@gmail.com>
2020-09-22 16:24:58 +0600

    #1 #11 refactored egyptian nation

--- solutions/41-tiasha/Age of Villagers/Nation/Egyptian.cs
+++ solutions/41-tiasha/Age of Villagers/Nation/Egyptian.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Age_of_Villagers
 {
-    class Egyptian
+    public class Egyptian : Nation
     {
+        public Egyptian()
+        {
+            this.hasTree = true;
+            this.hasWaterSource = true;
+            this.waterSourceSize = new ElementSize(12, 12);
+        }
+        public override void BackgoundColor(Graphics g)
+        {
+            g.Clear(Color.Yellow);
+            Debug.WriteLine("Yellow");
+        }
+
+        public override void DrawHouse(Graphics g, Point location)
+        {
+            EgyptianHouse egyptianHouse = new EgyptianHouse(location, houseSize);
+            egyptianHouse.Draw(g);
+            Debug.WriteLine("Drawing Egyptian House");
+        }
+
+        public override void DrawTree(Graphics g, Point location)
+        {
+            EgyptianTree egyptianTree = new EgyptianTree(location, treeSize);
+
+            egyptianTree.Draw(g);
+
+            Debug.WriteLine("Drawing Egyptian Tree");
+        }
+
+        public override void DrawWaterResource(Graphics g, Point location)
+        {
+            EgyptianWaterSource egyptianWaterSource = new EgyptianWaterSource(location, waterSourceSize);
+            egyptianWaterSource.Draw(g);
+
+            Debug.WriteLine("Drawing Egyptian Water Source");
+        }
     }
 }
--- solutions/41-tiasha/Age of Villagers/Nation/NationCreator.cs
+++ solutions/41-tiasha/Age of Villagers/Nation/NationCreator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AgeOfVillagers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace Age_of_Villagers
 {
     public class NationCreator
     {
-        
+       
     }
 }
