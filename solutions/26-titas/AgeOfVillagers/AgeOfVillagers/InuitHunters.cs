1d215472cdf3aa6bf2ca7833ea48a7202d38a75c
raihantitas <aburaihan@iut-dhaka.edu>
2020-09-23 02:00:59 +0600

    #12 InuitHunter Class added

---
+++ solutions/26-titas/AgeOfVillagers/AgeOfVillagers/InuitHunters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
+{
+    class InuitHunters : INation
+    {
+        public void paint_house(MouseEventArgs e, Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void paint_tree(MouseEventArgs e, Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void paint_waterSource(MouseEventArgs e, Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
