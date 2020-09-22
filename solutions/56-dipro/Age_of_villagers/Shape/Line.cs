8182824b4bc6b16b2752aaa72468cc846b50a329
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 00:14:25 +0600

    #7 line shape created

---
+++ solutions/56-dipro/Age_of_villagers/Shape/Line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Age_of_villagers.Shape
+{
+    class Line : IShape 
+    {
+        private Point p1, p2;
+        public Line(Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+        public void draw(Graphics g, Pen p)
+        {
+            g.DrawLine(p, p1, p2);
+        }
+    }
+}
