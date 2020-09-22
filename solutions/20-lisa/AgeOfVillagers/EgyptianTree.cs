38d24bf0df31151f74cf1044c47980a418b4a12c
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-22 21:21:52 +0600

    #1 #7 #11 Egyptian Kings all shapes functionality works

--- solutions/20-lisa/AgeOfVillagers/EgyptianTree.cs
+++ solutions/20-lisa/AgeOfVillagers/EgyptianTree.cs
@@ -9,21 +9,23 @@ namespace AgeOfVillagers
     {
         public EgyptianTree(Point point)
         {
-            Point p1 = new Point(point.X + 30, point.Y - 45);
-            Point p2 = new Point(point.X - 10, point.Y - 44);
-            Point p3 = new Point(point.X + 14, point.Y - 20);
-            Point p4 = new Point(point.X + 30, point.Y - 45);
-            Point p5 = new Point(point.X + 15, point.Y - 45);
-            Point p6 = new Point(point.X + 15, point.Y - 20);
-            Point p7 = new Point(point.X + 10, point.Y - 45);
-            Point p8 = new Point(point.X + 35, point.Y - 50);
+            Point p1 = new Point(point.X - 20, point.Y - 25);
+            Point p2 = new Point(point.X + 20, point.Y - 25);
+            Point p3 = new Point(p1.X + 15, p1.Y - 12);
+            Point p4 = new Point(p1.X - 15, p1.Y - 12);
+            Point p5 = new Point(p1.X, p1.Y - 15);
+            Point p6 = new Point(p2.X + 15, p2.Y - 12);
+            Point p7 = new Point(p2.X - 15, p2.Y - 12);
+            Point p8 = new Point(p2.X , p2.Y - 12);
 
             AddComponent(new Line(point, p1));
             AddComponent(new Line(point, p2));
-            AddComponent(new Line(p3, p4));
-            AddComponent(new Line(p3, p5));
-            AddComponent(new Line(p6, p7));
-            AddComponent(new Line(p6, p8));
+            AddComponent(new Line(p1, p3));
+            AddComponent(new Line(p1, p4));
+            AddComponent(new Line(p1, p5));
+            AddComponent(new Line(p2, p6));
+            AddComponent(new Line(p2, p7));
+            AddComponent(new Line(p2, p8));
 
         }
     }
