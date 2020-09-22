aeeb2ab148a720ecd0669cd21e565c40b1ac6c16
akib <akibahmed@iut-dhaka.edu>
2020-09-22 13:23:45 +0600

    #2 #7 Returned rectangle with 4 lines

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Rectangle.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Rectangle.cs
@@ -16,7 +16,12 @@
         }
         public string Draw_Shape()
         {
-            return "Drawing a Rectangle...";
+            Lines line1 = new Lines(x1, y1, x2, y2);
+            Lines line2 = new Lines(x2, y2, x3, y3);
+            Lines line3 = new Lines(x3, y3, x4, y4);
+            Lines line4 = new Lines(x4, y4, x1, y1);
+
+            return line1.Draw_Shape() + line2.Draw_Shape() + line3.Draw_Shape() + line4.Draw_Shape();
         }
     }
 }
