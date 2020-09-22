7df63bef79c6c0f1c7c01f894ef623b4fa1eea14
tiashaa <hriditatiasha@gmail.com>
2020-09-22 16:57:02 +0600

    #1 #10 Arab nation created annd refactored

--- solutions/41-tiasha/Age of Villagers/Nation/NationCreator.cs
+++ solutions/41-tiasha/Age of Villagers/Nation/NationCreator.cs
@@ -7,6 +7,18 @@ namespace Age_of_Villagers
 {
     public class NationCreator
     {
-       
+        public INation GetNation(String nationType)
+        {
+            if (nationType == "Bangladeshi Farmers")
+                return new Bangladeshi();
+            else if (nationType == "Arab Bedouin")
+                return new Arab();
+            else if (nationType == "Egyptian Kings")
+                return new Egyptian();
+            else if (nationType == "Inuit Hunters")
+                return new Inuit();
+
+            return null;
+        }
     }
 }
