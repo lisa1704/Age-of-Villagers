31fb7c1992165804e532bf2c4149018ab393f157
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-22 11:24:45 +0600

    #7 #12 Nultree added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/NulTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class NulTree : CompositeShape
+    {
+        public NulTree()
+        {
+
+        }
+
+        internal void Paint(Graphics g, Pen p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
