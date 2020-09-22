9d0d0e731881fd37d82118ca4796c6fb64da7aae
sharminnaj@iut-dhaka.edu <sharminnaj@iut-dhaka.edu>
2020-09-22 23:11:02 +0600

    #10 Arab house added

--- solutions/74-mou/AgeOfVillagers/ArabHouse.cs
+++ solutions/74-mou/AgeOfVillagers/ArabHouse.cs
@@ -7,18 +7,19 @@ namespace AgeOfVillagers
 {
     class ArabHouse : CompositeShapes
     {
-        private Point p1, p2, p3, p4;
+        private Point p1, p2, p3, p4, p5;
 
 
         public ArabHouse(Point p)
         {
-            this.p1 = p;
-            p2 = new Point(p1.X - 10, p1.Y + 10);
-            p3 = new Point(p1.X + 10, p1.Y + 20);
-            p4 = new Point(p3.X, p2.Y);
+            p1 = p;
+            p2 = new Point(p1.X - 20, p1.Y + 20);
+            p3 = new Point(p1.X + 20, p2.Y);
+            p4 = new Point(p3.X + 15, p3.Y - 12);
+            p5 = new Point(p1.X + 15, p1.Y - 12);
 
             add_shapes(new Triangle(p1, p2, p3));
-            add_shapes(new Rectangle(p1, p2, p3, p4));
+            add_shapes(new Rectangle(p1, p3, p4, p5));
 
         }
     }
