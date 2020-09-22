318587efbac74b12a819ac0d81caa33b6302455f
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-09-22 21:56:13 +0600

    #8 NullNation added

---
+++ solutions/36-riyad/AgeofVillagers/AgeofVillagers/AgeofVillagers/NullNation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeofVillagers
+{
+    public class NullNation : INation
+    {
+        public string DrawHouse(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWaterSource(Graphics g, Point mid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color TerrainColor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
