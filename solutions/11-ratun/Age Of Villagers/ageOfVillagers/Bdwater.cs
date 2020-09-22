8e1fca959c8b7739e63907aa715c5b580b68020b
Ratun <www.ratunrahman@gmail.com>
2020-09-22 14:01:46 +0600

    #9 add graphics on Bangladesh Water

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Bdwater.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Bdwater.cs
@@ -6,27 +6,27 @@ namespace ageOfVillagers
     {
         private int xLength = 24;
         private int yLength = 16;
-        Pen pen = new Pen(Color.Green);
+        Pen p = new Pen(Color.Green);
         public string draw(int x, int y, Graphics g)
         {
             Line l1 = new Line(x - 12, y, x - 6, y + 6);
-            l1.Draw();
+            l1.Draw(g,p);
             Line l2 = new Line(x - 6, y + 6, x - 2, y + 7);
-            l2.Draw();
+            l2.Draw(g,p);
             Line l3 = new Line(x - 2, y + 7, x - 1, y + 3);
-            l3.Draw();
+            l3.Draw(g,p);
             Line l4 = new Line(x - 1, y + 3, x + 6, y + 8);
-            l4.Draw();
+            l4.Draw(g,p);
             Line l5 = new Line(x + 6, y + 8, x + 12, y + 2);
-            l5.Draw();
+            l5.Draw(g,p);
             Line l6 = new Line(x + 12, y + 2, x + 5, y - 7);
-            l6.Draw();
+            l6.Draw(g,p);
             Line l7 = new Line(x + 5, y - 7, x - 1, y - 2);
-            l7.Draw();
+            l7.Draw(g,p);
             Line l8 = new Line(x - 1, y - 2, x - 6, y + 8);
-            l8.Draw();
+            l8.Draw(g,p);
             Line l9 = new Line(x - 6, y + 8, x - 12, y);
-            l9.Draw();
+            l9.Draw(g,p);
             return "Drawing BD water";
         }
     }
