cc888dc0ab9bc88e2f2964e7c686050bd87ffc13
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 02:07:27 +0600

    #10 Class for ArabBedouinHouse created.

---
+++ solutions/53-silvi/AgeOfVillagers/ArabBedouinHouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class ArabBedouinHouse : IVillageItem
+    {
+        public void Draw(Point e, Graphics g, Pen p)
+        {
+            
+        }
+    }
