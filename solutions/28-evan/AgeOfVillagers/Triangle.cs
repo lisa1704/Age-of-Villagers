e55dc5479403d96ee0d8fe0d7212ffd352f10ee7
Raihan Evan <raihan.evan@gmail.com>
2020-09-22 22:27:07 +0600

    #7 Triangle class added with properties

--- solutions/28-evan/AgeOfVillagers/Triangle.cs
+++ solutions/28-evan/AgeOfVillagers/Triangle.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgeOfVillagers
 {
-    class Triangle
+    class Triangle : IShape
     {
+        Point x, y, z;
+        Pen p;
+        Graphics g;
 
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+        public Triangle(Graphics g, Pen p, Point x, Point y,Point z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.g = g;
+            this.p = p;
+        }
+
+        public void DrawShape(Graphics g, Pen p)
+        {
+            g.DrawLine(p, x, y);
+            g.DrawLine(p, x, z);
+            g.DrawLine(p, y, y);
+        }
     }
 }
