4f1fb4f4a0bb770996413ad0d1d87626f5f6aa15
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-22 21:53:22 +0600

    #5 New Class is added to save the condition

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/VillageCondition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    class VillageCondition
+    {
+        public string name;
+        public List<Point> HousePoint, TreePoint, WaterResourcePoint;
+
+        public VillageCondition(string name, List<Point> HousePoint, List<Point> TreePoint, List<Point> WaterResourcePoint)
+        {
+            this.name = name;
+            this.HousePoint = HousePoint;
+            this.TreePoint = TreePoint;
+            this.WaterResourcePoint = WaterResourcePoint;
+        }
+    }
+}
