08b1215731c9c5598dadbf74d57a487fe3823364
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-22 21:12:37 +0600

    #7 village class created with housem, tree and water source points

--- solutions/55-wasif/age-of-villagers/Village.cs
+++ solutions/55-wasif/age-of-villagers/Village.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace age_of_villagers
 {
     class Village
     {
+        public string village_name;
+        public List<Point> housepoint, treepoint, waterpoint;
+
+        public Village()
+        {
+            housepoint = new List<Point>();
+            treepoint = new List<Point>();
+            waterpoint = new List<Point>();
+        }
+
+        public string getName()
+        {
+            return village_name;
+        }
+
+        public void setName(String villageName)
+        {
+            village_name = villageName;
+        }
     }
 }
+
