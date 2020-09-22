a1e28283a88af4bed6eca31815ca22d22608355e
Ratun <www.ratunrahman@gmail.com>
2020-09-22 13:02:24 +0600

    #1 add pen to triangle

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Triangle.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Triangle.cs
@@ -1,9 +1,11 @@
-﻿namespace ageOfVillagers
+﻿using System.Drawing;
+
+namespace ageOfVillagers
 {
     internal class Triangle
     {
         private string status = "";
-        public Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
+        public Triangle(Graphics g, Pen p, int x1, int y1, int x2, int y2, int x3, int y3)
         {
             Line l1 = new Line(x1, y1, x2, y2);
             Line l2 = new Line(x2, y2, x3, y3);
