a7bfc8a24e43cf9c47e8749f01f78120fe4767e8
Tasnuva12 <47001609+Tasnuva12@users.noreply.github.com>
2020-09-22 21:17:48 +0600

    #11#12Egyptian_kings class is created , Inuit_hunters  class is committed

--- solutions/37-tasnuva/AgeOfVillegers/Egyptian_kings.cs
+++ solutions/37-tasnuva/AgeOfVillegers/Egyptian_kings.cs
@@ -5,10 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace AgeOfVillegers   
-
-
+namespace AgeOfVillegers
 {
-    class Inuit_hunters : INations
+	class Egyptian_kings : INation
+	{
+
+	}
 }
