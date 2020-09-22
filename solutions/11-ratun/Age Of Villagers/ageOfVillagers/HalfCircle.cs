18033708720f2f75f7a582728d6a5cb4fa99d2ce
Ratun <www.ratunrahman@gmail.com>
2020-09-22 17:39:27 +0600

    #2 #7 Half circle with arc

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/HalfCircle.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/HalfCircle.cs
@@ -1,17 +1,28 @@
-﻿namespace ageOfVillagers
+﻿using System.Drawing;
+
+namespace ageOfVillagers
 {
     internal class HalfCircle
     {
-        private int x, y, radius;
-        public HalfCircle(int x, int y, int radius)
+        private int x, y, radius, XupperLeft, YupperLeft, height, width;
+        Graphics g;
+        Pen p;
+        public HalfCircle(Graphics g, Pen p,int x, int y, int radius)
         {
+            this.g = g;
+            this.p = p;
             this.x = x;
             this.y = y;
             this.radius = radius;
         }
-        public string Draw()
+        public void Draw()
         {
-            return "Drawing a half circle. ";
+            XupperLeft = x - radius;
+            YupperLeft = y - (radius/2);
+            height = 2 * radius;
+            width = 2 * radius;
+            Arc a = new Arc(g, p, XupperLeft, YupperLeft, height, width, 0, 180);
+            a.Draw();
         }
     }
 }
