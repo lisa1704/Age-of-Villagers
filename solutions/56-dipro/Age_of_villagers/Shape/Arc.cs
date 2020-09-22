e8f9971139cdca09b80d0471e23eba5af65cbc11
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 01:08:39 +0600

    #7 arc shape

---
+++ solutions/56-dipro/Age_of_villagers/Shape/Arc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace Age_of_villagers.Shape
+{
+    class Arc : IShape
+    {
+        private readonly Point point;
+        private float x, y, w, h, sA, swA;
+        public Arc(Point topLeft, Point bottomRight)
+        {
+            x = topLeft.X;
+            y = topLeft.Y;
+            w = bottomRight.X - topLeft.X;
+            h = bottomRight.Y - topLeft.Y;
+            sA = 0;
+            swA = 180;
+        }
+        public void draw(Graphics g, Pen p)
+        {
+            g.DrawArc(p, x, y, w, h, sA, swA);
+        }
+    }
+}
