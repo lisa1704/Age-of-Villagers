114b7d3eba68f0a323dcb273e23d1315c818d01e
Fatema Zohra Prottyasha <fatemazohra@iut-dhaka.edu>
2020-09-22 06:48:38 +0600

    error corrected

--- solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Village/Village.cs
+++ solutions/39-prottyasha/AgeOfVillagers/AgeOfVillagers/Village/Village.cs
@@ -6,9 +6,9 @@ namespace AgeOfVillagers
     public class Village
     {
         public string village_name;
-        public List<Point> trees_drawn;
-        public List<Point> houses_drawn;
-        public List<Point> rivers_drawn;
+        public List<Point> trees_drawn = new List<Point>();
+        public List<Point> houses_drawn = new List<Point>();
+        public List<Point> rivers_drawn = new List<Point>();
         public string nation;
     }
 }
