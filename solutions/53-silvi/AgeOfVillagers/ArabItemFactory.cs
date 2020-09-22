ad5a563f958a8e20f6fec1e68879819cdb0fed78
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 01:52:16 +0600

    #10 Factory for Arab bedouin nation created.

---
+++ solutions/53-silvi/AgeOfVillagers/ArabItemFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class ArabItemFactory : ItemFactory
+    {
+        public override IVillageItem getItem(string button)
+        {
+           
+        }
+    }
+}
