9d11c831e2dbb355138028dac229a50877e97475
66-Nowshad <nowshadulislam@iut-dhaka.edu>
2020-09-22 23:14:23 +0600

    #7  IShape created

---
+++ solutions/66-Nowshad/IShape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers.Age_of_Villagers
+{
+    public interface IShape
+    {
+        void Draw(Graphics g);
+    }
+}
