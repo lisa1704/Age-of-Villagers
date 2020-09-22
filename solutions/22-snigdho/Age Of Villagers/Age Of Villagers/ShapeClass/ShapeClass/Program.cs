ca4063f18e037ac18a2cd22fa54c8c36ad46a3dd
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-22 21:49:22 +0600

    #1 #7 Shape class created

---
+++ solutions/22-snigdho/Age Of Villagers/Age Of Villagers/ShapeClass/ShapeClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public interface IShape
+    {
+        void Draw(Graphics g);
+    }
+}
