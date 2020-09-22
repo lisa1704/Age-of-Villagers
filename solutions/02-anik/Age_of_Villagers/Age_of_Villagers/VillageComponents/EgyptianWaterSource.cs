a0b28f4f25e1a624f29d1177bd31558a8dce17d1
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2020-09-22 19:08:29 +0600

    #9 EgyptianWaterHouse created and implemented

--- solutions/02-anik/Age_of_Villagers/Age_of_Villagers/VillageComponents/EgyptianWaterSource.cs
+++ solutions/02-anik/Age_of_Villagers/Age_of_Villagers/VillageComponents/EgyptianWaterSource.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Age_of_Villagers.Shape;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Age_of_Villagers.VillageComponents
 {
-    class EgyptianWaterSource
+    class EgyptianWaterSource : VillageComponents
     {
+        private readonly Point topLeft, bottomRight;
 
+        public EgyptianWaterSource(Point point)
+        {
+            topLeft = new Point(point.X - 6, point.Y - 6);
+            bottomRight = new Point(point.X + 6, point.Y + 6);
+            addComponent(new Arc(topLeft, bottomRight));
+        }
+
+     
     }
 }
