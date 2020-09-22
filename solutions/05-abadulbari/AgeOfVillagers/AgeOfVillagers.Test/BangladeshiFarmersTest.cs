a74502da8e99ee1617e38229116d5b68269e6492
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-23 02:11:55 +0600

    #9 Nation name test

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers.Test/BangladeshiFarmersTest.cs
@@ -0,0 +1,21 @@
+﻿using AgeOfVillagers.Factories;
+using AgeOfVillagers.Nation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AgeOfVillagers.Test
+{
+    public class BangladeshiFarmersTest
+    {
+        INation bd = new NationFactory("Bangladeshi Farmers").getNation();
+
+        [Fact]
+        public void NationNameTest()
+        {
+            var expected = "Bangladeshi Farmers";
+            Assert.Equal(expected, bd.getNationName());
+        }
+    }
+}
