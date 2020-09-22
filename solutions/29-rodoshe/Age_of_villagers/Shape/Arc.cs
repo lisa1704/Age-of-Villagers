e8f393277daa9dcf4aeee2dccd41df980f8dfec7
afiarodoshe <afiamuntakim@iut-dhaka.edu>
2020-09-22 17:00:15 +0600

    #7 arc class implemented

--- solutions/29-rodoshe/Age_of_villagers/Shape/Arc.cs
+++ solutions/29-rodoshe/Age_of_villagers/Shape/Arc.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers.Shape
+{
+    class Arc : IShape
+    {
+        private readonly Point point;
+        private float x, y, width, height, startAngle, sweepAngle;
+
+        public Arc(Point topLeft, Point bottomRight)
+        {
+            x = topLeft.X;
+            y = topLeft.Y;
+            width = bottomRight.X - topLeft.X;
+            height = bottomRight.Y - topLeft.Y;
+            startAngle = 0;
+            sweepAngle = 180;
+        }
+        public void Draw(Graphics g, Pen p)
+        {
+            g.DrawArc(p, x, y, width, height, startAngle, sweepAngle);
+        }
+    }
+}
