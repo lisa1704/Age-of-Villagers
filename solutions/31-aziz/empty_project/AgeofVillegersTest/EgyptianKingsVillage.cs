0cd4a77bb5018ceb6912e08cdf6924e7882521a1
Asif Aziz <asifaziz2061@gmail.com>
2020-09-23 00:24:13 +0600

    #11 House,Tree and Water Source added to test Egyptian Kings Village

--- solutions/31-aziz/empty_project/AgeofVillegersTest/EgyptianKingsVillage.cs
+++ solutions/31-aziz/empty_project/AgeofVillegersTest/EgyptianKingsVillage.cs
@@ -8,7 +8,19 @@ using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace AgeofVillegersTest
 {
-    class EgyptianKingsVillage
+    public class EgyptianKingsVillage
     {
+        [Fact]
+        public void EgyptianVillageTest()
+        {
+            AbstractNation egyptianVillageHouse = new EgyptianKings(new Panel());
+            Assert.AreEqual(egyptianVillageHouse.getHouseType().GetType(), new EgyptianKingsHouse().GetType());
+
+            AbstractNation egyptianVillageTree = new EgyptianKings(new Panel());
+            Assert.AreEqual(egyptianVillageTree.getTreeType().GetType(), new EgyptianKingsTree().GetType());
+
+            AbstractNation egyptianVillageWaterSource = new EgyptianKings(new Panel());
+            Assert.AreEqual(egyptianVillageWaterSource.getWaterSourceType().GetType(), new EgyptianKingsWaterSource().GetType());
+        }
     }
 }
