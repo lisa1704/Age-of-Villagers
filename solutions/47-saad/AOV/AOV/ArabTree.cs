279cd97dabf3d93b93248cfd6c215d70d631b1f4
Saad <saadbinjohir@gmail.com>
2020-09-22 12:52:52 +0600

    #1 Arab tree 16*24

--- solutions/47-saad/AOV/AOV/ArabTree.cs
+++ solutions/47-saad/AOV/AOV/ArabTree.cs
@@ -12,23 +12,21 @@ namespace AOV
         public ArabTree(Point clicked)
         {
             Point topLeftCorner = new Point(clicked.X, clicked.Y);
-            Point bottomRightCorner = new Point(clicked.X + 6, clicked.Y + 60);
-            Point topCenter = new Point(clicked.X+3, clicked.Y);
-            Point topRightHalf = new Point(clicked.X+4, clicked.Y);
-            Point topRightCorner = new Point(clicked.X + 6, clicked.Y);
-            Point topLeftHalf = new Point(clicked.X+2, clicked.Y);
-            Point x1 = new Point(clicked.X - 76, clicked.Y - 30);
-            Point x2 = new Point(clicked.X - 42, clicked.Y - 50);
-            Point x3 = new Point(clicked.X + 3, clicked.Y - 60);
-            Point x4 = new Point(clicked.X + 50, clicked.Y - 50);
-            Point x5 = new Point(clicked.X + 82, clicked.Y - 30);
+            Point topCenter = new Point(clicked.X+1, clicked.Y);
+            Point topRightCorner = new Point(clicked.X + 2, clicked.Y);
+            Point bottomRightCorner = new Point(clicked.X + 2, clicked.Y + 12);
+            Point x1 = new Point(topLeftCorner.X - 7, clicked.Y - 6);
+            Point x2 = new Point(topLeftCorner.X - 3, clicked.Y - 10);
+            Point x3 = new Point(topCenter.X , clicked.Y - 12);
+            Point x4 = new Point(topRightCorner.X + 3, clicked.Y - 10);
+            Point x5 = new Point(topRightCorner.X + 7, clicked.Y - 6);
 
             addShape(new Rectangle(topLeftCorner, bottomRightCorner));
-            addShape(new DrawLine(topCenter, x3));
-            addShape(new DrawLine(topRightHalf, x4));
-            addShape(new DrawLine(topRightCorner, x5));
-            addShape(new DrawLine(topLeftHalf, x2));
+            addShape(new DrawLine(topCenter, x3));            
             addShape(new DrawLine(topLeftCorner, x1));
+            addShape(new DrawLine(topLeftCorner, x2));
+            addShape(new DrawLine(topRightCorner, x4));
+            addShape(new DrawLine(topRightCorner, x5));
         }
     }
 }
