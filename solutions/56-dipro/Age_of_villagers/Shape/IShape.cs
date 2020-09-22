8d9108c32b6534c5848baac05bd33ecfad4d05b2
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-22 23:54:31 +0600

    #4 #7 interface for shape has been created

---
+++ solutions/56-dipro/Age_of_villagers/Shape/IShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Age_of_villagers.Shape
+{
+    interface IShape
+    {
+        void draw(Graphics g, Pen p);
+
+    }
+}
