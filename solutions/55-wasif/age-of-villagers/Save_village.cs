cc1ba8044fd68c39f25e9681f16d8aafc2d114a7
Wasif Azmaeen <59744073+WasifAzmaeen@users.noreply.github.com>
2020-09-22 17:02:31 +0600

    #5 save village class created

---
+++ solutions/55-wasif/age-of-villagers/Save_village.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace age_of_villagers
+{
+    class Save_village
+    {
+
+    }
+}
