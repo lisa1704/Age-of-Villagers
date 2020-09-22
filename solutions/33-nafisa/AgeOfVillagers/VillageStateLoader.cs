8a537ec8fd50f1653ade681ace018558288cba9d
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-09-22 16:14:50 +0600

    #6 VillageStateLoader class created

---
+++ solutions/33-nafisa/AgeOfVillagers/VillageStateLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    class VillageStateLoader : IState
+    {
+        public void execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
