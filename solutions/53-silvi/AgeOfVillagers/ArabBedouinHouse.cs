b1250608d0ebf2bbe59ef3982085b6f3d8a0764b
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 02:14:44 +0600

    #10 ArabBedouinHouse class updated.

--- solutions/53-silvi/AgeOfVillagers/ArabBedouinHouse.cs
+++ solutions/53-silvi/AgeOfVillagers/ArabBedouinHouse.cs
@@ -11,6 +11,18 @@ namespace AgeOfVillagers
     {
         public void Draw(Point e, Graphics g, Pen p)
         {
-            
+            int x = e.X;
+            int y = e.Y;
+
+            g.DrawLine(p, x - 10, y, x + 10, y);
+            g.DrawLine(p, x, y - 10, x - 10, y);
+            g.DrawLine(p, x, y - 10, x + 10, y);
+
+            g.DrawLine(p, x, y - 10, x + 10, y - 15);
+            g.DrawLine(p, x + 10, y, x + 20, y - 5);
+            g.DrawLine(p, x + 10, y - 15, x + 20, y - 5);
+
+
+
         }
     }
