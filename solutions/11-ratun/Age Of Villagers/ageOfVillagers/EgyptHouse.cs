64143ecf14dc40381f102eab6340151554cc1a75
Ratun <www.ratunrahman@gmail.com>
2020-09-22 13:36:47 +0600

    #11 add pen on Egypt house

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptHouse.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/EgyptHouse.cs
@@ -4,10 +4,11 @@ namespace ageOfVillagers
 {
     internal class EgyptHouse : IShape
     {
+        Pen p = new Pen(Color.Yellow);
         public string draw(int x, int y, Graphics g)
         {
-            Triangle t1 = new Triangle(x, y + 8, x - 8, y - 4, x + 2, y - 8);
-            Triangle t2 = new Triangle(x, y + 8, x + 8, y, x + 2, y - 8);
+            Triangle t1 = new Triangle(g,p,x, y + 8, x - 8, y - 4, x + 2, y - 8);
+            Triangle t2 = new Triangle(g,p,x, y + 8, x + 8, y, x + 2, y - 8);
             return t1.Draw() + t2.Draw();
         }
     }
