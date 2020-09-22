4c184dc1440e2e18f85012cc3010187d2ab540ef
Ratun <www.ratunrahman@gmail.com>
2020-09-22 17:23:31 +0600

    #2 finish Arc class

--- solutions/11-ratun/Age Of Villagers/ageOfVillagers/Arc.cs
+++ solutions/11-ratun/Age Of Villagers/ageOfVillagers/Arc.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace ageOfVillagers
 {
     internal class Arc
     {
-
+        Graphics g;
+        Pen p;
+        private int x, y, height, width, startAngle, finishAngle;
+        public Arc(Graphics g, Pen p, int x, int y, int height, int width, int startAngle, int finishAngle)
+        {
+            this.g = g;
+            this.p = p;
+            this.x = x;
+            this.y = y;
+            this.height = height;
+            this.width = width;
+            this.startAngle = startAngle;
+            this.finishAngle = finishAngle;
+        }
+        public void Draw()
+        {
+            g.DrawArc(p, x, y, width, height, startAngle, finishAngle);
+            //return "Drawing an Arc. ";
+        }
     }
 }
