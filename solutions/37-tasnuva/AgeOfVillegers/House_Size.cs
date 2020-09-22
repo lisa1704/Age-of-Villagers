8a5f6cb56391fa52c1f9f61bbf74b7063e978683
Tasnuva12 <47001609+Tasnuva12@users.noreply.github.com>
2020-09-23 03:07:05 +0600

    #7 House_Size and Tree_Size classes are created

---
+++ solutions/37-tasnuva/AgeOfVillegers/House_Size.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AgeOfVillegers
+{
+	public class House_size : Size
+	{
+        public override int GetHeight()
+        {
+            return 16;
+        }
+
+        public override int GetWeidth()
+        {
+            return 16;
+        }
+    }
+}
---
+++ solutions/37-tasnuva/AgeOfVillegers/Tree_Size.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AgeOfVillegers 
+{
+    public class Tree_size : Size
+    {
+
+        public override int GetHeight()
+    {
+        return 24;
+    }
+
+    public override int GetWeidth()
+    {
+        return 16;
+    }
+}
+}
