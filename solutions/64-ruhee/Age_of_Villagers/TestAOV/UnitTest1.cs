b3e5d3c308730cc6d5ab95e1ea5fd07e827792df
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-09-23 01:02:28 +0600

    #1 #9 BD background test.

--- solutions/64-ruhee/Age_of_Villagers/TestAOV/UnitTest1.cs
+++ solutions/64-ruhee/Age_of_Villagers/TestAOV/UnitTest1.cs
@@ -1,5 +1,11 @@
 using System;
 using Xunit;
+using Age_of_Villagers;
+using System.Drawing;
+using Age_of_Villagers.BangladeshiFarmers;
+using Age_of_Villagers.EgyptianKings;
+using Age_of_Villagers.ArabBedouin;
+using Age_of_Villagers.Age_of_Villagers;
 
 namespace TestAOV
 {
@@ -10,6 +16,16 @@ namespace TestAOV
         {
             Assert.Equal(2,2);
         }
+
+        [Fact]
+        public void BangladeshiBackground()
+        {
+            BangladeshiFarmers back = new BangladeshiFarmers();
+            Color b1 = back.BackgroundColor();
+            Color b2 = Color.Green;
+            Assert.Equal(b1, b2);
+        }
+
 
     }
 }
