235543daaeab5d358a97ea329e3b23642b41d2ac
Maksuda Islam <maksudalima321@outlook.com>
2020-09-22 12:52:57 +0600

    #9 water source re-sized to 24 by 16 for bd farmers.

--- solutions/63-lima/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersWaterSource.cs
+++ solutions/63-lima/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersWaterSource.cs
@@ -6,14 +6,15 @@ public class BangladeshiFarmersWaterSource : IWaterSource
     Pen p = new Pen(Color.White);
     public void drawWaterSource(Graphics g, int X, int Y)
     {
-        g.DrawLine(p, X, Y + 50, X + 25, Y + 25);
-        g.DrawLine(p, X + 25, Y + 25, X + 50, Y + 50);
-        g.DrawLine(p, X + 50, Y + 50, X + 75, Y);
-        g.DrawLine(p, X + 75, Y, X + 50, Y - 25);
-        g.DrawLine(p, X + 50, Y - 25, X + 25, Y);
-        g.DrawLine(p, X + 25, Y, X + 25, Y - 25);
-        g.DrawLine(p, X + 25, Y - 25, X, Y - 25);
-        g.DrawLine(p, X, Y - 25, X - 25, Y + 25);
-        g.DrawLine(p, X - 25, Y + 25, X, Y + 50);
+        g.DrawLine(p, X, Y + 4, X + 4, Y);
+        g.DrawLine(p, X + 4, Y, X + 9, Y + 4);
+        g.DrawLine(p, X + 9, Y + 4, X + 24, Y );
+        g.DrawLine(p, X + 24, Y , X + 20, Y + 12);
+        g.DrawLine(p, X + 20, Y + 12, X + 15, Y + 16);
+        g.DrawLine(p, X + 15, Y + 16, X + 6, Y + 10);
+        g.DrawLine(p, X + 6, Y + 10, X + 7, Y + 14);
+        g.DrawLine(p, X + 7, Y + 14, X + 5, Y + 13);
+        g.DrawLine(p, X + 5, Y + 13 , X , Y + 4);
+
     }
 }
