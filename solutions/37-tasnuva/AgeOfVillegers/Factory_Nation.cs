ded18bde5ede1463816ceab598b70a57824d11e2
Tasnuva12 <47001609+Tasnuva12@users.noreply.github.com>
2020-09-22 21:10:46 +0600

    #8#9#10#11#12 Factory_Nation Class created

---
+++ solutions/37-tasnuva/AgeOfVillegers/Factory_Nation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillegers 
+{
+	class Factory_Nation
+	{
+	}
+}
