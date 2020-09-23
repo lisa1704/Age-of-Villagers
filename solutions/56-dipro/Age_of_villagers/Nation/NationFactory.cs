a41b9c531def0fee77f3715fdad765a8b65cbcd4
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 16:21:32 +0600

    #8 nation factory created

---
+++ solutions/56-dipro/Age_of_villagers/Nation/NationFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Age_of_villagers.Nation
+{
+    class NationFactory
+    {
+        string nationName;
+        public NationFactory(string NationType)
+        {
+            this.nationName = NationType;
+        }
+        public INation GetNation()
+        {
+            return null;
+        }
+    }
+}
