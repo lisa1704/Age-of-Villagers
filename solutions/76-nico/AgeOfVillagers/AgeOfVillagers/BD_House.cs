998bb776ae42403d2006bea68799a9c66b71eccf
Nabil Hossain Nico <hossaintalukder108@gmail.com>
2020-09-23 21:33:13 +0600

    #1 #9 BD house implemented

--- solutions/76-nico/AgeOfVillagers/AgeOfVillagers/BD_House.cs
+++ solutions/76-nico/AgeOfVillagers/AgeOfVillagers/BD_House.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,10 @@ namespace AgeOfVillagers
 {
     class BD_House:CompositeShape 
     {
-
+        public BD_House(Point top, Point topleft, Point bottomright)
+        {
+            AddComponent(new Rectangle(topleft, bottomright));
+            AddComponent(new Triangle(top, topleft, new Point(bottomright.X, topleft.Y)));
+        }
     }
 }
