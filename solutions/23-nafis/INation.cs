349916d20bedf836729a06229114521d97c9a777
LITHIUM39 <nafism415@gmail.com>
2020-09-23 16:53:18 +0600

    Paint_house ,paint_tree,paint_watersource method

--- solutions/23-nafis/INation.cs
+++ solutions/23-nafis/INation.cs
@@ -2,6 +2,9 @@
 {
     interface INation
 {
-    
-}
+        public void paint_house(MouseEventArgs e, Graphics g, Pen p);
+        public void paint_tree(MouseEventArgs e, Graphics g, Pen p);
+        public void paint_waterSource(MouseEventArgs e, Graphics g, Pen p);
+
+    }
 }
--- solutions/23-nafis/inuit_hunters.cs
+++ solutions/23-nafis/inuit_hunters.cs
@@ -1,6 +1,17 @@
 ﻿using System;
 
-public inuit_hunters()
+class InuitHunters : INation
 {
+    public void paint_house(MouseEventArgs e, Graphics g, Pen p)
+    {
+        int x, y;
+        x = e.X;
+        y = e.Y;
 
+        Point pt2 = new Point(x + 8, y);
+        Point pt3 = new Point(x - 8, y);
+        g.DrawArc(p, x - 8, y - 8, 16, 16, 0, -180);
+        g.DrawArc(p, x - 3, y - 3, 6, 6, 0, -180);
+        g.DrawLine(p, pt2, pt3);
+    }
 }
