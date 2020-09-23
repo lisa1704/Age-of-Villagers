e4a33a9f4da81a6f762772385a5ad9a9e602e88b
Sadat Bin Sharfuddin <sadat.bs@gmail.com>
2020-09-23 23:37:31 +0600

    Unit test for inuit hunter tree added

--- solutions/68-sadat/age-of-villagers/age-of-villagers/age-of-villagers.Tests/FeatureTesting.cs
+++ solutions/68-sadat/age-of-villagers/age-of-villagers/age-of-villagers.Tests/FeatureTesting.cs
@@ -11,7 +11,16 @@ namespace age_of_villagers.age_of_villagers.Tests
     public class FeatureTesting
     {
         
-        
+        [Fact]
+        public void InuitTreeTest()
+        {
+            Nation nation = new InuitHunter(new System.Windows.Forms.Panel());
+
+            var expected = typeof(NullTree);
+            var actual = nation.tree;
+
+            Assert.IsType(expected, actual);
+        }
         [Fact]
         public void InuitWaterSourceTest()
         {
