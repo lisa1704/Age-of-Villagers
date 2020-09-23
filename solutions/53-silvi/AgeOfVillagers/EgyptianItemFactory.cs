60e20a385ee809ff03eea4645957342eb3ab8e2d
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 09:53:12 +0600

    #10 EgyptianFactory class implemented.

--- solutions/53-silvi/AgeOfVillagers/EgyptianItemFactory.cs
+++ solutions/53-silvi/AgeOfVillagers/EgyptianItemFactory.cs
@@ -10,7 +10,22 @@ namespace AgeOfVillagers
     {
         public override IVillageItem getItem(string button)
         {
-            
+            if (button == "House")
+            {
+                return new EgyptianHouse();
+            }
+            else if (button == "Tree")
+            {
+                return new EgyptianTree();
+            }
+            else if (button == "WaterSource")
+            {
+                return new EgyptianWaterSource();
+            }
+
+            return null;
+
+
         }
     }
 }
