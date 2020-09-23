b23daa425bb6e80c16319679afcf19547bba20e2
musfiksifat <musfiksifat766@gmail.com>
2020-09-23 21:35:48 +0600

    #10. Testing:: all getItemShape Test for Arab nation complete

---
+++ solutions/88-musfik/AgeOfVillagers.Tests/Nationwise_ItemShapeTest.cs
@@ -0,0 +1,43 @@
+﻿using AgeOfVillagers.VillageItem;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Xunit;
+
+namespace AgeOfVillagers.Tests
+{
+    public class Nationwise_ItemShapeTest
+    {
+        private readonly Point point1 = new Point(1, 2);
+        private readonly Point point2 = new Point(2, 2);
+
+        [Fact]
+        public void ArabBedouin_getHouseShape_Test()
+        {
+            INation nation = new ArabBedouin();
+            var expected = nation.getItemShape(point1, "House");
+            var actual = new ArabHouse().getItemShape(point1);
+            actual.Equals(expected);
+        }
+
+        [Fact]
+        public void ArabBedouin_getTreeShape_Test()
+        {
+            INation nation = new ArabBedouin();
+            var expected = nation.getItemShape(point2, "Tree");
+            var actual = new ArabTree().getItemShape(point2);
+            actual.Equals(expected);
+        }
+
+        [Fact]
+        public void ArabBedouin_getWaterSourceShape_Test()
+        {
+            INation nation = new ArabBedouin();
+            var expected = nation.getItemShape(point2, "Water");
+            var actual = new NoItem().getItemShape(point2);
+            actual.Equals(expected);
+        }
+  
+    }
+}
