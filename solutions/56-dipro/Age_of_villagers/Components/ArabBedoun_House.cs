5d2e7e959daeb2b20f88393116d8420347b7abba
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 21:02:06 +0600

    #10 arab bedouin house class created

--- solutions/56-dipro/Age_of_villagers/Components/ArabBedoun_House.cs
+++ solutions/56-dipro/Age_of_villagers/Components/ArabBedoun_House.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
 
 namespace Age_of_villagers.Components
 {
-    class Egyptiankings_tree : VillageComponent
+    class ArabBedoun_House
     {
-
-
     }
 }
