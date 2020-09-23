aacff40f61f4c016892ba9bb3686c47d364593ce
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-23 23:15:55 +0600

    #1 #7 Changed measurements

---
+++ solutions/22-snigdho/Age Of Villagers/Age Of Villagers/ArabBedouin/ArabBedouin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AgeOfVillagers.Arab_Bedouin
+{
+    public class ArabBedouinTrees : Composite
+    {
+
+        public ArabBedouinTrees(Point point)
+        {
+            Point point1 = new Point(point.X + 10, point.Y - 5);
+            Point point2 = new Point(point.X - 10, point.Y - 5);
+            Point point3 = new Point(point.X + 6, point.Y - 8);
+            Point point4 = new Point(point.X - 6, point.Y - 8);
+            Point point5 = new Point(point.X, point.Y - 10);
+            Point leftPoint1 = new Point(point.X + 10, point.Y - 5);
+            Point rightPoint1 = new Point(point.X - 10, point.Y - 5);
+            Point leftPoint2 = new Point(point.X + 6, point.Y - 8);
+            Point rightPoint2 = new Point(point.X - 6, point.Y - 8);
+            Point topMost = new Point(point.X, point.Y - 10);
+            Point TopLeft = new Point(point.X - 1, point.Y);
+            Point RightBottom = new Point(point.X + 1, point.Y + 14);
+
+            AddComponent(new Rectangle(TopLeft, RightBottom));
+            AddComponent(new Line(point, point1));
+            AddComponent(new Line(point, point2));
+            AddComponent(new Line(point, point3));
+            AddComponent(new Line(point, point4));
+            AddComponent(new Line(point, point5));
+            AddComponent(new Line(point, leftPoint1));
+            AddComponent(new Line(point, rightPoint1));
+            AddComponent(new Line(point, leftPoint2));
+            AddComponent(new Line(point, rightPoint2));
+            AddComponent(new Line(point, topMost));
+        }
+    }
+}
