a2db92ed3f44927890bea4a3920298128a108bb2
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 07:56:35 +0600

    #12 Functionalities for Inuit Class added

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Inuit.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Inuit.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class Inuit
+    class Inuit : INation
     {
-
+        public Color getBackgroundColor()
+        {
+            return Color.White;
+        }
+        public IComponent getComponent(string component_type)
+        {
+            if (component_type == "house")
+            {
+                return new InuitHuntersHouse();
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
