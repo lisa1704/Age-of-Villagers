1d58b27d8da4015ea12b2a94f1597cd6fb553916
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 12:12:38 +0600

    #7 Line class updated.

--- solutions/53-silvi/AgeOfVillagers/Line.cs
+++ solutions/53-silvi/AgeOfVillagers/Line.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,19 @@ namespace AgeOfVillagers
 {
     class Line : IShape
     {
+        private readonly Point point_1;
+        private readonly Point point_2;
+        private object g;
+
+        public Line(Point point1, Point point2)
+        {
+            this.point_1 = point1;
+            this.point_2 = point2;
+        }
         public void Draw()
         {
-            
+            var my_pen = new Pen(Color.Black, 1);
+            g.DrawLine(my_pen, point_1, point_2);
         }
     }
 }
