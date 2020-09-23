3d2fac04ab6cb3f082a2954d39fafcbae8e94b7a
Sakib58 <rabiulahmed.sakib@gmail.com>
2020-09-23 18:45:21 +0600

    #11 transform rough house shape to proper house shape for Egyptian Kings

--- solutions/58-sakib-2/src/House/EgyptianKingsHouse.java
+++ solutions/58-sakib-2/src/House/EgyptianKingsHouse.java
@@ -1,13 +1,26 @@
 package House;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 import java.awt.*;
 
 public class EgyptianKingsHouse implements IHouse {
+    GraphicsContext gc;
 
     @Override
     public void draw(Canvas canvas, Point point) {
-
+        gc=canvas.getGraphicsContext2D();
+        gc.beginPath();
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()+5,point.getY()+5);
+        gc.lineTo(point.getX()+10,point.getY());
+        gc.lineTo(point.getX()+5,point.getY()-15);
+        gc.lineTo(point.getX(),point.getY());
+        gc.moveTo(point.getX()+5,point.getY()+5);
+        gc.lineTo(point.getX()+5,point.getY()-15);
+        gc.lineTo(point.getX(),point.getY());
+        gc.closePath();
+        gc.stroke();
     }
 }
