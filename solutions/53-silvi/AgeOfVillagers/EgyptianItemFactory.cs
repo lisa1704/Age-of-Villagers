f64d17e6ce4a1ab004b66e970d3c069ae6cd9f30
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 09:49:46 +0600

    #11 Factory class for Egyptian King created.

---
+++ solutions/53-silvi/AgeOfVillagers/EgyptianItemFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class EgyptianItemFactory : ItemFactory
+    {
+        public override IVillageItem getItem(string button)
+        {
+            
+        }
+    }
+}
