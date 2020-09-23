05fde59c223f7dfd7b4f7ed7afcdfe3e119ac4df
Sakib58 <rabiulahmed.sakib@gmail.com>
2020-09-23 20:01:20 +0600

    #11 transform rough tree shape to proper tree shape for Egyptian Kings

--- solutions/58-sakib-2/src/Tree/EgyptianKingsTree.java
+++ solutions/58-sakib-2/src/Tree/EgyptianKingsTree.java
@@ -1,12 +1,30 @@
 package Tree;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 import java.awt.*;
 
 public class EgyptianKingsTree implements ITree {
+    GraphicsContext gc;
     @Override
     public void draw(Canvas canvas, Point point) {
+        gc=canvas.getGraphicsContext2D();
+        gc.beginPath();
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()-8,point.getY()-12);
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()+8,point.getY()-12);
+        gc.moveTo(point.getX()-4,point.getY()-7);
+        gc.lineTo(point.getX()-10,point.getY()-10);
+        gc.moveTo(point.getX()-4,point.getY()-7);
+        gc.lineTo(point.getX()-2,point.getY()-12);
 
+        gc.moveTo(point.getX()+4,point.getY()-7);
+        gc.lineTo(point.getX()+10,point.getY()-10);
+        gc.moveTo(point.getX()+4,point.getY()-7);
+        gc.lineTo(point.getX()+2,point.getY()-12);
+        gc.closePath();
+        gc.stroke();
     }
 }
