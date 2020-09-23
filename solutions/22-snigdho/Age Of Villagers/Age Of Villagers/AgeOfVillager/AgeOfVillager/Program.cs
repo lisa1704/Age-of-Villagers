db9baf2ddf06c4cb38a65403a5df192cf90205ab
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-23 22:42:34 +0600

    #1  #7  #8  #9  #10 #11 #12 Refactoring done

--- solutions/22-snigdho/Age Of Villagers/Age Of Villagers/AgeOfVillager/AgeOfVillager/Program.cs
+++ solutions/22-snigdho/Age Of Villagers/Age Of Villagers/AgeOfVillager/AgeOfVillager/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
-
-namespace AgeOfVillager
+﻿using System
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+namespace AgeOfVillagers.Arab_Bedouin
 {
-    class Program
+    public class ArabBedouin : INation
     {
-        static void Main(string[] args)
+        public Color BackgroundColor()
         {
-            Console.WriteLine("Hello World!");
+            return Color.GreenYellow;
+        }
+
+        public Object House(Graphics g, Point point)
+        public IShape House(Point point)
+        {
+            Arab_Bedouin_House arabBedouinHouse = new Arab_Bedouin_House(point);
+            arabBedouinHouse.Draw(g);
+            Composite arabBedouinHouse = new Arab_Bedouin_House(point);
+            return arabBedouinHouse;
+        }
+
+        public Object Tree(Graphics g, Point point)
+        public IShape Tree(Point point)
+        {
+            Arab_Bedouin_Trees arabBedouinTree = new Arab_Bedouin_Trees(point);
+            arabBedouinTree.Draw(g);
+            Composite arabBedouinTree = new Arab_Bedouin_Trees(point);
+            return arabBedouinTree;
+        }
+
+        public Object WaterSource(Graphics g, Point point)
+        public IShape WaterSource(Point point)
+        {
+            NullItems arabBedouinWaterSource = new NullItems();
+            arabBedouinWaterSource.drawing();
+            Composite arabBedouinWaterSource = new NullItems();
+            return arabBedouinWaterSource;
         }
     }
 }
