0bcb04e51a26f6586c65e2a9b8b0d58e7340593c
Sakib58 <rabiulahmed.sakib@gmail.com>
2020-09-23 18:37:02 +0600

    #10 transform rough house shape to proper house shape for Arab bedouin

--- solutions/58-sakib-2/src/House/ArabBedouinHouse.java
+++ solutions/58-sakib-2/src/House/ArabBedouinHouse.java
@@ -1,13 +1,26 @@
 package House;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 import java.awt.*;
 
 public class ArabBedouinHouse implements IHouse{
+    public GraphicsContext gc;
 
     @Override
     public void draw(Canvas canvas, Point point) {
-
+        gc=canvas.getGraphicsContext2D();
+        gc.beginPath();
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()+20,point.getY());
+        gc.lineTo(point.getX()+40,point.getY()-10);
+        gc.lineTo(point.getX()+20+10,point.getY()-30);
+        gc.lineTo(point.getX()+10,point.getY()-20);
+        gc.moveTo(point.getX()+20,point.getY());
+        gc.lineTo(point.getX()+10,point.getY()-20);
+        gc.lineTo(point.getX(),point.getY());
+        gc.closePath();
+        gc.stroke();
     }
 }
