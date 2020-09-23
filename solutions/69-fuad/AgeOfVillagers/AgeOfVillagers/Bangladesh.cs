23344f74b106ac0e9da38f0c0342c4b5cd69806b
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 07:46:22 +0600

    #9 Functionalities added to the Banglaedesh Class

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Bangladesh.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Bangladesh.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AgeOfVillagers
 {
-    class Bangladesh
+    class Bangladesh : INation
     {
+        public Color getBackgroundColor()
+        {
+            return Color.LightGreen;
+        }
+        public IComponent getComponent(string component_type)
+        {
+            if (component_type == "tree")
+            {
+                return new BangladeshiFarmersTree();
+            }
+            else if (component_type == "house")
+            {
+                return new BangladeshiFarmersHouse();
+            }
+            else if (component_type == "water_source")
+            {
+                return new BangladeshiFarmersWaterSource();
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
