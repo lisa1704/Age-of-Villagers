ec2ebc247a0de25117be2c950e68676bf14e2aba
jarin_12 <jarintanzim@iut-dhaka.edu>
2020-09-23 23:34:48 +0600

    #12 arc modified and draww circle for inuithunter house

--- solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/Arc.cs
+++ solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/Arc.cs
@@ -14,8 +14,25 @@ namespace ageofvillagers
         Pen pen;
         private int x, y;
         int height, width, startAngle, sweepAngle;
+        private object pen1;
+        private int v1;
+        private int v2;
+        private int v3;
+        private int v4;
+        private int v5;
+        private int v6;
 
-    
+        public Arc(object pen1, int v1, int v2, int v3, int v4, int v5, int v6)
+        {
+            this.pen1 = pen1;
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.v6 = v6;
+        }
+
         public void DrawArc(System.Drawing. Pen pen, int x, int y, int width, int height, int startAngle, int sweepAngle)
         {
             this.pen = pen;
@@ -26,7 +43,7 @@ namespace ageofvillagers
             this.startAngle = startAngle;
             this.sweepAngle = sweepAngle;
         }
-        public void draw()
+        public void Draw()
         {
             DrawArc(pen, x, y, width, height, startAngle, sweepAngle);
         }
---
+++ solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/halfcircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ageofvillagers
+{
+    class halfcircle
+    {
+        private int x, y, radi;
+        Pen pen;
+        public halfcircle(Pen pen ,int x, int y, int radi)
+        {
+            this.x = x;
+            this.y = y;
+            this.radi = radi;
+        }
+        public string Draw()
+        {
+            Arc halfcircle = new Arc(pen, x - radi, y - radi, 2 * radi, 2 * radi, 0, 180);
+
+            halfcircle.Draw();
+            return null;
+
+        }
+        
+    }
+}
