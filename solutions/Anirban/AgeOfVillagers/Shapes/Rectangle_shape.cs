cd7f074f8881652c756a2f34d1aa8b9847cd20f3
Anirban Azad <azadarannya@gmail.com>
2020-09-23 06:16:45 +0600

    #9 #10 #11 triangle and rectangle shapes created

---
+++ solutions/Anirban/AgeOfVillagers/Shapes/Rectangle_shape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.Shapes
+{
+    class Rectangle_shape : IShapes
+    {
+        public Point pt1, pt2, pt3, pt4;
+        
+        public Rectangle_shape(Point point1, Point point2, Point point3, Point point4)
+        {
+            pt1 = point1;
+            pt2 = point2;
+            pt3 = point3;
+            pt4 = point4;
+        }
+
+        public void Draw_shapes(Point pt, Graphics g, Pen p)
+        {
+            g.DrawLine(p, pt1, pt2);
+            g.DrawLine(p, pt2, pt3);
+            g.DrawLine(p, pt3, pt4);
+            g.DrawLine(p, pt4, pt1);
+        }
+    }
+}
---
+++ solutions/Anirban/AgeOfVillagers/Shapes/Triangle_shape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.Shapes
+{
+    class Triangle_shape : IShapes
+    {
+        public Point apex, base1, base2;
+
+        public Triangle_shape(Point top, Point bottom1, Point bottom2)
+        {
+            apex = top;
+            base1 = bottom1;
+            base2 = bottom2;
+
+        }
+
+        public void Draw_shapes(Point pt, Graphics g, Pen p)
+        {
+            g.DrawLine(p, apex, base1);
+            g.DrawLine(p, base1, base2);
+            g.DrawLine(p, base2, apex);
+
+        }
+    }
+}
