a092689b12d06f8593fa2d28e95a7c6e2aec6fbe
Asif Aziz <asifaziz2061@gmail.com>
2020-09-23 09:29:50 +0600

    #1 Egyptian Kings House reshaped to 16px by 16 px

--- solutions/31-aziz/empty_project/empty_project/EgyptianKingsHouse.cs
+++ solutions/31-aziz/empty_project/empty_project/EgyptianKingsHouse.cs
@@ -13,13 +13,13 @@ namespace empty_project
             Pen pen = new Pen(Color.Black);
             Graphics graphics = panel.CreateGraphics();
 
-            graphics.DrawLine(pen, new PointF(axis.X,axis.Y), new PointF(axis.X + 20, axis.Y + 30));
-            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 20, axis.Y + 22));
+            graphics.DrawLine(pen, new PointF(axis.X,axis.Y), new PointF(axis.X + 8, axis.Y + 14));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 8, axis.Y + 12));
 
-            graphics.DrawLine(pen, new PointF(axis.X + 20, axis.Y + 30), new PointF(axis.X - 8, axis.Y + 40));
-            graphics.DrawLine(pen, new PointF(axis.X - 20, axis.Y + 22), new PointF(axis.X - 8, axis.Y + 40));
+            graphics.DrawLine(pen, new PointF(axis.X + 8, axis.Y + 14), new PointF(axis.X - 2, axis.Y + 16));
+            graphics.DrawLine(pen, new PointF(axis.X - 8, axis.Y + 12), new PointF(axis.X - 2, axis.Y + 16));
 
-            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 8, axis.Y + 40));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 2, axis.Y + 16));
         }
     }
 }
