7f73b2d3577ac1f0de806aa91bd4f508dd786e29
Tasnuva12 <47001609+Tasnuva12@users.noreply.github.com>
2020-09-23 14:21:48 +0600

    #8  NullNation  is  created

--- solutions/37-tasnuva/AgeOfVillegers/NullNation.cs
+++ solutions/37-tasnuva/AgeOfVillegers/NullNation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,29 @@ namespace AgeOfVillegers
 {
 	public class NullNation : INation
 	{
-
+		public IObject Draw_house(Point p )
+        {
+			warning();
+			return null_obj.Instance;
+        }
+		public IObject Draw_tree(Point p)
+		{
+			warning();
+			return null_obj.Instance;
+		}
+		public IObject Draw_watersource(Point p)
+		{
+			warning();
+			return null_obj.Instance;
+		}
+		public Color get_backcolor()
+		{
+			return Color.White;
+		}
+		public void warning()
+		{
+			DialogResult d;
+			d = MessageBox.Show("VillgeType is not selected");
+		}
 	}
 }
