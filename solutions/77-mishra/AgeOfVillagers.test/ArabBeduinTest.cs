fa31f25f5054fe1d36df5cea94124f7f1b7f1157
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-09-23 19:04:13 +0600

    #10 Arab beduin test case created

--- solutions/77-mishra/AgeOfVillagers.test/ArabBeduinTest.cs
+++ solutions/77-mishra/AgeOfVillagers.test/ArabBeduinTest.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+using Xunit;
+using System.Drawing;
 
 namespace AgeOfVillagers.test
 {
 	public class ArabBeduinTest
 	{
+		private static string stringtype = "Arab Beduin";
+		Nations nations = new Nations(stringtype);
+		Point point = new Point(90, 40);
+
+		
 		public ArabBeduinTest()
 		{
-
+			public void ArabTreetesting()
+            {
+				Point tests = nation.getNation(point).DrawHouse(p);
+				Assert.Equal(point, tests);
+            }
 		}
 	}
 
