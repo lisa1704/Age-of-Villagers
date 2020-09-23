51f3f27926b72c0d2d6163f61d862d730182e9c9
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 22:45:01 +0600

    #1

---
+++ solutions/53-silvi/XUnitTestProject1/BangladeshiFarmersTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AgeOfVillagers;
+
+namespace AgeOfVillagers53.test
+{
+    [TestClass()]
+    class BangladeshiFarmersTest
+    {
+
+    }
+}
