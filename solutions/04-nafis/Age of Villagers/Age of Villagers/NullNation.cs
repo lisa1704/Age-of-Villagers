d326a57e6ecf54da90e90aa10d8276e8975f4ef7
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-23 18:46:09 +0600

    #1 #11 #12 Implementing Null Nation for Arab and Inuit

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/NullNation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    class NullNation : INation
+    {
+        public NullNation(Point point)
+        {
+
+        }
+        public Point GetHousePoint()
+        {
+            return new Point();
+        }
+
+        public Color GetTerrainColor()
+        {
+            return Color.Empty;
+        }
+
+        public Point GetTreePoint()
+        {
+            return new Point();
+        }
+
+        public Point GetWaterSourcePoint()
+        {
+            return new Point();
+        }
+
+        public IShape HouseDraw()
+        {
+            return new NullShape();
+        }
+
+        public IShape TreeDraw()
+        {
+            return new NullShape();
+        }
+
+        public IShape WaterSourceDraw()
+        {
+            return new NullShape();
+        }
+    }
+}
