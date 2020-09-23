3cbfaf2e9fea6f73a10b8ede64069e792d423fbf
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 10:05:34 +0600

    #11 EgyptianWaterSource class implemented.

--- solutions/53-silvi/AgeOfVillagers/EgyptianWaterSource.cs
+++ solutions/53-silvi/AgeOfVillagers/EgyptianWaterSource.cs
@@ -6,8 +6,9 @@ namespace AgeOfVillagers
     {
         public void Draw(Point e, Graphics g, Pen p)
         {
-            
-
+            int x = e.X;
+            int y = e.Y;
+            g.DrawArc(p, x - 40, y - 50, 16, 16, 0, 360);
         }
     }
 }
