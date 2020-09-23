57bdd64b0d122e72a481357edf962aeb6922ec5a
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-09-23 18:44:51 +0600

    #11 EgyptianKings nation name test

---
+++ solutions/05-abadulbari/AgeOfVillagers/AgeOfVillagers.Test/EgyptianKingsTest.cs
@@ -0,0 +1,21 @@
+﻿using AgeOfVillagers.Factories;
+using AgeOfVillagers.Nation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AgeOfVillagers.Test
+{
+    public class EgyptianKingsTest
+    {
+        INation bd = new NationFactory("Egyptian Kings").getNation();
+
+        [Fact]
+        public void NationNameTest()
+        {
+            var expected = "Egyptian Kings";
+            Assert.Equal(expected, bd.getNationName());
+        }
+    }
+}
