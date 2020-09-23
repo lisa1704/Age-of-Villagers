d28cc19fc7de9ea9118352a4d1231a6d1c97f185
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 16:09:52 +0600

    #8 nation interface added

---
+++ solutions/56-dipro/Age_of_villagers/Nation/INation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Text;
+using Age_of_villagers.Components;
+
+namespace Age_of_villagers.Nation
+{
+    public interface INation
+    {
+        string getNationName();
+        Color getTerrainColor();
+        VillageComponent getHouse(Point p);
+        VillageComponent getTree(Point p);
+        VillageComponent getWaterSource(Point p);
+    }
+}
