7167a2e48a4a54e7318cfff3a8b5db2135c4f496
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 23:07:34 +0600

    #5 #6 SaveVillage & OpenVillage Class created, not fully implemented yet

---
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/OpenVillage.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers.Open
+{
+    public class OpenVillage
+    {
+        Village vill;
+        public Village getVill()
+        {
+            OpenFileDialog openfileddialog = new OpenFileDialog();
+            if (openfileddialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream fileStream = openfileddialog.OpenFile();
+
+                using (StreamReader sr = new StreamReader(fileStream))
+                {
+                    string json = sr.ReadToEnd();
+                    vill = JsonConvert.DeserializeObject<Village>(json);
+                }
+            }
+            Console.WriteLine(vill);
+            return vill;
+        }
+    }
+}
---
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/SaveVillage.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers.Save
+{
+    class SaveVillage
+    {
+        Village vill;
+        public SaveVillage(Village vill)
+        {
+            this.vill = vill;
+        }
+        public void SaveFile()
+        {
+            SaveFileDialog savefiledialog = new SaveFileDialog();
+            savefiledialog.FileName = "*.aov";
+            savefiledialog.Filter = "AoV file(*.aov)| *.aov";
+            savefiledialog.DefaultExt = "aov";
+            savefiledialog.Title = "Save an aov File";
+            savefiledialog.InitialDirectory = @"H:\aov_saved\";
+            savefiledialog.RestoreDirectory = true;
+            savefiledialog.ShowDialog();
+            if (savefiledialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream fileStream = savefiledialog.OpenFile();
+                using (StreamWriter sw = new StreamWriter(fileStream))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(sw, vill);
+                    sw.Close();
+                }
+                fileStream.Close();
+            }
+        }
+    }
+}
