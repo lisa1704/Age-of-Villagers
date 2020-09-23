ca40da04294534f52dfe7a04a0145a024089c4e0
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:26:16 +0600

    Village Class added

---
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Village.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace AgeOfVillagers
+{
+    public class Village
+    {
+        public string village_name,nation;
+        public List<Point> trees_drawn = new List<Point>();
+        public List<Point> houses_drawn = new List<Point>();
+        public List<Point> rivers_drawn = new List<Point>();
+    }
+}
