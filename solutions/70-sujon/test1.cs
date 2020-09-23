171f5937e2de0ee62ecc70689390370a7bc18436
Shawkat-Sujon <68417059+Shawkat-Sujon@users.noreply.github.com>
2020-09-23 23:40:17 +0600

    #15 test framework for random test

---
+++ solutions/70-sujon/test1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOV
+{
+    class test1
+    {
+            [Fact]
+            public void Test1()
+            {
+                Assert.Equal(10, 10);
+            }
+        
+    }
+}
