6b36057b635613d8ba7b19b831cc89f06d6aaf42
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:18:02 +0600

    #7 #10 draw method implemented in ArabBedouinTree

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/ArabBedouinTree.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/ArabBedouinTree.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class ArabBedouinTree
+    public class ArabBedouinTree : IComponent
     {
+        public Point makePoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, p, makePoint(p.X,p.Y-12));
+            g.DrawLine(pen, makePoint(p.X-2,p.Y-12), makePoint(p.X+2,p.Y-12));
+            g.DrawLine(pen, makePoint(p.X - 2, p.Y - 12), makePoint(p.X - 2, p.Y));
+            g.DrawLine(pen, makePoint(p.X - 2, p.Y), makePoint(p.X + 2, p.Y ));
+            g.DrawLine(pen, makePoint(p.X + 2, p.Y - 12), makePoint(p.X + 2, p.Y));
+            g.DrawLine(pen, makePoint(p.X-8,p.Y-24), makePoint(p.X,p.Y-12));
+            g.DrawLine(pen, makePoint(p.X-4,p.Y-24), makePoint(p.X, p.Y - 12));
+            g.DrawLine(pen, makePoint(p.X+4, p.Y - 24), makePoint(p.X, p.Y - 12));
+            g.DrawLine(pen, makePoint(p.X+8, p.Y - 24), makePoint(p.X, p.Y - 12));
+        }
     }
 }
