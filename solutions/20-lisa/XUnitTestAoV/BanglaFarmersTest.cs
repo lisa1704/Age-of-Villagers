799459d3cf94b73cc8472d1ed244977f6db40ccd
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-23 11:04:07 +0600

    #9 #15 Test case on terrain color of BangladeshiFarmers

---
+++ solutions/20-lisa/XUnitTestAoV/BanglaFarmersTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using AgeOfVillagers;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace UnitTestAoV
+{
+    [TestFixture]
+
+    public class BanglaFarmersTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [TestCase]
+        public void BDTerrainColorTest()
+        {
+            BangladeshiFarmers bdterrainColor = new BangladeshiFarmers();
+            Color expected = Color.Green;
+            Color actual = bdterrainColor.SetTerrain();
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
