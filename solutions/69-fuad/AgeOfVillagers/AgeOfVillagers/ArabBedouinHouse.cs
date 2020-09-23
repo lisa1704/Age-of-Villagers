45d9d7759ecf7be1fe8b7dd8d8db61f83eb301fb
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:17:30 +0600

    #7 #10 draw method implemented in ArabBedouinHouse

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/ArabBedouinHouse.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/ArabBedouinHouse.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class ArabBedouinHouse
+    public class ArabBedouinHouse : IComponent
     {
+        public Point makePoint(int x,int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, p, makePoint( p.X-8,p.Y+16));
+            g.DrawLine(pen, p, makePoint(p.X+8,p.Y+16));
+            g.DrawLine(pen, makePoint(p.X - 8, p.Y + 16),makePoint(p.X + 8, p.Y + 16));
+            g.DrawLine(pen, p, makePoint(p.X + 8, p.Y -3));
+            g.DrawLine(pen, makePoint(p.X + 8, p.Y -3), makePoint(p.X + 16, p.Y + 13));
+            g.DrawLine(pen, makePoint(p.X + 8, p.Y + 16), makePoint(p.X + 16, p.Y + 13));
+        }
     }
 }
