c38e8b0974961424465c37335c7f1de6139853d6
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 16:39:20 +0600

    #1 #9 bangladeshi farmers class implemented

--- solutions/56-dipro/Age_of_villagers/Nation/BangladeshiFarmers .cs
+++ solutions/56-dipro/Age_of_villagers/Nation/BangladeshiFarmers .cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using Age_of_villagers.Components;
 
 namespace Age_of_villagers.Nation
 {
-    class BangladeshiFarmers 
+    class BangladeshiFarmers : INation
     {
+        public string getNationName()
+        {
+            return "Bangladeshi Farmers";
+        }
+
+        public Color getTerrainColor()
+        {
+            return Color.Green;
+        }
+        public VillageComponent getHouse(Point point)
+        {
+            return new Bangladeshi_house(point);
+        }
+
+        public VillageComponent getTree(Point point)
+        {
+            return new Bangladeshi_tree(point);
+        }
+
+        public VillageComponent getWaterSource(Point point)
+        {
+            return new Bangladeshi_watersource(point);
+        }
     }
+
 }
