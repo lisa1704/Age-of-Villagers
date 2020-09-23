52d6ba1d8a51bc01fcdd390952e3087771c5d930
jarin_12 <jarintanzim@iut-dhaka.edu>
2020-09-23 15:39:08 +0600

    #11 egyptwater created

--- solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/egyptwater.cs
+++ solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/egyptwater.cs
@@ -11,13 +11,13 @@ namespace ageofvillagers
         public string draw(int x, int y)
         {
             {
-                Trianglecreate eh_triangle1 = new Trianglecreate(x, y, x - 8, y + 2, x - 2, y + 16);
-                Trianglecreate eh_triangle2 = new Trianglecreate(x, y, x + 8, y + 5, x - 2, y + 16);
+                Trianglecreate triangleone = new Trianglecreate(x, y, x - 8, y + 2, x - 2, y + 16);
+                Trianglecreate triangletwo = new Trianglecreate(x, y, x + 8, y + 5, x - 2, y + 16);
 
-                trinangle.Draws();
-                draw = draw + eh_triangle2.Draw();
+                triangleone.Draws();
+                 triangletwo.Draws();
 
-                return draw;
+                return "null";
             }
         }
     }
