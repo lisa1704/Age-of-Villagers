1f2d63a0cfc41fb8cc6da2d3df32136a62769a1d
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 19:27:43 +0600

    #1 #9 bangladeshi tree code changed

--- solutions/56-dipro/Age_of_villagers/Components/Bangladeshi_tree.cs
+++ solutions/56-dipro/Age_of_villagers/Components/Bangladeshi_tree.cs
@@ -7,20 +7,19 @@ namespace Age_of_villagers.Components
 {
     class Bangladeshi_tree : VillageComponent
     {
-        private readonly Point tL;
-        private readonly Point bR;
-        private readonly Point c;
+        private readonly Point top_Left,bottom_Right,center;
+ 
 
 
         public Bangladeshi_tree(Point point)
         {
-            c = point;
-            tL.X = point.X - 20;
-            tL.Y = point.Y - 20;
-            bR.X = point.X + 20;
-            bR.Y = point.Y + 20;
-            AddComp(new Arc(tL, bR));
-            AddComp(new Shape.Rectangle(new Point(c.X - 2, c.Y), new Point(c.X + 2, c.Y + 40)));
+            center = point;
+            top_Left.X = point.X - 20;
+            top_Left.Y = point.Y - 20;
+            bottom_Right.X = point.X + 20;
+            bottom_Right.Y = point.Y + 20;
+            AddComp(new Arc(top_Left, bottom_Right));
+            AddComp(new Shape.Rectangle(new Point(center.X - 2, center.Y), new Point(center.X + 2, center.Y + 40)));
         }
     }
 }
