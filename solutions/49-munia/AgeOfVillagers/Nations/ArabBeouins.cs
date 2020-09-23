475d2b8eb0f006551ba2f7f72231580b82c4ec8b
Maysha Afrin Munia <mayshaafrin@iut-dhaka.edu>
2020-09-23 19:47:49 +0600

    #10 #11 #12 all nation classes created

---
+++ solutions/49-munia/AgeOfVillagers/Nations/ArabBeouins.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers.Nations
+{
+    class ArabBeouins : INation
+
+    {
+        public void DrawHouse(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTree(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawWater(Graphics g, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color TerrainColor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
