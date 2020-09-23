6e2d81c1ed9262d85f1cb0439316dd20fc39a17a
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 12:23:27 +0600

    #7 Arc class implemented.

--- solutions/53-silvi/AgeOfVillagers/Arc.cs
+++ solutions/53-silvi/AgeOfVillagers/Arc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,20 @@ namespace AgeOfVillagers
 {
     class Arc : IShape
     {
+        private readonly Point point1;
+        private readonly Point point2;
+        public int x, y, w, h, a1, a2;
+        public Arc(Point point1, Point point2)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+        }
 
+        public void Draw(Graphics g)            
+        {
+            throw new NotImplementedException(); var pen = new Pen(Color.Black, 1);
+            g.DrawArc(pen, x, y, w, h, a1, a2);
+            
+        }
     }
 }
