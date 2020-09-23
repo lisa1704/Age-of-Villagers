69a742fd447e3d6c6c95809b0a63101568120ff8
Tanvirus06 <tanvirrahman3344@gmail.com>
2020-09-23 12:38:40 +0600

    Create circle.cs

---
+++ solutions/06-tanvir/AgeOfVillagers/circle.cs
@@ -0,0 +1,30 @@
+using System.Drawing;
+
+
+
+namespace AgeOfVillagers.Shapes
+{
+    class Circle : IShape
+    {
+        private Point x;
+        private int y, width, height, radius;
+        Graphics grap;
+        Pen pen;
+
+
+        public Circle(Graphics grap, Pen pen, Point x, int y, int width, int height, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.radius = radius;
+            this.g = grap;
+            this.pen = pen;
+        }
+        public void CreateShape()
+        {
+            g.DrawArc(pen, x.X, x.Y, y, width, height, radius);
+        }
+    }
+}
