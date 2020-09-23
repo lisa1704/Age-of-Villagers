03155d994443a45b0e868968cdb9e27a6d57209c
Rizwan Khan <rizwanulhaque19@gmail.com>
2020-09-23 07:41:13 +0600

    #1 #12 seperated InuitHunters house creation

---
+++ solutions/78-rizwan/AgeofVillagers/AgeofVillagers/InuitHouse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeofVillagers
+{
+    class InuitHouse : Ihouse
+    {
+        Graphics g;
+        Pen p = new Pen(Color.Red, 3);
+        public InuitHouse(Graphics g)
+        {
+            this.g = g;
+        }
+        public void createHouse(MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            g.DrawArc(p, x - 25, y - 25, 50, 50, 0, -180);
+            g.DrawArc(p, x - 50, y - 50, 100, 100, 0, -180);
+            g.DrawLine(p, x - 50, y, x + 50, y);
+        }
+    }
+}
--- solutions/78-rizwan/AgeofVillagers/AgeofVillagers/InuitHunters.cs
+++ solutions/78-rizwan/AgeofVillagers/AgeofVillagers/InuitHunters.cs
@@ -9,9 +9,7 @@ namespace AgeofVillagers
 
     class InuitHunters : Ination
     {
-        Pen p = new Pen(Color.Red, 3);
         Graphics g;
-        int x, y;
         public InuitHunters(Graphics g)
         {
             this.g = g;
@@ -19,12 +17,8 @@ namespace AgeofVillagers
 
         public void createHouse(MouseEventArgs e)
         {
-            int x = e.X;
-            int y = e.Y;
-
-            g.DrawArc(p, x - 25, y - 25, 50, 50, 0, -180);
-            g.DrawArc(p, x - 50, y - 50, 100, 100, 0, -180);
-            g.DrawLine(p, x - 50, y, x + 50, y);
+            InuitHouse inuithouse = new InuitHouse(g);
+            inuithouse.createHouse(e);
         }
 
         public void createTree(MouseEventArgs e)
