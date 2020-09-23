d0a2a6f7dad85f9606f7f0575009cfaadadd7902
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-23 20:09:35 +0600

    #2  #7  half cirlce created

---
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/HalfCricle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class HalfCirlce
+{
+    private int x, y, radius;
+    public HalfCircle(int x, int y, int radius)
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+
+    }
+    public string Draw()
+    {
+        return "Drawing a half circle";
+    }
+}
