22150ecc7e6f529eb29e8c222fecec4e5405381c
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:16:20 +0600

    #7 #9 draw method implemented in BangladeshiFarmersTree

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersTree.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersTree.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class BangladeshiFarmersTree
+    public class BangladeshiFarmersTree : IComponent
     {
+        public Point makePoint(int x,int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawArc(pen, p.X, p.Y, 16, 16, 0, 360);
+            g.DrawLine(pen, makePoint(p.X+6,p.Y+16), makePoint(p.X+10,p.Y+16));
+            g.DrawLine(pen, makePoint(p.X + 6, p.Y + 16), makePoint(p.X+6,p.Y+32));
+            g.DrawLine(pen, makePoint(p.X + 10, p.Y + 32), makePoint(p.X + 10, p.Y + 16));
+            g.DrawLine(pen, makePoint(p.X + 6, p.Y + 32), makePoint(p.X + 10, p.Y + 32));
+        }
     }
 }
