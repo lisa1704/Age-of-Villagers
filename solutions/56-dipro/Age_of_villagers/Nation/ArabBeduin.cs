4a8d88ab376f83237e3256f646b0739aab428c55
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 19:09:16 +0600

    #11 Egyptian king class

--- solutions/56-dipro/Age_of_villagers/Nation/ArabBeduin.cs
+++ solutions/56-dipro/Age_of_villagers/Nation/ArabBeduin.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
 using System.Text;
-using System.Threading.Tasks;
+using Age_of_villagers.Components;
+using Age_of_villagers.Nation;
 
-namespace Age_of_villagers.Nation
+namespace Age_of_villagers.Nations
 {
-    class ArabBeduin
+    class ArabBeduin : INation
     {
+        public VillageComponent getHouse(Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getNationName()
+        {
+            return "Arab Beduins";
+        }
+
+        public Color getTerrainColor()
+        {
+            return Color.LightYellow;
+        }
+
+        public VillageComponent getTree(Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VillageComponent getWaterSource(Point p)
+        {
+            throw new NotImplementedException();
+        }
     }
+
 }
--- solutions/56-dipro/Age_of_villagers/Nation/Egyptian_Kings.cs
+++ solutions/56-dipro/Age_of_villagers/Nation/Egyptian_Kings.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using Age_of_villagers.Components;
 
 namespace Age_of_villagers.Nation
 {
-    class Egyptian_Kings
+    class Egyptian_Kings : INation
     {
+        public string getNationName()
+        {
+            return "Egyptian Kings";
+        }
+
+        public Color getTerrainColor()
+        {
+            return Color.Yellow;
+        }
+        public VillageComponent getHouse(Point point)
+        {
+            return new Bangladeshi_house(point);
+        }
+
+        public VillageComponent getTree(Point point)
+        {
+            return new Bangladeshi_tree(point);
+        }
+
+        public VillageComponent getWaterSource(Point point)
+        {
+            return new Bangladeshi_watersource(point);
+        }
     }
+
 }
