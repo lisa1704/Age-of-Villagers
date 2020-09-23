385b5a8044ef1c97b5eb63f06330de96cb948eb0
jarin_12 <jarintanzim@iut-dhaka.edu>
2020-09-23 15:46:05 +0600

    #11 egypt tree created

--- solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/egypttree.cs
+++ solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/egypttree.cs
@@ -10,7 +10,25 @@ namespace ageofvillagers
     {
         public string draw(int x, int y)
         {
-            return "egypt tree";
+            Linecreate line1 = new Linecreate(x, y, x + 3, y + 13);
+            Linecreate line2 = new Linecreate(x, y, x - 4, y + 16);
+            Linecreate line3 = new Linecreate(x + 3, y + 13, x + 2, y + 24);
+            Linecreate line4 = new Linecreate(x + 3, y + 13, x + 6, y + 24);
+            Linecreate line5 = new Linecreate(x + 3, y + 13, x + 8, y + 23);
+            Linecreate line6 = new Linecreate(x - 4, y + 16, x - 3, y + 24);
+            Linecreate line7 = new Linecreate(x - 4, y + 16, x - 6, y + 22);
+            Linecreate line8 = new Linecreate(x - 4, y + 16, x - 8, y + 21);
+
+            line1.Draws();
+           line2.Draws();
+            line3.Draws();
+            line4.Draws();
+           line5.Draws();
+          line6.Draws();
+          line7.Draws();
+          line8.Draws();
+
+            return "null";
         }
     }
 }
