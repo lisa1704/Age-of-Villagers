deea57ff92f462f0c2636ba41d9bea09b93cbbd9
Istiaq Siaam <i.b.s.siaam@gmail.com>
2020-09-23 18:57:29 +0600

    #1 removed some unncessery code and library #4 clear screen redone

--- solutions/14-siaam/age of villagers/age of villagers/Form1.cs
+++ solutions/14-siaam/age of villagers/age of villagers/Form1.cs
@@ -100,6 +100,7 @@ namespace age_of_villagers
         private void new_vill_Click(object sender, EventArgs e)
         {
             houses.Clear();
+            trees.Clear();
             panel1.Refresh();
             panel1.BackColor = Color.Transparent;
             nation.ResetText();
