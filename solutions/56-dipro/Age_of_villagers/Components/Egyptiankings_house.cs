da1970e97b65b2d4b5192b763e9493ccb6b1f09c
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 20:09:51 +0600

    #11 Egyptian kings house partial implemented

--- solutions/56-dipro/Age_of_villagers/Components/Egyptiankings_house.cs
+++ solutions/56-dipro/Age_of_villagers/Components/Egyptiankings_house.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Age_of_villagers.Shape;
+
 
 namespace Age_of_villagers.Components
 {
-    class Egyptiankings_house
+    class Egyptiankings_house : VillageComponent
     {
+        private readonly Point top, mid_Left, mid_Right, bottom_Left, bottom_Right;
+
+        public Egyptiankings_house (Point point)
+        {
+            top = point;
+            mid_Left = new Point(top.X - 10, top.Y + 20);
+            mid_Right = new Point(top.X + 10, top.Y + 20);
+            bottom_Left = new Point(top.X - 10, top.Y + 40);
+            bottom_Right = new Point(top.X + 10, top.Y + 40);
+      
+        }
     }
 }
