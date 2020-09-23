76b9e65f00422c4d82f25a517089bd21a54c4167
Tanvirus06 <tanvirrahman3344@gmail.com>
2020-09-23 14:42:23 +0600

    Update house.cs

--- solutions/06-tanvir/AgeOfVillagers/house.cs
+++ solutions/06-tanvir/AgeOfVillagers/house.cs
@@ -13,5 +13,18 @@ namespace AgeOfVillagers
         protected int length, width;
         protected ShapeCombine sc = new ShapeCombine();
 
+        public house(Point p,int length, int width )
+        {
+            this.p = p;
+            this.l = length;
+            this.w = width;
+        }
+        public abstract void BuildHouse();
+
+        public void Draw(Graphics grap, Pen pen)
+        {
+            this.BuildHouse();
+            sc.Draw(grap, pen);
+        }
     }
 }
