d52bcc7c6cc46aa8db0ec51c11cecb685aacbc02
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2020-09-23 22:59:28 +0600

    #8 Nation Null Instance Added

---
+++ solutions/18-nafis/AoV/Nations/NullNation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AoV.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoV.Nations
+{
+    public class NullNation : INation
+    {
+        public Color TerrainColor()
+        {
+            return Color.Empty;
+        }
+
+        public string getName()
+        {
+            return "NullNation";
+        }
+        public void DrawHouse(Graphics g, Point p)
+        {
+            MessageBox.Show("Please select a nation to draw");
+        }
+
+        public void DrawTree(Graphics g, Point p)
+        {
+            MessageBox.Show("Please select a nation to draw");
+        }
+
+        public void DrawWaterSource(Graphics g, Point p)
+        {
+            MessageBox.Show("Please select a nation to draw");
+        }
+    }
+
+}
