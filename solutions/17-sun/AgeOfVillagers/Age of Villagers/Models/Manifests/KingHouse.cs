baa6b55e8f5d19fc9e9040648b5a8b93c8f084b7
khansun <khan.mahmud@outlook.com>
2020-09-23 22:57:33 +0600

    #11 Egyptians implemted fully

--- solutions/17-sun/AgeOfVillagers/Age of Villagers/Models/Manifests/KingHouse.cs
+++ solutions/17-sun/AgeOfVillagers/Age of Villagers/Models/Manifests/KingHouse.cs
@@ -1,19 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Age_of_Villagers.Models.Manifests
 {
     class KingHouse : IShape
     {
+        List<Line> LineList = new List<Line>();
         public KingHouse(Point start)
         {
+            Point end = start;
+            end.Y = end.Y - 12;
+            end.X = end.X + 6;
+            LineList.Add(new Line(start, end));
+            start = end;
+            end.X = end.X + 10;
+            end.Y = end.Y + 8;
+            LineList.Add(new Line(start, end));
+            start = end;
+            end.X = end.X - 6;
+            end.Y = end.Y + 8;
+            LineList.Add(new Line(start, end));
+            start = end;
+            end.Y = end.Y-16;
+            end.X = end.X - 4;
+            LineList.Add(new Line(start, end));
+            end.X = end.X - 6;
+            end.Y = end.Y + 12;
+            LineList.Add(new Line(start, end));
+
+
         }
 
         public void Draw(Graphics gfx, Pen p)
         {
-            throw new NotImplementedException();
+
+            foreach (Line iLine in LineList)
+            {
+                iLine.Draw(gfx, p);
+            }
         }
     }
 }
