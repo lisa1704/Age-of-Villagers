bfb84aab758ea4534746a3fdddcf4915b0277098
musfiksifat <musfiksifat766@gmail.com>
2020-09-23 20:52:05 +0600

    #10. Testing:: TerrainColorTest for arab nation complete

---
+++ solutions/88-musfik/AgeOfVillagers.Tests/TerrainColorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Xunit;
+
+namespace AgeOfVillagers.Tests
+{
+    public class TerrainColorTest
+    {
+        [Fact]
+        public void Arab_TerrainColorTest()
+        {
+            INation nation = new ArabBedouin();
+            var expected = Color.LightGoldenrodYellow;
+            var actual = nation.getTerrainColor();
+            Assert.Equal(expected, actual);
+        }
+
+        
+
+    }
+}
