671ff8d746813791beb56a84b4a9032f944721da
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 07:55:46 +0600

    #11 Functionalities for Egypt Class created

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Egypt.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Egypt.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class Egypt
+    class Egypt : INation
     {
+        public Color getBackgroundColor()
+        {
+            return Color.Yellow;
+        }
+        public IComponent getComponent(string component_type)
+        {
+            if (component_type == "tree")
+            {
+                return new EgyptianKingsTree();
+            }
+            else if (component_type == "house")
+            {
+                return new EgyptianKingsHouse();
+            }
+            else if (component_type == "water_source")
+            {
+                return new EgyptianKingsWaterSource();
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
