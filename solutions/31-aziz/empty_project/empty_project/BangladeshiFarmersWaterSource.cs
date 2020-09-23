782f7ce050af93979f281cf4e34fefb95c93a8d1
Asif Aziz <asifaziz2061@gmail.com>
2020-09-23 09:58:06 +0600

    #1 Bangladeshi Farmers Water Source reshaped to 24px by 16px

--- solutions/31-aziz/empty_project/empty_project/BangladeshiFarmersWaterSource.cs
+++ solutions/31-aziz/empty_project/empty_project/BangladeshiFarmersWaterSource.cs
@@ -13,15 +13,15 @@ namespace empty_project
             Pen pen = new Pen(Color.Black);
             Graphics graphics = panel.CreateGraphics();
 
-            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X + 15, axis.Y + 15));
-            graphics.DrawLine(pen, new PointF(axis.X + 15, axis.Y + 15), new PointF(axis.X + 25, axis.Y + 10));
-            graphics.DrawLine(pen, new PointF(axis.X + 25, axis.Y + 10), new PointF(axis.X + 35, axis.Y + 15));
-            graphics.DrawLine(pen, new PointF(axis.X + 35, axis.Y + 15), new PointF(axis.X + 50, axis.Y));
-            graphics.DrawLine(pen, new PointF(axis.X +50, axis.Y), new PointF(axis.X + 45, axis.Y -25));
-            graphics.DrawLine(pen, new PointF(axis.X + 45, axis.Y - 25), new PointF(axis.X + 25, axis.Y - 20));
-            graphics.DrawLine(pen, new PointF(axis.X + 25, axis.Y - 20), new PointF(axis.X + 20, axis.Y - 30));
-            graphics.DrawLine(pen, new PointF(axis.X + 20, axis.Y - 30), new PointF(axis.X + 10, axis.Y -20));
-            graphics.DrawLine(pen, new PointF(axis.X + 10, axis.Y - 20), new PointF(axis.X, axis.Y));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y), new PointF(axis.X - 3, axis.Y + 2));
+            graphics.DrawLine(pen, new PointF(axis.X - 3, axis.Y + 2), new PointF(axis.X - 8, axis.Y + 10));
+            graphics.DrawLine(pen, new PointF(axis.X - 8, axis.Y + 10), new PointF(axis.X -2, axis.Y + 16));
+            graphics.DrawLine(pen, new PointF(axis.X - 2, axis.Y + 16), new PointF(axis.X, axis.Y + 12));
+            graphics.DrawLine(pen, new PointF(axis.X, axis.Y + 12), new PointF(axis.X + 5, axis.Y + 16));
+            graphics.DrawLine(pen, new PointF(axis.X + 5, axis.Y + 16), new PointF(axis.X + 16, axis.Y + 8));
+            graphics.DrawLine(pen, new PointF(axis.X + 16, axis.Y + 8), new PointF(axis.X + 7, axis.Y));
+            graphics.DrawLine(pen, new PointF(axis.X + 7, axis.Y), new PointF(axis.X + 3, axis.Y + 4));
+            graphics.DrawLine(pen, new PointF(axis.X + 3, axis.Y + 4), new PointF(axis.X, axis.Y));
         }
     }
 }
