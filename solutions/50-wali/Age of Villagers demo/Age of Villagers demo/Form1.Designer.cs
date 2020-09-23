b6db80ab220af2182b391ff760468bfc5343cab0
alhussain50 <44936730+alhussain50@users.noreply.github.com>
2020-09-23 23:43:24 +0600

    created another textbox for name of nation

--- solutions/50-wali/Age of Villagers demo/Age of Villagers demo/Form1.Designer.cs
+++ solutions/50-wali/Age of Villagers demo/Age of Villagers demo/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // tree
@@ -70,11 +71,20 @@
             this.textBox2.TabIndex = 3;
             this.textBox2.Text = "Katakhali";
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(593, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 27);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Bangladeshi Farmers";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -92,6 +102,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
