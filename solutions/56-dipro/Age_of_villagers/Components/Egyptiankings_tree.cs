f1d611565866c3f5e3533e23b3cf1d8ecf702835
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 21:08:59 +0600

    #1 #7 #11 egyptain tree

--- solutions/56-dipro/Age_of_villagers/Components/Egyptiankings_tree.cs
+++ solutions/56-dipro/Age_of_villagers/Components/Egyptiankings_tree.cs
@@ -9,7 +9,21 @@ namespace Age_of_villagers.Components
 {
     class Egyptiankings_tree : VillageComponent
     {
-
+        public Point makePoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, makePoint(p.X - 5, p.Y + 12), makePoint(p.X, p.Y + 24));
+            g.DrawLine(pen, makePoint(p.X + 5, p.Y + 12), makePoint(p.X, p.Y + 24));
+            g.DrawLine(pen, makePoint(p.X - 5, p.Y + 12), makePoint(p.X - 8, p.Y + 4));
+            g.DrawLine(pen, makePoint(p.X - 5, p.Y + 12), makePoint(p.X - 5, p.Y + 2));
+            g.DrawLine(pen, makePoint(p.X - 5, p.Y + 12), makePoint(p.X - 2, p.Y));
+            g.DrawLine(pen, makePoint(p.X + 5, p.Y + 2), makePoint(p.X + 5, p.Y + 12));
+            g.DrawLine(pen, makePoint(p.X + 2, p.Y), makePoint(p.X + 5, p.Y + 12));
+            g.DrawLine(pen, makePoint(p.X + 8, p.Y + 4), makePoint(p.X + 5, p.Y + 12));
+        }
 
     }
 }
