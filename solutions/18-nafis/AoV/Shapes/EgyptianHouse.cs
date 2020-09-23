4a76345cea8abf05bd2054f7f0923d069101a149
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2020-09-23 21:46:50 +0600

    #7 Egyptian House Added

---
+++ solutions/18-nafis/AoV/Shapes/EgyptianHouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AoV.Shapes
+{
+    class EgyptianHouse : ComposeShape
+    {
+        public EgyptianHouse(Point clickPoint)
+        {
+            Point p1 = new Point(clickPoint.X - 24, clickPoint.Y + 50);
+            Point p2 = new Point(clickPoint.X + 16, clickPoint.Y + 50);
+            Point p3 = new Point(clickPoint.X + 35, clickPoint.Y + 40);
+
+
+            AddShape(new Triangle(p2, p1, clickPoint));
+            AddShape(new Triangle(p3, p2, clickPoint));
+        }
+    }
+}
