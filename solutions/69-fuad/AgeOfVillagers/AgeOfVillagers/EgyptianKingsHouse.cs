9ce052e0b3ed1de8e82be5f828f393ab276d0a28
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:18:33 +0600

    #7 #11 draw method implemented in EgyptianKingsHouse

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/EgyptianKingsHouse.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/EgyptianKingsHouse.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class EgyptianKingsHouse
+    public class EgyptianKingsHouse : IComponent
     {
+        public Point makePoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, p, makePoint(p.X + 4, p.Y + 16));
+            g.DrawLine(pen, p, makePoint(p.X - 8, p.Y + 13));
+            g.DrawLine(pen, p, makePoint(p.X + 8, p.Y + 9));
+            g.DrawLine(pen, makePoint(p.X + 8, p.Y + 9), makePoint(p.X + 4, p.Y + 16));
+            g.DrawLine(pen, makePoint(p.X - 8, p.Y + 13), makePoint(p.X + 4, p.Y + 16));
+        }
     }
 }
