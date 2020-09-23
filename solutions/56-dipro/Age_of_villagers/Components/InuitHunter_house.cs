857fe6848d7b80585d8660c159487914b3234f00
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 19:44:23 +0600

    #1 #7 arc shape refactored

--- solutions/56-dipro/Age_of_villagers/Components/InuitHunter_house.cs
+++ solutions/56-dipro/Age_of_villagers/Components/InuitHunter_house.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Age_of_villagers.Shape;
+
 
 namespace Age_of_villagers.Components
 {
-    class InuitHunter_house
+    class InuitHunter_house : VillageComponent
     {
+
     }
 }
--- solutions/56-dipro/Age_of_villagers/Shape/Arc.cs
+++ solutions/56-dipro/Age_of_villagers/Shape/Arc.cs
@@ -11,19 +11,19 @@ namespace Age_of_villagers.Shape
     class Arc : IShape
     {
         private readonly Point point;
-        private float x, y, w, h, sA, swA;
+        private float x, y, width, height, start_Angle, sweep_Angle;
         public Arc(Point topLeft, Point bottomRight)
         {
             x = topLeft.X;
             y = topLeft.Y;
-            w = bottomRight.X - topLeft.X;
-            h = bottomRight.Y - topLeft.Y;
-            sA = 0;
-            swA = 180;
+            width = bottomRight.X - topLeft.X;
+            height = bottomRight.Y - topLeft.Y;
+            start_Angle = 0;
+            sweep_Angle = 180;
         }
         public void draw(Graphics g, Pen p)
         {
-            g.DrawArc(p, x, y, w, h, sA, swA);
+            g.DrawArc(p, x, y, width, height, start_Angle, sweep_Angle);
         }
     }
 }
