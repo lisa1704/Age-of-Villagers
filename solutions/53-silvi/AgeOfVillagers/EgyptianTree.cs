88d3d7e26a97c2d514ee5bde30274e07c70d158b
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 10:03:42 +0600

    #11 EgyptianTree class implemented.

--- solutions/53-silvi/AgeOfVillagers/EgyptianTree.cs
+++ solutions/53-silvi/AgeOfVillagers/EgyptianTree.cs
@@ -6,7 +6,18 @@ namespace AgeOfVillagers
     {
         public void Draw(Point e, Graphics g, Pen p)
         {
-            
+            int x = e.X;
+            int y = e.Y;
+            g.DrawLine(p, x, y, x + 10, y + 10);
+            g.DrawLine(p, x + 10, y + 10, x + 20, y);
+            g.DrawLine(p, x - 10, y - 5, x, y);
+            g.DrawLine(p, x - 5, y - 10, x, y);
+            g.DrawLine(p, x + 5, y - 10, x, y);
+
+            g.DrawLine(p, x + 20, y - 10, x + 20, y);
+            g.DrawLine(p, x + 25, y - 10, x + 20, y);
+            g.DrawLine(p, x + 30, y - 5, x + 20, y);
+
         }
     }
 }
