af5216a4f62f98df9a1f4918870ee0afa9932715
akib <akibahmed@iut-dhaka.edu>
2020-09-23 20:07:35 +0600

    #12 Changed InuitHunter_House

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/InuitHunters_House.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/InuitHunters_House.cs
@@ -7,8 +7,10 @@
         public string Draw(int x, int y)
         {
             Half_Circle ihh_circle = new Half_Circle(x, y, 8);
+            Half_Circle ihh2_circle = new Half_Circle(x, y, 3);
 
             draw = draw + ihh_circle.Draw_Shape();
+            draw = draw + ihh2_circle.Draw_Shape();
 
             return draw;
         }
