3931d5d88fd28c43b6b127cc18bd8db195bb4461
tiashaa <hriditatiasha@gmail.com>
2020-09-23 22:31:08 +0600

    #1 Unit test refactored

--- solutions/41-tiasha/Age of Villagers Test/UnitTest1.cs
+++ solutions/41-tiasha/Age of Villagers Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 
 namespace Age_of_Villagers_Test
@@ -10,8 +11,32 @@ namespace Age_of_Villagers_Test
         [TestMethod]
         public void BangladeshiNation()
         {
-           
-            
+            var nationCreator = new NationCreator();
+            INation nation = nationCreator.GetNation("Bangladeshi Farmers");
+            var obj1Str = JsonConvert.SerializeObject(nation);
+            var obj2Str = JsonConvert.SerializeObject(new Bangladeshi());
+            Assert.Equals(obj1Str, obj2Str);
         }
     }
+
+    [TestMethod]
+    public void EgyptianNationn()
+    {
+        var nationCreator = new NationCreator();
+        INation nation = nationCreator.GetNation("Egyptian Kings");
+        var obj1Str = JsonConvert.SerializeObject(nation);
+        var obj2Str = JsonConvert.SerializeObject(new Egyptian());
+        Assert.Equals(obj1Str, obj2Str);
+    }
+
+    [TestMethod]
+    public void InuitNation()
+    {
+        var nationCreator = new NationCreator();
+        INation nation = nationCreator.GetNation("Inuit Hunters");
+        var obj1Str = JsonConvert.SerializeObject(nation);
+        var obj2Str = JsonConvert.SerializeObject(new Inuit());
+        Assert.Equals(obj1Str, obj2Str);
+    }
+
 }
--- solutions/41-tiasha/Age of Villagers/Nation/NationCreator.cs
+++ solutions/41-tiasha/Age of Villagers/Nation/NationCreator.cs
@@ -19,5 +19,8 @@ namespace Age_of_Villagers
 
             return null;
         }
+
+
+
     }
 }
