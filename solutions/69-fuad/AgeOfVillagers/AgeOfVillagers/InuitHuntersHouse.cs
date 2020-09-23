94a958527b2c1834104234d060a46592baecc38f
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:19:55 +0600

    #7 #12 draw method implemented in InuitHuntersHouse

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/InuitHuntersHouse.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/InuitHuntersHouse.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class InuitHuntersHouse
+
+    public class InuitHuntersHouse : IComponent
     {
+        public Point makePoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawArc(pen, p.X, p.Y, 16, 16, 180, 180);
+            g.DrawArc(pen, p.X + 5, p.Y + 5, 6, 6, 180, 180);
+            g.DrawLine(pen, makePoint(p.X, p.Y + 8), makePoint(p.X+16, p.Y + 8));
+        }
     }
 }
