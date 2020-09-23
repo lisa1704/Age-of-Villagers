a50282f5a5da320609ab3a36e70021d9507f539a
Tasnuva12 <47001609+Tasnuva12@users.noreply.github.com>
2020-09-23 11:46:36 +0600

    #6#7  village class is created

--- solutions/37-tasnuva/AgeOfVillegers/village.cs
+++ solutions/37-tasnuva/AgeOfVillegers/village.cs
@@ -7,6 +7,16 @@ namespace AgeOfVillegers
 {
 	public class  village 
 	{
-		
+		public string name;
+		public List<Point> house_point;
+		public List<Point> tree_point;
+		public List<Point> waterresource_point;
+		public village(string name, List<Point> house_point, List<Point> tree_point, List<Point> waterresource_point)
+		{
+			this.name = name;
+			this.house_point = house_point;
+			this.tree_point = tree_point;
+			this.waterresource_point = waterresource_point;
+		}
 	}
 }
