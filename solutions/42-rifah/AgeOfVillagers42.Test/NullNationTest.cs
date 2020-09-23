0ec2843e2a0ccd0ec9c57a6c9414ae5b70cd38dc
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-23 08:59:15 +0600

    #15 Null Nation BG Color tested

---
+++ solutions/42-rifah/AgeOfVillagers42.Test/NullNationTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AgeOfVillagers;
+using System.Drawing;
+
+namespace AgeOfVillagers42.Test
+{
+    [TestClass]
+    public class NullNationTest
+    {
+        [TestMethod]
+        public void BackgroundColorTest()
+        {
+            NullNation NullNationBackgroundColor = new NullNation();
+            Color actual = NullNationBackgroundColor.BackgroundColor();
+            Color expected = Color.White;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
