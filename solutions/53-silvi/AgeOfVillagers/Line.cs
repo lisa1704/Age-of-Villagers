d0c26f4243c8adbde8df5ff47e46a7c7a62b1761
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 11:56:44 +0600

    #7 class for line created.

---
+++ solutions/53-silvi/AgeOfVillagers/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class Line : IShape
+    {
+        public void Draw()
+        {
+            
+        }
+    }
+}
