20cfff822066302174eeb2325f159bbf2fb80abd
Rafid Islam <rafidislam@iut-dhaka.edu>
2020-09-23 19:52:27 +0600

    #1  #10 refactored arab tree

--- solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Arabtree.cs
+++ solutions/09-rafid/age of villagers/age of villagers/ageofvillagers/Arabtree.cs
@@ -2,25 +2,25 @@
 
 public class Arabtree : IShape
 {
-    public void CreateTree(int x, int y)
+    public void draw(int x, int y)
     {
         Rectangle arabt_rectangle = new Rectangle(x + 1, y, x + 1, y - 12, x - 1, y - 12, x - 1, y);
-        arabt_rectangle.Draw_Shape();
+        arabt_rectangle.Draw();
 
         Lines arabt_line_1 = new Lines(x, y, x, y + 12);
-        arabt_line_1.Draw_Shape();
+        arabt_line_1.Draw();
 
         Lines arabt_line_2 = new Lines(x, y, x + 8, y + 3);
-        arabt_line_2.Draw_Shape();
+        arabt_line_2.Draw();
 
         Lines arabt_line_3 = new Lines(x, y, x - 8, y + 3);
-        arabt_line_3.Draw_Shape();
+        arabt_line_3.Draw();
 
         Lines arabt_line_4 = new Lines(x, y, x + 5, y + 8);
-        arabt_line_4.Draw_Shape();
+        arabt_line_4.Draw();
 
         Lines arabt_line_5 = new Lines(x, y, x - 5, y + 8);
-        arabt_line_5.Draw_Shape();
+        arabt_line_5.Draw();
 
 
     }
