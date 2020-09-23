423b0f1d93acad2b490c641246ebcfa94eb819f1
Sakib58 <rabiulahmed.sakib@gmail.com>
2020-09-23 19:11:41 +0600

    #12 transform rough house shape to proper house shape for Inuit Hunters

--- solutions/58-sakib-2/src/House/InuitHuntersHouse.java
+++ solutions/58-sakib-2/src/House/InuitHuntersHouse.java
@@ -1,13 +1,18 @@
 package House;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.ArcType;
 
 import java.awt.*;
 
 public class InuitHuntersHouse implements IHouse {
+    GraphicsContext gc;
 
     @Override
     public void draw(Canvas canvas, Point point) {
-
+        gc=canvas.getGraphicsContext2D();
+        gc.strokeArc(point.getX()+15,point.getY()+2,10,5,0,180, ArcType.ROUND);
+        gc.strokeArc(point.getX()+10,point.getY(),20,10,0,180, ArcType.ROUND);
     }
 }
