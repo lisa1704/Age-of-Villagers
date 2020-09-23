652a3a53c4dc9e412a914060f2b2b8f6001e688b
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-09-23 15:51:42 +0600

    #9 #7  Bangladeshi house

---
+++ solutions/56-dipro/Age_of_villagers/Components/Bangladeshi_house.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Age_of_villagers.Shape;
+
+
+namespace Age_of_villagers.Components
+{
+    class Bangladeshi_house : VillageComponent
+    {
+        private readonly Point t, mL, mR, bL, bR;
+
+        public Bangladeshi_house(Point point)
+        {
+            t = point;
+            mL = new Point(t.X - 10, t.Y + 20);
+            mR = new Point(t.X + 10, t.Y + 20);
+            bR = new Point(t.X + 10, t.Y + 40);
+            AddComp(new Triangle(t, mL, mR));
+            AddComp(new Shape.Rectangle(mL, bR));
+        }
+    }
+}
