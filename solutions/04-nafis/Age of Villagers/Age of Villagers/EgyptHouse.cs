d947111560c4465b2da8f6ef959c1d0e2b93b1c8
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-23 16:19:15 +0600

    #11 Egypt House Drawing is added

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/EgyptHouse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    class EgyptHouse:CompositeShape
+    {
+        Point point;
+        public EgyptHouse(Point point)
+        {
+            this.point = point;
+            Point TopLeft = new Point(point.X - 8, point.Y + 24);
+            Point BottomRight = new Point(point.X + 8, point.Y + 16);
+            Point TopRight = new Point(BottomRight.X, TopLeft.Y);
+            Point BottomBehind = new Point(TopRight.X + 4, TopRight.Y - 16);
+
+
+            ShapePush(new Traingle(point,TopLeft,TopRight));
+            ShapePush(new Line(point,BottomBehind));
+            ShapePush(new Line(TopRight,BottomBehind));
+
+        }
+    }
+}
