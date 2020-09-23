0a4ba11c72f48ea5bd8ff127d1bbb1a4fc80a8a9
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-23 16:28:19 +0600

    #11 Egypt Tree Drawing is Added

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/EgyptTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    class EgyptTree:CompositeShape
+    {
+        Point point;
+
+        public EgyptTree(Point point)
+        {
+            this.point = point;
+
+            Point TopRight = new Point(point.X + 8, point.Y - 12);
+            Point TopLeft = new Point(point.X - 8, point.Y - 12);
+
+            ShapePush(new Line(point, TopLeft));
+            ShapePush(new Line(point, TopRight));
+            
+            ShapePush(new Line(TopRight, new Point(TopRight.X + 8,TopRight.Y - 8)));
+            ShapePush(new Line(TopRight, new Point(TopRight.X - 8, TopRight.Y - 8)));
+            ShapePush(new Line(TopRight, new Point(TopRight.X, TopRight.Y - 12)));
+
+            ShapePush(new Line(TopLeft, new Point(TopLeft.X + 8, TopLeft.Y - 8)));
+            ShapePush(new Line(TopLeft, new Point(TopLeft.X - 8, TopLeft.Y - 8)));
+            ShapePush(new Line(TopLeft, new Point(TopLeft.X, TopLeft.Y - 12)));
+
+        }
+
+    }
+}
