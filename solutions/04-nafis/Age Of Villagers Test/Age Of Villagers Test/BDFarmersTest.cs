d7a72ee4022c877dab93fee59b39341c9cfa699f
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-23 22:30:20 +0600

    #9 Test case for Bangladeshi Farmers

---
+++ solutions/04-nafis/Age Of Villagers Test/Age Of Villagers Test/BDFarmersTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Age_of_Villagers;
+using Xunit;
+
+namespace Age_Of_Villagers_Test
+{
+    public class BDFarmersTest
+    {
+        private static string check = "Bangladeshi Farmers";
+        NationController nationController = new NationController(check);
+        Point point = new Point(70,70);
+        
+        [Fact]
+        public void BDBackgroundTest()
+        {
+            var result = man.getNation(new Point()).GetTerrainColor();
+            Assert.Equal(Color.LightGreen, result);
+        }
+    }
+
+}
