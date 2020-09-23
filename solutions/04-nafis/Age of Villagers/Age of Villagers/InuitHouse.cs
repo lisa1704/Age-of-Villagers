4d4af85b4cdd8e97d2b83ede8da707db7976897c
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-23 18:15:54 +0600

    #12 Inuit Hunters House Drawing is Added

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/InuitHouse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    class InuitHouse:CompositeShape
+    {
+        Point point;
+
+        public InuitHouse(Point point)
+        {
+            this.point = point;
+            ShapePush(new Arc(point, 30, 30, 0, -180));
+            ShapePush(new Arc(new Point(point.X + 10, point.Y + 10), 10, 10, 0, -180));
+            ShapePush(new Line(new Point(point.X, point.Y + 15), new Point(point.X + 30, point.Y + 15)));
+
+        }
+    }
+}
--- solutions/04-nafis/Age of Villagers/Age of Villagers/InuitHunters.cs
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/InuitHunters.cs
@@ -15,7 +15,7 @@ namespace Age_of_Villagers
         }
         public IShape HouseDraw()
         {
-            throw new NotImplementedException();
+            return new InuitHouse(point);
         }
 
         public Point GetHousePoint()
