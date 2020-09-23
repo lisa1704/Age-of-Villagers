14fda87ee7f6482149ab5583567ca16f27192834
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-09-23 18:36:20 +0600

    #1 #11 #12 Nul Shape is implemented

---
+++ solutions/04-nafis/Age of Villagers/Age of Villagers/NullShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Age_of_Villagers
+{
+    class NullShape : IShape
+    {
+        public void draw(Graphics graphics, Pen pen)
+        {
+ 
+        }
+    }
+}
