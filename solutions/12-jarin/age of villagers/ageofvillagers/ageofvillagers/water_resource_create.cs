1806ee138bfc5498528dd228ce66d23fa6038cc2
jarin_12 <jarintanzim@iut-dhaka.edu>
2020-09-23 19:29:06 +0600

    #4 water_resource modified

---
+++ solutions/12-jarin/age of villagers/ageofvillagers/ageofvillagers/water_resource_create.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ageofvillagers
+{
+    abstract class water_resource_create
+    {
+        public abstract void water(int x, int y)
+    }
+}
