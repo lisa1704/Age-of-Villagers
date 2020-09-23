e24c263cead17021e24d4e84e31f22656bd41f5b
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2020-09-23 21:52:21 +0600

    #7 Egyptian Well Added

---
+++ solutions/18-nafis/AoV/Shapes/EgyptianWell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AoV.Shapes
+{
+    class EgyptianWell : ComposeShape
+    {
+        public EgyptianWell(Point clickPoint)
+        {
+            Point p1 = new Point(clickPoint.X + 50, clickPoint.Y + 50);
+            AddShape(new Ellipse(clickPoint, p1));
+        }
+    }
+}
