32fd7d021759eb5a5d1c6b9cef1bb3d4e56cdad3
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2020-09-23 20:35:18 +0600

    #1 Rectangle class refactored

--- solutions/18-nafis/AoV/Shapes/Rectangle.cs
+++ solutions/18-nafis/AoV/Shapes/Rectangle.cs
@@ -7,21 +7,19 @@ using System.Threading.Tasks;
 
 namespace AoV.Shapes
 {
-    class Rectangle : IShape
+    class Rectangle : ComposeShape
     {
         private Point p1, p2;
         public Rectangle(Point p1, Point p2)
         {
             this.p1 = p1;
             this.p2 = p2;
+            AddShape(new Line(p1, new Point(p2.X, p1.Y)));
+            AddShape(new Line(p1, new Point(p1.X, p2.Y)));
+            AddShape(new Line(new Point(p2.X, p1.Y), p2));
+            AddShape(new Line(new Point(p1.X, p2.Y), p2));
+
         }
-        public void Draw(Graphics g, Pen p)
-        {
-            Pen pen = new Pen(Color.Black, 2);
-            g.DrawLine(pen, p1, new Point(p2.X, p1.Y));
-            g.DrawLine(pen, p1, new Point(p1.X, p2.Y));
-            g.DrawLine(pen, new Point(p2.X, p1.Y), p2);
-            g.DrawLine(pen, new Point(p1.X, p2.Y), p2);
-        }
+
     }
 }
