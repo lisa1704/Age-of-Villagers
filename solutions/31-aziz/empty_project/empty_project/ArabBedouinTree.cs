2df4efcc9d0b960695a9331d1c9977c8ad575e27
Asif Aziz <asifaziz2061@gmail.com>
2020-09-23 09:39:28 +0600

    #1 Arab Bedouin Tree reshaped to 16px by 24px

--- solutions/31-aziz/empty_project/empty_project/ArabBedouinTree.cs
+++ solutions/31-aziz/empty_project/empty_project/ArabBedouinTree.cs
@@ -13,13 +13,13 @@ namespace empty_project
             Pen pen = new Pen(Color.Black);
             Graphics graphics = panel.CreateGraphics();
 
-            graphics.DrawRectangle(pen, axis.X, axis.Y, 4, 30);
+            graphics.DrawRectangle(pen, axis.X, axis.Y, 2, 12);
 
-            graphics.DrawLine(pen, new PointF(axis.X + 2, axis.Y), new PointF(axis.X + 2, axis.Y - 30));
-            graphics.DrawLine(pen, new PointF(axis.X + 2, axis.Y), new PointF(axis.X + 18, axis.Y - 20));
-            graphics.DrawLine(pen, new PointF(axis.X + 2, axis.Y), new PointF(axis.X + 28, axis.Y - 10));
-            graphics.DrawLine(pen, new PointF(axis.X + 2, axis.Y), new PointF(axis.X - 12, axis.Y - 20));
-            graphics.DrawLine(pen, new PointF(axis.X + 2, axis.Y), new PointF(axis.X - 22, axis.Y - 10));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X + 1, axis.Y - 12));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X + 9, axis.Y - 8));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X + 14, axis.Y - 4));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X - 6, axis.Y - 8));
+            graphics.DrawLine(pen, new PointF(axis.X + 1, axis.Y), new PointF(axis.X - 11, axis.Y - 4));
         }
     }
 }
