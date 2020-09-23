2edc1f2913e85f92953569f76219147fda431d40
Sadat Bin Sharfuddin <sadat.bs@gmail.com>
2020-09-23 23:35:56 +0600

    Unit test for arab bedouin water source added

---
+++ solutions/68-sadat/age-of-villagers/age-of-villagers/age-of-villagers.Tests/FeatureTesting.cs
@@ -0,0 +1,46 @@
+﻿using age_of_villagers.age_of_village.Items.Tree;
+using age_of_villagers.age_of_village.Items.WaterSource;
+using age_of_villagers.age_of_villagers.Nations;
+using age_of_villagers.age_of_villagrs.Nations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace age_of_villagers.age_of_villagers.Tests
+{
+    public class FeatureTesting
+    {
+        
+        [Fact]
+        public void InuitTreeTest()
+        {
+            Nation nation = new InuitHunter(new System.Windows.Forms.Panel());
+
+            var expected = typeof(NullTree);
+            var actual = nation.tree;
+
+            Assert.IsType(expected, actual);
+        }
+        [Fact]
+        public void InuitWaterSourceTest()
+        {
+            Nation nation = new InuitHunter(new System.Windows.Forms.Panel());
+
+            var expected = typeof(NullWaterSource);
+            var actual = nation.waterSource;
+
+            Assert.IsType(expected, actual);
+        }
+        [Fact]
+        public void ArabBedoinWaterSourceTest()
+        {
+            Nation nation = new ArabBedouin(new System.Windows.Forms.Panel());
+
+            var expected = typeof(NullWaterSource);
+            var actual = nation.waterSource;
+
+            Assert.IsType(expected, actual);
+        }
+
+    }
+}
