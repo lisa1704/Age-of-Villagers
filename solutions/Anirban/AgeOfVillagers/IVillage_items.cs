5bf0b2bd79d71f42dd99b7b6d34784f327254d33
Anirban Azad <azadarannya@gmail.com>
2020-09-23 19:11:33 +0600

    #8 village item abstract class fixed

--- solutions/Anirban/AgeOfVillagers/IVillage_items.cs
+++ solutions/Anirban/AgeOfVillagers/IVillage_items.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using AgeOfVillagers.Shapes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    interface IVillage_items
+    public abstract class VillageItem : IShapes
     {
-        int grid_height();
+        private readonly List<IShapes> v_item;
+        public VillageItem()
+        {
+            v_item = new List<IShapes>();
+        }
 
-        int grid_width();
+        public void Draw_shapes(Point pt, Graphics g, Pen p)
+        {
+            foreach (IShapes item in v_item)
+            {
+                item.Draw_shapes(pt, g, p);
+            }
+        }
+        
     }
 }
