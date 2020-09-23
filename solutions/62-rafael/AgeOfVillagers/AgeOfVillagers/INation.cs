1805b80398bcd9d166f1a6800adbb15567119a22
HackerHoteChai <aftabhossain@iut-dhaka.edu>
2020-09-23 23:15:59 +0600

    #8 Nation interface created

---
+++ solutions/62-rafael/AgeOfVillagers/AgeOfVillagers/INation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    interface INation
+    {
+        public Color getBackgroundColor();
+        public IComponent getComponent(string component_type);
+    }
+}
