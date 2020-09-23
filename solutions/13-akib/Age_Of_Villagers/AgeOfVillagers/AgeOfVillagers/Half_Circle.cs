d31ed3b8cb8de081a6a9df8c74a7fd10108666a7
akib <akibahmed@iut-dhaka.edu>
2020-09-23 18:51:17 +0600

    #2 #7 Implemented Half_Circle using Arc class

--- solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Half_Circle.cs
+++ solutions/13-akib/Age_Of_Villagers/AgeOfVillagers/AgeOfVillagers/Half_Circle.cs
@@ -1,4 +1,6 @@
-﻿internal class Half_Circle
+﻿using AgeOfVillagers;
+
+internal class Half_Circle
 {
     private int x, y, radius;
     public Half_Circle(int x, int y, int radius)
@@ -9,6 +11,9 @@
     }
     public string Draw_Shape()
     {
+        Arc arc_half_circle = new Arc(x - radius, y - radius, 2 * radius, 2 * radius, 0, 180);
+
+        arc_half_circle.Draw_Shape();
         return "Drawing a Half Circle...";
     }
 }
