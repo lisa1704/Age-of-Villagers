a5fbbde5460791ced71b2f29e221526190cf8134
Tasnuva12 <47001609+Tasnuva12@users.noreply.github.com>
2020-09-23 13:17:36 +0600

    #11 EgyptianWaterSourceSize is created

---
+++ solutions/37-tasnuva/AgeOfVillegers/EgyptianWaterSourceSize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace AgeOfVillagers
+{
+	class EgyptianWaterSourceSize :Size
+	{
+        public override int GetHeight()
+        {
+            return 12;
+        }
+
+        public override int GetWeidth()
+        {
+            return 12;
+        }
+    }
+}
+
