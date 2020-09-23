41a40b0f59d89ea01fa0ff2d9f237198da7db6b0
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 17:17:28 +0600

    Null Object pattern implemented

---
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/NullComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace AgeOfVillagers
+{
+    public class NullComponent : IComponent
+    {
+        public Point makePoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            //do nothing
+        }
+    }
+}
