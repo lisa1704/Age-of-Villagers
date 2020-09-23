4e23ea28d3edff3eb4618fa67cd6e10e8b445856
LITHIUM39 <nafism415@gmail.com>
2020-09-23 11:40:22 +0600

    Unit test framework created

---
+++ solutions/23-nafis/Unit_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace age_of_villagers_XUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(3, 3);
+        }
+    }
+}
