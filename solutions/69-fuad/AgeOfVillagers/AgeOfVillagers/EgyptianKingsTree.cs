3aaadc34dae08576b80c14cdd8678fdfa91a7465
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:18:46 +0600

    #7 #11 draw method implemented in EgyptianKingsTree

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/EgyptianKingsTree.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/EgyptianKingsTree.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class EgyptianKingsTree
+    public class EgyptianKingsTree : IComponent
     {
+        public Point makePoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, makePoint(p.X - 5, p.Y + 12), makePoint(p.X, p.Y + 24));
+            g.DrawLine(pen, makePoint(p.X + 5, p.Y + 12), makePoint(p.X, p.Y + 24));
+            g.DrawLine(pen, makePoint(p.X - 5, p.Y + 12), makePoint(p.X - 8, p.Y + 4));
+            g.DrawLine(pen, makePoint(p.X - 5, p.Y + 12), makePoint(p.X - 5, p.Y + 2));
+            g.DrawLine(pen, makePoint(p.X - 5, p.Y + 12), makePoint(p.X - 2, p.Y));
+            g.DrawLine(pen, makePoint(p.X + 5, p.Y + 2), makePoint(p.X + 5, p.Y + 12));
+            g.DrawLine(pen, makePoint(p.X + 2, p.Y), makePoint(p.X + 5, p.Y + 12));
+            g.DrawLine(pen, makePoint(p.X + 8, p.Y + 4), makePoint(p.X + 5, p.Y + 12));
+        }
     }
 }
