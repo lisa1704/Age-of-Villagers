47aef113edeeba89bf8bcffe97cf77d0f535c696
LITHIUM39 <nafism415@gmail.com>
2020-09-23 13:43:30 +0600

    Icomponent interface created

---
+++ solutions/23-nafis/IComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace AgeOfVillagers
+{
+    public interface IComponent
+    {
+        public void draw(Point p, Graphics g, Pen pen);
+    }
+}
