ca01f5cdbb2998a604002151119ecc07e6c12aca
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-23 13:42:20 +0600

    #1 #7 #12 Inuit Hunters House Resized

--- solutions/42-rifah/AgeOfVillagers/Inuit Hunters/InuitHuntersHouse.cs
+++ solutions/42-rifah/AgeOfVillagers/Inuit Hunters/InuitHuntersHouse.cs
@@ -9,14 +9,12 @@ namespace AgeOfVillagers.Inuit_Hunters
     {
         public InuitHuntersHouse(Point point)
         {
-            Point point1 = new Point(point.X - 40, point.Y - 40);
-            Point point2 = new Point(point.X - 10, point.Y - 10);
-            Point LeftBottom = new Point(point.X - 40, point.Y);
-            Point RightBottom = new Point(point.X + 40, point.Y);
+            Point LeftBottom = new Point(point.X - 8, point.Y + 8);
+            Point RightBottom = new Point(point.X + 8, point.Y + 8);
 
             AddComponent(new Line(LeftBottom, RightBottom));
-            AddComponent(new Arc(point.X - 10, point.Y - 10, 20, 20, 180.0F, 180.0F));
-            AddComponent(new Arc(point.X - 40, point.Y - 40, 80, 80, 180.0F, 180.0F));
+            AddComponent(new Arc(point.X - 4, point.Y + 5, 7, 7, 180.0F, 180.0F));
+            AddComponent(new Arc(point.X - 8, point.Y, 16, 16, 180.0F, 180.0F));
         }
     }
 }
