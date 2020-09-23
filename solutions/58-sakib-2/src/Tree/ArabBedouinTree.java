f080394deede3bf2af441153dbbb5490a90d0fee
Sakib58 <rabiulahmed.sakib@gmail.com>
2020-09-23 19:47:05 +0600

    #10 transform rough tree shape to proper tree shape for Arab Bedouin

--- solutions/58-sakib-2/src/Tree/ArabBedouinTree.java
+++ solutions/58-sakib-2/src/Tree/ArabBedouinTree.java
@@ -1,12 +1,32 @@
 package Tree;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 import java.awt.*;
 
 public class ArabBedouinTree implements ITree {
+    GraphicsContext gc;
     @Override
     public void draw(Canvas canvas, Point point) {
-
+        gc=canvas.getGraphicsContext2D();
+        gc.beginPath();
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()-1,point.getY());
+        gc.lineTo(point.getX()-1,point.getY()+15);
+        gc.lineTo(point.getX()+1,point.getY()+15);
+        gc.lineTo(point.getX()+1,point.getY());
+        gc.lineTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()+7,point.getY()-7);
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()+3,point.getY()-10);
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX(),point.getY()-13);
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()-3,point.getY()-10);
+        gc.moveTo(point.getX(),point.getY());
+        gc.lineTo(point.getX()-7,point.getY()-7);
+        gc.closePath();
+        gc.stroke();
     }
 }
