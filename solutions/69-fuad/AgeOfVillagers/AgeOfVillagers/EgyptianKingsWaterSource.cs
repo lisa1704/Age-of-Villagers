57ecb37d36361cee2845295a18a8ddb215745777
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:19:17 +0600

    #7 #11 draw method implemented in EgyptianKingsWaterSource

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/EgyptianKingsWaterSource.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/EgyptianKingsWaterSource.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class EgyptianKingsWaterSource
+
+    public class EgyptianKingsWaterSource : IComponent
     {
+        public Point makePoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawArc(pen, p.X, p.Y, 12, 12, 0, 360);
+        }
     }
 }
