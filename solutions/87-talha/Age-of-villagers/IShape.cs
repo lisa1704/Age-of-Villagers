0033d96bcdd8b3083851631d9c42611a2c02ca9a
TalhAulin <hoque.talha@gmail.com>
2020-09-23 22:12:15 +0600

    #8 IShape interface created

---
+++ solutions/87-talha/Age-of-villagers/IShape.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    public interface IShapes
+    {
+        void Draw_shapes(Point pt, Graphics g, Pen p);
+    }
+}
