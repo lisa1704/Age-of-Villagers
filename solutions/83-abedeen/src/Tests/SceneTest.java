7df8026d538210190f8e845c89a0ef845b53ae27
Iftekharul Abedeen <Iftekharul@iut-dhaka.edu>
2020-09-23 23:22:18 +0600

    #4 #8 Asset list generation tested for scene

---
+++ solutions/83-abedeen/src/Tests/SceneTest.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import Project.Utilities.AssetInfo;
+import Project.Utilities.SceneSate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SceneTest {
+    @Test
+    protected void scene_Name(){
+        SceneSate sceneSate = new SceneSate("My Village");
+        String expected = "My Village";
+        Assertions.assertEquals(expected,sceneSate.Village_name);
+    }
+
+    @Test
+    protected void scene_Content(){
+        SceneSate Test_sceneSate = new SceneSate("My Village");
+
+        SceneSate Expected_sceneSate = new SceneSate("My Village");
+
+        AssetInfo AI = new AssetInfo("tree", 10, 50);
+        Expected_sceneSate.assetList.add(AI);
+        
+        Test_sceneSate.add_asset_to_list("tree", 10, 50);
+
+        Assertions.assertEquals(Test_sceneSate.assetList,Test_sceneSate.assetList);
+    }
+}
