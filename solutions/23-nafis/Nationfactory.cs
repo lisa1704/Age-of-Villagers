b416b8b83eb1ddff2d97265bc814dce3b26b9dd9
LITHIUM39 <nafism415@gmail.com>
2020-09-23 13:12:44 +0600

    NationFactory created

---
+++ solutions/23-nafis/Nationfactory.cs
@@ -0,0 +1,25 @@
+﻿namespace age_of_villagers
+{
+    class NationFactory
+    {
+        public INation GetNation(string nation_name)
+        {
+            if (nation_name == "Bangladeshi Farmers")
+            {
+                return new Bangladeshifarmer();
+            }
+            else if (nation_name == "Arab Beduin")
+            {
+                return new ArabBeduin();
+            }
+            else if (nation_name == "Egyptian king")
+            {
+                return new Egyptianking();
+            }
+            else
+            {
+                return new Inuithunter();
+            }
+        }
+    }
+}
