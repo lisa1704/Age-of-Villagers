abff80c4e1fa30fc59c786da1616bcd808a21eca
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-23 10:29:50 +0600

    #12 #15 New Test cases added

--- solutions/42-rifah/AgeOfVillagers42.Test/InuitHuntersTest.cs
+++ solutions/42-rifah/AgeOfVillagers42.Test/InuitHuntersTest.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using AgeOfVillagers.Inuit_Hunters;
+using AgeOfVillagers;
 using System.Drawing;
 
 namespace AgeOfVillagers42.Test
@@ -10,6 +11,8 @@ namespace AgeOfVillagers42.Test
     [TestClass]
     public class InuitHuntersTest
     {
+        Point point = new Point(42, 42);
+
         [TestMethod]
         public void BackgroundColorTest()
         {
@@ -18,6 +21,33 @@ namespace AgeOfVillagers42.Test
             Color expected = Color.White;
 
             Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void TreeTest()
+        {
+            InuitHunters inuitHuntersTree = new InuitHunters();
+            Object actual = inuitHuntersTree.DrawTree(point);
+            Object expected = new NullItems();
+            actual.Equals(expected);
+        }
+
+        [TestMethod]
+        public void HouseTest()
+        {
+            InuitHunters inuitHuntersHouse = new InuitHunters();
+            Object actual = inuitHuntersHouse.DrawHouse(point);
+            Object expected = new InuitHuntersHouse(point);
+            actual.Equals(expected);
+        }
+
+        [TestMethod]
+        public void WaterSourceTest()
+        {
+            InuitHunters inuitHuntersWatersource = new InuitHunters();
+            Object actual = inuitHuntersWatersource.DrawWaterSource(point);
+            Object expected = new NullItems();
+            actual.Equals(expected);
         }
     }
 }
