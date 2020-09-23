742715ca9289697f7149661cff6345ced73b31dd
Istiaq Siaam <i.b.s.siaam@gmail.com>
2020-09-23 21:48:28 +0600

    #8 Null nation for handling null object initiated

---
+++ solutions/14-siaam/age of villagers/age of villagers/Null_Nation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace age_of_villagers
+{
+    public class Null_Nation : INation
+    {
+        public Color BgColor()
+        {
+            return Color.Empty;
+        }
+
+        public void drawHouse(Graphics g, Point point)
+        {
+            MessageBox.Show("Nation not selected or function not available");
+        }
+
+        public void drawTree(Graphics g, Point point)
+        {
+            MessageBox.Show("Nation not selected or function not available");
+        }
+    }
+}
