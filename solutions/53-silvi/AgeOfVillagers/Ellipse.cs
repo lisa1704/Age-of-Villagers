89492bd84f98bc6c1a4fd62386de50d317e1b08d
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 13:23:16 +0600

    #7 Ellipse class implemented.

--- solutions/53-silvi/AgeOfVillagers/Ellipse.cs
+++ solutions/53-silvi/AgeOfVillagers/Ellipse.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgeOfVillagers
 {
-    class Ellipse
+    class Ellipse : Composite
     {
+        public Ellipse(Point TopLeft, Point BottomRight)
+        {
+            AddComponent(new Arc(TopLeft.X, TopLeft.Y, BottomRight.X - TopLeft.X, BottomRight.Y - TopLeft.Y, 0, 360));
+        }
     }
 }
