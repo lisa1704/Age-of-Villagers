95710a1ab79c592da0caa452218e40c0e10390e5
Nabil Hossain Nico <hossaintalukder108@gmail.com>
2020-09-23 23:03:39 +0600

    #7 #9 BD farmers refactored

--- solutions/76-nico/AgeOfVillagers/AgeOfVillagers/BangladeshFarmers.cs
+++ solutions/76-nico/AgeOfVillagers/AgeOfVillagers/BangladeshFarmers.cs
@@ -12,9 +12,8 @@ namespace AgeOfVillagers
         Pen p = new Pen(Color.Pink);
         public void draw_house(Graphics g, Point pt)
         {
-            throw new NotImplementedException();
-            g.DrawLine(p, pt.X, pt.Y, 100, 100);
-           BD_House bdhouse = new BD_House(new Point(pt.X, pt.Y), new Point(pt.X - 50, pt.Y * 10), new Point(pt.X + 100, pt.Y * 10));
+            Rectangle rec = new Rectangle(new Point(pt.X, pt.Y), new Point(pt.X + 15, pt.Y + 25));
+            rec.Draw(g);
         }
 
         public void draw_house()
--- solutions/76-nico/AgeOfVillagers/AgeOfVillagers/Rectangle.cs
+++ solutions/76-nico/AgeOfVillagers/AgeOfVillagers/Rectangle.cs
@@ -7,25 +7,23 @@ using System.Threading.Tasks;
 
 namespace AgeOfVillagers
 {
-    public class Rectangle : IShape
+    public class Rectangle : CompositeShape
     {
         private readonly Point TopLeft;
         private readonly Point BottomRight;
         public Rectangle(Point topLeft, Point BottomRight)
         {
-            //TopLeft = topLeft;
-           // BottomRight = BottomRight;
-            this.TopLeft = topLeft;
-            this.BottomRight = BottomRight;
+            var TopRight = new Point(BottomRight.X, topLeft.Y);
+            var BottomLeft = new Point(topLeft.X, BottomRight.Y);
+
+            AddComponent(new Line(topLeft, BottomLeft));
+            AddComponent(new Line(topLeft, TopRight));
+            AddComponent(new Line(BottomLeft, BottomRight));
+            AddComponent(new Line(BottomRight, TopRight));
         }
         public void Draw(Graphics g)
         {
-            var pen = new Pen(Color.Black, 1);
-            g.DrawLine(pen, TopLeft.X, TopLeft.Y, BottomRight.X, TopLeft.Y);
-            g.DrawLine(pen, TopLeft.X, TopLeft.Y, TopLeft.X, BottomRight.Y);
-            g.DrawLine(pen, TopLeft.X, BottomRight.Y, BottomRight.X, BottomRight.Y);
-            g.DrawLine(pen, BottomRight.X, TopLeft.Y, BottomRight.X, BottomRight.Y);
-            //throw new NotImplementedException();
+          throw new NotImplementedException();
         }
     }
 }
