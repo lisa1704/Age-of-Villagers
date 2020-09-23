583633ce98ecc214ae3c37fa02311dacd52a1a7a
Mahbubul karim <mahbubulkarim@iut-dhaka.edu>
2020-09-23 23:38:56 +0600

    #1 #4 libraries added

--- solutions/22-snigdho/Age Of Villagers/Age Of Villagers/CreateNewVillage/CreateNewVillage/Program.cs
+++ solutions/22-snigdho/Age Of Villagers/Age Of Villagers/CreateNewVillage/CreateNewVillage/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Form1;
 namespace AgeOfVillagers
 {
     public partial class CreateNewVillage : Form1
