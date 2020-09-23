df991a24620eaa72723e98455961dd306e08d52a
alhussain50 <44936730+alhussain50@users.noreply.github.com>
2020-09-23 23:39:01 +0600

    created textbox for title

--- solutions/50-wali/Age of Villagers demo/Age of Villagers demo/Form1.Designer.cs
+++ solutions/50-wali/Age of Villagers demo/Age of Villagers demo/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.tree = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // tree
@@ -51,16 +52,26 @@
             this.button1.Text = "House";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(607, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Age of Villagers";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tree);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.Button tree;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
