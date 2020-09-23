eee00c87d074a4bf047e1ae1a9fbb000f4944b3e
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 09:43:59 +0600

    #10 ArabBedouinTree class created and implemented.

---
+++ solutions/53-silvi/AgeOfVillagers/ArabBedouinTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfVillagers
+{
+    class ArabBedouinTree : IVillageItem
+    {
+        public void Draw(Point e, Graphics g, Pen p)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            g.DrawLine(p, x - 2, y, x + 2, y);
+            g.DrawLine(p, x + 2, y, x + 2, y + 20);
+            g.DrawLine(p, x - 2, y, x - 2, y + 20);
+            g.DrawLine(p, x - 2, y + 20, x + 2, y + 20);
+
+            g.DrawLine(p, x, y - 15, x, y);
+            g.DrawLine(p, x + 15, y - 10, x, y);
+            g.DrawLine(p, x - 15, y - 10, x, y);
+            g.DrawLine(p, x + 17, y - 3, x, y);
+            g.DrawLine(p, x - 17, y - 3, x, y);
+        }
+    }
+}
