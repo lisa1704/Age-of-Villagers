e23ae47ac01cfbeac923f706eaf6fe3e7e5ec120
alhussain50 <44936730+alhussain50@users.noreply.github.com>
2020-09-23 23:30:34 +0600

    Added new project

---
+++ solutions/50-wali/Age of Villagers demo/Age of Villagers demo/Form1.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace Age_of_Villagers_demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
