67afb86fa0e496c143288b52c4fcc4f087dba3d5
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 18:15:54 +0600

    #7 #9 draw method implemented in BangladeshiFarmersHouse

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersHouse.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/BangladeshiFarmersHouse.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class BangladeshiFarmersHouse
+    public class BangladeshiFarmersHouse : IComponent
     {
+        public Point makePoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+        public void draw(Point p, Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, p, makePoint(p.X-8,p.Y+8));
+            g.DrawLine(pen, p, makePoint(p.X+8,p.Y+8));
+            g.DrawLine(pen, makePoint(p.X + 8, p.Y + 8), makePoint(p.X - 8, p.Y + 8));
+            g.DrawLine(pen, makePoint(p.X-8,p.Y+16), makePoint(p.X - 8, p.Y + 8));
+            g.DrawLine(pen, makePoint(p.X + 8, p.Y + 8), makePoint(p.X + 8, p.Y + 16));
+            g.DrawLine(pen, makePoint(p.X - 8, p.Y + 16), makePoint(p.X + 8, p.Y + 16));
+        }
     }
 }
