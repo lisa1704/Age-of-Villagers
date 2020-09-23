50a28d76a2df2083616ddd5b9a1ebe8677045cb2
Shawkat-Sujon <68417059+Shawkat-Sujon@users.noreply.github.com>
2020-09-23 23:47:33 +0600

    Components Added

---
+++ solutions/70-sujon/Components/Arc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOV.Components
+{
+    public class Arc : IComponent
+    {
+        public int x, y, width, height, startAngle, sweepAngle;
+
+        public Arc(int x, int y, int width, int height, int startAngle, int sweepAngle)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.startAngle = startAngle;
+            this.sweepAngle = sweepAngle;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+
+            g.DrawArc(pen, x, y, width, height, startAngle, sweepAngle);
+        }
+    }
+}
