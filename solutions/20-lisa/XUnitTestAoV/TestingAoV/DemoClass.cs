b87d1a43aff6ae43627fe39d10fa64bca8e387a3
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-09-23 10:14:54 +0600

    #15 Demo Test run

---
+++ solutions/20-lisa/XUnitTestAoV/TestingAoV/DemoClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingAoV
+{
+    public class DemoClass
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a,int b)
+        {
+            return a - b;
+        }
+    }
+}
