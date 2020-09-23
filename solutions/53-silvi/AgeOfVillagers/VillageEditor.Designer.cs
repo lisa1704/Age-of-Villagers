cf59f734c7f79d29f86f515a0de4e39d56d5fa2f
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 21:42:18 +0600

    #4

--- solutions/53-silvi/AgeOfVillagers/VillageEditor.Designer.cs
+++ solutions/53-silvi/AgeOfVillagers/VillageEditor.Designer.cs
@@ -1,6 +1,9 @@
-﻿namespace AgeOfVillagers
+﻿using System;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers
 {
-    partial class Form1
+    partial class VillageEditor
     {
         /// <summary>
         /// Required designer variable.
@@ -43,17 +46,23 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.ForeColor = System.Drawing.Color.DarkCyan;
             this.panel1.Location = new System.Drawing.Point(39, 29);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(600, 400);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // VillageBox
             // 
-            this.VillageBox.Location = new System.Drawing.Point(141, 56);
+            this.VillageBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.VillageBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.VillageBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.VillageBox.Location = new System.Drawing.Point(124, 30);
             this.VillageBox.Name = "VillageBox";
             this.VillageBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.VillageBox.Size = new System.Drawing.Size(231, 22);
+            this.VillageBox.Size = new System.Drawing.Size(231, 30);
             this.VillageBox.TabIndex = 1;
             this.VillageBox.Text = "Enter Village";
             this.VillageBox.TextChanged += new System.EventHandler(this.VillageBox_TextChanged);
@@ -61,7 +70,12 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(141, 107);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bangladeshi Farmers",
+            "Arab Bedouin",
+            "Egyptian Kings",
+            "Inuit Hunters"});
+            this.comboBox1.Location = new System.Drawing.Point(124, 83);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(266, 24);
             this.comboBox1.TabIndex = 2;
@@ -70,18 +84,19 @@
             // House_btn
             // 
             this.House_btn.AutoSize = true;
-            this.House_btn.Location = new System.Drawing.Point(141, 165);
+            this.House_btn.Location = new System.Drawing.Point(124, 189);
             this.House_btn.Name = "House_btn";
             this.House_btn.Size = new System.Drawing.Size(70, 21);
             this.House_btn.TabIndex = 3;
             this.House_btn.TabStop = true;
             this.House_btn.Text = "House";
             this.House_btn.UseVisualStyleBackColor = true;
+            this.House_btn.CheckedChanged += new System.EventHandler(this.House_btn_CheckedChanged);
             // 
             // Tree_btn
             // 
             this.Tree_btn.AutoSize = true;
-            this.Tree_btn.Location = new System.Drawing.Point(141, 209);
+            this.Tree_btn.Location = new System.Drawing.Point(241, 189);
             this.Tree_btn.Name = "Tree_btn";
             this.Tree_btn.Size = new System.Drawing.Size(59, 21);
             this.Tree_btn.TabIndex = 4;
@@ -93,7 +108,7 @@
             // Water_btn
             // 
             this.Water_btn.AutoSize = true;
-            this.Water_btn.Location = new System.Drawing.Point(141, 251);
+            this.Water_btn.Location = new System.Drawing.Point(359, 189);
             this.Water_btn.Name = "Water_btn";
             this.Water_btn.Size = new System.Drawing.Size(116, 21);
             this.Water_btn.TabIndex = 5;
@@ -104,7 +119,7 @@
             // 
             // Save_btn
             // 
-            this.Save_btn.Location = new System.Drawing.Point(136, 300);
+            this.Save_btn.Location = new System.Drawing.Point(119, 271);
             this.Save_btn.Name = "Save_btn";
             this.Save_btn.Size = new System.Drawing.Size(75, 23);
             this.Save_btn.TabIndex = 6;
@@ -114,7 +129,7 @@
             // 
             // New_btn
             // 
-            this.New_btn.Location = new System.Drawing.Point(332, 300);
+            this.New_btn.Location = new System.Drawing.Point(198, 318);
             this.New_btn.Name = "New_btn";
             this.New_btn.Size = new System.Drawing.Size(75, 23);
             this.New_btn.TabIndex = 7;
@@ -124,7 +139,7 @@
             // 
             // Open_btn
             // 
-            this.Open_btn.Location = new System.Drawing.Point(240, 349);
+            this.Open_btn.Location = new System.Drawing.Point(267, 271);
             this.Open_btn.Name = "Open_btn";
             this.Open_btn.Size = new System.Drawing.Size(75, 23);
             this.Open_btn.TabIndex = 8;
@@ -134,33 +149,44 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.New_btn);
             this.panel2.Controls.Add(this.Open_btn);
             this.panel2.Controls.Add(this.Water_btn);
-            this.panel2.Controls.Add(this.New_btn);
             this.panel2.Controls.Add(this.comboBox1);
             this.panel2.Controls.Add(this.Save_btn);
-            this.panel2.Controls.Add(this.VillageBox);
-            this.panel2.Controls.Add(this.House_btn);
             this.panel2.Controls.Add(this.Tree_btn);
+            this.panel2.Controls.Add(this.House_btn);
+            this.panel2.Controls.Add(this.VillageBox);
             this.panel2.Location = new System.Drawing.Point(694, 29);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(533, 400);
             this.panel2.TabIndex = 9;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
-            // Form1
+            // VillageEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(1263, 598);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "VillageEditor";
+            this.Text = "AgeOfVillagers";
+            this.Load += new System.EventHandler(this.VillageEditor_Load);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
+
+        private void VillageEditor_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Label AgeOfVillagers { get; private set; }
 
         #endregion
 
---
+++ solutions/53-silvi/AgeOfVillagers/VillageEditor.cs
@@ -0,0 +1,186 @@
+﻿using AgeOfVillagers.ItemFactory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace AgeOfVillagers
+{
+    public partial class VillageEditor : Form
+    {
+        string SelectedItemName = "";
+        string name;
+        string villageName;
+        string nation;
+        string VillageType;
+        string text;
+        private string rdButton;
+        List<Point> tree_point = new List<Point>();
+        List<Point> house_point = new List<Point>();
+        List<Point> waterSource_point = new List<Point>();
+        AgeOfVillagers.ItemFactory.NationFactory nationfactory = new AgeOfVillagers.ItemFactory.NationFactory();
+
+        ItemFactory myItems;
+
+        
+
+        public VillageEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void VillageBox_TextChanged(object sender, EventArgs e)
+        {
+            name = VillageBox.Text;
+        }
+        public void setVillageName(string Name)
+        {
+            VillageBox.Text = Name;
+            string villageName = Name;
+
+        }
+        public void radioButton_Click()
+        {
+            if (House_btn.Checked)
+            {
+                rdButton = House_btn.Text;
+            }
+            else if (Tree_btn.Checked)
+            {
+                rdButton = Tree_btn.Text;
+            }
+            else if (Water_btn.Checked)
+            {
+                rdButton = Water_btn.Text;
+            }
+            else
+                rdButton = "";
+
+
+        }
+        public void villageSaveState() //getting the info from form design
+        {
+            myItems.NationName = comboBox1.Text;
+            myItems.VillageName = VillageBox.Text;
+            myItems.VillageColor = this.BackColor;
+            myItems.Tree = this.Tree_btn;
+            myItems.House = this.House_btn;
+            myItems.WaterSource = this.Water_btn;
+        }
+
+        private void VillegeEditor_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Tree_btn_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedItemName = "Tree";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            nation = comboBox1.Text;
+            panel1.BackColor = nationfactory.GetNation(nation).BackgroundColor();
+            BackColor = panel1.BackColor;
+
+        }
+
+        private void Water_btn_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedItemName = "WaterSource";
+        }
+
+        private void Save_btn_Click(object sender, EventArgs e)
+        {
+            villageSaveState();
+            SettingsSavingEventHandler save_Village = new SettingsSavingEventHandler(myItems);
+            save_Village.saveMyVillage();
+            MessageBox.Show(VillageBox.Text + " Village is saved");
+            
+        }
+
+        private void New_btn_Click(object sender, EventArgs e)
+        {
+            house_point.Clear();
+            tree_point.Clear();
+            waterSource_point.Clear();
+            panel1.Refresh();
+            nation.ResetText();
+            VillageBox.ResetText();
+            panel1.BackColor = Color.White;
+            MessageBox.Show("Panel is ready for new village to be created");
+        }
+
+        private void Open_btn_Click(object sender, EventArgs e)
+        {
+            SettingsSavingEventHandler villageOpen = new SettingsSavingEventHandler(myItems);
+            villageOpen.openMyVillage();
+            myItems = villageOpen.getmyVillage();
+            setSavedVillage(myItems);
+            panel1.Refresh();
+
+        }
+        public void setSavedVillage(ItemFactory myitem)
+        {
+            nation.Text = myitem.NationName;
+            VillageBox.Text = myitem.VillageName;
+            BackColor = myitem.VillageColor;
+
+            foreach (Point point in myitem.House)
+            {
+                house_point.Add(point);
+            }
+
+            foreach (Point point in myitem.Tree)
+            {
+                tree_point.Add(point);
+            }
+
+            foreach (Point point in myitem.WaterSource)
+            {
+                waterSource_point.Add(point);
+            }
+        }
+
+        private void House_btn_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedItemName = "House";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Black);
+
+            foreach (Point point in tree_point)
+            {
+                nationfactory.GetNation(nation).DrawTree(point).Draw(p, g);
+            }
+
+            foreach (Point point in house_point)
+            {
+                nationfactory.GetNation(nation).DrawHouse(point).Draw(p, g);
+            }
+
+            foreach (Point point in waterSource_point)
+            {
+                nationfactory.GetNation(nation).DrawWaterSource(point).Draw(p, g);
+            }
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+    
+}
