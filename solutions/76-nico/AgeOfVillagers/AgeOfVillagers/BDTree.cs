4735a6a2118cd84a6eab77895cf88ecaf10c1d68
Nabil Hossain Nico <hossaintalukder108@gmail.com>
2020-09-23 21:53:39 +0600

    #1#7#9 Bd Tree implemented

--- solutions/76-nico/AgeOfVillagers/AgeOfVillagers/BDTree.cs
+++ solutions/76-nico/AgeOfVillagers/AgeOfVillagers/BDTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace AgeOfVillagers
 {
     class BDTree:CompositeShape
     {
+        public BDTree(Point MainPoint)
+        {
+            Point TopRight = new Point(MainPoint.X + 20, MainPoint.Y);
+            Point BottomRight = new Point(MainPoint.X + 25, MainPoint.Y + 50);
+            Point TopPoint = new Point(MainPoint.X + 40, MainPoint.Y + 40);
+            Point TopLeft = new Point(MainPoint.X + 20, MainPoint.Y + 20);
+
+            AddComponent(new Rectangle(TopLeft, BottomRight));
+            AddComponent(new Arc(MainPoint, TopPoint));
+        }
     }
 }
