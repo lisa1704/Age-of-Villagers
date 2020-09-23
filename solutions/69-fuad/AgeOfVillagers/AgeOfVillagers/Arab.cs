9f4f6e0c8fbe3f509fe075b6a9f65a3e79c76fe8
fuadulislam@iut-dhaka.edu <fuadulislam@iut-dhaka.edu>
2020-09-23 07:55:07 +0600

    #10 Functionalities for Arab Class created

--- solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Arab.cs
+++ solutions/69-fuad/AgeOfVillagers/AgeOfVillagers/Arab.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace AgeOfVillagers
 {
-    class Arab
+    class Arab : INation
     {
+        public Color getBackgroundColor()
+        {
+            return Color.YellowGreen;
+        }
+        public IComponent getComponent(string component_type)
+        {
+            if (component_type == "tree")
+            {
+                return new ArabBedouinTree();
+            }
+            else if (component_type == "house")
+            {
+                return new ArabBedouinHouse();
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
