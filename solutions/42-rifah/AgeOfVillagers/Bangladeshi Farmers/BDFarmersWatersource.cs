27cca050a1c8ba27a5326a6a96e5b270ce14eab7
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2020-09-23 13:21:22 +0600

    #1 #7 #9 BD Farmers Watersource resized

--- solutions/42-rifah/AgeOfVillagers/Bangladeshi Farmers/BDFarmersWatersource.cs
+++ solutions/42-rifah/AgeOfVillagers/Bangladeshi Farmers/BDFarmersWatersource.cs
@@ -9,14 +9,14 @@ namespace AgeOfVillagers.Bangladeshi_Farmers
     {
         public BDFarmersWatersource(Point Point1)
         {
-            Point point2 = new Point(Point1.X - 6, Point1.Y + 2);
-            Point point3 = new Point(Point1.X - 11, Point1.Y + 14);
-            Point point4 = new Point(Point1.X - 3, Point1.Y + 20);
-            Point point5 = new Point(Point1.X + 5, Point1.Y + 12);
-            Point point6 = new Point(Point1.X + 12, Point1.Y + 16);
-            Point point7 = new Point(Point1.X + 15, Point1.Y + 6);
-            Point point8 = new Point(Point1.X + 8, Point1.Y - 2);
-            Point point9 = new Point(Point1.X, Point1.Y + 7);
+            Point point2 = new Point(Point1.X - 7, Point1.Y + 1);
+            Point point3 = new Point(Point1.X - 11, Point1.Y + 10);
+            Point point4 = new Point(Point1.X - 3, Point1.Y + 14);
+            Point point5 = new Point(Point1.X + 3, Point1.Y + 8);
+            Point point6 = new Point(Point1.X + 9, Point1.Y + 12);
+            Point point7 = new Point(Point1.X + 13, Point1.Y + 5);
+            Point point8 = new Point(Point1.X + 7, Point1.Y - 2);
+            Point point9 = new Point(Point1.X, Point1.Y + 4);
 
             AddComponent(new Line(Point1, point2));
             AddComponent(new Line(point2, point3));
