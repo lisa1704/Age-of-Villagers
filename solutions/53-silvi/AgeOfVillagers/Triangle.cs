cf7307e347165d97631ca9ca807a0378b33519b6
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 13:20:30 +0600

    #7 Triangle class implemented.

--- solutions/53-silvi/AgeOfVillagers/Triangle.cs
+++ solutions/53-silvi/AgeOfVillagers/Triangle.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgeOfVillagers
 {
-    class Triangle
+    class Triangle : Composite
     {
+        public Triangle(Point point_1, Point point_2, Point point_3)
+        {
+            AddComponent(new Line(point_1, point_2));
+            AddComponent(new Line(point_2, point_3));
+            AddComponent(new Line(point_3, point_1));
+        }
     }
 }
