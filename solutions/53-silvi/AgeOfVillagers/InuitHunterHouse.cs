2ad68a30a01c9988771a7dc35426ab26d539838e
Silviyasmin <silviyasmin@iut-dhaka.edu>
2020-09-23 10:18:09 +0600

    #12 InuitHouse class implemented.

--- solutions/53-silvi/AgeOfVillagers/InuitHunterHouse.cs
+++ solutions/53-silvi/AgeOfVillagers/InuitHunterHouse.cs
@@ -6,7 +6,13 @@ namespace AgeOfVillagers
     {
         public void Draw(Point e, Graphics g, Pen p)
         {
-            
+            int x = e.X;
+            int y = e.Y;
+
+
+            g.DrawLine(p, x, y, x + 20, y);
+            g.DrawArc(p, x, y - 15, 20, 30, 180, 180);
+            g.DrawArc(p, x + 6, y - 6, 8, 15, 180, 180);
         }
     }
 }
